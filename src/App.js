8846897025676f47acafeb6df7a93ed81df3e011
Yurii Holiuk <bionicuss@gmail.com>
2019-10-24 17:26:03 +0300

    change export to named

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
-function App() {
+export function App() {
   return (
     <div>
 
     </div>
   );
 }
-
-export default App;
