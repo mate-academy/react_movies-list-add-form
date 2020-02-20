1d3a1ed1f509dc4ab534ea6ca02560794d55f42d
Dan Denysovets <alen250299@gmail.com>
2020-02-21 00:23:14 +0200

    first solution, without validation, just fild added

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,132 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleMovieSubmit = (event) => {
+    event.preventDefault();
+    const { title, description, imgUrl, imdbUrl, imdbId } = this.state;
+
+    this.props.addMovie({
+      title: title.trim(),
+      description: description.trim(),
+      imgUrl,
+      imdbUrl,
+      imdbId: imdbId.trim(),
+    });
+  }
+
+  handleTitle = (event) => {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
+  handleDescription = (event) => {
+    this.setState({
+      description: event.target.value,
+    });
+  }
+
+  handleImgUrl = (event) => {
+    this.setState({
+      imgUrl: event.target.value,
+    });
+  }
+
+  handleImdbUrl = (event) => {
+    this.setState({
+      imdbUrl: event.target.value,
+    });
+  }
+
+  handleImdbId = (event) => {
+    this.setState({
+      imdbId: event.target.value,
+    });
+  }
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form
+        onSubmit={this.handleMovieSubmit}
+        className="movie-adder"
+      >
+        <label
+          htmlFor="title"
+          className="movie-adder__label"
+        >
+          Movie title:
+        </label>
+        <input
+          id="title"
+          onChange={this.handleTitle}
+          className="movie-adder__input"
+        />
+        <label
+          htmlFor="description"
+          className="movie-adder__label"
+        >
+          Movie description:
+        </label>
+        <textarea
+          rows="10"
+          id="description"
+          onChange={this.handleDescription}
+          className="movie-adder__input movie-adder__textarea"
+        />
+        <label
+          htmlFor="imgUrl"
+          className="movie-adder__label"
+        >
+          Movie imgUrl:
+        </label>
+        <input
+          id="imgUrl"
+          onChange={this.handleImgUrl}
+          className="movie-adder__input"
+        />
+        <label
+          htmlFor="imdbUrl"
+          className="movie-adder__label"
+        >
+          Movie imdbUrl:
+        </label>
+        <input
+          id="imdbUrl"
+          onChange={this.handleImdbUrl}
+          className="movie-adder__input"
+        />
+        <label
+          htmlFor="imdbId"
+          className="movie-adder__label"
+        >
+          Movie imdbId:
+        </label>
+        <input
+          id="imdbId"
+          onChange={this.handleImdbId}
+          className="movie-adder__input"
+        />
+        <button
+          type="submit"
+          className="movie-adder__submit"
+        >
+          Add Movie
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
