7e3b1ad87dc86f327bb89a2caa44ce2311f655f5
masha <mariafil2107@gmail.com>
2020-02-20 19:16:01 +0200

    added validation

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
+
+  Object.values(formErrors).forEach((val) => {
+    val.length > 0 && (valid = false);
+  });
+
+  Object.values(rest).forEach((val) => {
+    val === '' && (valid = false);
+  });
+
+  return valid;
+};
+
 export class NewMovie extends Component {
   static propTypes = {
     addMovie: PropTypes.func.isRequired,
@@ -12,25 +26,28 @@ export class NewMovie extends Component {
     imgUrl: '',
     imdbUrl: '',
     imdbId: '',
-    isRequiredtitle: false,
-    isRequiredimdbUrl: false,
-    isRequiredimdbId: false,
-    isValidUrl: false,
-    isError: false,
+    formErrors: {
+      title: '',
+      imgUrl: '',
+      imdbId: '',
+      imdbUrl: '',
+    },
 
   };
 
   handleInput = (e) => {
+    const { name, value } = e.target;
+
     this.setState({
-      [e.target.name]: e.target.value,
+      [name]: value,
     });
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { title, description, imgUrl, imdbUrl, imdbId, isError } = this.state;
+    const { title, description, imgUrl, imdbUrl, imdbId } = this.state;
 
-    if (!isError) {
+    if (formValid(this.state)) {
       this.props.addMovie({
         title,
         description,
@@ -49,45 +66,48 @@ export class NewMovie extends Component {
     }
   }
 
-  handleBlurRequired = (e) => {
-    const { value, name } = e.target;
-
-    if (value.trim().length === 0) {
-      this.setState({
-        [`isRequired${name}`]: true,
-        isError: true,
-      });
-    }
-  }
-
-  handleBlurValid = (e) => {
-    const { value } = e.target;
-    /* eslint-disable-next-line */
-    if (!value.match(/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/)) {
-      this.setState({
-        isValidUrl: true,
-        isError: true,
-      });
-    }
-  }
-
   handleFocus = (e) => {
-    const { isRequiredtitle,
-      isRequiredimdbId,
-      isRequiredimdbUrl,
-      isValidUrl } = this.state;
+    const { name } = e.target;
+    const { formErrors } = this.state;
 
-    if (isRequiredtitle || isRequiredimdbId || isRequiredimdbUrl) {
+    if (formErrors[name].length > 0) {
+      formErrors[name] = ';';
       this.setState({
-        [`isRequired${e.target.name}`]: false,
+        formErrors,
       });
     }
+  }
 
-    if (isValidUrl) {
-      this.setState({
-        isValidUrl: false,
-      });
+  handleBlur = (e) => {
+    const { value, name } = e.target;
+    const { formErrors } = this.state;
+
+    switch (name) {
+      case 'title':
+        formErrors.title
+          = value.length < 3 ? 'minimum 3 characaters required' : '';
+        break;
+      case 'imdbUrl':
+        formErrors.imdbUrl
+          = value.length < 3 ? 'minimum 3 characaters required' : '';
+        break;
+      case 'imdbId':
+        formErrors.imdbId
+          = value.length < 3 ? 'minimum 3 characaters required' : '';
+        break;
+      case 'imgUrl':
+        formErrors.imgUrl
+        /* eslint-disable-next-line */
+          = !value.match(/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/)
+            ? 'not valid URL' : '';
+        break;
+      default:
+        break;
     }
+
+    this.setState({
+      formErrors,
+    });
   }
 
   render() {
@@ -96,11 +116,7 @@ export class NewMovie extends Component {
       imgUrl,
       imdbUrl,
       imdbId,
-      isValidUrl,
-      isRequiredtitle,
-      isRequiredimdbUrl,
-      isRequiredimdbId,
-      isError } = this.state;
+      formErrors } = this.state;
 
     return (
       <form
@@ -110,19 +126,16 @@ export class NewMovie extends Component {
         <label>
           Title:
           <input
-            className={isRequiredtitle ? 'input error' : 'input'}
+            className={formErrors.title.length > 0 ? 'input error' : 'input'}
             type="text"
             name="title"
             value={title}
             onChange={this.handleInput}
-            onBlur={this.handleBlurRequired}
+            onBlur={this.handleBlur}
             onFocus={this.handleFocus}
           />
-          {(isRequiredtitle || isError)
-          && (
-            <span className="errorText">
-            This field is reqiered!
-            </span>
+          {formErrors.title.length > 0 && (
+            <span className="errorText">{formErrors.title}</span>
           )}
         </label>
         <label>
@@ -138,50 +151,46 @@ export class NewMovie extends Component {
         <label>
           Image URL:
           <input
-            className={isValidUrl ? 'input error' : 'input'}
+            className={formErrors.imgUrl.length > 0 ? 'input error' : 'input'}
             type="text"
             name="imgUrl"
             value={imgUrl}
             onChange={this.handleInput}
-            onBlur={this.handleBlurValid}
+            onBlur={this.handleBlur}
             onFocus={this.handleFocus}
           />
-          {isValidUrl && <span className="errorText">Not valid Url!</span>}
+          {formErrors.imgUrl.length > 0 && (
+            <span className="errorText">{formErrors.imgUrl}</span>
+          )}
         </label>
         <label>
           Imdb URL:
           <input
-            className={isRequiredimdbUrl ? 'input error' : 'input'}
+            className={formErrors.imdbUrl.length > 0 ? 'input error' : 'input'}
             type="text"
             name="imdbUrl"
             value={imdbUrl}
             onChange={this.handleInput}
-            onBlur={this.handleBlurRequired}
+            onBlur={this.handleBlur}
             onFocus={this.handleFocus}
           />
-          {isRequiredimdbUrl
-          && (
-            <span className="errorText">
-            This field is reqiered!
-            </span>
+          {formErrors.imdbUrl.length > 0 && (
+            <span className="errorText">{formErrors.imdbUrl}</span>
           )}
         </label>
         <label>
           Imdb Id:
           <input
-            className={isRequiredimdbId ? 'input error' : 'input'}
+            className={formErrors.imdbId.length > 0 ? 'input error' : 'input'}
             type="text"
             name="imdbId"
             value={imdbId}
             onChange={this.handleInput}
-            onBlur={this.handleBlurRequired}
+            onBlur={this.handleBlur}
             onFocus={this.handleFocus}
           />
-          {isRequiredimdbId
-          && (
-            <span className="errorText">
-            This field is reqiered!
-            </span>
+          {formErrors.imdbId.length > 0 && (
+            <span className="errorText">{formErrors.imdbId}</span>
           )}
         </label>
         <button className="button" type="submit">Add Movie</button>
