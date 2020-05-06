389858617942989eaf655c333042344c665ecc6c
ShapovalDenys <denys.igorevich@gmail.com>
2020-05-06 09:52:57 +0300

    new fix

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './NewMovie.scss';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 const validation = new RegExp(/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/);
 const validationTitle = new RegExp(/\w/);
@@ -21,26 +22,27 @@ export class NewMovie extends Component {
   };
 
   handleSubmit = (event) => {
+    event.preventDefault();
+
     const { title, description, imdbId, imdbUrl, imgUrl } = this.state;
 
-    event.preventDefault();
-    if (title.length === 0) {
+    if (title.trim().length === 0) {
       this.setState({ inputErrorTitle: true });
     } else if (!validationTitle.test(title)) {
       this.setState({ inputErrorTitle: true });
-    } else if (description.length === 0) {
+    } else if (description.trim().length === 0) {
       this.setState({ inputErrorDescription: true });
     } else if (!validation.test(imgUrl)) {
       this.setState({ inputErrorImgUrl: true });
     } else if (!validation.test(imdbUrl)) {
       this.setState({ inputErrorImdbUrl: true });
-    } else if (imdbId.length === 0) {
+    } else if (imdbId.trim().length === 0) {
       this.setState({ inputErrorImdbId: true });
     } else {
       this.props.addMovie({
         title,
         description,
-        imgUrl: imdbUrl,
+        imgUrl,
         imdbUrl,
         imdbId,
       });
@@ -108,17 +110,13 @@ export class NewMovie extends Component {
   buttonSwitch = () => {
     const { title, description, imdbId, imdbUrl, imgUrl } = this.state;
 
-    if (imdbId.length > 0
-        && title.length > 0
-        && description.length > 0
+    if (imdbId.trim().length > 0
+        && title.trim().length > 0
+        && description.trim().length > 0
         && imgUrl.length > 0
         && imdbUrl.length > 0) {
       this.setState({ activeButton: false });
     }
-  }
-
-  blur = (event) => {
-    event.target.style.background = 'rgba(119, 156, 255, 0.1)';
   }
 
   render() {
@@ -126,56 +124,62 @@ export class NewMovie extends Component {
 
     return (
       <form onSubmit={this.handleSubmit} onChange={this.buttonSwitch}>
+
         <label htmlFor="inputTitle">Title:&nbsp;</label>
         {this.state.inputErrorTitle
         && (<span className="inputError">Please, enter the title!</span>)}
+
         <input
           id="inputTitle"
-          onBlur={this.blur}
-          className={`input${this.state.inputErrorTitle ? 'active' : ''}`}
+          className={classNames('input', {active: this.state.inputErrorTitle})}
           value={title}
           onChange={this.onTitleChange}
         />
+
         <label htmlFor="inputDescription">Description:&nbsp;</label>
         {this.state.inputErrorDescription
         && (<span className="inputError">Please, enter the description!</span>)}
+
         <input
           id="inputDescription"
-          onBlur={this.blur}
-          className={`input ${this.state.inputErrorDescription && 'active'}`}
+          className={classNames('input', {active: this.state.inputErrorDescription})}
           value={description}
           onChange={this.onDescriptionChange}
         />
+
         <label htmlFor="imgUrl">ImgUrl:&nbsp;</label>
         {this.state.inputErrorImgUrl
         && (<span className="inputError">Please, enter correct ImgUrl!</span>)}
+
         <input
           id="imgUrl"
-          onBlur={this.blur}
-          className={`input ${this.state.inputErrorImgUrl && 'active'}`}
+          className={classNames('input', {active: this.state.inputErrorImgUrl})}
           value={imgUrl}
           onChange={this.onImgUrlChange}
         />
+
         <label htmlFor="imdbUrl">ImdbUrl:&nbsp;</label>
         {this.state.inputErrorImdbUrl
         && (<span className="inputError">Please, enter correct ImgUrl!</span>)}
+
         <input
           id="imdbUrl"
-          onBlur={this.blur}
-          className={`input ${this.state.inputErrorImdbUrl && 'active'}`}
+          className={classNames('input', {active: this.state.inputErrorImdbUrl})}
           value={imdbUrl}
           onChange={this.onImdbUrlChange}
         />
+
         <label htmlFor="idbId">ImdbId:&nbsp;</label>
         {this.state.inputErrorImdbId
         && (<span className="inputError">Please, enter the ImgUrl!</span>)}
+
         <input
           id="idbId"
-          onBlur={this.blur}
-          className={`input ${this.state.inputErrorImdbId && 'active'}`}
+          className={classNames('input', {active: this.state.inputErrorImdbId})}
           value={imdbId}
           onChange={this.onImdbIdChange}
         />
+
         <button
           disabled={this.state.activeButton}
           type="submit"
