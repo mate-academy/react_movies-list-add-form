8a7dd0e68cb8f8681fadbeb68e3c005b7bbed42c
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-07 20:16:39 +0300

    readme, favicon, functionality: add movie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
-import { NewMovie } from './components/NewMovie';
+import { NewMovie } from './components/NewMovie/NewMovie';
 import moviesFromServer from './api/movies.json';
 
 export class App extends Component {
@@ -9,8 +9,15 @@ export class App extends Component {
     movies: moviesFromServer,
   };
 
-  addMovie = (movie) => {
-    // put your code here
+  addMovie = (event, movie) => {
+    event.preventDefault();
+
+    this.setState(prevState => ({
+      movies: [
+        ...prevState.movies,
+        movie,
+      ],
+    }));
   };
 
   render() {
@@ -22,7 +29,9 @@ export class App extends Component {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie
+            addMovie={this.addMovie}
+          />
         </div>
       </div>
     );
--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,137 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+
+import { MovieCard } from '../MovieCard';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    values: {
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    },
+    touched: {
+      title: false,
+      description: false,
+      imgUrl: false,
+      imdbUrl: false,
+      imdbId: false,
+    },
+  };
+
+  handleChange = (event) => {
+    const { target: { name, value } } = event;
+
+    this.setState(prevState => ({
+      ...prevState,
+      values: {
+        ...prevState.values,
+        [name]: value,
+      },
+    }));
+  }
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state.values;
+
+    const movie = this.state.values;
+    const { addMovie } = this.props;
+
     return (
-      <form>
-        Put the form here
+      <form
+        name="addMovie"
+        onSubmit={event => addMovie(event, movie)}
+      >
+        <h1 className="form__header">
+          Add new movie
+        </h1>
+        <label>
+          <span className="label">
+            Title:
+          </span>
+          <input
+            type="text"
+            name="title"
+            onChange={event => this.handleChange(event)}
+            value={title}
+            placeholder="write the title"
+          />
+        </label>
+        <label>
+          <span className="label">
+            Description:
+          </span>
+          <input
+            type="textarea"
+            name="description"
+            onChange={event => this.handleChange(event)}
+            value={description}
+            placeholder="write the description"
+          />
+        </label>
+        <label>
+          <span className="label">
+            Poster link:
+          </span>
+          <input
+            type="text"
+            name="imgUrl"
+            onChange={event => this.handleChange(event)}
+            value={imgUrl}
+            placeholder="paste the link to poster"
+          />
+        </label>
+        <label>
+          <span className="label">
+            IMDB link:
+          </span>
+          <input
+            type="text"
+            name="imdbUrl"
+            onChange={event => this.handleChange(event)}
+            value={imdbUrl}
+            placeholder="paste the link to IMDB"
+          />
+        </label>
+        <label>
+          <span className="label">
+            Description:
+          </span>
+          <input
+            type="textarea"
+            name="imdbId"
+            onChange={event => this.handleChange(event)}
+            value={imdbId}
+            placeholder="paste the IMDB ID"
+          />
+        </label>
+        <br />
+        <button
+          type="submit"
+          className="form__submit-button"
+        >
+          Submit
+        </button>
+        <br />
+
+        <h2 className="preview-header">
+          Preview:
+        </h2>
+
+        <MovieCard key={movie.imdbId} {...movie} />
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
