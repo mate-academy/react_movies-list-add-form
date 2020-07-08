cd58afb6f174102569926bfd82a2504cd2d5d6a6
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-08 16:44:22 +0300

    solution 1 - 5**

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
+import cn from 'classnames';
 
 import { MovieCard } from '../MovieCard';
+
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
   state = {
@@ -14,7 +17,7 @@ export class NewMovie extends Component {
     },
     touched: {
       title: false,
-      description: false,
+      description: true,
       imgUrl: false,
       imdbUrl: false,
       imdbId: false,
@@ -28,17 +31,45 @@ export class NewMovie extends Component {
     },
   };
 
+  resetState = () => {
+    this.setState({
+      values: {
+        title: '',
+        description: '',
+        imgUrl: '',
+        imdbUrl: '',
+        imdbId: '',
+      },
+      touched: {
+        title: false,
+        description: true,
+        imgUrl: false,
+        imdbUrl: false,
+        imdbId: false,
+      },
+      errors: {
+        title: null,
+        description: null,
+        imgUrl: null,
+        imdbUrl: null,
+        imdbId: null,
+      },
+    });
+  }
+
   isTextValid = (text) => {
-    return text && (text.search(/\w/) !== -1);
+    return (text.search(/\w/) !== -1);
   }
 
   isUrlValid = (url) => {
     // eslint-disable-next-line max-len
-    return url && url.test(/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/);
+    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
+    return url && pattern.test(url);
   }
 
   manageTextFieldError = (name, value) => {
-    if (this.isTextValid(value)) {
+    if (!this.isTextValid(value)) {
       this.setState(prevState => ({
         ...prevState,
         errors: {
@@ -46,32 +77,16 @@ export class NewMovie extends Component {
           [name]: `${name} field should not be empty`,
         },
       }));
-    } else {
-      this.setState(prevState => ({
-        ...prevState,
-        errors: {
-          ...prevState.errors,
-          [name]: null,
-        },
-      }));
     }
   }
 
   manageUrlError = (name, value) => {
-    if (this.isUrlValid(value)) {
+    if (!this.isUrlValid(value)) {
       this.setState(prevState => ({
         ...prevState,
         errors: {
           ...prevState.errors,
           [name]: `${name} URL should be correct`,
-        },
-      }));
-    } else {
-      this.setState(prevState => ({
-        ...prevState,
-        errors: {
-          ...prevState.errors,
-          [name]: null,
         },
       }));
     }
@@ -90,23 +105,43 @@ export class NewMovie extends Component {
         ...prevState.touched,
         [name]: true,
       },
+      errors: {
+        ...prevState.errors,
+        [name]: null,
+      },
     }));
   }
 
   handleBlur = ({ name, value }) => {
-    // eslint-disable-next-line no-console
-    // console.log('blur', name, value);
+    const trimmedValue = value.trim();
 
     if (name === 'imgUrl' || name === 'imdbUrl') {
-      this.manageUrlError(name, value);
+      if (this.isTextValid(trimmedValue)) {
+        this.manageUrlError(name, trimmedValue);
+      } else {
+        this.manageTextFieldError(name, trimmedValue);
+      }
     } else {
-      this.manageTextFieldError(name, value);
+      this.manageTextFieldError(name, trimmedValue);
     }
   }
 
-  handleFocus = ({ name, value }) => {
-    // eslint-disable-next-line no-console
-    // console.log('focus', name, value);
+  isFormValid = () => {
+    const formHasErrors = Object.values(this.state.errors)
+      .some(error => !!error === true);
+    const isFormTouched = Object.values(this.state.touched)
+      .every(touch => touch === true);
+
+    return !formHasErrors && isFormTouched;
+  }
+
+  handleSubmit = (event, callback, movie) => {
+    event.preventDefault();
+
+    if (this.isFormValid()) {
+      callback(event, movie);
+      this.resetState();
+    }
   }
 
   render() {
@@ -121,94 +156,105 @@ export class NewMovie extends Component {
     const movie = this.state.values;
     const { addMovie } = this.props;
 
+    const hasError = name => (
+      this.state.errors[name] && this.state.touched[name]
+    );
+
     return (
       <form
         name="addMovie"
-        onSubmit={event => addMovie(event, movie)}
+        onSubmit={event => this.handleSubmit(event, addMovie, movie)}
       >
         <h1 className="form__header">
           Add new movie
         </h1>
-        <label>
-          <span className="label">
+        <label className="labelName">
+          <span className="label__title">
             Title:
           </span>
           <input
             type="text"
             name="title"
+            className={cn({ 'wrong-input': hasError('title') })}
             onChange={event => this.handleChange(event)}
             onBlur={event => this.handleBlur(event.target)}
-            onFocus={event => this.handleFocus(event.target)}
             value={title}
             placeholder="Enter the title"
           />
+          {hasError('title')
+            && <span className="error">{this.state.errors.title}</span>}
         </label>
-        <label>
-          <span className="label">
+        <label className="labelName">
+          <span className="label__title">
             Description:
           </span>
           <input
             type="textarea"
             name="description"
+            className={cn({ 'wrong-input': hasError('description') })}
             onChange={event => this.handleChange(event)}
-            onBlur={event => this.handleBlur(event.target)}
-            onFocus={event => this.handleFocus(event.target)}
+            // onBlur={event => this.handleBlur(event.target)}
             value={description}
             placeholder="Enter the description"
           />
+          {hasError('description')
+            && <span className="error">{this.state.errors.description}</span>}
         </label>
-        <label>
-          <span className="label">
+        <label className="labelName">
+          <span className="label__title">
             Poster link:
           </span>
           <input
             type="text"
             name="imgUrl"
+            className={cn({ 'wrong-input': hasError('imgUrl') })}
             onChange={event => this.handleChange(event)}
             onBlur={event => this.handleBlur(event.target)}
-            onFocus={event => this.handleFocus(event.target)}
             value={imgUrl}
             placeholder="paste the link to poster"
           />
+          {hasError('imgUrl')
+            && <span className="error">{this.state.errors.imgUrl}</span>}
         </label>
-        <label>
-          <span className="label">
+        <label className="labelName">
+          <span className="label__title">
             IMDB link:
           </span>
           <input
             type="text"
             name="imdbUrl"
+            className={cn({ 'wrong-input': hasError('imdbUrl') })}
             onChange={event => this.handleChange(event)}
             onBlur={event => this.handleBlur(event.target)}
-            onFocus={event => this.handleFocus(event.target)}
             value={imdbUrl}
             placeholder="paste the link to IMDB"
           />
+          {hasError('imdbUrl')
+            && <span className="error">{this.state.errors.imdbUrl}</span>}
         </label>
-        <label>
-          <span className="label">
+        <label className="labelName">
+          <span className="label__title">
             IMDB ID:
           </span>
           <input
             type="textarea"
             name="imdbId"
+            className={cn({ 'wrong-input': hasError('imdbId') })}
             onChange={event => this.handleChange(event)}
             onBlur={event => this.handleBlur(event.target)}
-            onFocus={event => this.handleFocus(event.target)}
             value={imdbId}
             placeholder="paste the IMDB ID"
           />
-          {this.state.errors.imdbId
+          {hasError('imdbId')
             && <span className="error">{this.state.errors.imdbId}</span>}
         </label>
-        <br />
         <button
+          disabled={!this.isFormValid()}
           type="submit"
           className="form__submit-button"
         >
           Submit
         </button>
-        <br />
 
         <h2 className="preview-header">
           Preview:
