6658cdbcb7d802d03f1b94654fc4977d02d538a5
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-09-03 15:58:13 +0300

    solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,84 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
 
   render() {
+    const { addMovie } = this.props;
+
     return (
-      <form>
-        Put the form here
-      </form>
+      <>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            addMovie(this.state);
+          }}
+        >
+
+          <input
+            type="text"
+            value={this.state.title}
+            onChange={(e) => {
+              this.setState({
+                title: e.target.value,
+              });
+            }}
+            placeholder="enter please title"
+          />
+          <input
+            type="text"
+            value={this.state.description}
+            onChange={(e) => {
+              this.setState({
+                description: e.target.value,
+              });
+            }}
+            placeholder="enter please description"
+          />
+          <input
+            type="text"
+            value={this.state.imgUrl}
+            onChange={(e) => {
+              this.setState({
+                imgUrl: e.target.value,
+              });
+            }}
+            placeholder="enter please url for image"
+          />
+          <input
+            type="text"
+            value={this.state.imdbUrl}
+            onChange={(e) => {
+              this.setState({
+                imdbUrl: e.target.value,
+              });
+            }}
+            placeholder="enter please Url from imdb"
+          />
+          <input
+            type="text"
+            value={this.state.imdbId}
+            onChange={(e) => {
+              this.setState({
+                imdbId: e.target.value,
+              });
+            }}
+            placeholder="enter please imdb id"
+          />
+          <input type="submit" />
+        </form>
+      </>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
