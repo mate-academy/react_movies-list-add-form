bc8a0a2d751f7ca2d1889ea9ea9c49c2d5eccd40
ArtemiiKab <artemiikab@gmail.com>
2020-09-04 12:14:20 +0300

    s

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './NewMovie.scss';
 
-export class NewMovie extends Component {
-  state = {};
+export const NewMovie = ({ addMovie }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
 
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
+  function handleSubmit(e) {
+    e.preventDefault();
+    addMovie({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
   }
-}
+
+  return (
+    <form
+      className="Form"
+      onSubmit={event => handleSubmit(event)}
+    >
+      <input
+        type="text"
+        placeholder="Title"
+        maxLength={20}
+        onChange={e => setTitle(e.target.value)}
+        value={title}
+        className="Form__input"
+        required
+      />
+      <textarea
+        placeholder="Description"
+        maxLength={200}
+        onChange={e => setDescription(e.target.value)}
+        value={description}
+        className="Form__description Form__input"
+      />
+      <input
+        type="url"
+        placeholder="Image URL"
+        maxLength={40}
+        onChange={e => setImgUrl(e.target.value)}
+        value={imgUrl}
+        className="Form__input"
+        required
+      />
+      <input
+        type="url"
+        placeholder="Imdb URL"
+        maxLength={40}
+        onChange={e => setImdbUrl(e.target.value)}
+        value={imdbUrl}
+        className="Form__input"
+        required
+      />
+      <input
+        type="text"
+        placeholder="Imdb ID"
+        maxLength={20}
+        onChange={e => setImdbId(e.target.value)}
+        value={imdbId}
+        className="Form__input"
+        required
+      />
+      <input type="submit" className="Form__submit" />
+    </form>
+  );
+};
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
