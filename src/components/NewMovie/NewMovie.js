00793ab32455da5cb047c6a5ce363471a9687808
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-09-05 18:05:05 +0300

    Solution 1-4

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class NewMovie extends Component {
+export class NewMovie extends React.PureComponent {
   state = {};
+
+  Add = (event) => {
+    event.preventDefault();
+
+    const { title, description, imgUrl, imdbUrl, imdbId } = event.target;
+
+    this.props.onAdd({
+      title: title.value,
+      description: description.value,
+      imgUrl: imgUrl.value,
+      imdbUrl: imdbUrl.value,
+      imdbId: imdbId.value,
+    });
+
+    event.target.reset();
+  }
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form
+        onSubmit={this.Add}
+      >
+        <input
+          name="title"
+          placeholder="title"
+          type="text"
+          className="input"
+        />
+        <textarea
+          name="description"
+          placeholder="description"
+          className="input textarea"
+        />
+        <input
+          name="imgUrl"
+          placeholder="url of image"
+          type="text"
+          className="input"
+        />
+        <input
+          name="imdbUrl"
+          placeholder="url of IMDB page"
+          type="text"
+          className="input"
+        />
+        <input
+          name="imdbId"
+          placeholder="IMDB id"
+          type="number-"
+          className="input"
+        />
+        <button
+          type="submit"
+          className="button"
+        >
+          Add
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
