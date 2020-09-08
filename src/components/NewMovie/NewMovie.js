bef8dd2ff0d4d82535ac13229371957c08b797ab
oggyrock <steelo.sun@gmail.com>
2020-09-08 19:13:06 +0300

    solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,83 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.addMovie(this.state);
+    this.clearForm();
+  };
+
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  clearForm() {
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  }
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form onSubmit={this.submitHandler}>
+        <input
+          name="title"
+          placeholder="title"
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        <br />
+        <textarea
+          name="description"
+          placeholder="description"
+          value={this.state.description}
+          onChange={this.onChange}
+        />
+        <br />
+        <input
+          name="imgUrl"
+          placeholder="imgUrl"
+          value={this.state.imgUrl}
+          onChange={this.onChange}
+        />
+        <br />
+        <input
+          name="imdbUrl"
+          placeholder="imdbUrl"
+          value={this.state.imdbUrl}
+          onChange={this.onChange}
+        />
+        <br />
+        <input
+          name="imdbId"
+          placeholder="imdbId"
+          value={this.state.imdbId}
+          onChange={this.onChange}
+        />
+        <br />
+        <button type="submit">
+          Add a movie
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
