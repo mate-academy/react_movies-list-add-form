972100387cad05bdd767facb3ce66075a1dd0b78
uni-8 <julia.zhol.a@gmail.com>
2020-09-08 13:18:12 +0300

    solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,129 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = ({ name, value }) => this.setState({ [name]: value });
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onAdd(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  }
 
   render() {
+    const { title, description, imgUrl, imdbUrl, imdbId } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form
+        className="NewMovie"
+        onSubmit={this.handleSubmit}
+      >
+        <h2 className="NewMovie__header">
+          Add new movie
+        </h2>
+
+        <label>
+          <h3>
+            Movie title:
+          </h3>
+
+          <input
+            className="NewMovie__input"
+            type="text"
+            name="title"
+            value={title}
+            onChange={(event => this.handleChange(event.target))}
+            required
+          />
+        </label>
+
+        <label>
+          <h3>
+            Movie description:
+          </h3>
+
+          <textarea
+            className="NewMovie__textarea"
+            name="description"
+            value={description}
+            onChange={(event => this.handleChange(event.target))}
+          />
+        </label>
+
+        <label>
+          <h3>
+            Link to movie poster:
+          </h3>
+
+          <input
+            className="NewMovie__input"
+            type="url"
+            name="imgUrl"
+            value={imgUrl}
+            onChange={(event => this.handleChange(event.target))}
+            required
+          />
+        </label>
+
+        <label>
+          <h3>
+            Link to IMDB page of the movie:
+          </h3>
+
+          <input
+            className="NewMovie__input"
+            type="url"
+            name="imdbUrl"
+            value={imdbUrl}
+            onChange={(event => this.handleChange(event.target))}
+            required
+          />
+        </label>
+
+        <label>
+          <h3>
+            Movie&apos;s IMDB id:
+          </h3>
+
+          <input
+            className="NewMovie__input"
+            type="text"
+            name="imdbId"
+            value={imdbId}
+            onChange={(event => this.handleChange(event.target))}
+            required
+          />
+        </label>
+
+        <button
+          className="NewMovie__submit"
+          type="submit"
+        >
+          Add
+        </button>
+
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
