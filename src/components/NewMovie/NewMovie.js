4b43c8dba3dff86c631cf27c0f917248e04811c4
Gabbasova Oksana <oksana.gabba@gmail.com>
2020-09-10 13:49:20 +0300

    Solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  submitHandler = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const title = form.title.value;
+    const description = form.description.value;
+    const imgUrl = form.imgUrl.value;
+    const imdbUrl = form.imdbUrl.value;
+    const imdbId = form.imdbId.value;
+
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    this.props.addMovie(newMovie);
+  }
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form onSubmit={this.submitHandler}>
+        <input
+          type="text"
+          id="title"
+          className="movie-title"
+          placeholder="Enter the title"
+          required
+        />
+        <textarea
+          id="description"
+          className="movie-description"
+          placeholder="Enter the description"
+        />
+        <input
+          id="imgUrl"
+          className="movie-imgUrl"
+          type="url"
+          placeholder="Enter the imgUrl"
+          required
+        />
+        <input
+          type="text"
+          id="imdbUrl"
+          className="movie-imdbUrl"
+          placeholder="Enter the imdbUrl"
+          required
+        />
+        <input
+          type="text"
+          id="imdbId"
+          className="movie-imdbId"
+          placeholder="Enter the imdbId"
+          required
+        />
+        <button type="submit">Add</button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
