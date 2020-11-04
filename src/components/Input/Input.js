bb4e6e1e29cfd615a52a5715bd89a7561f8653ba
Vitalik <ztx25sss@gmail.com>
2020-11-04 23:28:22 +0200

    Solution

---
+++ src/components/Input/Input.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import classNames from 'classnames';
+import { InputShape } from '../Shapes/InputShape';
+
+export const Input = ({ inputValue, inputName, error, addChange }) => {
+  const titleFormatting = (title) => {
+    return title[0].toUpperCase() + title.slice(1);
+  };
+
+  return (
+    <div className="field">
+      <label
+        htmlFor={inputName}
+      >
+        {
+          titleFormatting(inputName)
+        }
+        <input
+          type="text"
+          name={inputName}
+          id={inputName}
+          value={inputValue}
+          onChange={addChange}
+          placeholder="Write here"
+        />
+
+        <p className={classNames(
+          { 'ui red message': error },
+        )}
+        >
+          {
+            error
+              ? `Field ${inputName} is required`
+              : ''
+          }
+        </p>
+      </label>
+    </div>
+  );
+};
+
+Input.propTypes = InputShape;
