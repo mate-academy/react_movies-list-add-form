c162feb90049d77a8d0078b5038027be2d112057
Anastasia Chervona <av.chervona@gmail.com>
2020-11-04 10:44:17 +0200

    state fixed

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -5,17 +5,21 @@ import { InputField } from '../InputField';
 // eslint-disable-next-line max-len
 const urlRegEx = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
 
+const initialState = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+  titleError: false,
+  imgUrlError: false,
+  imdbUrlError: false,
+  imdbIdError: false,
+};
+
 export class NewMovie extends PureComponent {
   state = {
-    title: '',
-    description: '',
-    imgUrl: '',
-    imdbUrl: '',
-    imdbId: '',
-    titleError: false,
-    imgUrlError: false,
-    imdbUrlError: false,
-    imdbIdError: false,
+    ...initialState,
   };
 
   onAdd = (event) => {
@@ -46,15 +50,7 @@ export class NewMovie extends PureComponent {
     });
 
     this.setState({
-      title: '',
-      description: '',
-      imgUrl: '',
-      imdbUrl: '',
-      imdbId: '',
-      titleError: false,
-      imgUrlError: false,
-      imdbUrlError: false,
-      imdbIdError: false,
+      ...initialState,
     });
   };
 
