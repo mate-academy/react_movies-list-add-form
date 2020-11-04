ea6a56c2ab484659f2a3324b0927c7b16af426f7
Nikolay Filiuk <Filiuk98@gmail.com>
2020-11-04 11:34:11 +0200

    Solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,115 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+    submitDisabled: true,
+  };
+
+  onInputChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState(state => ({
+      [name]: value,
+      submitDisabled: !(
+        state.title !== '' && state.imgUrl !== '' && state.imdbUrl !== ''
+        && state.imdbId !== ''
+      ),
+    }));
+  }
+
+  clearForm() {
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+      submitDisabled: true,
+    });
+  }
 
   render() {
+    const { onAdd } = this.props;
+    const {
+      title, description, imgUrl, imdbUrl, imdbId, submitDisabled,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form
+        name="newMovie"
+        className="form"
+        onSubmit={(event) => {
+          event.preventDefault();
+          onAdd(title, description, imgUrl, imdbUrl, imdbId);
+          this.clearForm();
+        }}
+      >
+        <label className="form__label" htmlFor="title">Movie title:</label>
+        <input
+          className="form__input"
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          onChange={this.onInputChange}
+        />
+        <label className="form__label" htmlFor="description">
+          Movie description:
+        </label>
+        <input
+          className="form__input"
+          type="text"
+          name="description"
+          id="description"
+          value={description}
+          onChange={this.onInputChange}
+        />
+        <label className="form__label" htmlFor="imgUrl">Image Url:</label>
+        <input
+          className="form__input"
+          type="text"
+          name="imgUrl"
+          id="imgUrl"
+          value={imgUrl}
+          onChange={this.onInputChange}
+        />
+        <label className="form__label" htmlFor="imdbUrl">IMDB Url:</label>
+        <input
+          className="form__input"
+          type="text"
+          name="imdbUrl"
+          id="imdbUrl"
+          value={imdbUrl}
+          onChange={this.onInputChange}
+        />
+        <label className="form__label" htmlFor="imdbId">IMDB Id:</label>
+        <input
+          className="form__input"
+          type="text"
+          name="imdbId"
+          id="imdbId"
+          value={imdbId}
+          onChange={this.onInputChange}
+        />
+        <button
+          type="submit"
+          className="form__button"
+          disabled={submitDisabled}
+        >
+          Add
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
