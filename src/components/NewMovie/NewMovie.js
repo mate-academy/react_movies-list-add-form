922b0ccfad3520235cf1baf9f6ed752d0af5f7a0
dmitriy <dmitriy.bondarenko.it@gmail.com>
2020-11-05 09:23:09 +0200

    create movie add form

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,91 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Input } from '../Input';
+import './NewMovie.scss';
+
+const initialtState = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    inputValues: initialtState,
+    inputErrors: initialtState,
+  };
+
+  saveMovieProperty = (name, error = '') => {
+    this.setState((prevState) => {
+      return {
+        inputValues: {
+          ...prevState.inputValues,
+          [name]: prevState.inputValues[name],
+        },
+        inputErrors: {
+          ...prevState.inputErrors,
+          [name]: error,
+        },
+      };
+    });
+  }
+
+  addMovie = (event) => {
+    event.preventDefault();
+
+    if (Object.values(this.state.inputErrors).every(error => !error)
+      && Object.values(this.state.inputValues).every(value => value)) {
+      this.props.addMovie({ ...this.state.inputValues });
+
+      this.setState({
+        inputValues: initialtState,
+        inputErrors: initialtState,
+      });
+    }
+  }
+
+  changeInputValue = (event, name) => {
+    const { value } = event.target;
+
+    this.setState((prevState) => {
+      return {
+        inputValues: {
+          ...prevState.inputValues,
+          [name]: value,
+        },
+      };
+    });
+  }
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form className="needs-validation" onSubmit={this.addMovie}>
+        {
+          Object.keys(this.state.inputValues).map(key => (
+            <Input
+              key={key}
+              name={key}
+              value={this.state.inputValues[key]}
+              error={this.state.inputErrors[key]}
+              changeValue={this.changeInputValue}
+              saveValue={this.saveMovieProperty}
+            />
+          ))
+        }
+
+        <button
+          type="submit"
+          className="btn btn-primary w-100"
+        >
+          add new movie
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
