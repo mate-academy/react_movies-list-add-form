326506431c32c6ca3032dd02405f99c47bc79ba3
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-11-10 00:58:08 +0200

    Fixed Solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,28 +1,25 @@
 /* eslint-disable max-len */
 import React, { Component } from 'react';
+import uuid from 'uuid-random';
 import { NewMovieTypes } from './NewMovieTypes';
 
 import './NewMovie.scss';
 
 const INITIAL_STATE = {
-  title: '',
-  description: '',
-  imgUrl: '',
-  imdbUrl: '',
-  imdbId: '',
-  errors: {
-    validTitle: false,
-    emptyTitle: false,
-    validDescription: false,
-    emptyDescription: false,
-    validImgUrl: true,
-    emptyImgUrl: false,
-    validImdbUrl: true,
-    emptyImdbUrl: false,
-    validImdbId: true,
-    emptyImdbId: false,
+  movie: {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
   },
-  submitDisabled: true,
+  errors: {
+    title: [],
+    description: [],
+    imgUrl: [],
+    imdbUrl: [],
+    imdbId: [],
+  },
   uniqueImdbId: true,
 };
 const REGEXP_TITLE = /[^\w0-9 ]+/g;
@@ -32,76 +29,56 @@ const REGEXP_URL = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]
 
 export class NewMovie extends Component {
   state = {
-    title: '',
-    description: '',
-    imgUrl: '',
-    imdbUrl: '',
-    imdbId: '',
-    errors: {
-      validTitle: false,
-      emptyTitle: false,
-      validDescription: false,
-      emptyDescription: false,
-      validImgUrl: true,
-      emptyImgUrl: false,
-      validImdbUrl: true,
-      emptyImdbUrl: false,
-      validImdbId: true,
-      emptyImdbId: false,
+    movie: {
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
     },
-    submitDisabled: true,
+    errors: {
+      title: [],
+      description: [],
+      imgUrl: [],
+      imdbUrl: [],
+      imdbId: [],
+    },
     uniqueImdbId: true,
   };
 
   checkUniqueImdbId = () => {
     const { listImdbId } = this.props;
 
-    this.setState(state => ({
-      uniqueImdbId: !listImdbId.includes(state.imdbId),
+    this.setState(({ movie }) => ({
+      uniqueImdbId: !listImdbId.includes(movie.imdbId),
     }));
-  }
-
-  validateForm = () => {
-    const {
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
-      errors,
-      uniqueImdbId,
-    } = this.state;
-
-    if (title && description && imgUrl && imdbUrl && imdbId) {
-      if (!errors.validTitle && !errors.validDescription
-      && errors.validImgUrl && errors.validImdbUrl
-      && errors.validImdbId && uniqueImdbId) {
-        return true;
-      }
-    }
-
-    return false;
   }
 
   validate = (regexp, event) => {
     const { name, value } = event.target;
+    const errors = {
+      title: [],
+      description: [],
+      imgUrl: [],
+      imdbUrl: [],
+      imdbId: [],
+    };
+
+    if (value === '') {
+      errors[name].push(`Please enter ${name}`);
+    }
+
+    const test = (name === 'title' || name === 'description')
+      ? !value.match(regexp) : regexp.test(value);
+
+    if (!test && value !== '') {
+      errors[name].push('Special characters are not allowed');
+    }
 
     this.setState(state => ({
       errors: {
         ...state.errors,
-        [`valid${name}`]: (name === 'Title' || name === 'Description')
-          ? value.match(regexp) : regexp.test(value),
-      },
-    }));
-  }
-
-  checkEmptyFailds = (event) => {
-    const { name, value } = event.target;
-
-    this.setState(state => ({
-      errors: {
-        ...state.errors,
-        [`empty${name}`]: value === '',
+        [name]: [...errors[name]],
       },
     }));
   }
@@ -109,174 +86,97 @@ export class NewMovie extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
-    if (this.validateForm()) {
-      this.props.onAdd(this.state);
-      this.setState(INITIAL_STATE);
-    }
+    this.props.onAdd(this.state.movie);
+    this.setState(INITIAL_STATE);
   }
 
   handleChange = (event) => {
     const { name, value } = event.target;
-    const key = name.split('').map((letter, index) => {
-      if (index === 0) {
-        return letter.toLowerCase();
-      }
 
-      return letter;
-    }).join('');
-
-    this.setState({
-      [key]: value,
-    });
+    this.setState(({ movie }) => ({
+      movie: {
+        ...movie,
+        [name]: value,
+      },
+    }));
   }
 
-  handleDisabledSubmit = () => {
-    if (this.validateForm()) {
-      this.setState({
-        submitDisabled: false,
-      });
+  unlockSubmit = () => {
+    const { errors, movie } = this.state;
+
+    return Object.values(errors).some(error => error.length > 0)
+      || Object.values(movie).some(value => value === '');
+  }
+
+  handleOnBluer = (event, key) => {
+    switch (event.target.name) {
+      case 'title':
+        this.validate(REGEXP_TITLE, event);
+        break;
+      case 'description':
+        this.validate(REGEXP_DESCRIPTION, event);
+        break;
+      case 'imgUrl':
+      case 'imdbUrl':
+        this.validate(REGEXP_URL, event);
+        break;
+      case 'imdbId':
+        this.validate(REGEXP_IMDBID, event);
+        break;
+      default:
+        break;
+    }
+
+    if (key === 'imdbId') {
+      this.checkUniqueImdbId();
     }
   }
 
   render() {
     const {
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
+      movie,
       errors,
-      submitDisabled,
       uniqueImdbId,
     } = this.state;
 
     return (
       <form>
-        <label>
-          Title
-          <input
-            type="text"
-            name="Title"
-            value={title}
-            placeholder="Title"
-            style={{
-              borderColor: errors.emptyTitle || errors.validTitle ? 'red' : '',
-            }}
-            onBlur={(event) => {
-              this.checkEmptyFailds(event);
-              this.validate(REGEXP_TITLE, event);
-              this.handleDisabledSubmit();
-            }}
-            onChange={this.handleChange}
-          />
-        </label>
-        <div className="error">
-          {errors.emptyTitle && 'Please enter title'}
-          {errors.validTitle && 'Special characters are not allowed'}
-        </div>
-
-        <label>
-          Description
-          <textarea
-            name="Description"
-            type="text"
-            value={description}
-            placeholder="Description"
-            style={{
-              borderColor: errors.emptyDescription || errors.validDescription ? 'red' : '',
-            }}
-            onBlur={(event) => {
-              this.validate(REGEXP_DESCRIPTION, event);
-              this.checkEmptyFailds(event);
-              this.handleDisabledSubmit();
-            }}
-            onChange={this.handleChange}
-          />
-        </label>
-        <div className="error">
-          {errors.emptyDescription && 'Please enter description'}
-          {errors.validDescription && 'Special characters are not allowed'}
-          {}
-        </div>
-
-        <label>
-          ImgUrl
-          <input
-            type="text"
-            name="ImgUrl"
-            value={imgUrl}
-            placeholder="ImgUrl"
-            style={{
-              borderColor: errors.emptyImgUrl || !errors.validImgUrl ? 'red' : '',
-            }}
-            onBlur={(event) => {
-              this.validate(REGEXP_URL, event);
-              this.checkEmptyFailds(event);
-              this.handleDisabledSubmit();
-            }}
-            onChange={this.handleChange}
-          />
-        </label>
-        <div className="error">
-          {errors.emptyImgUrl && 'Please enter ImgUrl'}
-          {(!errors.validImgUrl && !errors.emptyImgUrl) && 'Special characters are not allowed'}
-        </div>
-
-        <label>
-          ImdbUrl
-          <input
-            type="text"
-            name="ImdbUrl"
-            value={imdbUrl}
-            placeholder="ImdbUrl"
-            style={{
-              borderColor: errors.emptyImdbUrl || !errors.validImdbUrl ? 'red' : '',
-            }}
-            onBlur={(event) => {
-              this.checkEmptyFailds(event);
-              this.validate(REGEXP_URL, event);
-              this.handleDisabledSubmit();
-            }}
-            onChange={this.handleChange}
-          />
-        </label>
-        <div className="error">
-          {errors.emptyImdbUrl && 'Please enter ImdbUrl'}
-          {(!errors.validImdbUrl && !errors.emptyImdbUrl) && 'Special characters are not allowed'}
-        </div>
-
-        <label>
-          ImdbId
-          <input
-            type="text"
-            name="ImdbId"
-            value={imdbId}
-            placeholder="ImdbId"
-            style={{
-              borderColor: errors.emptyImdbId || !errors.validImdbId ? 'red' : '',
-            }}
-            onBlur={(event) => {
-              this.validate(REGEXP_IMDBID, event);
-              this.checkEmptyFailds(event);
-              this.handleDisabledSubmit();
-              this.checkUniqueImdbId();
-            }}
-            onChange={this.handleChange}
-          />
-        </label>
-        <div className="example">
-          [Exp.: tt95430396016]
-        </div>
-        <div className="error">
-          {errors.emptyImdbId && 'Please enter ImdbId'}
-          {(!errors.emptyImdbId && !errors.validImdbId) && 'Special characters are not allowed'}
-          {!uniqueImdbId && 'This ImdbId also exist. Please enter new ImdbId'}
-        </div>
+        {Object.keys(movie).map(key => (
+          <div
+            className="container"
+            key={uuid()}
+          >
+            <label>
+              {key.toUpperCase()}
+              <input
+                type="text"
+                className={errors[key].length > 0 ? 'error-input' : ''}
+                name={key}
+                value={movie[key]}
+                placeholder={key.toUpperCase()}
+                onBlur={event => this.handleOnBluer(event, key)}
+                onChange={this.handleChange}
+              />
+            </label>
+            {key === 'imdbId'
+              ? (
+                <div className="example">
+                  [Exp.: tt95430396016]
+                </div>
+              )
+              : null
+            }
+            <div className="error-message">
+              { errors[key] }
+              {(!uniqueImdbId && key === 'imdbId') && 'This ImdbId also exist. Please enter new ImdbId'}
+            </div>
+          </div>
+        ))}
 
         <input
           type="submit"
           value="Add Movie"
-          disabled={submitDisabled}
+          disabled={this.unlockSubmit()}
           onClick={this.handleSubmit}
         />
       </form>
