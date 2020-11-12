c90a5e10cebcc5ac6c046896bc6ad180b54790f0
kshvetsova <katshvetsova@gmail.com>
2020-11-12 02:57:07 +0200

    fixed Solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -5,36 +5,36 @@ import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
   state = {
-    title: '',
-    description: '',
-    imgUrl: '',
-    imdbUrl: '',
-    imdbId: '',
-    titleError: false,
-    descriptionError: false,
-    imgUrlError: false,
-    imdbUrlError: false,
-    imdbIdError: false,
-    errors: {},
-    imgUrlValid: true,
-    imdbUrlValid: true,
+    movie: {
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    },
+    errors: {
+      title: [],
+      description: [],
+      imgUrl: [],
+      imdbUrl: [],
+      imdbId: [],
+    },
   };
 
   handleChange = (e) => {
     const { value, name } = e.target;
 
-    this.setState({
-      [name]: value,
-      [`${name}Error`]: false,
-      imgUrlValid: true,
-      imdbUrlValid: true,
-    });
+    this.setState(state => ({
+      movie: {
+        ...state.movie,
+        [name]: value,
+      },
+    }));
   }
 
-  validate = () => {
-    const { title, description, imgUrl, imdbUrl, imdbId } = this.state;
+  validate = (name, value) => {
     // eslint-disable-next-line max-len
-    const url = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+    const regex = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
     const errors = {
       title: [],
       description: [],
@@ -42,150 +42,87 @@ export class NewMovie extends Component {
       imdbUrl: [],
       imdbId: [],
     };
-    let valid = true;
 
-    if (!title) {
-      this.setState({ titleError: !title });
-      errors.title.push('Enter title please!');
-      valid = false;
+    if (!value) {
+      errors[name].push(`Enter ${name} please!`);
     }
 
-    if (!description) {
-      this.setState({ descriptionError: !description });
-      errors.description.push('Enter description please!');
-      valid = false;
-    }
-
-    if (!imgUrl) {
-      this.setState({
-        imgUrlError: !imgUrl,
-        imgUrlValid: true,
-      });
-      errors.imgUrl.push('Enter imgUrl please!');
-      valid = false;
-    }
-
-    if (!imdbUrl) {
-      this.setState({
-        imdbUrlError: !imdbUrl,
-        imdbUrlValid: true,
-      });
-      errors.imdbUrl.push('Enter imdbUrl please!');
-      valid = false;
-    }
-
-    if (!imdbId) {
-      this.setState({ imdbIdError: !imdbId });
-      errors.imdbId.push('Enter description please!');
-      valid = false;
-    }
-
-    if (!url.test(imgUrl) && imgUrl) {
-      this.setState({
-        imgUrlError: imgUrl,
-        imgUrlValid: false,
-      });
-      errors.imgUrl.push('* imgUrl is not valid!');
-      valid = false;
-    }
-
-    if (!url.test(imdbUrl) && imdbUrl) {
-      this.setState({
-        imdbUrlError: imdbUrl,
-        imdbUrlValid: false,
-      });
-      errors.imdbUrl.push('* imdbUrl is not valid!');
-      valid = false;
+    if (!regex.test(value) && value
+      && (name === 'imdbUrl' || name === 'imgUrl')) {
+      errors[name].push(`* ${name} is not valid!`);
     }
 
     this.setState(state => ({
       ...state,
       errors,
     }));
+  }
+
+  handleOnBluer = (e) => {
+    const { value, name } = e.target;
+
+    this.validate(name, value);
+  }
+
+  hasErrors = () => {
+    const { errors, movie } = this.state;
+    let valid;
+
+    if (Object.values(errors).some(arr => arr.length > 0)
+        || Object.values(movie).some(arr => arr !== '')) {
+      valid = true;
+    }
+
+    if (Object.values(movie).every(arr => arr !== '')) {
+      valid = false;
+    }
 
     return valid;
   }
 
   handleSubmit = (e) => {
     const { addMovie } = this.props;
+    const { movie } = this.state;
 
     e.preventDefault();
-    if (this.validate()) {
-      addMovie(this.state);
-      this.setState({
+    if (Object.values(movie).every(arr => arr === '')) {
+      return;
+    }
+
+    addMovie(this.state.movie);
+    this.setState({
+      movie: {
         title: '',
         description: '',
         imgUrl: '',
         imdbUrl: '',
         imdbId: '',
-      });
-    }
+      },
+    });
   }
 
   render() {
-    const {
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
-      titleError,
-      descriptionError,
-      imgUrlError,
-      imdbUrlError,
-      imdbIdError,
-      imdbUrlValid,
-      imgUrlValid,
-      errors,
-    } = this.state;
+    const { movie, errors } = this.state;
 
     return (
       <form className="NewMovie" onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          value={title}
-          className={classNames('input')}
-          onChange={this.handleChange}
-        />
-        {titleError && (<span>{errors.title[0]}</span>)}
-        <input
-          name="description"
-          placeholder="Description"
-          value={description}
-          className={classNames('input')}
-          onChange={this.handleChange}
-        />
-        {descriptionError && (<span>{errors.description[0]}</span>)}
-        <input
-          type="text"
-          name="imgUrl"
-          placeholder="imgUrl"
-          value={imgUrl}
-          className={classNames('input', { invalid: !imgUrlValid })}
-          onChange={this.handleChange}
-        />
-        {imgUrlError && (<span>{errors.imgUrl[0]}</span>)}
-        <input
-          type="text"
-          name="imdbUrl"
-          placeholder="imdbUrl"
-          value={imdbUrl}
-          className={classNames('input', { invalid: !imdbUrlValid })}
-          onChange={this.handleChange}
-        />
-        {imdbUrlError && (<span>{errors.imdbUrl[0]}</span>)}
-        <input
-          type="text"
-          name="imdbId"
-          placeholder="imdbId"
-          value={imdbId}
-          className={classNames('input')}
-          onChange={this.handleChange}
-        />
-        {imdbIdError && (<span>{errors.imdbId[0]}</span>)}
-        <button type="submit">Add movie</button>
+        {Object.keys(movie).map(item => (
+          <div key={item}>
+            <input
+              type="text"
+              name={item}
+              placeholder={item}
+              value={movie[item]}
+              className={classNames('input', {
+                invalid: errors[item][0],
+              })}
+              onChange={this.handleChange}
+              onBlur={this.handleOnBluer}
+            />
+            <span>{errors[item]}</span>
+          </div>
+        ))}
+        <button type="submit" disabled={this.hasErrors()}>Add movie</button>
       </form>
     );
   }
