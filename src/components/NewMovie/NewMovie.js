549e4f1227d7c32bce053673f97d47a5e6a52672
vovador <vovadorko@gmail.com>
2021-01-20 14:27:44 +0200

    Solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,79 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imbdUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  }
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        this.props.onAdd(this.state);
+      }}
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        <textarea
+          name="description"
+          placeholder="What is the movie about?"
+          value={this.state.description}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          name="imgUrl"
+          placeholder="add imgUrl"
+          value={this.state.imgUrl}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          name="imbdUrl"
+          placeholder="add imbdUrl"
+          value={this.state.imbdUrl}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          name="imdbId"
+          placeholder="add movie imdbId"
+          value={this.state.imdbId}
+          onChange={this.handleChange}
+        />
+        <br />
+        <button type="submit">
+          Add movie
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
