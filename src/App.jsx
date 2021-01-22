af0929c27eac89468e7eff059b09194800cf3bb9
KirillLutsenko <mnrovski68@gmail.com>
2021-01-22 18:58:33 +0200

    Create form for add New Movie on React

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.scss';
+import { MoviesList } from './components/MoviesList';
+import { NewMovie } from './components/NewMovie';
+import moviesFromServer from './api/movies.json';
+
+export class App extends Component {
+  state = {
+    movies: moviesFromServer,
+  };
+
+  addMovie = (title, description, imgUrl, imdbUrl, imdbId) => {
+    this.setState(state => ({
+      movies: [
+        ...state.movies,
+        {
+          title,
+          description,
+          imgUrl,
+          imdbUrl,
+          imdbId,
+        },
+      ],
+    }));
+  };
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <div className="page">
+        <div className="page-content">
+          <MoviesList movies={movies} />
+        </div>
+        <div className="sidebar">
+          <NewMovie onAdd={this.addMovie} />
+        </div>
+      </div>
+    );
+  }
+}
