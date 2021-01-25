71a0b28ffe13fa22f832731177d5d417cb470e9d
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2021-01-25 15:56:24 +0200

    Redone after code review

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -10,8 +10,6 @@ export class NewMovie extends React.Component {
     imgUrl: '',
     imdbUrl: '',
     imdbId: '',
-    errors: [],
-    isValied: true,
   };
 
   handleChange = (event) => {
@@ -25,8 +23,6 @@ export class NewMovie extends React.Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
-    // if (!this.controlsValuesOnBlur()) {
-    // }
     this.props.onAdd(this.state);
 
     this.resetForm();
@@ -42,110 +38,20 @@ export class NewMovie extends React.Component {
     });
   }
 
-  // controlsValuesOnBlur = () => {
-  //   const {
-  //     title,
-  //     imgUrl,
-  //     imdbUrl,
-  //     imdbId,
-  //     errors,
-  //   } = this.state;
-
-  //   let {
-  //     isValied,
-  //   } = this.state;
-
-  //   if (!title) {
-  //     isValied = !isValied;
-  //     errors.push('Please, input the title');
-  //   }
-
-  //   // eslint-disable-next-line
-  //   if (!/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A
-  //   -Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[
-  //   +~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/.test(imgUrl)) {
-  //     isValied = false;
-  //     errors.push('Please, input the valid imgUrl');
-  //   }
-
-  //   // eslint-disable-next-line
-  //   if (!/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+
-  // |(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-
-  //  +=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/.test(imdbUrl)) {
-  //     isValied = false;
-  //     errors.push('Please, input the valid imdbUrl');
-  //   }
-
-  //   if (!imdbId) {
-  //     isValied = false;
-  //     errors.push('Please, input the imdbId');
-  //   }
-
-  //   this.setState({
-  //     errors,
-  //   });
-
-  //   return isValied;
-  // }
-
   render() {
-    const {
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
-      errors,
-    } = this.state;
-
     return (
       <form onSubmit={this.handleSubmit}>
-
-        <input
-          type="text"
-          name="title"
-          placeholder="Enter a title"
-          value={title}
-          onChange={this.handleChange}
-        />
-
-        <input
-          type="text"
-          name="description"
-          placeholder="Enter a description"
-          value={description}
-          onChange={this.handleChange}
-        />
-
-        <input
-          type="text"
-          name="imgUrl"
-          placeholder="Enter a imgUrl"
-          value={imgUrl}
-          onChange={this.handleChange}
-        />
-
-        <input
-          type="text"
-          name="imdbUrl"
-          placeholder="Enter a imdbUrl"
-          value={imdbUrl}
-          onChange={this.handleChange}
-        />
-
-        <input
-          type="text"
-          name="imdbId"
-          placeholder="Enter a imdbId"
-          value={imdbId}
-          onChange={this.handleChange}
-        />
-
-        <ol className="errors">
-          {errors.map(error => (
-            <li>{error}</li>
-          ))}
-        </ol>
+        {Object.keys(this.state).map(key1 => (
+          <input
+            key={Math.random()}
+            type="text"
+            name={key1}
+            placeholder={key1}
+            required
+            value={this.state[key1]}
+            onChange={this.handleChange}
+          />
+        ))}
 
         <button
           type="submit"
