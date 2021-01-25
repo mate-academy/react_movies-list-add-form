02007ef324746596f547a80e3732d46dc68f8f79
Vitalii Siedykh <siedykh.v@gmail.com>
2021-01-25 14:25:40 +0200

    fix

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -72,16 +72,19 @@ export class NewMovie extends Component {
       errors.push('IMDB ID is empty');
     }
 
+    // eslint-disable-next-line no-unused-vars,max-len
+    const regex = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
     // eslint-disable-next-line
-    if (!/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/.test(imdbUrl)) {
+    if (!regex.test(imdbUrl)) {
       isValid = false;
       errors.push('IMDB URL is not valid');
     }
 
     // eslint-disable-next-line
-    if (!/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/.test(imdbId)) {
+    if (!regex.test(imgUrl)) {
       isValid = false;
-      errors.push('IMDB ID is not valid');
+      errors.push('ImgUrl is not valid');
     }
 
     this.setState({
@@ -95,46 +98,67 @@ export class NewMovie extends Component {
     const { title, description, imgUrl, imdbUrl, imdbId, errors } = this.state;
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          value={title}
-          onChange={this.handleChange}
-        />
+      <form
+        className="form"
+        onSubmit={this.handleSubmit}
+      >
 
-        <input
-          type="text"
-          name="description"
-          placeholder="Description"
-          value={description}
-          onChange={this.handleChange}
-        />
+        <label htmlFor="text" className="label">
+          <h2>Movie Title</h2>
 
-        <input
-          type="text"
-          name="imgUrl"
-          placeholder="Image URL"
-          value={imgUrl}
-          onChange={this.handleChange}
-        />
+          <input
+            type="text"
+            name="title"
+            placeholder="Name"
+            value={title}
+            onChange={this.handleChange}
+          />
+        </label>
 
-        <input
-          type="text"
-          name="imdbUrl"
-          placeholder="IMDB URL"
-          value={imdbUrl}
-          onChange={this.handleChange}
-        />
+        <label htmlFor="description" className="label">
+          <h2>Movie description</h2>
 
-        <input
-          type="text"
-          name="imdbId"
-          placeholder="IMDB ID"
-          value={imdbId}
-          onChange={this.handleChange}
-        />
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={description}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <label htmlFor="imgUrl" className="label">
+          <h2>IMG Url</h2>
+          <input
+            type="text"
+            name="imgUrl"
+            placeholder="Image URL"
+            value={imgUrl}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <label htmlFor="imdbUrl" className="label">
+          <h2>IMDB Url</h2>
+          <input
+            type="text"
+            name="imdbUrl"
+            placeholder="IMDB URL"
+            value={imdbUrl}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <label htmlFor="imdbId" className="label">
+          <h2>IMDB ID</h2>
+          <input
+            type="text"
+            name="imdbId"
+            placeholder="IMDB ID"
+            value={imdbId}
+            onChange={this.handleChange}
+          />
+        </label>
 
         <ol className="errors">
           {errors.map(error => (
