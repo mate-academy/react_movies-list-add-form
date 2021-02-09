d2d6cbd878af4653edf1e54db09c5d47f9faa50b
Olga Rubalska <olga.anikeieva@gmail.com>
2021-02-09 16:27:20 +0200

    Solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,86 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './NewMovie.scss';
+
+const initState = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
 
 export class NewMovie extends Component {
-  state = {};
+  state = initState;
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const newMovie = this.creatNewMovie();
+
+    this.props.onAdd(newMovie);
+    this.clearForm();
+  }
+
+  creatNewMovie = () => {
+    const { title, description, imgUrl, imdbUrl, imdbId } = this.state;
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+      error: {
+        title: '',
+        imgUrl: '',
+        imdbUrl: '',
+        imdbId: '',
+      },
+      disabled: false,
+    };
+
+    return newMovie;
+  }
+
+  clearForm = () => {
+    this.setState(initState);
+  }
 
   render() {
+    const newMovie = Object.keys(this.state).slice(0, 5);
+
     return (
-      <form>
-        Put the form here
+      <form
+        className="form"
+        onSubmit={this.onSubmit}
+      >
+        {newMovie.map(item => (
+          <div key={item}>
+            <input
+              key={item}
+              className="input"
+              name={item}
+              type="text"
+              value={this.state[item]}
+              placeholder={`Enter ${item}`}
+              onChange={(event) => {
+                this.handleChange(event);
+              }}
+            />
+          </div>
+        ))}
+        <button type="submit">Add</button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
