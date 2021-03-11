5738b91c579ff898525789ff7acb5ca69fe43628
Bohdan Dobak <chudolipton@gmail.com>
2021-03-11 21:56:28 +0200

    made form component for adding new films to MoviesList

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,128 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form, TextArea } from 'semantic-ui-react';
 
-export class NewMovie extends Component {
-  state = {};
+export class NewMovie extends React.Component {
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { onAdd } = this.props;
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
+    if (
+      !title
+      || !description
+      || !imgUrl
+      || !imdbUrl
+      || !imdbId
+    ) {
+      return;
+    }
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+
+    const movie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    onAdd(movie);
+  }
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl, imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
-      </form>
+      <Form
+        onSubmit={this.handleSubmit}
+        success
+      >
+        <Form.Group widths="equal">
+          <Form.Input
+            transparent
+            placeholder="Title"
+            type="text"
+            name="title"
+            value={title}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <TextArea
+          placeholder="Description"
+          name="description"
+          value={description}
+          onChange={this.handleChange}
+        />
+        <Form.Group widths="equal">
+          <Form.Input
+            transparent
+            placeholder="ImgUrl"
+            type="url"
+            name="imgUrl"
+            value={imgUrl}
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            transparent
+            placeholder="ImdbUrl"
+            type="url"
+            name="imdbUrl"
+            value={imdbUrl}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Input
+          transparent
+          placeholder="ImdbId"
+          type="text"
+          name="imdbId"
+          value={imdbId}
+          onChange={this.handleChange}
+        />
+
+        <Form.Button>
+          Add Film
+        </Form.Button>
+      </Form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
