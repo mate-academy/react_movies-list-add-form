8d020ee9a34bc0f092cf5670e18a373c0687ab30
Misha Lysak <myhaillysak@gmail.com>
2021-03-15 11:19:33 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ export class App extends Component {
   };
 
   addMovie = (movie) => {
-    // put your code here
+    this.setState((state) => {
+      const newMovie = {
+        ...movie,
+      };
+
+      return {
+        movies: [...state.movies, newMovie],
+      };
+    });
   };
 
   render() {
@@ -22,7 +30,7 @@ export class App extends Component {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie addMovie={this.addMovie} />
         </div>
       </div>
     );
--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,12 +1,97 @@
 import React, { Component } from 'react';
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  changeHandler = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // не можу ніяк написати пропси до цієї функції
+    // eslint-disable-next-line react/prop-types
+    this.props.addMovie(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  }
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form
+        method="GET"
+        onSubmit={this.onSubmitHandler}
+      >
+        <h2>Add a new movie</h2>
+        <label>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={this.changeHandler}
+            required
+          />
+        </label>
+        <label>
+          <textarea
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={this.state.description}
+            onChange={this.changeHandler}
+            required
+          />
+        </label>
+        <label>
+          <input
+            type="url"
+            name="imgUrl"
+            placeholder="ImgUrl"
+            value={this.state.imgUrl}
+            onChange={this.changeHandler}
+            required
+          />
+        </label>
+        <label>
+          <input
+            type="url"
+            name="imdbUrl"
+            placeholder="ImdbUrl"
+            value={this.state.imdbUrl}
+            onChange={this.changeHandler}
+            required
+          />
+        </label>
+        <label>
+          <input
+            type="text"
+            name="imdbId"
+            placeholder="ImdbId"
+            value={this.state.imdbId}
+            onChange={this.changeHandler}
+            required
+          />
+        </label>
+        <button type="submit">Add</button>
       </form>
     );
   }
