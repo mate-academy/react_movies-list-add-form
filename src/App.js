7ace04fed5b83d0beb5fa5ecfca6fcadffe75729
vitaliiholubinka <holybinka-1996@outlook.com>
2021-03-15 13:06:30 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -9,8 +9,58 @@ export class App extends Component {
     movies: moviesFromServer,
   };
 
+  validate = {
+    title: title => this.titleOrIdValidation('Title', title),
+    imgUrl: url => this.urlValidation(url),
+    imdbUrl: url => this.urlValidation(url),
+    imdbId: id => this.titleOrIdValidation('ID', id),
+  };
+
+  initialValues = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
   addMovie = (movie) => {
-    // put your code here
+    this.setState(state => ({
+      movies: [...state.movies, movie],
+    }));
+  };
+
+  titleOrIdValidation = (fieldName, fieldValue) => {
+    if (fieldValue.trim() === '') {
+      return `${fieldName} is required`;
+    }
+
+    if (fieldName === 'Title' && /[^a-zA-Z -]/.test(fieldValue)) {
+      return 'Invalid characters';
+    }
+
+    if (fieldValue.trim().length < 1) {
+      return `${fieldName} needs to be at least one characters`;
+    }
+
+    return null;
+  };
+
+  urlValidation = (url) => {
+    if (
+      // eslint-disable-next-line max-len
+      /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/.test(
+        url,
+      )
+    ) {
+      return null;
+    }
+
+    if (url.trim() === '') {
+      return 'Url is required';
+    }
+
+    return 'Please enter a valid Url';
   };
 
   render() {
@@ -22,7 +72,11 @@ export class App extends Component {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie
+            onAdd={this.addMovie}
+            validate={this.validate}
+            initialValues={this.initialValues}
+          />
         </div>
       </div>
     );
--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,200 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    values: {
+      imdbId: '',
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+    },
+    errors: {},
+    maxlength: 30,
+    charsLeft: 30,
+  };
+
+  handleChange = (event) => {
+    const { name, value, type } = event.target;
+    const { maxlength } = this.state;
+
+    event.persist();
+
+    this.setState(state => ({
+      values: {
+        ...state.values,
+        [name]: type === 'text' ? value.replace(/^\s/g, '') : value,
+      },
+      charsLeft: type === 'text' && name === 'title'
+        ? maxlength - value.length
+        : state.charsLeft,
+    }));
+  }
+
+  handleBlur = (event) => {
+    const { name, value } = event.target;
+    const { validate } = this.props;
+    const { errors } = this.state;
+
+    const { [name]: removedError, ...rest } = errors;
+    const error = validate[name](value);
+
+    this.setState({
+      errors: {
+        ...rest,
+        ...(error && { [name]: error }),
+      },
+    });
+  };
+
+  handleSubmit = (event) => {
+    const { values, errors, maxlength } = this.state;
+    const { validate, onAdd, initialValues } = this.props;
+
+    event.preventDefault();
+
+    const formValidation = Object.keys(values).reduce(
+      (acc, key) => {
+        const isDescriptionKey = key === 'description';
+        const newError = isDescriptionKey ? null : validate[key](values[key]);
+
+        return {
+          errors: {
+            ...acc.errors,
+            ...(newError && { [key]: newError }),
+          },
+        };
+      },
+      {
+        errors: { ...errors },
+      },
+    );
+
+    this.setState({
+      errors: formValidation.errors,
+    });
+
+    if (
+      !Object.values(formValidation.errors).length // errors object is empty
+    ) {
+      onAdd(values);
+      this.setState({
+        values: initialValues,
+        charsLeft: maxlength,
+      });
+    }
+  };
 
   render() {
+    const {
+      maxlength,
+      charsLeft,
+      values,
+      errors,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
-      </form>
+      <div className="newMovie">
+        <form onSubmit={this.handleSubmit} autoComplete="off">
+          <div>
+            <input
+              type="text"
+              name="imdbId"
+              className={errors.imdbId ? 'error' : ''}
+              value={values.imdbId}
+              placeholder="Please enter a ID"
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+            />
+            {errors.imdbId && (
+              <div className="errorText">
+                {errors.imdbId}
+              </div>
+            )}
+          </div>
+          <div>
+            <input
+              type="text"
+              name="title"
+              maxLength={maxlength}
+              className={errors.title ? 'error' : ''}
+              value={values.title}
+              placeholder="Please enter a title"
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+            />
+            <p>
+              Characters Left:
+              {' '}
+              { charsLeft }
+            </p>
+            {errors.title && (
+            <div className="errorText">
+              {errors.title}
+            </div>
+            )}
+          </div>
+          <div>
+            <textarea
+              name="description"
+              value={values.description}
+              placeholder="Please enter a description"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="imgUrl"
+              className={errors.imgUrl ? 'error' : ''}
+              value={values.imgUrl}
+              placeholder="Please enter a image Url"
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+            />
+            {errors.imgUrl && (
+            <div className="errorText">
+              {errors.imgUrl}
+            </div>
+            )}
+          </div>
+          <div>
+            <input
+              type="text"
+              name="imdbUrl"
+              className={errors.imdbUrl ? 'error' : ''}
+              value={values.imdbUrl}
+              placeholder="Please enter a Imdb Url"
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+            />
+            {errors.imdbUrl && (
+            <div className="errorText">
+              {errors.imdbUrl}
+            </div>
+            )}
+          </div>
+          <button
+            className="add"
+            type="submit"
+            disabled={Object.keys(errors).length !== 0}
+          >
+            Add
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+  initialValues: PropTypes.shape().isRequired,
+  validate: PropTypes.shape().isRequired,
+};
+
+NewMovie.default = {
+  description: '',
+};
