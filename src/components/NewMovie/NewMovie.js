8567295c86e7e50ed7c00959daabab6f079518f9
Artem Topolskyi <topolskyiav@gmail.com>
2021-03-18 10:58:06 +0200

    added validation

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ClassNames from 'classnames';
 
 import './NewMovie.scss';
 
 export class NewMovie extends Component {
   state = {
     title: '',
+    isTitleCorrect: true,
     description: '',
+    isDescriptionCorrect: true,
     imgUrl: '',
+    isImgUrlCorrect: true,
     imdbUrl: '',
+    isImdbUrlCorrect: true,
     imdbId: '',
+    isImdbIdCorrect: true,
   };
 
   clearForm = () => {
@@ -28,6 +34,41 @@ export class NewMovie extends Component {
     this.setState({ [name]: value });
   };
 
+  blurHandler = (event) => {
+    const { name } = event.target;
+
+    if (this.validateData(name)) {
+      this.setState(
+        { [`is${name[0].toUpperCase() + name.slice(1)}Correct`]: true },
+      );
+    } else {
+      this.setState(
+        { [`is${name[0].toUpperCase() + name.slice(1)}Correct`]: false },
+      );
+    }
+  }
+
+  validateData = (name) => {
+    // eslint-disable-next-line
+    const regexTester = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
+    const value = this.state[name];
+
+    if (name === 'description') {
+      return true;
+    }
+
+    if (value.length < 4) {
+      return false;
+    }
+
+    if (name === 'imdbUrl' || name === 'imgUrl') {
+      return regexTester.test(value);
+    }
+
+    return true;
+  }
+
   createFilm = () => {
     const { title, description, imgUrl, imdbUrl, imdbId } = this.state;
 
@@ -40,9 +81,55 @@ export class NewMovie extends Component {
     };
   };
 
+  submitForm = (event) => {
+    event.preventDefault();
+
+    let isFormValid = true;
+
+    if (!this.validateData('title')) {
+      isFormValid = false;
+      this.setState({ isTitleCorrect: false });
+    }
+
+    if (!this.validateData('description')) {
+      isFormValid = false;
+      this.setState({ isDescriptionCorrect: false });
+    }
+
+    if (!this.validateData('imgUrl')) {
+      isFormValid = false;
+      this.setState({ isImgUrlCorrect: false });
+    }
+
+    if (!this.validateData('imdbUrl')) {
+      isFormValid = false;
+      this.setState({ isImdbUrlCorrect: false });
+    }
+
+    if (!this.validateData('imdbId')) {
+      isFormValid = false;
+      this.setState({ isImdbIdCorrect: false });
+    }
+
+    if (isFormValid) {
+      this.clearForm();
+      this.props.onAdd(this.createFilm());
+    }
+  }
+
   render() {
-    const { onAdd } = this.props;
-    const { title, description, imgUrl, imdbUrl, imdbId } = this.state;
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+      isTitleCorrect,
+      isDescriptionCorrect,
+      isImgUrlCorrect,
+      isImdbUrlCorrect,
+      isImdbIdCorrect,
+    } = this.state;
 
     return (
       <>
@@ -51,11 +138,7 @@ export class NewMovie extends Component {
         </h2>
 
         <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            this.clearForm();
-            onAdd(this.createFilm());
-          }}
+          onSubmit={this.submitForm}
         >
           <input
             type="text"
@@ -63,8 +146,13 @@ export class NewMovie extends Component {
             placeholder="Enter title"
             value={title}
             onChange={this.changeHandler}
-            className="input"
+            onBlur={this.blurHandler}
+            className={ClassNames('input', { 'wrong-input': !isTitleCorrect })}
           />
+
+          {isTitleCorrect || (
+            <div className="error-message">Incorrect title</div>
+          )}
 
           <textarea
             type="text"
@@ -72,8 +160,15 @@ export class NewMovie extends Component {
             placeholder="Enter description"
             value={description}
             onChange={this.changeHandler}
-            className="input"
+            onBlur={this.blurHandler}
+            className={ClassNames(
+              'input', { 'wrong-input': !isDescriptionCorrect },
+            )}
           />
+
+          {isDescriptionCorrect || (
+            <div className="error-message">Incorrect description</div>
+          )}
 
           <input
             type="text"
@@ -81,8 +176,13 @@ export class NewMovie extends Component {
             placeholder="Enter image Url"
             value={imgUrl}
             onChange={this.changeHandler}
-            className="input"
+            onBlur={this.blurHandler}
+            className={ClassNames('input', { 'wrong-input': !isImgUrlCorrect })}
           />
+
+          {isImgUrlCorrect || (
+            <div className="error-message">Incorrect image Url</div>
+          )}
 
           <input
             type="text"
@@ -90,8 +190,15 @@ export class NewMovie extends Component {
             placeholder="Enter IMDB Url"
             value={imdbUrl}
             onChange={this.changeHandler}
-            className="input"
+            onBlur={this.blurHandler}
+            className={ClassNames(
+              'input', { 'wrong-input': !isImdbUrlCorrect },
+            )}
           />
+
+          {isImdbUrlCorrect || (
+            <div className="error-message">Incorrect IMBD Url</div>
+          )}
 
           <input
             type="text"
@@ -99,12 +206,17 @@ export class NewMovie extends Component {
             placeholder="Enter IMDB Id"
             value={imdbId}
             onChange={this.changeHandler}
-            className="input"
+            onBlur={this.blurHandler}
+            className={ClassNames('input', { 'wrong-input': !isImdbIdCorrect })}
           />
+
+          {isImdbIdCorrect || (
+            <div className="error-message">Incorrect IMBD Id</div>
+          )}
 
           <button
             type="submit"
-            className="button"
+            className={ClassNames('button')}
           >
             Add film
           </button>
