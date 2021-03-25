9ecfc87e98d94de2f4649dba970542348129bc4e
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-25 19:40:01 +0200

    solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,109 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    id: 0,
+  };
+
+  setData = (key) => {
+    return (event) => {
+      this.setState({
+        [key]: event.target.value,
+      });
+    };
+  }
+
+  addFilm = (event) => {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+    } = this.state;
+
+    event.preventDefault();
+
+    if (title === '' || description === '' || imgUrl === '' || imdbUrl === '') {
+      return;
+    }
+
+    this.props.add(this.state);
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      id: 0,
+    });
+  }
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form className="form">
+        <label>
+          Title:
+          <input
+            type="text"
+            onChange={this.setData('title')}
+            value={title}
+          />
+        </label>
+        <label>
+          Description:
+          <input
+            type="text"
+            onChange={this.setData('description')}
+            value={description}
+          />
+        </label>
+        <label>
+          Image Url:
+          <input
+            type="text"
+            onChange={this.setData('imgUrl')}
+            value={imgUrl}
+          />
+        </label>
+        <label>
+          IMDB Url:
+          <input
+            type="text"
+            onChange={this.setData('imdbUrl')}
+            value={imdbUrl}
+          />
+        </label>
+        <label>
+          IMDB Id:
+          <input
+            type="number"
+            onChange={this.setData('id')}
+            value={this.state.id}
+          />
+        </label>
+        <button
+          type="button"
+          onClick={this.addFilm}
+        >
+          Submit
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  add: PropTypes.func.isRequired,
+};
