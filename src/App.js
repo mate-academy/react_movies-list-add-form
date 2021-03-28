90a213c22e3b3099e3bf02f81b4ef03f20a3b348
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-28 14:10:04 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ export class App extends Component {
   };
 
   addMovie = (movie) => {
-    // put your code here
+    this.setState(prevState => ({
+      movies: [
+        ...prevState.movies,
+        movie,
+      ],
+    }));
   };
 
   render() {
@@ -19,10 +24,14 @@ export class App extends Component {
     return (
       <div className="page">
         <div className="page-content">
-          <MoviesList movies={movies} />
+          <MoviesList
+            movies={movies}
+          />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie
+            addMovie={this.addMovie}
+          />
         </div>
       </div>
     );
--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,109 @@
 import React, { Component } from 'react';
+import './NewMovie.scss';
+import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleSubmit = () => {
+    const { addMovie } = this.props;
+
+    addMovie(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  }
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form
+        className="form"
+        onSubmit={(event) => {
+          event.preventDefault();
+          this.handleSubmit();
+        }}
+      >
+        <input
+          className="input"
+          type="text"
+          placeholder="Title"
+          value={this.state.title}
+          onChange={(event) => {
+            this.setState({
+              title: event.target.value,
+            });
+          }}
+          required
+        />
+        <input
+          className="input"
+          type="textarea"
+          placeholder="Description"
+          value={this.state.description}
+          onChange={(event) => {
+            this.setState({
+              description: event.target.value,
+            });
+          }}
+        />
+        <input
+          className="input"
+          type="text"
+          placeholder="ImgUrl"
+          value={this.state.ImgUrl}
+          onChange={(event) => {
+            this.setState({
+              ImgUrl: event.target.value,
+            });
+          }}
+          required
+        />
+        <input
+          className="input"
+          type="text"
+          placeholder="ImdbUrl"
+          value={this.state.ImdbUrl}
+          onChange={(event) => {
+            this.setState({
+              ImdbUrl: event.target.value,
+            });
+          }}
+          required
+        />
+        <input
+          className="input"
+          type="text"
+          placeholder="ImdbId"
+          value={this.state.ImdbId}
+          onChange={(event) => {
+            this.setState({
+              ImdbId: event.target.value,
+            });
+          }}
+          required
+        />
+        <button
+          className="button"
+          type="submit"
+        >
+          Add movie
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
