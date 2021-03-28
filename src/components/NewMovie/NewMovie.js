c746e04f4a1ff0341d7cacd71d42467a15f7854c
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-28 21:36:35 +0300

    fixedCheck

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -9,6 +9,7 @@ export class NewMovie extends Component {
     imgUrl: '',
     imdbUrl: '',
     imdbId: '',
+    hasEmptyField: false,
   };
 
   handleChange = (event) => {
@@ -16,15 +17,29 @@ export class NewMovie extends Component {
 
     this.setState({
       [name]: value,
+      hasEmptyField: false,
     });
   }
 
   handleSubmit = (event) => {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     event.preventDefault();
 
-    this.props.onAdd(this.state);
+    if ((title && description && imgUrl
+      && imdbUrl && imdbId) === '') {
+      return this.setState({ hasEmptyField: true });
+    }
 
-    this.setState({
+    this.props.onAdd({ ...this.state });
+
+    return this.setState({
       title: '',
       description: '',
       imgUrl: '',
@@ -40,59 +55,63 @@ export class NewMovie extends Component {
       imgUrl,
       imdbUrl,
       imdbId,
+      hasEmptyField,
     } = this.state;
 
     return (
-      <form
-        onSubmit={this.handleSubmit}
-      >
-        <input
-          name="title"
-          className="input is-medium input-margin"
-          type="text"
-          placeholder="title"
-          value={title}
-          onChange={this.handleChange}
-        />
-        <input
-          name="description"
-          className="input is-medium input-margin"
-          type="text"
-          placeholder="description"
-          value={description}
-          onChange={this.handleChange}
-        />
-        <input
-          name="imgUrl"
-          className="input is-medium input-margin"
-          type="text"
-          placeholder="imgUrl"
-          value={imgUrl}
-          onChange={this.handleChange}
-        />
-        <input
-          name="imdbUrl"
-          className="input is-medium input-margin"
-          type="text"
-          placeholder="imdbUrl"
-          value={imdbUrl}
-          onChange={this.handleChange}
-        />
-        <input
-          name="imdbId"
-          className="input is-medium input-margin"
-          type="text"
-          placeholder="imdbId"
-          value={imdbId}
-          onChange={this.handleChange}
-        />
-        <button
-          className="button"
-          type="submit"
+      <>
+        <p>{hasEmptyField && 'Need to complete form'}</p>
+        <form
+          onSubmit={this.handleSubmit}
         >
-          Add Movie
-        </button>
-      </form>
+          <input
+            name="title"
+            className="input is-medium input-margin"
+            type="text"
+            placeholder="title"
+            value={title}
+            onChange={this.handleChange}
+          />
+          <input
+            name="description"
+            className="input is-medium input-margin"
+            type="text"
+            placeholder="description"
+            value={description}
+            onChange={this.handleChange}
+          />
+          <input
+            name="imgUrl"
+            className="input is-medium input-margin"
+            type="text"
+            placeholder="imgUrl"
+            value={imgUrl}
+            onChange={this.handleChange}
+          />
+          <input
+            name="imdbUrl"
+            className="input is-medium input-margin"
+            type="text"
+            placeholder="imdbUrl"
+            value={imdbUrl}
+            onChange={this.handleChange}
+          />
+          <input
+            name="imdbId"
+            className="input is-medium input-margin"
+            type="text"
+            placeholder="imdbId"
+            value={imdbId}
+            onChange={this.handleChange}
+          />
+          <button
+            className="button"
+            type="submit"
+          >
+            Add Movie
+          </button>
+        </form>
+      </>
     );
   }
 }
