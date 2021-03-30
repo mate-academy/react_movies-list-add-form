89519cd967393e6a11c58c7054c11a36c503189b
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-30 10:09:13 +0300

    soluion

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,198 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+
+    hasimdbUrlError: false,
+    hasimgUrlError: false,
+  };
+
+  setValue = (event) => {
+    const { name } = event.target;
+
+    this.setState({
+      [name]: event.target.value,
+    });
+  }
+
+  testValid = (currentURL) => {
+    // eslint-disable-next-line
+    const isValidURL = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
+    return isValidURL.test(currentURL);
+  };
+
+  handleSubmit = (event) => {
+    const { title, description, imgUrl, imdbUrl, imdbId } = this.state;
+
+    event.preventDefault();
+
+    if (!this.testValid(imdbUrl) && !this.testValid(imgUrl)) {
+      this.setState({
+        hasimdbUrlError: true,
+        hasimgUrlError: true,
+      });
+
+      return;
+    }
+
+    if (!this.testValid(imgUrl)) {
+      this.setState({
+        hasimgUrlError: true,
+      });
+
+      return;
+    }
+
+    if (!this.testValid(imdbUrl)) {
+      this.setState({
+        hasimdbUrlError: true,
+      });
+
+      return;
+    }
+
+    this.props.onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  }
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
-      </form>
+      <>
+        <h2 className="title">
+          Add a movie
+        </h2>
+
+        <form
+          className="form"
+          onSubmit={(event) => {
+            this.handleSubmit(event);
+          }}
+        >
+
+          <label htmlFor="title-movie">
+            <input
+              name="title"
+              className="form__input"
+              id="title-movie"
+              placeholder="Enter a title"
+              value={title}
+              onChange={(event) => {
+                this.setValue(event);
+              }}
+              required
+            />
+          </label>
+          <br />
+          <label htmlFor="description-movie">
+            <textarea
+              name="description"
+              className="form__input description"
+              type="text"
+              id="description-movie"
+              placeholder="Enter a description"
+              value={description}
+              onChange={(event) => {
+                this.setValue(event);
+              }}
+            />
+          </label>
+          <br />
+
+          <label htmlFor="image-movie">
+            {this.state.hasimgUrlError && (
+              <span className="error-font">Please enter the imgURL</span>
+            )}
+            <input
+              name="imgUrl"
+              className={classNames('form__input movie-image', {
+                'error-message': this.state.hasimgUrlError,
+              })}
+              id="movie-image"
+              placeholder="Enter an image URL"
+              value={imgUrl}
+              onChange={(event) => {
+                this.setValue(event);
+              }}
+              required
+            />
+          </label>
+          <br />
+          <label htmlFor="movie-imdb">
+            {this.state.hasimgUrlError && (
+              <span className="error-font">Please enter the imdbURL</span>
+            )}
+            <input
+              name="imdbUrl"
+              className={classNames('form__input movie-imdb', {
+                'error-message': this.state.hasimdbUrlError,
+              })}
+              id="movie-imdb"
+              placeholder="Enter IMDB URL"
+              value={imdbUrl}
+              onChange={(event) => {
+                this.setValue(event);
+              }}
+              required
+            />
+          </label>
+          <br />
+          <label htmlFor="movie-imdb-id">
+            <input
+              name="imdbId"
+              className="form__input movie-imdb-id"
+              id="movie-imdb-id"
+              placeholder="Enter IMDB Id"
+              value={imdbId}
+              onChange={(event) => {
+                this.setValue(event);
+              }}
+              required
+            />
+          </label>
+          <br />
+
+          <button
+            className="form__button"
+            type="submit"
+          >
+            Add movie
+          </button>
+        </form>
+      </>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
