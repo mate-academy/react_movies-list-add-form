1ae3198aca26c6be251511231b4d1c1af1d8c5a5
Olga Briukhanova <oaleksandrovva@gmail.com>
2021-03-30 13:26:50 +0300

    solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,106 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: 'Description...',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { onAddMovie } = this.props;
+
+    onAddMovie(this.state);
+
+    this.setState({
+      title: '',
+      description: 'Description...',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form onSubmit={this.handleSubmit} className="form">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          className="form__field"
+          value={title}
+          onChange={this.handleChange}
+        />
+
+        <textarea
+          name="description"
+          className="form__field"
+          value={description}
+          onChange={this.handleChange}
+          onClick={() => this.setState({
+            description: '',
+          })}
+        >
+          Description
+        </textarea>
+
+        <input
+          type="text"
+          name="imgUrl"
+          placeholder="ImgUrl"
+          className="form__field"
+          value={imgUrl}
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          name="imdbUrl"
+          placeholder="ImdbUrl"
+          className="form__field"
+          value={imdbUrl}
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          name="imdbId"
+          placeholder="ImdbId"
+          className="form__field"
+          value={imdbId}
+          onChange={this.handleChange}
+        />
+
+        <button type="submit" className="form__button">Add Movie</button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAddMovie: PropTypes.func.isRequired,
+};
