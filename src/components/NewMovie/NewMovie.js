86958fd74cfee8502c13631e7ff98f05db4cf6ea
Ihor Drul <druleger@gmail.com>
2021-04-08 09:15:19 +0300

    Changed regex

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 import './NewMovie.scss';
 
 // eslint-disable-next-line
-const regex = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+const regex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
 
 export class NewMovie extends Component {
   state = {
