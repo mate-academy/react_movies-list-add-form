012fa36d1ad7f5c05d086276fdc7abec2ca12db7
Julia <mjulia1794@gmail.com>
2021-04-12 16:04:04 +0300

    solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,108 @@
 import React, { Component } from 'react';
+import './NewMovie.scss';
+import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.addMovie({
+      title: this.state.title,
+      description: this.state.description,
+      imgUrl: this.state.imgUrl,
+      imdbUrl: this.state.imdbUrl,
+      imdbId: this.state.imdbId,
+    });
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  }
 
   render() {
+    const { title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId } = this.state;
+
     return (
-      <form>
-        Put the form here
-      </form>
+      <div className="form">
+        <h1 className="display-4">Add Movie</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="form-control"
+            placeholder="Title"
+            value={title}
+            onChange={(event) => {
+              this.setState({
+                title: event.target.value,
+              });
+            }}
+            required
+          />
+          <input
+            className="form-control"
+            placeholder="Description"
+            value={description}
+            onChange={(event) => {
+              this.setState({
+                description: event.target.value,
+              });
+            }}
+          />
+          <input
+            className="form-control"
+            placeholder="imgUrl"
+            value={imgUrl}
+            onChange={(event) => {
+              this.setState({
+                imgUrl: event.target.value,
+              });
+            }}
+            required
+          />
+          <input
+            className="form-control"
+            placeholder="imdbUrl"
+            value={imdbUrl}
+            onChange={(event) => {
+              this.setState({
+                imdbUrl: event.target.value,
+              });
+            }}
+            required
+          />
+          <input
+            className="form-control"
+            placeholder="imdbId"
+            value={imdbId}
+            onChange={(event) => {
+              this.setState({
+                imdbId: event.target.value,
+              });
+            }}
+            required
+          />
+          <button className="btn btn-success" type="submit">Add movie</button>
+        </form>
+      </div>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
