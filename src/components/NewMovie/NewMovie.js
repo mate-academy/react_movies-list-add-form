0c0d2dbc68e18269fbee2ace7dd2dd2924d93636
Svyat Krit <krit.mgmn@gmail.com>
2021-04-13 12:30:46 +0300

    Solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,118 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState(state => ({
+      ...state,
+      [name]: value,
+    }));
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newMovie = { ...this.state };
+
+    this.props.addMovie(newMovie);
+    this.resetForm();
+  }
+
+  resetForm = () => {
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  }
 
   render() {
+    const { title, description, imgUrl, imdbUrl, imdbId } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form
+        onSubmit={(event) => {
+          this.handleSubmit(event);
+        }}
+      >
+        <span className="mb-1">Title</span>
+        <input
+          className="input is-small mb-3"
+          name="title"
+          type="text"
+          value={title}
+          placeholder="Title"
+          onChange={(event) => {
+            this.handleChange(event);
+          }}
+        />
+        <span className="mb-1">Description</span>
+        <input
+          className="input is-small mb-3"
+          name="description"
+          type="text"
+          value={description}
+          placeholder="Description"
+          onChange={(event) => {
+            this.handleChange(event);
+          }}
+        />
+        <span className="mb-1">Image Url</span>
+        <input
+          className="input is-small mb-3"
+          name="imgUrl"
+          type="text"
+          value={imgUrl}
+          placeholder="Image Url"
+          onChange={(event) => {
+            this.handleChange(event);
+          }}
+        />
+        <span className="mb-1">Imdb Url</span>
+        <input
+          className="input is-small mb-3"
+          name="imdbUrl"
+          type="text"
+          value={imdbUrl}
+          placeholder="Imdb Url"
+          onChange={(event) => {
+            this.handleChange(event);
+          }}
+        />
+        <span className="mb-1">Imdb Id</span>
+        <input
+          className="input is-small mb-3"
+          name="imdbId"
+          type="text"
+          value={imdbId}
+          placeholder="Imdb Id"
+          onChange={(event) => {
+            this.handleChange(event);
+          }}
+        />
+        <button
+          type="submit"
+          className="button is-small mb-3"
+        >
+          Add Movie
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
