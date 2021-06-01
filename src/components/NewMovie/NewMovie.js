a2d58fc6a529856a21a161c9ff3a2aef241a2e08
Georgy Dzumenko <erterj05@gmail.com>
2021-06-01 23:59:28 +0300

    new solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -11,76 +11,141 @@ export class NewMovie extends Component {
     description: '',
   };
 
+  // я не зміг з'ясувати як перенести regex
+  // eslint-disable-next-line max-len
+  onlyUrl = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
   render() {
+    const { title, imgUrl, imdbUrl, imdbId, description } = this.state;
+
+    const isImdbUrlValid = imdbUrl.match(this.onlyUrl) || imdbUrl === '';
+    const isImgUrlValid = imgUrl.match(this.onlyUrl) || imgUrl === '';
+
     return (
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          this.props.addMovie(
-            {
-              title: this.state.title,
-              description: this.state.description,
-              imgUrl: this.state.imgUrl,
-              imdbUrl: this.state.imdbUrl,
-              imdbId: this.state.imdbId,
-            },
-          );
-        }}
-      >
-        <label>
-          Title:
-          <input
-            className="addInfoBar"
-            id="Title"
-            placeholder="Title"
-            onChange={event => this.setState({ title: event.target.value })}
-            required
-          />
-        </label>
-        <label>
-          Image url:
-          <input
-            className="addInfoBar"
-            placeholder="Image url"
-            onChange={event => this.setState({ imgUrl: event.target.value })}
-            required
-          />
-        </label>
-        <label>
-          imdbUrl:
-          <input
-            className="addInfoBar"
-            placeholder="imdbUrl"
-            onChange={event => this.setState({ imdbUrl: event.target.value })}
-            required
-          />
-        </label>
-        <label>
-          imdbId:
-          <input
-            className="addInfoBar"
-            placeholder="imdbId"
-            onChange={event => this.setState({ imdbId: event.target.value })}
-            required
-          />
-        </label>
-        <label>
-          Description*:
-          <textarea
-            className="addInfoBar description"
-            placeholder="Description*"
-            onChange={event => this.setState({
-              description: event.target.value,
-            })}
-          />
-        </label>
-        <button
-          type="submit"
-          className="submitButton"
+      <>
+        <h1 className="addFilmTitle">
+          Add film form
+        </h1>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+
+            if (!imgUrl.match(this.onlyUrl) || !imdbUrl.match(this.onlyUrl)) {
+              return;
+            }
+
+            this.setState(() => ({
+              title: '',
+              imgUrl: '',
+              imdbUrl: '',
+              imdbId: '',
+              description: '',
+            }));
+
+            this.props.addMovie(
+              {
+                title,
+                description,
+                imgUrl,
+                imdbUrl,
+                imdbId,
+              },
+            );
+          }}
         >
-          Submit
-        </button>
-      </form>
+          <ul>
+            <li>
+              <label>
+                Title*:
+                <input
+                  value={title}
+                  className="addInfoBar"
+                  id="Title"
+                  placeholder="Title*"
+                  onChange={event => this.setState({
+                    title: event.target.value,
+                  })}
+                  required
+                />
+              </label>
+            </li>
+            <li>
+              <label>
+                Image url*:
+                <input
+                  value={imgUrl}
+                  className={`addInfoBar ${!isImgUrlValid ? 'invalid' : ''
+                  }`}
+                  placeholder="Image url*"
+                  onChange={event => this.setState({
+                    imgUrl: event.target.value,
+                  })}
+                  required
+                />
+              </label>
+              <text className="err">
+                {!isImgUrlValid ? 'this is invalid url' : ''}
+              </text>
+            </li>
+            <li>
+              <label>
+                imdbUrl*:
+                <input
+                  value={imdbUrl}
+                  className={`addInfoBar ${!isImdbUrlValid ? 'invalid' : ''}`}
+                  placeholder="imdbUrl*"
+                  onChange={event => this.setState({
+                    imdbUrl: event.target.value,
+                  })}
+                  required
+                />
+                <text className="err">
+                  {!isImdbUrlValid ? 'this is invalid url' : ''}
+                </text>
+              </label>
+            </li>
+            <li>
+              <label>
+                imdbId*:
+                <input
+                  value={imdbId}
+                  className="addInfoBar"
+                  placeholder="imdbId*"
+                  onChange={event => this.setState({
+                    imdbId: event.target.value,
+                  })}
+                  required
+                />
+              </label>
+            </li>
+            <li>
+              <label>
+                Description:
+                <textarea
+                  value={description}
+                  className="addInfoBar description"
+                  placeholder="Description"
+                  onChange={event => this.setState({
+                    description: event.target.value,
+                  })}
+                />
+              </label>
+            </li>
+          </ul>
+          <button
+            disabled={
+              !imdbUrl.match(this.onlyUrl)
+              || !imgUrl.match(this.onlyUrl)
+              || title === ''
+              || imdbId === ''
+            }
+            type="submit"
+            className="submitButton"
+          >
+            Submit
+          </button>
+        </form>
+      </>
     );
   }
 }
