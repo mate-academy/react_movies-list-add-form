bc4c32dbb37b5759d7575527bf865cda34f039cf
Oleksandr Vasilkov <vasilkoff1337@gmail.com>
2021-06-19 19:51:45 +0300

    solution

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,86 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit = () => {
+    this.props.onAdd(this.state);
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  }
 
   render() {
     return (
       <form>
-        Put the form here
+        <input
+          name="title"
+          type="text"
+          placeholder="title"
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          name="description"
+          type="text"
+          placeholder="description"
+          value={this.state.description}
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          name="mgUrl"
+          type="text"
+          placeholder="imgUrl"
+          value={this.state.imgUrl}
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          name="imdbUrl"
+          type="text"
+          placeholder="imdbUrl"
+          value={this.state.imdbUrl}
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          name="imdbId"
+          type="text"
+          placeholder="imdbId"
+          value={this.state.imdbId}
+          onChange={this.handleChange}
+        />
+        <br />
+        <button
+          type="button"
+          onClick={this.handleSubmit}
+        >
+          add
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
