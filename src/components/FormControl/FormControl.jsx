e393a9baa06d964f29f2b36442588e60e0ee6e20
David Dohru <tronsmartmy@gmail.com>
2021-07-27 01:51:30 +0300

    fixed

--- src/components/FormControl/FormControl.jsx
+++ src/components/FormControl/FormControl.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PropTypes from 'prop-types';
 import { Form } from 'react-bootstrap';
+import './FormControl.scss';
 
 export const FormControl = (
   { name, placeHolder, onChange, row = 1, as = 'input' },
 ) => (
-  <Form.Control
-    onChange={({ target }) => onChange(placeHolder, target)}
-    value={name}
-    placeholder={placeHolder}
-    rows={row}
-    as={as}
-    type="text"
-  />
+  <div className="form__input">
+    <span className="form__phrase">
+      {(name.length > 0) && 'Looks good!'}
+    </span>
+    <Form.Control
+      className={name.length && 'active'}
+      onChange={({ target }) => onChange(placeHolder, target)}
+      value={name}
+      placeholder={placeHolder}
+      rows={row}
+      as={as}
+      type="text"
+    />
+  </div>
 );
 
 FormControl.propTypes = {
