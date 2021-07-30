013f0dd4d76eb18b2bf70e38b98499845286b667
grynkiv0x001 <grynkiv0x001@gmail.com>
2021-07-30 15:22:41 +0300

    Solution without advanced

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,91 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
+
+    this.props.addMovie(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  changeHandler = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
+    const { changeHandler, submitHandler } = this;
+
     return (
-      <form>
-        Put the form here
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={title}
+          onChange={changeHandler}
+        />
+        <textarea
+          type="text"
+          name="description"
+          placeholder="Description..."
+          value={description}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          name="imgUrl"
+          placeholder="img url"
+          value={imgUrl}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          name="imdbUrl"
+          placeholder="imdb url"
+          value={imdbUrl}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          name="imdbId"
+          placeholder="imdb id"
+          value={imdbId}
+          onChange={changeHandler}
+        />
+        <button type="submit">Add movie</button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
