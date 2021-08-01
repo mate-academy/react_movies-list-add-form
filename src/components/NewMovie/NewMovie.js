7900379fe4b8d2d90e1f64d829430b98b9e938b7
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-08-01 15:00:01 +0300

    test 1

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,90 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './NewMovie.scss';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
 
   render() {
+    const { addMovie } = this.props;
+    const { state } = this;
+
     return (
-      <form>
-        Put the form here
+      <form
+        className="newMovie"
+        onSubmit={(event) => {
+          event.preventDefault();
+          addMovie(state);
+        }}
+      >
+        <p className="newMovie__input">
+          <span className="">title</span>
+          <input
+            type="text"
+            onChange={(event) => {
+              this.setState({ title: event.target.value });
+            }}
+            value={this.state.title}
+          />
+        </p>
+        <p className="newMovie__input">
+          <span>description</span>
+          <input
+            type="text"
+            onChange={(event) => {
+              this.setState({ description: event.target.value });
+            }}
+            value={this.state.description}
+          />
+        </p>
+        <p className="newMovie__input">
+          <span>imgUrl</span>
+          <input
+            type="text"
+            onChange={(event) => {
+              this.setState({ imgUrl: event.target.value });
+            }}
+            value={this.state.imgUrl}
+          />
+        </p>
+        <p className="newMovie__input">
+          <span>imdbUrl</span>
+          <input
+            type="text"
+            onChange={(event) => {
+              this.setState({ imdbUrl: event.target.value });
+            }}
+            value={this.state.imdbUrl}
+          />
+        </p>
+        <p className="newMovie__input">
+          <span>imdbId</span>
+          <input
+            type="text"
+            onChange={(event) => {
+              this.setState({ imdbId: event.target.value });
+            }}
+            value={this.state.imdbId}
+          />
+        </p>
+
+        <button
+          className="newMovie__button"
+          type="submit"
+        >
+          Add
+        </button>
       </form>
     );
   }
 }
+
+NewMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};
