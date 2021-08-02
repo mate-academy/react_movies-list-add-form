cd3adbd7663621b0f4e714b9f2457dd05b56598d
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-08-02 12:58:48 +0300

    solution update with localStorage

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
+const moviesKey = 'movies';
+
 export class App extends Component {
+  moviesFromStorage = JSON.parse(window.localStorage.getItem(moviesKey));
+
   state = {
-    movies: moviesFromServer,
+    movies: this.moviesFromStorage || moviesFromServer,
   };
 
   addMovie = (movie) => {
     this.setState(prevState => ({
       movies: [...prevState.movies, movie],
-    }));
+    }),
+    () => {
+      const stringifiedMovies = JSON.stringify(this.state.movies);
+
+      window.localStorage.setItem(moviesKey, stringifiedMovies);
+    });
   };
 
   render() {
