5f55f6d4ce36a333c5ba197284d9aa878c7de92e
tkachukione <tkachukione@gmail.com>
2021-08-02 18:40:03 +0300

    first try

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,13 +1,93 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class NewMovie extends Component {
-  state = {};
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbld: '',
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onAdd(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbld: '',
+    });
+  }
 
   render() {
     return (
-      <form>
-        Put the form here
+
+      <form onSubmit={this.handleSubmit} className="form">
+        <input
+          className="input"
+          placeholder="title"
+          type="text"
+          name="title"
+          value={this.state.title}
+          onChange={this.handleChange}
+          required
+        />
+        <textarea
+          className="input"
+          placeholder="description"
+          type="text"
+          name="description"
+          value={this.state.description}
+          onChange={this.handleChange}
+          required
+        />
+        <input
+          className="input"
+          placeholder="imgUrl"
+          type="text"
+          name="imgUrl"
+          value={this.state.imgUrl}
+          onChange={this.handleChange}
+          required
+        />
+        <input
+          className="input"
+          placeholder="imdbUrl"
+          type="text"
+          name="imdbUrl"
+          value={this.state.imdbUrl}
+          onChange={this.handleChange}
+          required
+        />
+        <input
+          className="input"
+          placeholder="imdbld"
+          type="text"
+          name="imdbld"
+          value={this.state.imdbld}
+          onChange={this.handleChange}
+          required
+        />
+        <button
+          className="button"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     );
   }
 }
+NewMovie.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
