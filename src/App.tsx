aee7f554ee2e2e895d7f43fe1c96faeda342f7df
Kirill Ivanchenko <ivanchenkok557@gmail.com>
2021-09-07 18:16:16 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,10 @@ export class App extends React.Component<{}, State> {
     movies: moviesFromServer,
   };
 
-  addMovie = (/* movie: Movie */) => {
-    // put your code here
+  addMovie = (movie: Movie) => {
+    this.setState(prevState => ({
+      movies: [movie, ...prevState.movies],
+    }));
   };
 
   render() {
@@ -26,7 +28,7 @@ export class App extends React.Component<{}, State> {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie onAdd={this.addMovie} />
         </div>
       </div>
     );
