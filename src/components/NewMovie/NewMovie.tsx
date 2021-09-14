1fcf32284005e4e6ee03525b4251eb312fb39bdf
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-09-14 17:45:07 +0300

    pilot

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,94 @@
 import { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  onSubmit: (movie: Movie) => void;
+};
+
+type State = Movie;
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    } as Pick<State, keyof State>);
+  };
+
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    this.props.onSubmit({ ...this.state });
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form
+        className="Form"
+        onSubmit={this.handleSubmit}
+      >
+        <input
+          className="form-control"
+          placeholder="Title"
+          value={title}
+          type="text"
+          name="title"
+          onChange={this.handleInput}
+        />
+        <input
+          className="form-control"
+          placeholder="Description"
+          value={description}
+          type="text"
+          name="description"
+          onChange={this.handleInput}
+        />
+        <input
+          className="form-control"
+          placeholder="imgUrl"
+          value={imgUrl}
+          type="text"
+          name="imgUrl"
+          onChange={this.handleInput}
+        />
+        <input
+          className="form-control"
+          placeholder="imdbUrl"
+          value={imdbUrl}
+          type="text"
+          name="imdbUrl"
+          onChange={this.handleInput}
+        />
+        <input
+          className="form-control"
+          placeholder="imdbId"
+          value={imdbId}
+          type="text"
+          name="imdbId"
+          onChange={this.handleInput}
+        />
+        <button
+          className="btn btn-primary"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     );
   }
