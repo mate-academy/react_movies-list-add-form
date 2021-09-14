ca7dc07c6c96c0d27b0be36c6d7128a484d23dcd
Dima Tkachenko <DimaTkachenko3@gmail.com>
2021-09-14 15:27:46 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,139 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-type Props = {};
-type State = {};
+import './NewMovie.scss';
+
+type Props = {
+  addMovie: (movie: Movie) => void;
+};
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  newMovie = () => {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    return newMovie;
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    const key: keyof State = name as keyof State;
+
+    this.setState((currentState) => ({
+      ...currentState,
+      [key]: value,
+    }));
+  };
+
+  setClearState = () => {
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newMovie = this.newMovie();
+
+    this.props.addMovie(newMovie);
+    this.setClearState();
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
-      </form>
+      <>
+        <h1>Add New Movie</h1>
+        <form
+          className="form"
+          onSubmit={this.onSubmit}
+        >
+          <input
+            className="input"
+            type="text"
+            name="title"
+            placeholder="add Title"
+            value={title}
+            onChange={this.handleChange}
+          />
+          <input
+            className="input"
+            type="text"
+            name="description"
+            placeholder="add Description"
+            value={description}
+            onChange={this.handleChange}
+          />
+          <input
+            className="input"
+            type="text"
+            name="imgUrl"
+            placeholder="add ImgUrl"
+            value={imgUrl}
+            onChange={this.handleChange}
+          />
+          <input
+            className="input"
+            type="text"
+            name="imdbUrl"
+            placeholder="add ImdbUrl"
+            value={imdbUrl}
+            onChange={this.handleChange}
+          />
+          <input
+            className="input"
+            type="text"
+            name="imdbId"
+            placeholder="add ImdbId"
+            value={imdbId}
+            onChange={this.handleChange}
+          />
+          <button
+            className="button"
+            type="submit"
+          >
+            Add New Film
+          </button>
+        </form>
+      </>
     );
   }
 }
