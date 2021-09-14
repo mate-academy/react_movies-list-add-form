05a9698da066b04d2c268fca7b84ebc171a0ddda
Eugene Gnedenko <deadup4@gmail.com>
2021-09-14 22:14:50 +0300

    react movies list add form by Eugene Gnedenko

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,106 @@
-import { Component } from 'react';
+import React from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (value: Movie) => void;
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+type State = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
+export class NewMovie extends React.Component<Props, State> {
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.addMovie(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    const key: keyof State = name as keyof State;
+
+    this.setState({
+      [key]: value,
+    } as Pick<State, keyof State>);
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form onSubmit={this.handleSubmit} className="d-flex flex-column justify-content-between">
+        <input
+          type="text"
+          name="title"
+          required
+          placeholder="Title"
+          value={title}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="description"
+          required
+          placeholder="Description"
+          value={description}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="imgUrl"
+          required
+          placeholder="imgUrl"
+          value={imgUrl}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="imdbId"
+          required
+          placeholder="imdbId"
+          value={imdbId}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="imdbUrl"
+          required
+          placeholder="imdbUrl"
+          value={imdbUrl}
+          onChange={this.handleChange}
+        />
+        <button
+          type="submit"
+          className="btn btn-primary d-block"
+        >
+          Submit
+        </button>
       </form>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
 import { App } from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
