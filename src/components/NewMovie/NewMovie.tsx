a015bcd2a52b40a5e03a3bb6189e8470c6d3e158
Sasha Lifashkin <alifashkin@outlook.com>
2021-09-15 19:46:31 +0300

    simple solution done

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,102 @@
-import { Component } from 'react';
+import React, { FormEvent } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addNewMovie: (movie: Movie) => void;
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+type State = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
+export class NewMovie extends React.Component<Props, State> {
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event: EventsForm) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    } as Pick<State, keyof State>);
+  };
+
+  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newMovie = { ...this.state };
+
+    this.props.addNewMovie(newMovie);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form
+        className="Form"
+        onSubmit={this.handleSubmit}
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter the title"
+          className="Form__input"
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <textarea
+          name="description"
+          placeholder="Description"
+          value={this.state.description}
+          className="Form__textarea"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="imgUrl"
+          placeholder="Enter the link of image"
+          className="Form__input"
+          value={this.state.imgUrl}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="imdbUrl"
+          placeholder="Enter the link of IMDB"
+          className="Form__input"
+          value={this.state.imdbUrl}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="imdbId"
+          placeholder="Enter the IMDB Id"
+          className="Form__input"
+          value={this.state.imdbId}
+          onChange={this.handleChange}
+        />
+        <button
+          type="submit"
+          className="Form__button"
+        >
+          Add movie
+        </button>
       </form>
     );
   }
