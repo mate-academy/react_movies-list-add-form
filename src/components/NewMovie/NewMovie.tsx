8ca3b41bf94a761980ab21ec7ec553d3e9a33ad0
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-10-18 12:22:27 +0300

    react_movies-list-add-form done + bootstrap

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,52 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (movie: Movie) => void;
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+const newForm = (): Movie => ({
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+});
+
+export class NewMovie extends Component<Props, {}> {
+  state: Movie = newForm();
+
+  addValueToForm = (key: string, value: string) => {
+    this.setState({
+      [key]: value,
+    } as Pick<Movie, keyof Movie>);
+  };
+
+  onAdd = (event: React.FormEvent) => {
+    event.preventDefault();
+    this.props.addMovie(this.state);
+    this.setState(newForm());
+  };
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form onSubmit={this.onAdd}>
+        <div className="form-group">
+          {Object.entries(this.state).map(([key, value]) => (
+            <input
+              type="text"
+              className="form-control"
+              key={key}
+              placeholder={key}
+              value={value}
+              onChange={e => this.addValueToForm(key, e.target.value)}
+              required
+            />
+          ))}
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Add Movie
+        </button>
       </form>
     );
   }
