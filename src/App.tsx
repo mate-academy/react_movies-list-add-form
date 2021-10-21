5892cecf2de4230ced2779d2509cf3e27ffbe0ef
Denis Demyanok <DenisRusis@ex.ua>
2021-10-21 18:25:18 +0300

    Move list Form

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,15 @@ export class App extends React.Component<{}, State> {
     movies: moviesFromServer,
   };
 
-  addMovie = (/* movie: Movie */) => {
-    // put your code here
+  addMovie = (movie: Movie) => {
+    this.setState(state => ({
+      movies: [...state.movies, movie],
+    }));
   };
 
   render() {
     const { movies } = this.state;
+    const { addMovie } = this;
 
     return (
       <div className="page">
@@ -26,7 +29,7 @@ export class App extends React.Component<{}, State> {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie addMovie={addMovie} />
         </div>
       </div>
     );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,90 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (movie: Movie) => void;
+};
+
+type State = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  addMovieForm = (name: string, value: string) => {
+    this.setState({
+      [name]: value,
+    } as Pick<State, keyof State>);
+  };
+
+  hangleFormSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    this.props.addMovie(newMovie);
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
 
   render() {
+    const { hangleFormSubmit, addMovieForm } = this;
+
     return (
-      <form>
-        Put the form here
+      <form
+        onSubmit={hangleFormSubmit}
+        className="AddMovie"
+      >
+        <h2 className="AddMovie__title">
+          Add Movie Form
+        </h2>
+        {Object.entries(this.state).map(([key, value]) => (
+          <input
+            type="text"
+            key={key}
+            placeholder={key}
+            className="AddMovie__input"
+            value={value}
+            required
+            onChange={event => addMovieForm(key, event.target.value)}
+          />
+        ))}
+        <button
+          type="submit"
+          className="AddMovie__button"
+        >
+          Add movie
+        </button>
       </form>
     );
   }
