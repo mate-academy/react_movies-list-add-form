29d61de6034df4295461ff5607a8ee0146f6df5f
Vladyslav Banul <vladyslav.banul@gmail.com>
2021-10-21 17:53:28 +0300

    Solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,92 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = { addMovie:(movie: Movie) => void };
+type State = Movie;
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  eventHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    const fieldName = event.currentTarget.name;
+    const fieldValue = event.currentTarget.value;
+
+    this.setState({
+      [fieldName]: fieldValue,
+    } as Pick<Movie, keyof Movie>);
+  };
+
+  newMovie = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.addMovie(this.state);
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
 
   render() {
+    const {
+      title, description, imgUrl, imdbUrl, imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form className="form" onSubmit={this.newMovie}>
+        <input
+          type="text"
+          name="title"
+          className="form__input"
+          placeholder="title"
+          value={title}
+          required
+          onChange={this.eventHandler}
+        />
+        <input
+          type="text"
+          name="description"
+          className="form__input"
+          placeholder="description"
+          value={description}
+          required
+          onChange={this.eventHandler}
+        />
+        <input
+          type="text"
+          name="imgUrl"
+          className="form__input"
+          placeholder="imgUrl"
+          value={imgUrl}
+          required
+          onChange={this.eventHandler}
+        />
+        <input
+          type="text"
+          name="imdbUrl"
+          className="form__input"
+          placeholder="imdbUrl"
+          value={imdbUrl}
+          required
+          onChange={this.eventHandler}
+        />
+        <input
+          type="text"
+          name="imdbId"
+          className="form__input"
+          placeholder="imdbId"
+          value={imdbId}
+          required
+          onChange={this.eventHandler}
+        />
+        <button type="submit" className="form__submit">Add new film</button>
       </form>
     );
   }
