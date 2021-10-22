8ea4a678db66af90663cfaad5625d9a5b2056887
AVronskyi <vronskyi.a@gmail.com>
2021-10-22 14:11:46 +0300

    solution without style

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,104 @@
 import { Component } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (movie: Movie) => void;
+};
+type State = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState(() => ({
+      [name]: value,
+    } as State));
+  };
+
+  clearForm = () => {
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    this.props.addMovie({ ...this.state });
+    this.clearForm();
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imdbId,
+      imdbUrl,
+      imgUrl,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form
+        className="form"
+        onSubmit={this.handleSubmit}
+      >
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={this.handleChange}
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          name="description"
+          value={description}
+          onChange={this.handleChange}
+          placeholder="Description"
+        />
+        <input
+          type="text"
+          name="imdbId"
+          value={imdbId}
+          onChange={this.handleChange}
+          placeholder="ImdbId"
+        />
+        <input
+          type="text"
+          name="imgUrl"
+          value={imgUrl}
+          onChange={this.handleChange}
+          placeholder="ImgUrl"
+        />
+        <input
+          type="text"
+          name="imdbUrl"
+          value={imdbUrl}
+          onChange={this.handleChange}
+          placeholder="ImdbUrl"
+        />
+        <button
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     );
   }
