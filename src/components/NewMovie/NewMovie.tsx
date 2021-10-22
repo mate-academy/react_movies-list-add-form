80176815623a99dcd065c256066a6c7f1de48aab
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-10-22 19:03:43 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,101 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAddMovie: (movie: Movie) => void,
+};
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    this.props.onAddMovie(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  handleChange = (value: string, key: string) => {
+    this.setState((prevState) => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          name="title"
+          value={title}
+          required
+          onChange={(event) => this.handleChange(event.target.value, 'title')}
+        />
+        <textarea
+          placeholder="Description"
+          name="description"
+          value={description}
+          required
+          onChange={(event) => this.handleChange(event.target.value, 'description')}
+        />
+        <input
+          type="text"
+          placeholder="ImgUrl"
+          name="imgurl"
+          value={imgUrl}
+          required
+          onChange={(event) => this.handleChange(event.target.value, 'imgUrl')}
+        />
+        <input
+          type="text"
+          placeholder="ImbdUrl"
+          name="imbdUrl"
+          value={imdbUrl}
+          required
+          onChange={(event) => this.handleChange(event.target.value, 'imdbUrl')}
+        />
+        <input
+          type="text"
+          placeholder="ImdbId"
+          name="imdbId"
+          value={imdbId}
+          required
+          onChange={(event) => this.handleChange(event.target.value, 'imdbId')}
+        />
+        <button
+          type="submit"
+        >
+          add
+        </button>
       </form>
     );
   }
