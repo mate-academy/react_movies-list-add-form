4508ac127cf6c2f7ac2667813f17096974ffef5f
Igor Igor <you@example.com>
2021-10-22 16:13:34 +0300

    solution without advanced

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,154 @@
 import { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (newMovie: Movie) => void
+};
+
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (value: string, key: string) => {
+    this.setState((state) => ({
+      ...state,
+      [key]: value,
+    }));
+  };
+
+  addMovie = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+    const { onAdd } = this.props;
+
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    onAdd(newMovie);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form
+        className="new-movie"
+        onSubmit={this.addMovie}
+      >
+        <label
+          htmlFor="title"
+          className="new-movie__label"
+        >
+          Title
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(event) => (
+              this.handleChange(event.currentTarget.value, 'title')
+            )}
+          />
+        </label>
+        <label
+          htmlFor="description"
+          className="new-movie__label"
+        >
+          Description
+          <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={(event) => (
+              this.handleChange(event.currentTarget.value, 'description')
+            )}
+          />
+        </label>
+        <label
+          htmlFor="imgUrl"
+          className="new-movie__label"
+        >
+          imgUrl
+          <input
+            type="text"
+            name="imgUrl"
+            value={imgUrl}
+            onChange={(event) => (
+              this.handleChange(event.currentTarget.value, 'imgUrl')
+            )}
+          />
+        </label>
+        <label
+          htmlFor="imdbUrl"
+          className="new-movie__label"
+        >
+          imdbUrl
+          <input
+            type="text"
+            name="imdbUrl"
+            value={imdbUrl}
+            onChange={(event) => (
+              this.handleChange(event.currentTarget.value, 'imdbUrl')
+            )}
+          />
+        </label>
+        <label
+          htmlFor="imdbId"
+          className="new-movie__label"
+        >
+          imdbId
+          <input
+            type="text"
+            name="imdbId"
+            value={imdbId}
+            onChange={(event) => (
+              this.handleChange(event.currentTarget.value, 'imdbId')
+            )}
+          />
+        </label>
+        <button
+          type="submit"
+        >
+          Add
+        </button>
       </form>
     );
   }
