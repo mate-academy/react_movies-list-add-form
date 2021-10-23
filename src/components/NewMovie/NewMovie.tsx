4f8790515b6b0f60092d551bf7ebdb7012c3bfdb
Bogdan <bogdanemails@gmail.com>
2021-10-24 00:08:06 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,112 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+type NewMovieState = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
+
+export class NewMovie extends Component<Props, NewMovieState> {
+  state: NewMovieState = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    this.props.onAdd({ ...this.state });
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState(() => ({
+      [name]: value,
+    } as Pick<NewMovieState, keyof NewMovieState>));
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imdbId,
+      imdbUrl,
+      imgUrl,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form
+        onSubmit={this.handleSubmit}
+      >
+        <input
+          type="text"
+          className="field"
+          name="title"
+          value={title}
+          placeholder="Please enter title"
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          className="field"
+          name="description"
+          value={description}
+          placeholder="Please enter description"
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          className="field"
+          name="imgUrl"
+          value={imgUrl}
+          placeholder="Please enter imgUrl"
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          className="field"
+          name="imdbUrl"
+          value={imdbUrl}
+          placeholder="Please enter imdbUrl"
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          className="field"
+          name="imdbId"
+          value={imdbId}
+          placeholder="Please enter imdbId"
+          onChange={this.handleChange}
+        />
+        <button
+          type="submit"
+          className="field"
+        >
+          Add
+        </button>
       </form>
     );
   }
