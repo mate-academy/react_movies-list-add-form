36fa7f6b664bc4ccdbbda245b61bec0572caca92
Tarasulko <internalpeace10@outlook.com>
2021-10-23 21:48:29 +0300

    fixes

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,26 @@
 import { Component } from 'react';
+import classNames from 'classnames';
 
 import './NewMovie.scss';
 
 type Props = {
   addMovie: (movie: Movie) => void;
 };
+
 type State = {
   title: string,
   description: string,
   imgUrl: string,
   imdbUrl: string,
   imdbId: string,
+  errors: Errors,
+};
+
+type Errors = {
+  title: boolean,
+  description: boolean,
+  imgUrl: boolean,
+  imdbUrl: boolean,
 };
 
 export class NewMovie extends Component<Props, State> {
@@ -20,6 +30,12 @@ export class NewMovie extends Component<Props, State> {
     imgUrl: '',
     imdbUrl: '',
     imdbId: '',
+    errors: {
+      title: false,
+      description: false,
+      imgUrl: false,
+      imdbUrl: false,
+    },
   };
 
   getImdbId = (imdbLink: string) => {
@@ -39,6 +55,20 @@ export class NewMovie extends Component<Props, State> {
       imdbId,
     } = this.state;
 
+    if (!title || !description || !imgUrl || !imdbUrl) {
+      this.setState((state => ({
+        ...state,
+        errors: {
+          title: !state.title,
+          description: !state.description,
+          imgUrl: !state.imgUrl,
+          imdbUrl: !state.imdbUrl,
+        },
+      })));
+
+      return;
+    }
+
     const newMovie: Movie = {
       title,
       description,
@@ -48,39 +78,94 @@ export class NewMovie extends Component<Props, State> {
     };
 
     this.props.addMovie(newMovie);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+      errors: {
+        title: false,
+        description: false,
+        imgUrl: false,
+        imdbUrl: false,
+      },
+    });
   };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+    } = this.state.errors;
+
     return (
       <form className="form">
         <input
           type="text"
           placeholder="Write title here"
+          className={classNames({ 'form__error-field': title })}
+          value={this.state.title}
           onChange={(event) => {
-            this.setState({ title: event.target.value });
+            this.setState(state => ({
+              title: event.target.value,
+              errors: {
+                ...state.errors,
+                title: false,
+              },
+            }));
           }}
         />
+        {title && <span className="form__error-text">Enter title please</span>}
         <textarea
           placeholder="Write describe here"
+          className={classNames({ 'form__error-field': description })}
+          value={this.state.description}
           onChange={(event) => {
-            this.setState({ description: event.target.value });
+            this.setState(state => ({
+              description: event.target.value,
+              errors: {
+                ...state.errors,
+                description: false,
+              },
+            }));
           }}
         />
-        <textarea
+        {description && <span className="form__error-text">Enter description please</span>}
+        <input
           placeholder="Insert image link here"
+          className={classNames({ 'form__error-field': imgUrl })}
+          value={this.state.imgUrl}
           onChange={(event) => {
-            this.setState({ imgUrl: event.target.value });
-          }}
-        />
-        <textarea
-          placeholder="Insert imdb link here"
-          onChange={(event) => {
-            this.setState({
+            this.setState(state => ({
               imgUrl: event.target.value,
-              imdbId: this.getImdbId(event.target.value),
-            });
+              errors: {
+                ...state.errors,
+                imgUrl: false,
+              },
+            }));
           }}
         />
+        {imgUrl && <span className="form__error-text">insert image please</span>}
+        <input
+          placeholder="Insert imdb link here"
+          className={classNames({ 'form__error-field': imdbUrl })}
+          value={this.state.imdbUrl}
+          onChange={(event) => {
+            this.setState(state => ({
+              imdbUrl: event.target.value,
+              imdbId: this.getImdbId(event.target.value),
+              errors: {
+                ...state.errors,
+                imdbUrl: false,
+              },
+            }));
+          }}
+        />
+        {imdbUrl && <span className="form__error-text">insert imdb link please</span>}
         <button
           type="submit"
           onClick={(event) => this.onAdd(event)}
