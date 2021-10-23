1841f6643d69fad40dce03a5b2147279f264884b
DY-JS <delo1604@ukr.net>
2021-10-23 18:33:27 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ export class App extends React.Component<{}, State> {
     movies: moviesFromServer,
   };
 
-  addMovie = (/* movie: Movie */) => {
-    // put your code here
+  addMovie = ( movie: Movie ) => {
+    this.setState( ({ movies }) => ({ movies: [movie, ...movies] }));
   };
 
   render() {
@@ -26,7 +26,7 @@ export class App extends React.Component<{}, State> {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie addMovie={this.addMovie} />
         </div>
       </div>
     );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,121 @@
 import { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (movie: Movie) => void;
+};
+type MovieState = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export class NewMovie extends Component<Props, MovieState> {
+  state: MovieState = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({
+
+      [name]: value,
+    } as Pick<MovieState, keyof MovieState>);
+  };
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const propItems = Object.keys(this.state);
+
+    event.preventDefault();
+    if ([...propItems].every((prop) => prop !== '')) {
+      const newMovie: Movie = { ...this.state };
+
+      this.props.addMovie(newMovie);
+    }
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <h2>Input title:</h2>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            placeholder="Input title"
+            onChange={this.handleChange}
+            pattern="[A-Za-z]{1,25}"
+            required
+          />
+        </div>
+        <div>
+          <h2>Input description:</h2>
+          <textarea
+            name="description"
+            value={description}
+            placeholder="Input description"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <h2>Input imgUrl:</h2>
+          <input
+            name="imgUrl"
+            value={imgUrl}
+            placeholder="Input imgUrl"
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+        <div>
+          <h2>Input imdbUrl:</h2>
+          <input
+            name="imdbUrl"
+            value={imdbUrl}
+            placeholder="Input imdbUrl"
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+        <div>
+          <h2>Input imdbId:</h2>
+          <input
+            name="imdbId"
+            value={imdbId}
+            placeholder="Input imdbId"
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+        <input
+          className="button"
+          type="button"
+          value="Add New Movie"
+        />
       </form>
     );
   }
