1ffd1dd53c53eeb0b99d680a785c087694ca23bf
Taras Kozak <taraskozakmail@gmail.com>
2021-11-15 15:21:53 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,271 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+  isImgUrlValid: boolean,
+  isImdbUrlValid: boolean,
+  isTitleEntered: boolean,
+  isImdbIdEntered: boolean,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+    isImgUrlValid: false,
+    isImdbUrlValid: false,
+    isTitleEntered: false,
+    isImdbIdEntered: false,
+  };
+
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
+    this.props.onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbId,
+      imdbUrl,
+    });
+    this.clearState();
+  };
+
+  clearState = () => {
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+      isImgUrlValid: false,
+      isImdbUrlValid: false,
+      isTitleEntered: false,
+      isImdbIdEntered: false,
+    });
+  };
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    } as unknown as Pick<State, keyof State>);
+  };
+
+  canSubmit = () => {
+    const {
+      title,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+      isImgUrlValid,
+      isImdbUrlValid,
+      isTitleEntered,
+      isImdbIdEntered,
+    } = this.state;
+
+    return title && imdbUrl && imdbUrl && imgUrl
+      && imdbId && !isImdbIdEntered
+      && !isImgUrlValid && !isImdbUrlValid && !isTitleEntered;
+  };
+
+  handleUrlOnBlurr = (event:React.FocusEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    const expression = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+    const regExp = new RegExp(expression);
+
+    switch (name) {
+      case 'imgUrl':
+        if (value.match(regExp)) {
+          this.setState({
+            isImgUrlValid: false,
+          } as Pick<State, keyof State>);
+        } else {
+          this.setState({
+            isImgUrlValid: true,
+          } as Pick<State, keyof State>);
+        }
+
+        break;
+      case 'imdbUrl':
+        if (value.match(regExp)) {
+          this.setState({
+            isImdbUrlValid: false,
+          } as Pick<State, keyof State>);
+        } else {
+          this.setState({
+            isImdbUrlValid: true,
+          } as Pick<State, keyof State>);
+        }
+
+        break;
+      case 'title':
+        if (this.state.title) {
+          this.setState({
+            isTitleEntered: false,
+          } as Pick<State, keyof State>);
+        } else {
+          this.setState({
+            isTitleEntered: true,
+          } as Pick<State, keyof State>);
+        }
+
+        break;
+      case 'imdbId':
+        if (this.state.imdbId) {
+          this.setState({
+            isImdbIdEntered: false,
+          } as Pick<State, keyof State>);
+        } else {
+          this.setState({
+            isImdbIdEntered: true,
+          } as Pick<State, keyof State>);
+        }
+
+        break;
+
+      default:
+    }
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+      isImgUrlValid,
+      isImdbUrlValid,
+      isTitleEntered,
+      isImdbIdEntered,
+    } = this.state;
+    const isDesabled = this.canSubmit();
+
     return (
-      <form>
-        Put the form here
+      <form
+        className="from"
+        name="form"
+        action="POST"
+        onSubmit={this.handleSubmit}
+      >
+        <div className="form__item">
+          <label htmlFor="title">
+            Title:
+            {' '}
+            <input
+              required
+              className={isTitleEntered ? 'input error' : 'input'}
+              name="title"
+              placeholder="Enter title here"
+              value={title}
+              type="text"
+              onChange={this.handleInputChange}
+              onBlur={this.handleUrlOnBlurr}
+            />
+          </label>
+          <div className="error-message">
+            {isTitleEntered && (<span>Please enter the title</span>)}
+          </div>
+        </div>
+
+        <div className="form__item">
+          <label htmlFor="imgUrl">
+            Image url:
+            {' '}
+            <input
+              required
+              className={isImgUrlValid ? 'input error' : 'input'}
+              name="imgUrl"
+              type="text"
+              value={imgUrl}
+              placeholder="Enter image url"
+              onChange={this.handleInputChange}
+              onBlur={this.handleUrlOnBlurr}
+            />
+          </label>
+          <div className="error-message">
+            {isImgUrlValid && (<span>Url is invalid</span>)}
+          </div>
+        </div>
+
+        <div className="form__item">
+          <label htmlFor="imdbUrl">
+            Imdb Url:
+            {' '}
+            <input
+              required
+              className={isImdbUrlValid ? 'input error' : 'input'}
+              name="imdbUrl"
+              type="text"
+              value={imdbUrl}
+              placeholder="Enter imdb url"
+              onChange={this.handleInputChange}
+              onBlur={this.handleUrlOnBlurr}
+            />
+          </label>
+          <div className="error-message">
+            {isImdbUrlValid && (<span>Url is invalid</span>)}
+          </div>
+        </div>
+
+        <div className="form__item">
+          <label htmlFor="imdbId">
+            Imdb Id:
+            {' '}
+            <input
+              required
+              className={isImdbIdEntered ? 'input error' : 'input'}
+              name="imdbId"
+              type="text"
+              value={imdbId}
+              placeholder="Enter imdb Id"
+              onChange={this.handleInputChange}
+              onBlur={this.handleUrlOnBlurr}
+            />
+          </label>
+          <div className="error-message">
+            {isImdbIdEntered && (<span>Enter valid Id</span>)}
+          </div>
+        </div>
+        <div className="form__item">
+          <textarea
+            className="textarea"
+            placeholder="Enter a movie description"
+            name="desctiption"
+            value={description}
+            onChange={(event) => {
+              this.setState({ description: event.target.value });
+            }}
+          />
+        </div>
+        <button
+          className="button"
+          disabled={!isDesabled}
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     );
   }
