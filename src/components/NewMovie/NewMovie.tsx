81f51d250b86576731014d31409577f64bdea38b
Denys Stryzhalko <samu3lkacis@gmail.com>
2021-11-17 12:53:25 +0200

    movies-list-add-form v1

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,115 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (movie: Movie) => void;
+};
+type State = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleMovieTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      title: event.target.value,
+    });
+  };
+
+  handleDescription = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      description: event.target.value,
+    });
+  };
+
+  handleImgUrl = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      imgUrl: event.target.value,
+    });
+  };
+
+  handleImdbUrl = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      imdbUrl: event.target.value,
+    });
+  };
+
+  handleImdbId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      imdbId: event.target.value,
+    });
+  };
+
+  clearForm = () => {
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.addMovie({
+      ...this.state,
+    });
+    this.clearForm();
+  };
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form className="form" onSubmit={this.handleSubmit}>
+        <input
+          className="form__input"
+          type="text"
+          value={this.state.title}
+          onChange={this.handleMovieTitle}
+          placeholder="Enter the movie title"
+        />
+        <input
+          className="form__input"
+          type="text"
+          value={this.state.description}
+          onChange={this.handleDescription}
+          placeholder="Movie Description"
+        />
+        <input
+          className="form__input"
+          type="text"
+          value={this.state.imgUrl}
+          onChange={this.handleImgUrl}
+          placeholder="imgUrl"
+        />
+        <input
+          className="form__input"
+          type="text"
+          value={this.state.imdbUrl}
+          onChange={this.handleImdbUrl}
+          placeholder="imdbUrl"
+        />
+        <input
+          className="form__input"
+          type="text"
+          value={this.state.imdbId}
+          onChange={this.handleImdbId}
+          placeholder="imdbId"
+        />
+        <button type="submit" className="form__submit">
+          Add New Movie
+        </button>
       </form>
     );
   }
