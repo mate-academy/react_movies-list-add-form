6fa643fe2ca7e68f7262f2f2c0bcb85bd6f90c21
Olena Kopyl <olenatriasii@gmail.com>
2021-11-17 22:25:04 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,146 @@
-import { Component } from 'react';
+import { ChangeEvent, Component, FormEvent } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void
+};
+
+type State = {
+  title: string
+  description: string
+  imgUrl: string
+  imdbUrl: string
+  imdbId: string
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (
+    event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = event.target;
+
+    this.setState(state => ({
+      ...state,
+      [name]: value,
+    }));
+  };
+
+  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
+    this.props.onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form
+        className="NewMovie--form"
+        onSubmit={this.handleSubmit}
+      >
+        <div className="NewMovie--input">
+          Title*:
+          <br />
+          <input
+            value={title}
+            type="text"
+            name="title"
+            placeholder="title"
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+
+        <div className="NewMovie--input">
+          Description:
+          <br />
+          <textarea
+            value={description}
+            name="description"
+            placeholder="description"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div className="NewMovie--input">
+          imgUrl*:
+          <br />
+          <input
+            value={imgUrl}
+            type="text"
+            name="imgUrl"
+            placeholder="imgUrl"
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+
+        <div className="NewMovie--input">
+          imdbUrl*:
+          <br />
+          <input
+            value={imdbUrl}
+            type="text"
+            name="imdbUrl"
+            placeholder="imdbUrl"
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+
+        <div className="NewMovie--input">
+          imdbId*:
+          <br />
+          <input
+            value={imdbId}
+            type="text"
+            name="imdbId"
+            placeholder="imdbId"
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+
+        <button type="submit">
+          onAdd
+        </button>
       </form>
     );
   }
