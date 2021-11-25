7bf2dc2c84c63394a9d7db1bf9afdc9394cffb2c
Viktormaz <vitecc91@gmail.com>
2021-11-25 12:44:40 +0200

    Solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,111 @@
 import { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (movie: Movie) => void;
+};
+
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState(state => ({
+      ...state,
+      [name]: value,
+    }));
+  };
+
+  handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.addMovie(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
 
   render() {
+    const activButton = !!this.state.title
+    && !!this.state.imgUrl
+    && !!this.state.imdbUrl
+    && !!this.state.imdbId;
+
     return (
-      <form>
-        Put the form here
+      <form
+        onSubmit={this.handleSubmit}
+        className="form__fields"
+      >
+
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          value={this.state.title}
+          onChange={this.handleChange}
+          required
+        />
+
+        <input
+          type="text"
+          name="description"
+          placeholder="description"
+          value={this.state.description}
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          name="imgUrl"
+          placeholder="imgUrl"
+          value={this.state.imgUrl}
+          onChange={this.handleChange}
+          required
+        />
+
+        <input
+          type="text"
+          name="imdbUrl"
+          placeholder="imdbUrl"
+          value={this.state.imdbUrl}
+          onChange={this.handleChange}
+          required
+        />
+
+        <input
+          type="text"
+          name="imdbId"
+          placeholder="imdbId"
+          value={this.state.imdbId}
+          onChange={this.handleChange}
+          required
+        />
+
+        <button
+          type="submit"
+          disabled={!activButton}
+        >
+          Add
+        </button>
       </form>
     );
   }
