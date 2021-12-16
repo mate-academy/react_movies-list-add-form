de777c70b99f41692e912bd32b68d92aadb35cac
Dariia Romanova <dariia.romanova@gmail.com>
2021-12-16 11:09:00 +0200

    Optimizations

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -7,78 +7,112 @@ type Props = {
 };
 
 type State = {
-  title: string,
-  isTitleValid: boolean,
-  description: string,
-  imgUrl: string,
-  isImgUrlValid: boolean,
-  imdbUrl: string,
-  isImdbUrlValid: boolean,
-  imdbId: string,
-  isImdbIdValid: boolean,
+  movie: {
+    title: string,
+    description: string,
+    imgUrl: string,
+    imdbUrl: string,
+    imdbId: string,
+  }
+
+  isValid: {
+    title: boolean,
+    imdbUrl: boolean,
+    imdbId: boolean,
+    imgUrl: boolean,
+  },
 };
 
 export class NewMovie extends Component<Props, State> {
   state: State = {
-    title: '',
-    isTitleValid: true,
-    description: '',
-    imgUrl: '',
-    isImgUrlValid: true,
-    imdbUrl: '',
-    isImdbUrlValid: true,
-    imdbId: '',
-    isImdbIdValid: true,
+    movie: {
+      title: '',
+      description: '',
+      imdbUrl: '',
+      imdbId: '',
+      imgUrl: '',
+    },
+    isValid: {
+      title: true,
+      imdbUrl: true,
+      imdbId: true,
+      imgUrl: true,
+    },
   };
 
   linkValidation = (link: string) => {
     const linkRegExp = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
 
-    return link.match(linkRegExp);
+    return linkRegExp.test(link);
   };
 
   clearForm = () => {
     this.setState({
-      title: '',
-      description: '',
-      imgUrl: '',
-      imdbUrl: '',
-      imdbId: '',
+      movie: {
+        title: '',
+        description: '',
+        imgUrl: '',
+        imdbUrl: '',
+        imdbId: '',
+      },
     });
   };
 
   handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    const {
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
-    } = this.state;
 
-    this.props.onAdd({
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
-    });
+    this.props.onAdd({ ...this.state.movie });
     this.clearForm();
   };
 
+  handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { value, name } = event.target;
+
+    this.setState((state) => ({
+      movie: {
+        ...state.movie,
+        [name]: value,
+      },
+      isValid: {
+        ...state.isValid,
+        [name]: true,
+      },
+    }));
+  };
+
+  handleBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+
+    switch (name) {
+      case ('title'):
+      case ('imdbId'):
+        this.setState(state => (
+          {
+            isValid: {
+              ...state.isValid,
+              [name]: !(value === ''),
+            },
+          }
+        ));
+        break;
+      case ('imdbUrl'):
+      case ('imgUrl'):
+        this.setState(state => (
+          {
+            isValid: {
+              ...state.isValid,
+              [name]: this.linkValidation(value),
+            },
+          }
+        ));
+        break;
+      default:
+        break;
+    }
+  };
+
   render() {
-    const {
-      title,
-      isTitleValid,
-      description,
-      imgUrl,
-      isImgUrlValid,
-      imdbUrl,
-      isImdbUrlValid,
-      imdbId,
-      isImdbIdValid,
-    } = this.state;
+    const { movie, isValid } = this.state;
 
     return (
       <form
@@ -93,21 +127,13 @@ export class NewMovie extends Component<Props, State> {
           <input
             type="text"
             id="title"
-            value={title}
-            className={classNames('new-movie__input', { 'new-movie__input--invalid': !isTitleValid })}
-            onChange={(event) => {
-              this.setState({
-                title: event.target.value,
-                isTitleValid: true,
-              });
-            }}
-            onBlur={() => {
-              if (!title) {
-                this.setState({ isTitleValid: false });
-              }
-            }}
+            name="title"
+            value={movie.title}
+            className={classNames('new-movie__input', { 'new-movie__input--invalid': !isValid.title })}
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
           />
-          {isTitleValid || <p className="new-movie__error">Add title</p>}
+          {isValid.title || <p className="new-movie__error">Add title</p>}
         </label>
 
         <label htmlFor="description" className="new-movie__field">
@@ -115,11 +141,10 @@ export class NewMovie extends Component<Props, State> {
 
           <textarea
             id="description"
-            value={description}
+            name="description"
+            value={movie.description}
             className="new-movie__input"
-            onChange={(event) => {
-              this.setState({ description: event.target.value });
-            }}
+            onChange={this.handleChange}
           />
         </label>
 
@@ -129,21 +154,13 @@ export class NewMovie extends Component<Props, State> {
           <input
             type="text"
             id="imgUrl"
-            value={imgUrl}
-            className={classNames('new-movie__input', { 'new-movie__input--invalid': !isImgUrlValid })}
-            onChange={(event) => {
-              this.setState({
-                isImgUrlValid: true,
-                imgUrl: event.target.value,
-              });
-            }}
-            onBlur={() => {
-              if (!this.linkValidation(imgUrl)) {
-                this.setState({ isImgUrlValid: false });
-              }
-            }}
+            name="imgUrl"
+            value={movie.imgUrl}
+            className={classNames('new-movie__input', { 'new-movie__input--invalid': !isValid.imgUrl })}
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
           />
-          {isImgUrlValid || <p className="new-movie__error">Url is invalid</p>}
+          {isValid.imgUrl || <p className="new-movie__error">Url is invalid</p>}
         </label>
 
         <label htmlFor="imdbUrl" className="new-movie__field">
@@ -152,21 +169,13 @@ export class NewMovie extends Component<Props, State> {
           <input
             type="text"
             id="imdbUrl"
-            value={imdbUrl}
-            className={classNames('new-movie__input', { 'new-movie__input--invalid': !isImdbUrlValid })}
-            onChange={(event) => {
-              this.setState({
-                isImdbUrlValid: true,
-                imdbUrl: event.target.value,
-              });
-            }}
-            onBlur={() => {
-              if (!this.linkValidation(imdbUrl)) {
-                this.setState({ isImdbUrlValid: false });
-              }
-            }}
+            name="imdbUrl"
+            value={movie.imdbUrl}
+            className={classNames('new-movie__input', { 'new-movie__input--invalid': !isValid.imdbUrl })}
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
           />
-          {isImdbUrlValid || <p className="new-movie__error">Url is invalid</p>}
+          {isValid.imdbUrl || <p className="new-movie__error">Url is invalid</p>}
         </label>
 
         <label htmlFor="imdbId" className="new-movie__field">
@@ -175,28 +184,23 @@ export class NewMovie extends Component<Props, State> {
           <input
             type="text"
             id="imdbId"
-            value={imdbId}
-            className={classNames('new-movie__input', { 'new-movie__input--invalid': !isImdbIdValid })}
-            onChange={(event) => {
-              this.setState({
-                isImdbIdValid: true,
-                imdbId: event.target.value,
-              });
-            }}
-            onBlur={() => {
-              if (!imdbId) {
-                this.setState({ isImdbIdValid: false });
-              }
-            }}
+            name="imdbId"
+            value={movie.imdbId}
+            className={classNames('new-movie__input', { 'new-movie__input--invalid': !isValid.imdbId })}
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
           />
-          {isImdbIdValid || <p className="new-movie__error">Add IMDB Id</p>}
+          {isValid.imdbId || <p className="new-movie__error">Add IMDB Id</p>}
         </label>
 
         <button
           type="submit"
           className="new-movie__button"
           disabled={
-            !title || !imdbId || !this.linkValidation(imdbUrl) || !this.linkValidation(imgUrl)
+            !movie.title
+              || !movie.imdbId
+              || !this.linkValidation(movie.imdbUrl)
+              || !this.linkValidation(movie.imgUrl)
           }
         >
           Add movie
