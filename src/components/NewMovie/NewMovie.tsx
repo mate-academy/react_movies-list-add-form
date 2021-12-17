6d3aad83956a99cd0f8eb1c1ca80c5443600cfa2
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-12-17 16:00:35 +0200

    solution not full

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,63 @@
+/* eslint-disable react/no-unused-state */
 import { Component } from 'react';
+import './NewMovie.scss';
 
 type Props = {};
-type State = {};
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form
+        onSubmit={(event) => event.preventDefault}
+        className="form"
+      >
+        <div className="form__fields">
+          <input
+            name="title"
+            placeholder="title"
+            type="text"
+            className="form__fields--field"
+          />
+          <input
+            name="description"
+            placeholder="description"
+            type="text"
+            className="form__fields--field"
+          />
+          <input
+            name="imgUrl"
+            placeholder="imgUrl"
+            type="text"
+            className="form__fields--field"
+          />
+          <input
+            name="imdbUrl"
+            placeholder="imdbUrl"
+            type="text"
+            className="form__fields--field"
+          />
+          <input
+            name="imdbId"
+            placeholder="imdbId"
+            type="text"
+            className="form__fields--field"
+          />
+        </div>
       </form>
     );
   }
