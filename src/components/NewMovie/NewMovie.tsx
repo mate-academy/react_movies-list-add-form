ec3377150d4f3790ff44130e1924eb71ae5509fb
Oleksiy Pochkayev <oleksiypochkayev@gmail.com>
2021-12-22 18:18:53 +0200

    fix

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import './NewMovie.scss';
-
+import classNames from 'classnames';
+import { InvalidLink } from '../Invalid/Invalid';
 import { Movie } from '../../interfaces/Movie';
 
 type Props = {
@@ -9,6 +10,9 @@ type Props = {
 
 type State = {
   newMovie: Movie;
+  validation: boolean | null;
+  imgUrlValid: boolean | null,
+  imdbUrlValid: boolean | null,
 };
 
 export class NewMovie extends Component<Props, State> {
@@ -20,6 +24,42 @@ export class NewMovie extends Component<Props, State> {
       imdbUrl: '',
       imdbId: '',
     },
+    validation: false,
+    imgUrlValid: null,
+    imdbUrlValid: null,
+  };
+
+  validation = (fieldName: string) => {
+    // eslint-disable-next-line no-useless-escape
+    const regExp = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
+    if (fieldName === 'imgUrl') {
+      const isValidField = regExp.test(this.state.newMovie.imgUrl);
+
+      if (isValidField) {
+        this.setState({ imgUrlValid: true });
+      } else {
+        this.setState({ imgUrlValid: false });
+      }
+    }
+
+    if (fieldName === 'imdbUrl') {
+      const isValidField = regExp.test(this.state.newMovie.imdbUrl);
+
+      if (isValidField) {
+        this.setState({ imdbUrlValid: true });
+      } else {
+        this.setState({ imdbUrlValid: false });
+      }
+    }
+
+    this.setState(state => {
+      const isValid = state.imdbUrlValid && state.imgUrlValid;
+
+      return {
+        validation: isValid,
+      };
+    });
   };
 
   movieCreate = () => {
@@ -51,6 +91,10 @@ export class NewMovie extends Component<Props, State> {
   };
 
   render() {
+    const { imdbUrlValid, imgUrlValid } = this.state;
+    const invalidImdbUrl = imdbUrlValid === false;
+    const invalidImgUrl = imgUrlValid === false;
+
     return (
       <form
         className="form"
@@ -63,6 +107,7 @@ export class NewMovie extends Component<Props, State> {
           Title:
           {' '}
           <input
+            required
             className="form__field"
             type="text"
             name="title"
@@ -89,32 +134,43 @@ export class NewMovie extends Component<Props, State> {
           imgUrl:
           {' '}
           <input
-            className="form__field"
+            required
+            className={classNames('form__field', { form__invalid: invalidImgUrl })}
             type="text"
             name="imgUrl"
             id="imgUrl"
             value={this.state.newMovie.imgUrl}
             onChange={this.handleChange}
+            onBlur={() => {
+              this.validation('imgUrl');
+            }}
           />
+          {invalidImgUrl && <InvalidLink />}
         </label>
 
         <label htmlFor="imdbUrl" className="form__label">
           imdbUrl:
           {' '}
           <input
-            className="form__field"
+            required
+            className={classNames('form__field', { form__invalid: invalidImdbUrl })}
             type="text"
             name="imdbUrl"
             id="imdbUrl"
             value={this.state.newMovie.imdbUrl}
             onChange={this.handleChange}
+            onBlur={() => {
+              this.validation('imdbUrl');
+            }}
           />
+          {invalidImdbUrl && <InvalidLink />}
         </label>
 
         <label htmlFor="imdbId" className="form__label">
           imdbId:
           {' '}
           <input
+            required
             className="form__field"
             type="text"
             name="imdbId"
@@ -124,7 +180,12 @@ export class NewMovie extends Component<Props, State> {
           />
         </label>
 
-        <button type="submit">Add movie</button>
+        <button
+          type="submit"
+          disabled={!this.state.validation}
+        >
+          Add movie
+        </button>
       </form>
     );
   }
