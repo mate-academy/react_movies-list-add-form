26b979698134db5ee3d8e9befd44c4cf5f6a4d19
Artur Ermolenko <ermolenkoartyr90@gmail.com>
2022-01-27 19:40:36 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,86 @@
 import { Component } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string
+  imdbId: string,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState(state => ({
+      ...state,
+      [name]: value,
+    }));
+  };
+
+  handleClear = () => {
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    this.props.onAdd(this.state);
+    this.handleClear();
+  };
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form onSubmit={this.handleSubmit}>
+        {Object.keys(this.state).map(item => (
+          <div key={item}>
+            <label htmlFor={item} key={item}>
+              <span
+                key={item}
+                className="item_key"
+                style={{ color: 'blueviolet' }}
+              >
+                {item.toUpperCase()}
+              </span>
+              <br />
+              <div className="ui inverted segment">
+                <input
+                  key={item}
+                  id={item}
+                  type="text"
+                  name={item}
+                  value={this.state[item as keyof State]}
+                  placeholder={`Enter ${item}`}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </label>
+          </div>
+        ))}
+        <br />
+        <button
+          type="submit"
+          className="positive ui button"
+        >
+          Add Movie
+        </button>
       </form>
     );
   }
