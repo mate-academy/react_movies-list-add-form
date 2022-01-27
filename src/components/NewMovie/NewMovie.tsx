4c58cf96ba33ab638e4b1cd8d435282a79e73a9c
Basil-Panasiuk <vasylpanasjuk2001@gmail.com>
2022-01-27 16:38:54 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,73 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+type State = Movie;
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChangeField = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    this.setState((state) => ({
+      ...state,
+      [name]: value,
+    }));
+  };
+
+  handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    this.props.onAdd(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form
+        onSubmit={this.handleSubmit}
+      >
+        {
+          Object.keys(this.state).map(property => (
+            <div>
+              <label
+                key={property}
+                htmlFor={property}
+              >
+                {property}
+                <br />
+                <input
+                  type="text"
+                  name={property}
+                  id={property}
+                  value={this.state[property as keyof State]}
+                  onChange={this.handleChangeField}
+                />
+              </label>
+            </div>
+          ))
+        }
+        <button
+          type="submit"
+        >
+          Add movie
+        </button>
       </form>
     );
   }
