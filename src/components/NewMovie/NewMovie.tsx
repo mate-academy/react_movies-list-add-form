22b1c54e2e664464cdcf9abb0f60303eb4816135
Ernesto Fernandez <ernesto.fernandez.1980@gmail.com>
2022-02-02 20:03:59 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import './NewMovie.scss';
 
 type Props = {
   addMovie: (movie: Movie) => void,
@@ -10,6 +12,10 @@ type State = {
   imgUrl?: string,
   imdbUrl?: string,
   imdbId?: string,
+  showErrorTitle?: boolean,
+  showErrorImgUrl?: boolean,
+  showErrorImdbUrl?: boolean,
+  showErrorImdbId?: boolean,
 };
 
 export class NewMovie extends Component<Props, State> {
@@ -19,18 +25,60 @@ export class NewMovie extends Component<Props, State> {
     imgUrl: '',
     imdbUrl: '',
     imdbId: '',
+    showErrorTitle: false,
+    showErrorImgUrl: false,
+    showErrorImdbUrl: false,
+    showErrorImdbId: false,
   };
 
-  handlerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { value, name } = event.target;
 
-    this.setState({ [name]: value });
+    // eslint-disable-next-line no-console
+    console.log(name, value.length, value);
+
+    const nameErrorFlag = `showError${name[0].toUpperCase() + name.slice(1)}`;
+    const showErrorFlag = !value.length;
+
+    this.setState({
+      [name]: value,
+      [nameErrorFlag]: showErrorFlag,
+    });
   };
 
   handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
+    if (!title) {
+      this.setState({ showErrorTitle: true });
+    }
+
+    if (!imgUrl) {
+      this.setState({ showErrorImgUrl: true });
+    }
+
+    if (!imdbUrl) {
+      this.setState({ showErrorImdbUrl: true });
+    }
+
+    if (!imdbId) {
+      this.setState({ showErrorImdbId: true });
+    }
+
+    if (!title || !description || !imgUrl || !imdbUrl || !imdbId) {
+      return;
+    }
+
     this.props.addMovie(this.state);
+
     this.setState({
       title: '',
       description: '',
@@ -47,55 +95,96 @@ export class NewMovie extends Component<Props, State> {
       imgUrl,
       imdbUrl,
       imdbId,
+      showErrorTitle,
+      showErrorImgUrl,
+      showErrorImdbUrl,
+      showErrorImdbId,
     } = this.state;
 
     return (
       <div>
         <form
+          className="form "
           onSubmit={this.handleSubmit}
         >
-          <h1>{title}</h1>
-          <input
-            name="title"
-            type="text"
-            placeholder="Please input title"
-            value={title}
-            onChange={(event) => this.handlerChange(event)}
-          />
-          <h1>{description}</h1>
-          <input
-            name="description"
-            type="text"
-            placeholder="Please input description"
-            value={description}
-            onChange={this.handlerChange}
-          />
-          <h1>{imgUrl}</h1>
-          <input
-            name="imgUrl"
-            type="text"
-            placeholder="input smth"
-            value={imgUrl}
-            onChange={this.handlerChange}
-          />
-          <h1>{imdbUrl}</h1>
-          <input
-            name="imdbUrl"
-            type="text"
-            placeholder="input smth"
-            value={imdbUrl}
-            onChange={this.handlerChange}
-          />
-          <h1>{imdbId}</h1>
-          <input
-            name="imdbId"
-            type="text"
-            placeholder="input smth"
-            value={imdbId}
-            onChange={this.handlerChange}
-          />
+          <div className="form__item">
+            <h1 className="error">{showErrorTitle && 'title is not correct'}</h1>
+            <input
+              className={classNames(
+                'form__input',
+                'card',
+                (showErrorTitle && 'form__input--error'),
+              )}
+              name="title"
+              type="text"
+              placeholder="Please input title"
+              value={title}
+              onChange={(event) => this.handleChange(event)}
+            />
+          </div>
+          <div className="form__item">
+            <textarea
+              className="form__input form__input--description card"
+              name="description"
+              placeholder="Please input description"
+              value={description}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form__item">
+            <h1 className="error">{showErrorImgUrl && 'ImgUrl is not correct'}</h1>
+            <input
+              className={classNames(
+                'form__input',
+                'card',
+                (showErrorTitle && 'form__input--error'),
+              )}
+              name="imgUrl"
+              type="text"
+              placeholder="Please input imgUrl"
+              value={imgUrl}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form__item">
+            <h1 className="error">{showErrorImdbUrl && 'imdbUrl is not correct'}</h1>
+            <input
+              className={classNames(
+                'form__input',
+                'card',
+                (showErrorTitle && 'form__input--error'),
+              )}
+              name="imdbUrl"
+              type="text"
+              placeholder="Please input imdbUrl"
+              value={imdbUrl}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form__item">
+            <h1 className="error">{showErrorImdbId && 'imdbId is not correct'}</h1>
+            <input
+              className={classNames(
+                'form__input',
+                'card',
+                (showErrorTitle && 'form__input--error'),
+              )}
+              name="imdbId"
+              type="text"
+              placeholder="Please input imdbId"
+              value={imdbId}
+              onChange={this.handleChange}
+            />
+          </div>
           <button
+            className="form__button"
             type="submit"
+            disabled={
+              showErrorTitle
+              || showErrorImgUrl
+              || showErrorImdbUrl
+              || showErrorImdbId
+            }
           >
             Add
           </button>
