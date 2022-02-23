5eab762e65422d7cac64c5b171af8652a4e32f95
Misha Novikov <novikov1807@gmail.com>
2022-02-23 23:56:04 +0200

    Failed to compile

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,79 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovies: (movie: Movie) => void
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export const NewMovie:React.FC<Props> = ({ addMovies }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
 
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
-}
+  const clearInputs = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const addNewMovie = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    clearInputs();
+    addMovies(newMovie);
+  };
+
+  return (
+    <form className="form" onSubmit={addNewMovie}>
+      <input
+        className="form__input"
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={event => setTitle(event.target.value)}
+      />
+      <input
+        className="form__input"
+        type="text"
+        placeholder="description"
+        value={description}
+        onChange={event => setDescription(event.target.value)}
+      />
+      <input
+        className="form__input"
+        type="text"
+        placeholder="imgUrl"
+        value={imgUrl}
+        onChange={event => setImgUrl(event.target.value)}
+      />
+      <input
+        className="form__input"
+        type="text"
+        placeholder="imdbUrl"
+        value={imdbUrl}
+        onChange={event => setImdbUrl(event.target.value)}
+      />
+      <input
+        className="form__input"
+        type="text"
+        placeholder="imdbId"
+        value={imdbId}
+        onChange={event => setImdbId(event.target.value)}
+      />
+      <button className="form__button" type="submit">
+        Add movie
+      </button>
+    </form>
+  );
+};
