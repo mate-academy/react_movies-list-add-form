12e6214000b2ba91ca34217acf75ab1d3f51c9e4
mariiaStorozhyk <xmsha722@gmail.com>
2022-03-15 07:38:10 +0200

    Develop

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,106 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (newTitle: string,
+    newDesctiption: string,
+    newImgUrl: string,
+    newImdbUrl: string,
+    newImdbId: string,) => void
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [title, setTitle] = useState('');
+  const [hasTitileError, setTitileError] = useState(false);
 
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
-}
+  const [description, setDescription] = useState('');
+  const [hasDescriptionError, setDescriptionError] = useState(false);
+
+  const [imgUrl, setImgUrl] = useState('');
+  const [hasImgUrlError, setImgUrlError] = useState(false);
+
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [hasImdbUrlError, setImdbUrlError] = useState(false);
+
+  const [imdbId, setImdbId] = useState('');
+  const [hasImdbIdError, setImdbIdError] = useState(false);
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        setTitileError(!title);
+        setDescriptionError(!description);
+        setImgUrlError(!imgUrl);
+        setImdbUrlError(!imdbUrl);
+        setImdbIdError(!imdbId);
+
+        if (title && description && imgUrl && imdbUrl && imdbId) {
+          onAdd(title, description, imgUrl, imdbUrl, imdbId);
+          setTitle('');
+          setDescription('');
+          setImgUrl('');
+          setImdbUrl('');
+          setImdbId('');
+        }
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Title"
+        className={hasTitileError ? 'error' : ''}
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+          setTitileError(false);
+        }}
+      />
+
+      <input
+        type="text"
+        className={hasDescriptionError ? 'error' : ''}
+        placeholder="Description"
+        value={description}
+        onChange={(event) => {
+          setDescription(event.target.value);
+          setDescriptionError(false);
+        }}
+      />
+
+      <input
+        type="text"
+        className={hasImgUrlError ? 'error' : ''}
+        placeholder="Image URL"
+        value={imgUrl}
+        onChange={(event) => {
+          setImgUrl(event.target.value);
+          setImgUrlError(false);
+        }}
+      />
+
+      <input
+        type="text"
+        className={hasImdbUrlError ? 'error' : ''}
+        placeholder="IMDB URL"
+        value={imdbUrl}
+        onChange={(event) => {
+          setImdbUrl(event.target.value);
+          setImdbUrlError(false);
+        }}
+      />
+
+      <input
+        type="text"
+        className={hasImdbIdError ? 'error' : ''}
+        placeholder="IMDB Id"
+        value={imdbId}
+        onChange={(event) => {
+          setImdbId(event.target.value);
+          setImdbIdError(false);
+        }}
+      />
+
+      <button type="submit">Add</button>
+    </form>
+  );
+};
