cc51496e796ad3691f233eb0379049767a11b42b
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-03-16 16:33:51 +0200

    fix

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable no-console */
 import React, { useState } from 'react';
 
-// eslint-disable-next-line no-useless-escape
-const regexForUrl = '/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/';
+// eslint-disable-next-line max-len
+const regexForUrl = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
 
 type Props = {
   addMovie: (title: string,
@@ -18,60 +19,67 @@ export const NewMovie: React.FC<Props> = ({ addMovie }) => {
   const [imdbUrl, setimdbUrl] = useState('');
   const [imdbId, setimdbId] = useState('');
 
-  const values = [title, description, imgUrl, imdbUrl, imdbId];
+  const [validTitle, setValidTitle] = useState(true);
+  const [validUrl, setValidUrl] = useState(true);
+
+  const values = [imgUrl, imdbUrl, imdbId];
+  const validValues = [validTitle, validUrl];
+  const disabledButton = values.some(a => a.length === 0) || undefined;
 
   const addNewFilm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    addMovie(title, description, imgUrl, imdbUrl, imdbId);
-    setTitle('');
-    setDescription('');
-    setimgUrl('');
-    setimdbUrl('');
-    setimdbId('');
+    setValidTitle(title.length > 0);
+
+    setValidUrl(values.every(value => !value.replace(regexForUrl, '').length));
+
+    console.log(imgUrl.replace(regexForUrl, ''));
+
+    if (validValues.every(value => value === true)) {
+      addMovie(title, description, imgUrl, imdbUrl, imdbId);
+      setTitle('');
+      setDescription('');
+      setimgUrl('');
+      setimdbUrl('');
+      setimdbId('');
+    }
   };
 
   return (
     <form onSubmit={(event) => addNewFilm(event)}>
+      {!validTitle && <h4>Минимальная длина названия 1 символ</h4>}
       <input
         type="text"
         placeholder="title"
         value={title}
         onChange={(event) => setTitle(event.target.value)}
-        required
-        minLength={5}
       />
       <input
         type="text"
         placeholder="description"
         value={description}
         onChange={(event) => setDescription(event.target.value)}
-        minLength={15}
       />
+      {!validUrl && <h4>Введите правильный URL-адрес</h4>}
       <input
-        type="url"
+        type="text"
         placeholder="imgUrl"
-        pattern={regexForUrl}
         value={imgUrl}
         onChange={(event) => setimgUrl(event.target.value)}
-        required
-      />
-      <input
-        type="url"
-        placeholder="imdbUrl"
-        pattern={regexForUrl}
-        value={imdbUrl}
-        onChange={(event) => setimdbUrl(event.target.value)}
-        required
       />
       <input
         type="text"
+        placeholder="imdbUrl"
+        value={imdbUrl}
+        onChange={(event) => setimdbUrl(event.target.value)}
+      />
+      <input
+        type="url"
         placeholder="imdbId"
         value={imdbId}
         onChange={(event) => setimdbId(event.target.value)}
-        required
       />
-      <button type="submit" disabled={values.some(a => a.length === 0) || undefined}>Add</button>
+      <button type="submit" disabled={disabledButton}>Add</button>
     </form>
   );
 };
