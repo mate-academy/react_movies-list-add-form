9f5dff06a4d182778eb5f36aed1194116b0d5c98
Ivan Vorobiov <vorobiov.ivan01@gmail.com>
2022-03-16 23:59:56 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,93 @@
 import { Component } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '0',
+  };
 
   render() {
+    const { onAdd } = this.props;
+
     return (
-      <form>
-        Put the form here
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          onAdd(this.state);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter a title"
+          value={this.state.title}
+          onChange={
+            event => {
+              this.setState({
+                title: event.target.value,
+              });
+            }
+          }
+        />
+        <input
+          type="text"
+          placeholder="Enter a description"
+          value={this.state.description}
+          onChange={event => {
+            this.setState({
+              description: event.target.value,
+            });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Enter a image adress"
+          value={this.state.imgUrl}
+          onChange={event => {
+            this.setState({
+              imgUrl: event.target.value,
+            });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Enter a imdb link"
+          value={this.state.imdbUrl}
+          onChange={event => {
+            this.setState({
+              imdbUrl: event.target.value,
+            });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Enter a imdb id"
+          value={this.state.imdbId}
+          onChange={event => {
+            this.setState({
+              imdbId: event.target.value,
+            });
+          }}
+        />
+        <button
+          type="submit"
+        >
+          CREATE
+        </button>
       </form>
     );
   }
