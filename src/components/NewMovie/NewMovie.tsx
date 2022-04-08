8b87df2517ae43505d74e829194002a1ae4b0c98
Roman Khemych <roman19940912@gmail.com>
2022-04-08 10:54:12 +0300

    Chang Name

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -55,9 +55,7 @@ export const NewMovie: React.FC<Props> = React.memo(({
           id="title"
           placeholder="Write the title"
           value={title}
-          onChange={(e) => (
-            setTitle(e.target.value)
-          )}
+          onChange={(e) => setTitle(e.target.value)}
           className="form_text"
         />
         <textarea
@@ -65,9 +63,7 @@ export const NewMovie: React.FC<Props> = React.memo(({
           id="description"
           placeholder="Write the description"
           value={description}
-          onChange={(e) => (
-            setDescription(e.target.value)
-          )}
+          onChange={(e) => setDescription(e.target.value)}
           className="form_description"
         />
         <input
@@ -76,9 +72,7 @@ export const NewMovie: React.FC<Props> = React.memo(({
           id="imgUrl"
           placeholder="Write the imgUrl"
           value={imgUrl}
-          onChange={(e) => (
-            setImgUrl(e.target.value)
-          )}
+          onChange={(e) => setImgUrl(e.target.value)}
           className="form_text"
         />
         <input
@@ -87,9 +81,7 @@ export const NewMovie: React.FC<Props> = React.memo(({
           id="imdbUrl"
           placeholder="Write the imdbUrl"
           value={imdbUrl}
-          onChange={(e) => (
-            setImdbUrl(e.target.value)
-          )}
+          onChange={(e) => setImdbUrl(e.target.value)}
           className="form_text"
         />
         <input
@@ -98,9 +90,7 @@ export const NewMovie: React.FC<Props> = React.memo(({
           id="imdbId"
           placeholder="Write the imdbId"
           value={imdbId}
-          onChange={(e) => (
-            setImdbId(e.target.value)
-          )}
+          onChange={(e) => setImdbId(e.target.value)}
           className="form_text"
         />
         <button className="button_add" type="submit">
