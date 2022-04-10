33567240e457d2fe85cfce35da241317eab726e4
Vlad Brynzya <vladbrynzya@gmail.com>
2022-04-10 16:50:26 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,70 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 
-type Props = {};
-type State = {};
-
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
-
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
+interface Props {
+  onAdd: (movie: Movie) => void
 }
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const resetForm = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const createMovie = () => {
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    createMovie();
+    resetForm();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        placeholder="Enter title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Enter description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        placeholder="Enter imgUrl"
+        value={imgUrl}
+        onChange={(e) => setImgUrl(e.target.value)}
+      />
+      <input
+        placeholder="Enter imdbUrl"
+        value={imdbUrl}
+        onChange={(e) => setImdbUrl(e.target.value)}
+      />
+      <input
+        placeholder="Enter imdbId"
+        value={imdbId}
+        onChange={(e) => setImdbId(e.target.value)}
+      />
+      <button type="submit">
+        Add movie
+      </button>
+    </form>
+  );
+};
