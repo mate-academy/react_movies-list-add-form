50c8d9f2c275a1e145762c56f432a6b239f2a6a9
Vitaliy Zhyvyliak <veltjj@gmail.com>
2022-04-11 23:38:27 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,102 @@
-import { Component } from 'react';
+import { useState, memo, FormEvent } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
-
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
-
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
+interface Props {
+  onAdd: (movie: Movie) => void,
 }
+
+export const NewMovie: React.FC<Props> = memo(({ onAdd }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const resetForm = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    resetForm();
+  };
+
+  return (
+    <form
+      className="form"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        id="title"
+        className="form__field"
+        placeholder="Title"
+        value={title}
+        onChange={(event) => (
+          setTitle(event.target.value)
+        )}
+      />
+
+      <input
+        type="text"
+        id="description"
+        className="form__field"
+        placeholder="Description"
+        value={description}
+        onChange={(event) => (
+          setDescription(event.target.value)
+        )}
+      />
+
+      <input
+        type="text"
+        id="imgUrl"
+        className="form__field"
+        placeholder="ImgUrl"
+        value={imgUrl}
+        onChange={(event => (
+          setImgUrl(event.target.value)
+        ))}
+      />
+      <input
+        type="text"
+        id="imdbUrl"
+        className="form__field"
+        placeholder="ImdbUrl"
+        value={imdbUrl}
+        onChange={(event => (
+          setImdbUrl(event.target.value)
+        ))}
+      />
+      <input
+        type="text"
+        id="imdbId"
+        className="form__field"
+        placeholder="ImdbId"
+        value={imdbId}
+        onChange={(event => (
+          setImdbId(event.target.value)
+        ))}
+      />
+
+      <button
+        type="submit"
+        className="form__submit"
+      >
+        Add
+      </button>
+    </form>
+  );
+});
