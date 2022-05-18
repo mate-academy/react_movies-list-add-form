900fa78c144eccadc94e471b300632edc7239c02
Viacheslav Petrovskyi <v.petrovskyi@icloud.com>
2022-05-18 22:49:10 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,176 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+  errorTitlt: boolean,
+  errorDescription: boolean,
+  errorImdbId: boolean,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+    errorTitlt: false,
+    errorDescription: false,
+    errorImdbId: false,
+  };
+
+  handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (this.state.title.trim() === '') {
+      this.setState({
+        title: '',
+        errorTitlt: true,
+      });
+    } else {
+      this.setState({
+        errorTitlt: false,
+      });
+    }
+
+    if (this.state.description.trim() === '') {
+      this.setState({
+        description: '',
+        errorDescription: true,
+      });
+    } else {
+      this.setState({
+        errorDescription: false,
+      });
+    }
+
+    if (this.state.imdbId.trim() === '') {
+      this.setState({
+        imdbId: '',
+        errorImdbId: true,
+      });
+    } else {
+      this.setState({
+        errorImdbId: false,
+      });
+    }
+
+    if (this.state.title.trim() !== ''
+      && this.state.description.trim() !== ''
+      && this.state.imdbId.trim() !== '') {
+      this.props.onAdd(this.state);
+
+      this.setState({
+        title: '',
+        description: '',
+        imgUrl: '',
+        imdbUrl: '',
+        imdbId: '',
+        errorTitlt: false,
+        errorDescription: false,
+        errorImdbId: false,
+      });
+    }
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+      errorTitlt,
+      errorDescription,
+      errorImdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
+      <form className="form" onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            className="form__input"
+            placeholder="title"
+            type="text"
+            value={title}
+            onChange={(event) => {
+              this.setState({
+                title: event.target.value,
+              });
+            }}
+          />
+          {errorTitlt && (
+            <span className="error">enter a valid title</span>
+          )}
+        </div>
+        <div>
+          <input
+            className="form__input"
+            placeholder="description"
+            type="text"
+            value={description}
+            onChange={(event) => {
+              this.setState({
+                description: event.target.value,
+              });
+            }}
+          />
+          {errorDescription && (
+            <span className="error">enter a valid description</span>
+          )}
+        </div>
+        <div>
+          <input
+            className="form__input"
+            placeholder="imgUrl"
+            type="url"
+            value={imgUrl}
+            onChange={(event) => {
+              this.setState({
+                imgUrl: event.target.value,
+              });
+            }}
+          />
+        </div>
+        <div>
+          <input
+            className="form__input"
+            placeholder="imdbUrl"
+            type="url"
+            value={imdbUrl}
+            onChange={(event) => {
+              this.setState({
+                imdbUrl: event.target.value,
+              });
+            }}
+          />
+        </div>
+        <div>
+          <input
+            className="form__input"
+            placeholder="imdbId"
+            type="text"
+            value={imdbId}
+            onChange={(event) => {
+              this.setState({
+                imdbId: event.target.value,
+              });
+            }}
+          />
+          {errorImdbId && (
+            <span className="error">enter a valid imdbId</span>
+          )}
+        </div>
+        <button className="button__add" type="submit">Add</button>
       </form>
     );
   }
