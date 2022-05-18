7c68b4a88300ad6b4c0f59eddc3b34f19ba06245
SeKaM0 <polozhentsew02@gmail.com>
2022-05-18 18:20:29 +0300

    Finish task

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,15 @@ export class App extends React.Component<{}, State> {
     movies: moviesFromServer,
   };
 
-  addMovie = (/* movie: Movie */) => {
-    // put your code here
+  addMovie = (value:Movie) => {
+    this.setState((state) => {
+      return {
+        movies: [
+          ...state.movies,
+          value,
+        ],
+      };
+    });
   };
 
   render() {
@@ -26,7 +33,9 @@ export class App extends React.Component<{}, State> {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie
+            onAdd={this.addMovie}
+          />
         </div>
       </div>
     );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,115 @@
-import { Component } from 'react';
+import { ChangeEvent, Component } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (x:Movie) => void,
+};
+type State = {
+  movie:Movie
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    movie: {
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    },
+  };
+
+  onSubmit = (event:React.SyntheticEvent) => {
+    event.preventDefault();
+    this.props.onAdd(this.state.movie);
+  };
+
+  onChange = (event:ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState(state => ({
+      ...state,
+      movie: {
+        ...state.movie,
+        [name]: value,
+      },
+    }));
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state.movie;
+
     return (
-      <form>
-        Put the form here
-      </form>
+      <>
+        <h1>Add movie</h1>
+        <form onSubmit={this.onSubmit}>
+          <label>
+            title:
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={this.onChange}
+              required
+            />
+          </label>
+          <br />
+          <label>
+            description:
+            <input
+              type="text"
+              name="description"
+              value={description}
+              onChange={this.onChange}
+              required
+            />
+          </label>
+          <br />
+          <label>
+            imgUrl:
+            <input
+              type="text"
+              name="imgUrl"
+              value={imgUrl}
+              onChange={this.onChange}
+              required
+            />
+          </label>
+          <br />
+          <label>
+            imdbUrl:
+            <input
+              type="text"
+              name="imdbUrl"
+              value={imdbUrl}
+              onChange={this.onChange}
+              required
+            />
+          </label>
+          <br />
+          <label>
+            imdbId:
+            <input
+              type="text"
+              name="imdbId"
+              value={imdbId}
+              onChange={this.onChange}
+              required
+            />
+          </label>
+
+          <p>
+            <button type="submit">
+              Add movie
+            </button>
+          </p>
+        </form>
+      </>
     );
   }
 }
