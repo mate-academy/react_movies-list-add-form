644c3bc28f56ae2bc284dfdc8361cb1161a1ac7b
Oleksii Fedorenko <alex4rest09@gmail.com>
2022-05-18 20:16:15 +0300

    solution without validation

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,88 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setimgUrln] = useState('');
+  const [imdbUrl, setimdbUrl] = useState('');
+  const [imdbId, setimdbId] = useState('');
 
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
-}
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    if (title) {
+      onAdd(newMovie);
+    }
+
+    setTitle('');
+    setDescription('');
+    setimgUrln('');
+    setimdbUrl('');
+    setimdbId('');
+  };
+
+  return (
+    <form
+      className="newMovie"
+      name="newMovie"
+      onSubmit={onSubmit}
+    >
+      <input
+        className="newMovie__input"
+        name="title"
+        type="text"
+        placeholder="Enter the title"
+        value={title}
+        onChange={event => setTitle(event?.target.value)}
+      />
+      <input
+        className="newMovie__input"
+        name="description"
+        type="text"
+        placeholder="Enter the description"
+        value={description}
+        onChange={event => setDescription(event?.target.value)}
+      />
+      <input
+        className="newMovie__input"
+        name="imgUrl"
+        type="text"
+        placeholder="Enter the imgUrl"
+        value={imgUrl}
+        onChange={event => setimgUrln(event?.target.value)}
+      />
+      <input
+        className="newMovie__input"
+        name="imdbUrl"
+        type="text"
+        placeholder="Enter the imdbUrl"
+        value={imdbUrl}
+        onChange={event => setimdbUrl(event?.target.value)}
+      />
+      <input
+        className="newMovie__input"
+        name="imdbId"
+        type="text"
+        placeholder="Enter the title"
+        value={imdbId}
+        onChange={event => setimdbId(event?.target.value)}
+      />
+      <div className="newMovie__button">
+        <button type="submit">Add Movie</button>
+      </div>
+
+    </form>
+  );
+};
