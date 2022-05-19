101f7324262ea80eac74e8793e5bc0ab819649c3
Anton Levchuk <antonlevchuk626@gmail.com>
2022-05-19 16:04:22 +0300

    Main task, withot validation

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,103 @@
-import { Component } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (movie: Movie) => void,
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export const NewMovie: React.FC<Props> = ({ addMovie }) => {
+  const initialMovie = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
 
-  render() {
-    return (
-      <form>
-        Put the form here
+  const [movie, setMovie] = useState(initialMovie);
+
+  const movieHandler = (
+    event: ChangeEvent<HTMLInputElement>
+    | ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = event.target;
+
+    setMovie({ ...movie, [name]: value });
+  };
+
+  const submitHandler = (
+    event: FormEvent<HTMLFormElement>,
+  ) => {
+    event.preventDefault();
+    addMovie(movie);
+  };
+
+  return (
+    <div className="NewMovie">
+      <form onSubmit={submitHandler} className="NewMovie__form">
+        <h1 className="NewMovie__title">Add new movie</h1>
+
+        <label className="NewMovie__label">
+          Title
+          <input
+            type="text"
+            name="title"
+            value={movie.title}
+            className="NewMovie__input"
+            placeholder="Title"
+            onChange={movieHandler}
+          />
+        </label>
+
+        <label className="NewMovie__label">
+          Description
+          <textarea
+            name="description"
+            value={movie.description}
+            className="NewMovie__input NewMovie__textarea"
+            placeholder="Description"
+            onChange={movieHandler}
+          />
+        </label>
+
+        <label className="NewMovie__label">
+          ImgUrl
+          <input
+            type="text"
+            name="imgUrl"
+            value={movie.imgUrl}
+            className="NewMovie__input"
+            placeholder="imgUrl"
+            onChange={movieHandler}
+          />
+        </label>
+
+        <label className="NewMovie__label">
+          ImdbUrl
+          <input
+            type="text"
+            name="imdbUrl"
+            value={movie.imdbUrl}
+            className="NewMovie__input"
+            placeholder="imdbUrl"
+            onChange={movieHandler}
+          />
+        </label>
+
+        <label className="NewMovie__label">
+          ImdbId
+          <input
+            type="text"
+            name="imdbId"
+            value={movie.imdbId}
+            className="NewMovie__input"
+            placeholder="imdbId"
+            onChange={movieHandler}
+          />
+        </label>
+
+        <button type="submit" className="NewMovie__button">Add movie</button>
       </form>
-    );
-  }
-}
+    </div>
+  );
+};
