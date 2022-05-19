3a8051c685f90f73ad01206bfdbce51e2f433a38
yurii837 <gorogotskiy@gmail.com>
2022-05-19 19:43:38 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,10 @@ export class App extends React.Component<{}, State> {
     movies: moviesFromServer,
   };
 
-  addMovie = (/* movie: Movie */) => {
-    // put your code here
+  addMovie = (newMovie: Movie) => {
+    this.setState(prevSt => ({
+      movies: [...prevSt.movies, newMovie],
+    }));
   };
 
   render() {
@@ -26,7 +28,7 @@ export class App extends React.Component<{}, State> {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie addMovie={this.addMovie} />
         </div>
       </div>
     );
