1b712d5ab902472588af894bb487b2f654e2b03a
yuriyshvorob <zyuraz1993@gmail.com>
2022-05-23 14:09:49 +0300

    ref-sol added

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ interface State {
 }
 
 export class App extends React.Component<{}, State> {
-  state: State = {
+  state = {
     movies: moviesFromServer,
   };
 
-  addMovie = (/* movie: Movie */) => {
-    // put your code here
+  addMovie = (movie: Movie) => {
+    this.setState((state) => ({
+      movies: [...state.movies, movie],
+    }));
   };
 
   render() {
@@ -26,7 +28,7 @@ export class App extends React.Component<{}, State> {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <NewMovie />
+          <NewMovie addMovie={this.addMovie} />
         </div>
       </div>
     );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,131 @@
 import { Component } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (newItem: Movie) => void
+};
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export class NewMovie extends Component <Props, State> {
+  state = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.addMovie(this.state);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState((state) => ({
+      ...state,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
   render() {
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
     return (
-      <form>
-        Put the form here
-      </form>
+      <div className="wrapper-movie">
+        <h1 className="wrapper-movie__title">
+          ADD NEW MOVIE
+        </h1>
+        <form
+          className="movie-form"
+          onSubmit={this.handleSubmit}
+        >
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            className="movie-form__item"
+            data-cy="form-title"
+            value={title}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="Description"
+            placeholder="Description"
+            className="movie-form__item"
+            data-cy="form-description"
+            value={description}
+            onChange={(event) => {
+              this.setState({
+                description: event.target.value,
+              });
+            }}
+            required
+          />
+          <input
+            type="text"
+            name="ImgUrl"
+            placeholder="ImgUrl"
+            className="movie-form__item"
+            data-cy="form-imgUrl"
+            value={imgUrl}
+            onChange={(event) => {
+              this.setState({
+                imgUrl: event.target.value,
+              });
+            }}
+            required
+          />
+          <input
+            type="text"
+            name="imdbUrl"
+            placeholder="imdbUrl"
+            className="movie-form__item"
+            data-cy="form-imdbUrl"
+            value={imdbUrl}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="imdbId"
+            placeholder="imdbId"
+            className="movie-form__item"
+            data-cy="form-imdbId"
+            value={imdbId}
+            onChange={this.handleChange}
+            required
+          />
+          <button
+            type="submit"
+            className="button-form"
+            data-cy="form-submit-button"
+          >
+            Add
+          </button>
+        </form>
+      </div>
     );
   }
 }
