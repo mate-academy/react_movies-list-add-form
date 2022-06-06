d84d75bd37db0e3dd1da8c2e442c488e0bc14f92
yuriyshvorob <zyuraz1993@gmail.com>
2022-06-06 11:19:13 +0300

    component test added and test code refactored

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,47 +1,60 @@
-describe('basic level tests', () => {
-  const fillAllFields = () => {
-    cy.getByDataCy('form-title')
-    .type('movie-title');
-    cy.getByDataCy('form-description')
-    .type('movie-description');
-    cy.getByDataCy('form-imgUrl')
-    .type('movie-img');
-    cy.getByDataCy('form-imdbUrl')
-    .type('movie-url');
-    cy.getByDataCy('form-imdbId')
-    .type('movie-id');
-  };
+describe('movies page tests', () => {
+  const form = {
+    fillAllFields() {
+      return cy.getByDataCy('form-title')
+               .type('movie-title') &&
+             cy.getByDataCy('form-description')
+               .type('movie-description') &&
+             cy.getByDataCy('form-imgUrl')
+               .type('movie-img') &&
+             cy.getByDataCy('form-imdbUrl')
+               .type('movie-url') &&
+             cy.getByDataCy('form-imdbId')
+               .type('movie-id');
+    }
+  }
 
   before('visit main page', () => {
     cy.visit('/');
   });
   
   it('should not reload the page after clicking submit button', () => {
-    fillAllFields();
-    cy.window().then(w => w.beforeReload = true);
-    cy.window().should('have.prop', 'beforeReload', true);
-    cy.getByDataCy('form-submit-button').click();
-    cy.window().should('have.prop', 'beforeReload');
+    form.fillAllFields();
+    cy.window()
+      .then(w => w.beforeReload = true);
+    cy.window()
+      .should('have.prop', 'beforeReload', true);
+    cy.getByDataCy('form-submit-button')
+      .click();
+    cy.window()
+      .should('have.prop', 'beforeReload');
   });
 
   it('should clean the fields after clicking submit button', () => {
-    fillAllFields();
-    cy.getByDataCy('form-submit-button').click();
-    cy.getByDataCy('form-title').should('be.empty');
-    cy.getByDataCy('form-description').should('be.empty');
-    cy.getByDataCy('form-imgUrl').should('be.empty');
-    cy.getByDataCy('form-imdbUrl').should('be.empty');
-    cy.getByDataCy('form-imdbId').should('be.empty');
+    form.fillAllFields();
+    cy.getByDataCy('form-submit-button')
+      .click();
+    cy.getByDataCy('form-title')
+      .should('be.empty');
+    cy.getByDataCy('form-description')
+      .should('be.empty');
+    cy.getByDataCy('form-imgUrl')
+      .should('be.empty');
+    cy.getByDataCy('form-imdbUrl')
+      .should('be.empty');
+    cy.getByDataCy('form-imdbId')
+      .should('be.empty');
   });
 
   it('should add the movie with the correct data', () => {
-    fillAllFields();
-    cy.getByDataCy('form-submit-button').click();
-    cy.get(`:nth-child(6) > .card-image > .image > img[src="movie-img"]`)
-      .should('exist');
-    cy.get(':nth-child(6) > .card-content > .media > .media-content > .title')
+    form.fillAllFields();
+    cy.getByDataCy('form-submit-button')
+      .click();
+    cy.get('.title')
+      .eq(5)
       .should('have.text', 'movie-title');
-    cy.get(':nth-child(6) > .card-content > .content')
+    cy.get('.content')
+      .eq(5)
       .should('contain', 'movie-description')
     cy.get(`a[href ="movie-url"]`)
       .should('exist');
---
+++ src/components/NewMovie/NewMovie.spec.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import React from 'react';
+import { mount } from '@cypress/react';
+import { NewMovie } from './NewMovie';
+// import { onAdd } from './NewMovie';
+
+describe('Button', () => {
+  const form = {
+    fillAllFields() {
+      return cy.getByDataCy('form-title')
+        .type('movie-title')
+        && cy.getByDataCy('form-description')
+          .type('movie-description')
+        && cy.getByDataCy('form-imgUrl')
+          .type('movie-img')
+        && cy.getByDataCy('form-imdbUrl')
+          .type('movie-url')
+        && cy.getByDataCy('form-imdbId')
+          .type('movie-id');
+    },
+  };
+
+  it('should call the onAdd function', () => {
+    const onAdd = cy.stub();
+
+    mount(<NewMovie onAdd={onAdd} />);
+
+    form.fillAllFields();
+
+    cy.get('button')
+      .click()
+      .then(() => {
+        expect(onAdd).to.be.called;
+      });
+  });
+});
