b20bf0de1c21c512b7dfbd8ead15759595618aed
unknown <avicostov2@gmail.com>
2022-06-10 22:11:23 +0200

    movie add form

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,111 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+// eslint-disable-next-line
+const check = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
 
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
-}
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [imgUrl, setImgUrl] = useState<string>('');
+  const [imdbUrl, setImdbUrl] = useState<string>('');
+  const [imdbId, setImdbId] = useState<string>('');
+
+  const [urlErr, setUrlErr] = useState<boolean>(false);
+
+  const handleChange = (
+    value: string,
+    area: (value: string) => void,
+  ) => {
+    area(value);
+  };
+
+  const resetTitles = (...inputs: Array<(a: string) => void>) => {
+    inputs.forEach(input => {
+      input('');
+    });
+  };
+
+  return (
+    <form onSubmit={(event) => {
+      event.preventDefault();
+
+      if (!check.test(imgUrl)) {
+        resetTitles(setImgUrl);
+        setUrlErr(true);
+
+        return;
+      }
+
+      onAdd({
+        title,
+        description,
+        imgUrl,
+        imdbUrl,
+        imdbId,
+      });
+
+      resetTitles(
+        setTitle,
+        setDescription,
+        setImgUrl,
+        setImdbUrl,
+        setImdbId,
+      );
+    }}
+    >
+      Title
+      <br />
+      <input
+        value={title}
+        type="text"
+        onChange={({ target }) => handleChange(target.value, setTitle)}
+        required
+      />
+      <br />
+      Description
+      <br />
+      <input
+        value={description}
+        type="text"
+        onChange={({ target }) => handleChange(target.value, setDescription)}
+        required
+      />
+      <br />
+      imgUrl
+      <br />
+      <input
+        type="text"
+        value={imgUrl}
+        onChange={({ target }) => {
+          handleChange(target.value, setImgUrl);
+          setUrlErr(false);
+        }}
+      />
+      <br />
+      {urlErr && (<p style={{ color: 'red' }}><b>Wrong URL!!!</b></p>)}
+      imdbUrl
+      <br />
+      <input
+        type="text"
+        value={imdbUrl}
+        onChange={({ target }) => handleChange(target.value, setImdbUrl)}
+      />
+      <br />
+      imdbId
+      <br />
+      <input
+        type="text"
+        value={imdbId}
+        onChange={({ target }) => handleChange(target.value, setImdbId)}
+      />
+      <br />
+      <button type="submit">
+        ADD
+      </button>
+    </form>
+  );
+};
