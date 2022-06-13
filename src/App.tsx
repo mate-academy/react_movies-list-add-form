e9724ad84e8e2882735a7aa374945094f04311b8
Daria Liubymova <dellachaise@gmail.com>
2022-06-13 15:14:25 +0300

    Refactored code, deletet redundand part

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,20 @@ import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
-  const [newMovie, setNewMovie] = useState([...moviesFromServer]);
+  const [movies, setMovie] = useState(moviesFromServer);
 
   const addMovie = (newMovieInfo : Movie) => {
     const newMovieObject = {
       ...newMovieInfo,
     };
 
-    setNewMovie((currentMovies) => [...currentMovies, newMovieObject]);
+    setMovie((currentMovies) => [...currentMovies, newMovieObject]);
   };
 
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={newMovie} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
         <NewMovie addNewMovie={addMovie} />
