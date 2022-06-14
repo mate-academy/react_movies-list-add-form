62ab194bc681218ceaf7df3bbe9a69b803cb66dd
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2022-06-14 18:14:39 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,104 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
+import { Movie } from '../../react-app-env';
 
-type Props = {};
-type State = {};
-
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
-
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
+interface Props {
+  onAdd: (movie: Movie) => void,
 }
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const handleSubmit = () => {
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    onAdd(newMovie);
+
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  return (
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      handleSubmit();
+    }}
+    >
+      <h2 className="title is-3">Add a movie:</h2>
+
+      <input
+        className="input is-primary mb-4"
+        type="text"
+        placeholder="Enter a title"
+        required
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
+      />
+
+      <input
+        className="input is-primary mb-4"
+        type="text"
+        placeholder="Enter a description"
+        required
+        value={description}
+        onChange={(event) => {
+          setDescription(event.target.value);
+        }}
+      />
+
+      <input
+        className="input is-primary mb-4"
+        type="text"
+        placeholder="Enter a imgUrl"
+        required
+        value={imgUrl}
+        onChange={(event) => {
+          setImgUrl(event.target.value);
+        }}
+      />
+
+      <input
+        className="input is-primary mb-4"
+        type="text"
+        placeholder="Enter a imdbUrl"
+        required
+        value={imdbUrl}
+        onChange={(event) => {
+          setImdbUrl(event.target.value);
+        }}
+      />
+
+      <input
+        className="input is-primary mb-4"
+        type="text"
+        placeholder="Enter a imdbId"
+        required
+        value={imdbId}
+        onChange={(event) => {
+          setImdbId(event.target.value);
+        }}
+      />
+
+      <button
+        className="button is-success"
+        type="submit"
+      >
+        Add
+      </button>
+    </form>
+  );
+};
