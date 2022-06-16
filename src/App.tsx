a0d962a901c49626acc4e56c7e081c46e6116d94
Andriy Simyachenko <simyachenko@gmail.com>
2022-06-16 11:53:21 +0300

    commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState(moviesFromServer);
 
-export class App extends React.Component<{}, State> {
-  state: State = {
-    movies: moviesFromServer,
-  };
-
-  addMovie = (/* movie: Movie */) => {
-    // put your code here
-  };
-
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <NewMovie />
-        </div>
-      </div>
+  const addMovie = (FullMovie: Movie) => {
+    setMovies(
+      current => [...current, FullMovie],
     );
-  }
-}
+  };
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
+      </div>
+      <div className="sidebar">
+        <NewMovie addNewMovie={addMovie} />
+      </div>
+    </div>
+  );
+};
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,90 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  addNewMovie: (FullMovie: Movie) => void
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export const NewMovie: React.FC<Props> = ({ addNewMovie }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
 
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
-}
+  const reset = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  return (
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      addNewMovie({
+        title,
+        description,
+        imgUrl,
+        imdbUrl,
+        imdbId,
+      });
+      reset();
+    }}
+    >
+      <input
+        className="input"
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
+      />
+      <input
+        className="textarea"
+        type="text"
+        placeholder="Description"
+        value={description}
+        onChange={(event) => {
+          setDescription(event.target.value);
+        }}
+      />
+      <input
+        className="input"
+        type="text"
+        placeholder="ImgUrl"
+        value={imgUrl}
+        onChange={(event) => {
+          setImgUrl(event.target.value);
+        }}
+      />
+      <input
+        className="input"
+        type="text"
+        placeholder="ImdbUrl"
+        value={imdbUrl}
+        onChange={(event) => {
+          setImdbUrl(event.target.value);
+        }}
+      />
+      <input
+        className="input"
+        type="text"
+        placeholder="ImdbId"
+        value={imdbId}
+        onChange={(event) => {
+          setImdbId(event.target.value);
+        }}
+      />
+
+      <button
+        type="submit"
+        className="button"
+      >
+        Add
+      </button>
+    </form>
+  );
+};
