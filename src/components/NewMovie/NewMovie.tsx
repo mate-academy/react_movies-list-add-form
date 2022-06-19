b80fbc59ac19d62dfbda01e05c7a883e96d45932
Andriy Fesych <andriyhimself@gmail.com>
2022-06-19 16:14:39 +0300

    Solution_0.1

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,49 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
+import './NewMovie.scss';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (arg0: Movie) => void,
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const initialState: any = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+  const [form, setForm] = useState({ ...initialState });
 
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
-}
+  const handleSubmit = () => {
+    onAdd(form);
+    setForm(initialState);
+  };
+
+  return (
+    <>
+      <h1>New Movie Form</h1>
+      {Object.keys(initialState).map(key => (
+        <input
+          className="input"
+          type="text"
+          name={key}
+          placeholder={`Movie ${key}`}
+          value={form[key]}
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          onChange={(e) => setForm((pre: any) => (
+            { ...pre, [key]: e.target.value }))}
+        />
+      ))}
+      <div>
+        <button
+          type="button"
+          onClick={handleSubmit}
+        >
+          Add new Movie
+        </button>
+      </div>
+    </>
+  );
+};
