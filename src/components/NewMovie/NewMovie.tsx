1d9a6f01a6866844a6efb047fba782aa65908d6d
Amit Patil <amitp0156@gmail.com>
2022-06-22 19:28:41 +0530

    initial commit

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,103 @@
-import { Component } from 'react';
+import { FC, useState } from 'react';
 
-type Props = {};
-type State = {};
+type Props = {
+  addMovie: (movie: Movie) => void;
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export const NewMovie: FC<Props> = ({ addMovie }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
 
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
-}
+  const movie = {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (title && description && imgUrl && imdbUrl && imdbId) {
+      addMovie(movie);
+      setTitle('');
+      setDescription('');
+      setImgUrl('');
+      setImdbUrl('');
+      setImdbId('');
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleFormSubmit}
+    >
+      <div>
+        <div>
+          <input
+            type="text"
+            value={title}
+            placeholder="Put Title here"
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <textarea
+            value={description}
+            placeholder="Write Description "
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            value={imgUrl}
+            placeholder="Image url"
+            onChange={(event) => {
+              setImgUrl(event.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            value={imdbUrl}
+            placeholder="Imdb url"
+            onChange={(event) => {
+              setImdbUrl(event.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            value={imdbId}
+            placeholder="Imdb Id"
+            onChange={(event) => {
+              setImdbId(event.target.value);
+            }}
+          />
+        </div>
+
+        <button
+          type="submit"
+        >
+          Submit
+        </button>
+      </div>
+
+    </form>
+  );
+};
