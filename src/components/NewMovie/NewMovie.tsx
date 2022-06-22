502d9db01707ab7d8cc7e069f66877565db0f53b
Amit Patil <amitp0156@gmail.com>
2022-06-22 20:52:53 +0530

    added some stylling

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,4 +1,6 @@
 import { FC, useState } from 'react';
+import cn from 'classnames';
+import './NewMovie.scss';
 
 type Props = {
   addMovie: (movie: Movie) => void;
@@ -11,6 +13,12 @@ export const NewMovie: FC<Props> = ({ addMovie }) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
 
+  const [hasinvalidTitle, setHasinvalidTitle] = useState(false);
+  const [hasinvalidDescription, setHasinvalidDescription] = useState(false);
+  const [hasinvalidImgUrl, setHasinvalidImgUrl] = useState(false);
+  const [hasinvalidImdbUrl, setHasinvalidImdbUrl] = useState(false);
+  const [hasinvalidImdbId, setHasinvalidImdbId] = useState(false);
+
   const movie = {
     title,
     description,
@@ -19,8 +27,32 @@ export const NewMovie: FC<Props> = ({ addMovie }) => {
     imdbId,
   };
 
+  const validateInput = () => {
+    if (!title) {
+      setHasinvalidTitle(true);
+    }
+
+    if (!description) {
+      setHasinvalidDescription(true);
+    }
+
+    if (!imgUrl) {
+      setHasinvalidImgUrl(true);
+    }
+
+    if (!imdbUrl) {
+      setHasinvalidImdbUrl(true);
+    }
+
+    if (!imdbId) {
+      setHasinvalidImdbId(true);
+    }
+  };
+
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    validateInput();
 
     if (title && description && imgUrl && imdbUrl && imdbId) {
       addMovie(movie);
@@ -37,62 +69,111 @@ export const NewMovie: FC<Props> = ({ addMovie }) => {
       onSubmit={handleFormSubmit}
     >
       <div>
-        <div>
-          <input
-            type="text"
-            value={title}
-            placeholder="Put Title here"
-            onChange={(event) => {
-              setTitle(event.target.value);
-            }}
-          />
-        </div>
+        <h1 className="title">
+          ADD MOVIES
+        </h1>
+        <label>
+          Movie Title
+          <div>
+            <input
+              type="text"
+              value={title}
+              placeholder="Put Title here"
+              data-cy="form-title"
+              className={cn(
+                'input',
+                { error: hasinvalidTitle },
+              )}
+              onChange={(event) => {
+                setTitle(event.target.value);
+                setHasinvalidTitle(false);
+              }}
+            />
+          </div>
+        </label>
 
-        <div>
-          <textarea
-            value={description}
-            placeholder="Write Description "
-            onChange={(event) => {
-              setDescription(event.target.value);
-            }}
-          />
-        </div>
+        <label>
+          Description
+          <div>
+            <textarea
+              value={description}
+              data-cy="form-description"
+              placeholder="Write Description "
+              className={cn(
+                'textarea',
+                { error: hasinvalidDescription },
+              )}
+              onChange={(event) => {
+                setDescription(event.target.value);
+                setHasinvalidDescription(false);
+              }}
+            />
+          </div>
+        </label>
 
-        <div>
-          <input
-            type="text"
-            value={imgUrl}
-            placeholder="Image url"
-            onChange={(event) => {
-              setImgUrl(event.target.value);
-            }}
-          />
-        </div>
+        <label>
+          Image url
+          <div>
+            <input
+              type="text"
+              value={imgUrl}
+              placeholder="Image url"
+              data-cy="form-imgUrl"
+              className={cn(
+                'input',
+                { error: hasinvalidImgUrl },
+              )}
+              onChange={(event) => {
+                setImgUrl(event.target.value);
+                setHasinvalidImgUrl(false);
+              }}
+            />
+          </div>
+        </label>
 
-        <div>
-          <input
-            type="text"
-            value={imdbUrl}
-            placeholder="Imdb url"
-            onChange={(event) => {
-              setImdbUrl(event.target.value);
-            }}
-          />
-        </div>
+        <label>
+          Imdb url
+          <div>
+            <input
+              type="text"
+              value={imdbUrl}
+              data-cy="form-imdbUrl"
+              placeholder="Imdb url"
+              className={cn(
+                'input',
+                { error: hasinvalidImdbUrl },
+              )}
+              onChange={(event) => {
+                setImdbUrl(event.target.value);
+                setHasinvalidImdbUrl(false);
+              }}
+            />
+          </div>
+        </label>
 
-        <div>
-          <input
-            type="text"
-            value={imdbId}
-            placeholder="Imdb Id"
-            onChange={(event) => {
-              setImdbId(event.target.value);
-            }}
-          />
-        </div>
+        <label>
+          Imdb Id
+          <div>
+            <input
+              type="text"
+              value={imdbId}
+              placeholder="Imdb Id"
+              data-cy="form-imdbId"
+              className={cn(
+                'input',
+                { error: hasinvalidImdbId },
+              )}
+              onChange={(event) => {
+                setImdbId(event.target.value);
+                setHasinvalidImdbId(false);
+              }}
+            />
+          </div>
+        </label>
 
         <button
           type="submit"
+          className="button is-dark my-5"
         >
           Submit
         </button>
