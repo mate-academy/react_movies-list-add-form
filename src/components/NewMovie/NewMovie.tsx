a16e9686688a5dce8bedc37f57c865437af3a119
PRO-GRAM-MER <ankitbaage3@gmail.com>
2022-06-27 00:12:30 +0530

    Solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,94 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 
-type Props = {};
-type State = {};
-
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
-
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
+interface Props {
+  onAddMovie: (movie: Movie) => void
 }
+
+export const NewMovie: React.FC<Props> = ({ onAddMovie }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const clearForm = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    onAddMovie(newMovie);
+    clearForm();
+  };
+
+  return (
+    <form onSubmit={event => handleSubmit(event)}>
+      <input
+        type="text"
+        placeholder="title"
+        required
+        value={title}
+        data-cy="form-title"
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="description"
+        value={description}
+        required
+        data-cy="form-description"
+        onChange={(event) => {
+          setDescription(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="imgUrl"
+        value={imgUrl}
+        required
+        data-cy="form-imgUrl"
+        onChange={(event) => {
+          setImgUrl(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="imbdUrl"
+        value={imdbUrl}
+        required
+        data-cy="form-imdbUrl"
+        onChange={(event) => {
+          setImdbUrl(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="imbdId"
+        value={imdbId}
+        required
+        data-cy="form-imdbId"
+        onChange={(event) => {
+          setImdbId(event.target.value);
+        }}
+      />
+      <button type="submit" data-cy="form-submit-button">
+        Add film
+      </button>
+    </form>
+  );
+};
