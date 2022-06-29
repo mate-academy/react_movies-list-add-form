a70a8b5e43f72170291709b97e5a98370b6baed3
Lilia Mazur <liliamazur2508@gmail.com>
2022-06-29 11:22:19 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,15 +1,97 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-type Props = {};
-type State = {};
+import './NewMovie.scss';
+
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+type State = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
 
 export class NewMovie extends Component<Props, State> {
-  state: State = {};
+  state: State = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    } as { [Key in keyof State]: State[Key] });
+  };
+
+  hendleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const { onAdd } = this.props;
+
+    const {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = this.state;
+
+    const newMovie: Movie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    onAdd(newMovie);
+
+    this.setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  setInput = (name: string, value: string) => {
+    return (
+      <input
+        type="text"
+        data-cy={`form-${name}`}
+        placeholder={name}
+        required
+        name={name}
+        value={value}
+        id={name}
+        onChange={this.handleChange}
+      />
+    );
+  };
 
   render() {
     return (
-      <form>
-        Put the form here
+      <form
+        className="NewMovie"
+        onSubmit={this.hendleSubmit}
+      >
+        {this.setInput('title', this.state.title)}
+        {this.setInput('description', this.state.description)}
+        {this.setInput('imgUrl', this.state.imgUrl)}
+        {this.setInput('imdbUrl', this.state.imdbUrl)}
+        {this.setInput('imdbId', this.state.imdbId)}
+
+        <button type="submit">
+          Add movie
+        </button>
       </form>
     );
   }
