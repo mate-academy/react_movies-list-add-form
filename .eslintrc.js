05465549de956c1adf8a8126c32b67e3f26d480c
Wojtek <wojciech.cz.bor@gmail.com>
2022-07-04 13:25:56 +0200

    added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  extends: ['plugin:cypress/recommended'],
   rules: {
     'max-len': ['error', {
       ignoreTemplateLiterals: true,
       ignoreComments: true,
     }],
-    'react/no-access-state-in-setstate': 'off',
-    'react/require-default-props': 'off',
-    'react/prefer-stateless-function': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'no-console': 'off',
     'jsx-a11y/label-has-associated-control': ["error", {
       assert: "either",
     }],
