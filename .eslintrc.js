ee9e97638eb1f5def30d87ae1e1021d674557f2c
Wojtek <wojciech.cz.bor@gmail.com>
2022-07-04 13:24:30 +0200

    added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,11 @@ module.exports = {
       ignoreTemplateLiterals: true,
       ignoreComments: true,
     }],
+    'react/no-access-state-in-setstate': 'off',
+    'react/require-default-props': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-console': 'off',
     'jsx-a11y/label-has-associated-control': ["error", {
       assert: "either",
     }],
