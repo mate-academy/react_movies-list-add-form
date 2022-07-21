b5325889d375d7301ba3adb4b64bfa2e6b27cafc
Ostap Salo <jlumon211@gmail.com>
2022-07-21 17:23:17 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState(moviesFromServer);
+  const [moviesError, setMoviesError] = useState(false);
 
-export class App extends React.Component<{}, State> {
-  state: State = {
-    movies: moviesFromServer,
+  const addMovie = (movie: Movie): void => {
+    if (movie.title.length === 0
+      || movie.description.length === 0
+      || movie.imdbId.length === 0
+      || movie.imdbUrl.length === 0
+      || movie.imgUrl.length === 0
+    ) {
+      setMoviesError(true);
+
+      return;
+    }
+
+    setMovies([
+      ...movies,
+      movie,
+    ]);
+
+    setMoviesError(false);
   };
 
-  addMovie = (/* movie: Movie */) => {
-    // put your code here
-  };
-
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <NewMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <NewMovie
+          onAdd={addMovie}
+        />
+        {moviesError && (
+          <h2 className="sidebar__error-movie">
+            All field must be valid
+          </h2>
+        )}
+      </div>
+    </div>
+  );
+};
