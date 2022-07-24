d0e43f91a876e755f9b1ba2b8b54e2e162b7a584
Illia Vodonis <illavodonis@gmail.com>
2022-07-24 18:11:43 +0300

    movies list add form

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -21,15 +21,9 @@ export const TextField: React.FC<Props> = ({
   onChange = () => {},
 
 }) => {
-  // eslint-disable-next-line max-len
-  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
   const [touched, setToched] = useState(false);
-  const hasError = touched
-    && required
-    && (name === 'imgUrl'
-      ? pattern.test(value)
-      : !value);
+  const hasError = touched && required && !value;
 
   return (
     <div className="field">
