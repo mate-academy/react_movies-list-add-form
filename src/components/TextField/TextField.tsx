b83880bdc61f621d601a8b8ef377a91c1b1ac52e
Ivan Grabovsky <ivan.grabovsky.ua@gmail.com>
2022-07-25 07:58:24 +0300

    add solution without validation url

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -22,12 +22,9 @@ export const TextField: React.FC<Props> = ({
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-  // eslint-disable-next-line max-len
-  // const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
 
   // To show errors only if the field was touched (onBlur)
   const [touched, setToched] = useState(false);
-  // const [isValidUrl, setIsValidUrl] = useState(false);
   const hasError = touched && required && !value;
 
   return (
