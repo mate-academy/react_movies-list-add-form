16f8c0b795cad62ffcd98946903af3c4cea36a59
Sasha Skrinnik <sasha.skrinnik@gmail.com>
2022-07-25 13:27:56 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import React from 'react';
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState(moviesFromServer);
 
-export class App extends React.Component<{}, State> {
-  state: State = {
-    movies: moviesFromServer,
+  const addMovie = (newMovie: Movie) => {
+    setMovies((prevMovies) => {
+      return [...prevMovies, newMovie];
+    });
   };
 
-  addMovie = (/* movie: Movie */) => {
-    // put your code here
-  };
-
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <NewMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <NewMovie onAdd={addMovie} />
+      </div>
+    </div>
+  );
+};
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,103 @@
-import { Component } from 'react';
+import { useState } from 'react';
+import { Movie } from '../../types/Movie';
+import { TextField } from '../TextField';
 
-type Props = {};
-type State = {};
+type Props = {
+  onAdd: (movie: Movie) => void
+};
 
-export class NewMovie extends Component<Props, State> {
-  state: State = {};
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  // Increase the count after successful form submission
+  // to reset touched status of all the `Field`s
+  const [count, setCount] = useState(0);
 
-  render() {
-    return (
-      <form>
-        Put the form here
-      </form>
-    );
-  }
-}
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    if (title && imgUrl && imdbUrl && imdbId) {
+      onAdd(newMovie);
+      setCount((currentCount) => (currentCount + 1));
+      setTitle('');
+      setDescription('');
+      setImgUrl('');
+      setImdbUrl('');
+      setImdbId('');
+    }
+  };
+
+  return (
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleSubmit}
+    >
+      <h2 className="title">Add a movie</h2>
+
+      <TextField
+        name="title"
+        label="Title"
+        value={title}
+        onChange={setTitle}
+        required
+      />
+
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={setDescription}
+      />
+
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        onChange={setImgUrl}
+        required
+      />
+
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrl}
+        onChange={setImdbUrl}
+        required
+      />
+
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbId}
+        onChange={setImdbId}
+        required
+      />
+
+      <div className="field is-grouped">
+        <div className="control">
+          <button
+            type="submit"
+            data-cy="submit-button"
+            className="button is-link"
+            disabled={!title || !imgUrl || !imdbUrl || !imdbId}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
