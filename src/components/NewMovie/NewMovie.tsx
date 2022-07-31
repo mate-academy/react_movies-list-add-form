fff73185e83d9722cc3df012127fae2b691b03c9
Dmitrii Pirohov <dmitriy_pirogov@live.com>
2022-07-31 16:46:36 +0300

    first version

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,56 +1,106 @@
-import { useState } from 'react';
+import 'bulma/css/bulma.min.css';
+import { Button } from 'react-bulma-components';
+import React, { FC, useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+export const NewMovie: FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+
+  const [newTitle, setNewTitle] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+  const [newImgUrl, setNewImgUrl] = useState('');
+  const [newImdbUrl, setNewImdbUrl] = useState('');
+  const [newImdbId, setNewImdbId] = useState('');
+
+  const ghost = newTitle && newImgUrl && newImdbUrl && newImdbId;
+
+  const hundleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newMovie = {
+      title: newTitle,
+      description: newDescription,
+      imgUrl: newImgUrl,
+      imdbUrl: newImdbUrl,
+      imdbId: newImdbId,
+    };
+
+    if (ghost) {
+      onAdd(newMovie);
+      setCount((a) => (a + 1));
+      setNewTitle('');
+      setNewDescription('');
+      setNewImgUrl('');
+      setNewImdbUrl('');
+      setNewImdbId('');
+    }
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={(event) => hundleSubmit(event)}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={newTitle}
+        onChange={setNewTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={newDescription}
+        onChange={setNewDescription}
+        required
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={newImgUrl}
+        onChange={setNewImgUrl}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={newImdbUrl}
+        onChange={setNewImdbUrl}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={newImdbId}
+        onChange={setNewImdbId}
+        required
       />
 
       <div className="field is-grouped">
         <div className="control">
-          <button
+          <Button
             type="submit"
             data-cy="submit-button"
-            className="button is-link"
+            className="button is-link is-outlined"
+            disabled={!newTitle || !newImgUrl || !newImdbUrl || !newImdbId}
+            color="red"
+            rounded
           >
             Add
-          </button>
+          </Button>
         </div>
       </div>
     </form>
