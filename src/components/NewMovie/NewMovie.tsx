775f951f8c79493c097b9aeb17ad5ac0eda70e10
DimaShm <shmagindmitry@gmail.com>
2022-08-13 19:46:45 +0300

    Solution_react-movies-list-add-form

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,10 +1,45 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const emptyField = () => {
+    if (title !== '' && imgUrl !== '' && imdbUrl !== '' && imdbId !== '') {
+      return false;
+    }
+
+    return true;
+  };
+
+  const newMovie = () => {
+    return {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+  };
+
+  const clearForm = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+    setCount(prevCount => prevCount + 1);
+  };
 
   return (
     <form className="NewMovie" key={count}>
@@ -13,33 +48,41 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={(newValue) => setTitle(newValue)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={(newValue) => setDescription(newValue)}
+        required={false}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={(newValue) => setImgUrl(newValue)}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={(newValue) => setImdbUrl(newValue)}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={(newValue) => setImdbId(newValue)}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +91,12 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={emptyField()}
+            onClick={(event) => {
+              event.preventDefault();
+              onAdd(newMovie());
+              clearForm();
+            }}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -5,7 +5,7 @@ type Props = {
   name: string,
   value: string,
   label?: string,
-  required?: boolean,
+  required: boolean,
   onChange?: (newValue: string) => void,
 };
 
@@ -17,15 +17,19 @@ export const TextField: React.FC<Props> = ({
   name,
   value,
   label = name,
-  required = false,
+  required,
   onChange = () => {},
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
-  // To show errors only if the field was touched (onBlur)
   const [touched, setToched] = useState(false);
   const hasError = touched && required && !value;
+  let wrongUrl = false;
+  /* eslint-disable */
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
+  if (label === 'Image URL' || label === 'Imdb URL') {
+    wrongUrl = touched && required && !value.match(pattern);
+  }
 
   return (
     <div className="field">
@@ -48,9 +52,9 @@ export const TextField: React.FC<Props> = ({
         />
       </div>
 
-      {hasError && (
-        <p className="help is-danger">{`${label} is required`}</p>
-      )}
+      {hasError
+        ? <p className="help is-danger">{`${label} is required`}</p>
+        : wrongUrl && <p className="help is-danger">{`Wrong ${label}`}</p>}
     </div>
   );
 };
