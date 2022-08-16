3e35625119505d6f62d3f0cf386a2c947d06407e
Olha Stakhova <olha.stakhova@gmail.com>
2022-08-16 20:23:54 +0300

    solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -6,7 +6,8 @@ type Props = {
   value: string,
   label?: string,
   required?: boolean,
-  onChange?: (newValue: string) => void,
+  onChange?: (name: string, value: string) => void,
+  count: number,
 };
 
 function getRandomDigits() {
@@ -19,13 +20,12 @@ export const TextField: React.FC<Props> = ({
   label = name,
   required = false,
   onChange = () => {},
+  count,
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
-  // To show errors only if the field was touched (onBlur)
   const [touched, setToched] = useState(false);
-  const hasError = touched && required && !value;
+  const hasError = touched && required && !value && count !== 0;
 
   return (
     <div className="field">
@@ -43,8 +43,10 @@ export const TextField: React.FC<Props> = ({
           type="text"
           placeholder={`Enter ${label}`}
           value={value}
-          onChange={event => onChange(event.target.value)}
-          onBlur={() => setToched(true)}
+          onChange={event => onChange(name, event.target.value)}
+          onBlur={() => {
+            setToched(true);
+          }}
         />
       </div>
 
