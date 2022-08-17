913d062d2e55f29b5aeb0f8579c50a94e4fa72a1
silchyn <ivan.silchin@gmail.com>
2022-08-17 04:38:03 +0300

    added movies list add form

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -7,10 +7,17 @@ type Props = {
   label?: string,
   required?: boolean,
   onChange?: (newValue: string) => void,
+  pattern?: RegExp,
 };
 
-function getRandomDigits() {
-  return Math.random().toString().slice(2);
+function getNewId(name: string) {
+  const similarIds = Array.from<HTMLInputElement>(document
+    .querySelectorAll('.input')).filter(input => input.id.startsWith(name))
+    .map(input => +input.id.slice(name.length + 1));
+
+  const maxId = similarIds.reduce((acc, curr) => Math.max(acc, curr), -1);
+
+  return maxId + 1;
 }
 
 export const TextField: React.FC<Props> = ({
@@ -19,13 +26,13 @@ export const TextField: React.FC<Props> = ({
   label = name,
   required = false,
   onChange = () => {},
+  pattern,
 }) => {
-  // generage a unique id once on component load
-  const [id] = useState(() => `${name}-${getRandomDigits()}`);
+  const [id] = useState(() => `${name}-${getNewId(name)}`);
 
   // To show errors only if the field was touched (onBlur)
-  const [touched, setToched] = useState(false);
-  const hasError = touched && required && !value;
+  const [touched, setTouched] = useState(false);
+  const error = touched && required && !value;
 
   return (
     <div className="field">
@@ -38,18 +45,22 @@ export const TextField: React.FC<Props> = ({
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': error,
           })}
           type="text"
           placeholder={`Enter ${label}`}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setToched(true)}
+          onBlur={() => setTouched(true)}
         />
       </div>
 
-      {hasError && (
+      {error && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+
+      {!error && touched && pattern && !pattern.test(value) && (
+        <p className="help is-danger">{`${label} does not match expected format`}</p>
       )}
     </div>
   );
