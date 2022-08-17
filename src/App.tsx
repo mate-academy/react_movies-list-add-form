2c2b19882164532ab3f8a69311c18a3f2cf43623
Yura Lysenko <yuralysenko1@gmail.com>
2022-08-17 18:30:37 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
+import { useState } from 'react';
+import { Movie } from './types/Movie';
 import { NewMovie } from './components/NewMovie';
+import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App = () => {
+  const [movie, setMovie] = useState(moviesFromServer);
+
+  const addNewMovie = (newMovie: Movie) => (
+    setMovie((movies) => (
+      [...movies, newMovie]
+    ))
+  );
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movie} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addNewMovie} />
       </div>
     </div>
   );
