fb9b36a7e84f51c62076a13c5a4a4d7d18e659b1
Roman Nikitenko <Lomkaaa@ukr.net>
2022-08-30 15:18:57 +0300

    Task complete

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -6,6 +6,7 @@ type Props = {
   value: string,
   label?: string,
   required?: boolean,
+  urlField?: boolean,
   onChange?: (newValue: string) => void,
 };
 
@@ -18,6 +19,7 @@ export const TextField: React.FC<Props> = ({
   value,
   label = name,
   required = false,
+  urlField = false,
   onChange = () => {},
 }) => {
   // generage a unique id once on component load
@@ -26,6 +28,14 @@ export const TextField: React.FC<Props> = ({
   // To show errors only if the field was touched (onBlur)
   const [touched, setToched] = useState(false);
   const hasError = touched && required && !value;
+  let urlError = false;
+
+  if (urlField && value) {
+    // eslint-disable-next-line max-len
+    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
+    urlError = !value.match(pattern);
+  }
 
   return (
     <div className="field">
@@ -50,6 +60,9 @@ export const TextField: React.FC<Props> = ({
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+      {urlError && (
+        <p className="help is-danger">{`${label} valid URL is required`}</p>
       )}
     </div>
   );
