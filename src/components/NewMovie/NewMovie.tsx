2343debeef3f210838d63594101460ec6f355cb6
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-05 21:05:30 +0300

    Task solution code

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,103 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  onAdd: (newMovie: Movie) => void;
+};
+
+const emptyMovie: Movie = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
+export const NewMovie: React.FC<Props> = (props) => {
+  const { onAdd } = props;
+  const [movie, setMovie] = useState(emptyMovie);
+  const [errors, setErrors] = useState({
+    title: true,
+    description: false,
+    imgUrl: true,
+    imdbUrl: true,
+    imdbId: true,
+  });
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
+  const handleChange = (name: string, value: string): void => {
+    setMovie(prevMovie => ({
+      ...prevMovie,
+      [name]: value,
+    }));
+  };
+
+  const handleErrors = (name: string, value: boolean): void => {
+    setErrors(prevErrors => ({
+      ...prevErrors,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    onAdd(movie);
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      onSubmit={handleSubmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movie.title}
+        onChange={handleChange}
+        onError={handleErrors}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={movie.description}
+        onChange={handleChange}
+        onError={handleErrors}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={movie.imgUrl}
+        onChange={handleChange}
+        onError={handleErrors}
+        pattern={pattern}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={movie.imdbUrl}
+        onChange={handleChange}
+        onError={handleErrors}
+        pattern={pattern}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={movie.imdbId}
+        onChange={handleChange}
+        onError={handleErrors}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +106,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={Object.values(errors).some(error => error)}
           >
             Add
           </button>
