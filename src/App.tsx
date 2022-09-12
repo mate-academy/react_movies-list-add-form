754cb933465ca11f63358d38bc3a35aed45bdf65
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-09-12 23:46:22 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
+import { Movie } from './types/Movie';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => {
+export const App: React.FC = () => {
+  // state = {
+  //   movies: moviesFromServer,
+
+  // };
+
+  const [movies, setMovies] = useState(moviesFromServer);
+
+  const addMovie = (movie: Movie) => {
+    setMovies([...movies, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovie} />
       </div>
     </div>
   );
 };
+
+// function setMovies(arg0: any[]) {
+//   throw new Error('Function not implemented.');
+// }
