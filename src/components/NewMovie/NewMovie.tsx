0a39b558860f6c2a95fc1da04e15fbf28df1f533
Vadls <vadim_tanasienko54541@ukr.net>
2022-09-21 16:57:25 +0300

    Implement movies add form

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,121 @@
-import { useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [validation, setValidation] = useState(true);
+  const isEmpty = !!title && !!imgUrl && !!imdbUrl && !!imdbId;
+
+  const urlRegex = new RegExp(`${
+    /(?:(?:(https?|ftp):)?\/\/)/.source
+  }${/(?:([^:\n\r]+):([^@\n\r]+)@)?/.source
+  }${/(?:(?:www\.)?([^/\n\r]+))/.source
+  }${/(\/[^?\n\r]+)?/.source
+  }${/(\?[^#\n\r]*)?/.source
+  }${/(#?[^\n\r]*)?/.source}`);
+
+  const urlValidation = (value: string) => {
+    if (!urlRegex.test(value)) {
+      setValidation(false);
+    } else {
+      setValidation(true);
+    }
+  };
+
+  const handleAddMovie = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    const movie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    setCount(current => current + 1);
+    onAdd(movie);
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={(event) => handleAddMovie(event)}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={(value) => {
+          setTitle(value);
+        }}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={(value) => {
+          setDescription(value);
+        }}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={(value) => {
+          setImgUrl(value);
+        }}
+        onBlur={(value) => {
+          urlValidation(value);
+        }}
+        validation={validation}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={(value) => {
+          setImdbUrl(value);
+        }}
+        onBlur={(value) => {
+          urlValidation(value);
+        }}
+        validation={validation}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={(value) => {
+          setImdbId(value);
+        }}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +124,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isEmpty === false}
           >
             Add
           </button>
