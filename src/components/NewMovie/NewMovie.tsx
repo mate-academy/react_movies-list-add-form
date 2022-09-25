98267f6a2ecf868feb0e5c6acceeb197f9fce9ba
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-09-25 11:58:37 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,114 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Movie = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
+
+type Props = {
+  onAdd: (arg: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [movie, setMovie] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const [isRequiared] = useState({
+    title: true,
+    description: false,
+    imgUrl: true,
+    imdbUrl: true,
+    imdbId: true,
+  });
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setCount(count + 1);
+
+    onAdd(movie);
+    setMovie({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  const isButtonDisabled = !movie.title || !movie.imdbId
+  || !movie.imdbUrl || !movie.imgUrl;
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={(event) => submitHandler(event)}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
-        required
+        value={movie.title}
+        onChange={(val) => setMovie(prev => ({
+          ...prev,
+          title: val,
+        }))}
+        required={isRequiared.title}
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={movie.description}
+        onChange={(val) => setMovie(prev => ({
+          ...prev,
+          description: val,
+        }))}
+        required={isRequiared.description}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={movie.imgUrl}
+        onChange={(val) => setMovie(prev => ({
+          ...prev,
+          imgUrl: val,
+        }))}
+        required={isRequiared.imgUrl}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={movie.imdbUrl}
+        onChange={(val) => setMovie(prev => ({
+          ...prev,
+          imdbUrl: val,
+        }))}
+        required={isRequiared.imdbUrl}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={movie.imdbId}
+        onChange={(val) => setMovie(prev => ({
+          ...prev,
+          imdbId: val,
+        }))}
+        required={isRequiared.imdbId}
       />
 
       <div className="field is-grouped">
@@ -48,6 +117,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isButtonDisabled}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -20,12 +20,17 @@ export const TextField: React.FC<Props> = ({
   required = false,
   onChange = () => {},
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
-  // To show errors only if the field was touched (onBlur)
   const [touched, setToched] = useState(false);
   const hasError = touched && required && !value;
+
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\w]*))?)$/;
+
+  const [isValidImgUrl, setisValidImgUrl] = useState(false);
+  const currentUrl = name === 'imgUrl' || name === 'imdbUrl';
+  const isImgUrlErrorShown = currentUrl && touched
+  && required && value && !isValidImgUrl;
 
   return (
     <div className="field">
@@ -43,13 +48,23 @@ export const TextField: React.FC<Props> = ({
           type="text"
           placeholder={`Enter ${label}`}
           value={value}
-          onChange={event => onChange(event.target.value)}
-          onBlur={() => setToched(true)}
+          onChange={event => {
+            setisValidImgUrl(pattern.test(event.target.value));
+            onChange(event.target.value);
+          }}
+          onBlur={() => {
+            return setToched(true);
+          }}
+          required={required}
         />
       </div>
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+
+      {isImgUrlErrorShown && (
+        <p className="help is-danger">{`${label} is not valid`}</p>
       )}
     </div>
   );
