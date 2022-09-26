ccc9c95cb29c6f5ba461fe9accd5b087ad839bbf
Anthony Buhaienko <dubsterok@gmail.com>
2022-09-26 20:46:12 +0300

    MovieList + form implemented

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,50 +1,102 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setimgUrl] = useState('');
+  const [imdbUrl, setimdbUrl] = useState('');
+  const [imdbId, setimdbId] = useState('');
+
+  const onBlur = !title || !imgUrl || !imdbUrl || !imdbId;
+
+  const newMovie = {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  };
+
+  const handleCheck = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    onAdd(newMovie);
+    setCount(prev => prev + 1);
+    setTitle('');
+    setDescription('');
+    setimgUrl('');
+    setimdbId('');
+    setimdbUrl('');
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleCheck}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={(event) => {
+          setTitle(event);
+        }}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={(event) => {
+          setDescription(event);
+        }}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        required
+        value={imgUrl}
+        onChange={(event) => {
+          setimgUrl(event);
+        }}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        required
+        value={imdbUrl}
+        onChange={(event) => {
+          setimdbUrl(event);
+        }}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        required
+        value={imdbId}
+        onChange={(event) => {
+          setimdbId(event);
+        }}
       />
 
       <div className="field is-grouped">
         <div className="control">
           <button
+            disabled={onBlur}
             type="submit"
             data-cy="submit-button"
             className="button is-link"
