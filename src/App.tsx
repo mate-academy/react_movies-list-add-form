63c267066280967737d9718c7b9192ddebd6f71a
Synytsyn Denys <yungdena54@gmail.com>
2022-09-26 12:10:22 +0300

    add form for movies list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { FC, useState } from 'react';
 import './App.scss';
+import { Movie } from './types/Movie';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => {
+export const App: FC = () => {
+  const [movies, setMovies] = useState(moviesFromServer);
+
+  const addMovie = (newMovie: Movie) => {
+    setMovies([...movies, newMovie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovie} />
       </div>
     </div>
   );
