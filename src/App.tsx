7c637d95ae550146e1870c79f873fc31129048e4
HusakOleh <husakoleh91@gmail.com>
2022-10-05 14:15:10 +0300

    need fix blur after add

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
 export const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onAdd = (
+    title: string,
+    description: string,
+    imgUrl: string,
+    imdbUrl: string,
+    imdbId: string,
+  ) => {
+    moviesFromServer.push({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+    setCount(count + 1);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+
+        <MoviesList
+          movies={moviesFromServer}
+        />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie
+          onAdd={onAdd}
+        />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,103 @@
-import { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
+import classNames from 'classnames';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+interface Props {
+  onAdd: (
+    title: string,
+    description: string,
+    imgUrl: string,
+    imdbUrl: string,
+    imdbId: string
+  ) => void;
+}
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [titleValue, setTitleValue] = useState('');
+  const [descriptionValue, setDescriptionValue] = useState('');
+  const [imageValue, setImageValue] = useState('');
+  const [imdbUrlValue, setImdbUrlValue] = useState('');
+  const [imdbIdValue, setImdbIdValue] = useState('');
+
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
+  const fillInput
+    = titleValue
+    && imageValue
+    && imdbUrlValue
+    && imdbIdValue;
+
+  const correctUrl
+    = pattern.test(imageValue)
+    && pattern.test(imdbUrlValue);
+
+  const addMovie = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (fillInput && correctUrl) {
+      setTitleValue('');
+      setDescriptionValue('');
+      setImageValue('');
+      setImdbUrlValue('');
+      setImdbIdValue('');
+
+      onAdd(
+        titleValue,
+        descriptionValue,
+        imageValue,
+        imdbUrlValue,
+        imdbIdValue,
+      );
+    }
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      onSubmit={(event) => addMovie(event)}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={titleValue}
+        onChange={setTitleValue}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={descriptionValue}
+        onChange={setDescriptionValue}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imageValue}
+        onChange={setImageValue}
+        correctUrl={correctUrl}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrlValue}
+        onChange={setImdbUrlValue}
+        correctUrl={correctUrl}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbIdValue}
+        onChange={setImdbIdValue}
+        required
       />
 
       <div className="field is-grouped">
@@ -47,7 +105,10 @@ export const NewMovie = () => {
           <button
             type="submit"
             data-cy="submit-button"
-            className="button is-link"
+            className={classNames(
+              'button is-link',
+              { 'button is-link is-light': !fillInput || !correctUrl },
+            )}
           >
             Add
           </button>
