c23d80b1a456faeb31aa22d2de3122d0df202cdd
Andriy Andrieiev <proph7000@gmail.com>
2022-10-23 20:32:17 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
+import { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
-export const App = () => {
-  return (
-    <div className="page">
-      <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
-      </div>
-      <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
-      </div>
-    </div>
-  );
+type State = {
+  movies: Movie[],
 };
+
+export class App extends Component {
+  state: Readonly<State> = {
+    movies: moviesFromServer,
+  };
+
+  addMovie = (
+    title: string,
+    description: string,
+    imgUrl: string,
+    imdbUrl: string,
+    imdbId: string,
+  ) => {
+    this.setState((state: Readonly<State>) => {
+      const newMovie = {
+        title,
+        description,
+        imgUrl,
+        imdbUrl,
+        imdbId,
+      };
+
+      return {
+        movies: [...state.movies, newMovie],
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <div className="page-content">
+          <MoviesList movies={this.state.movies} />
+        </div>
+        <div className="sidebar">
+          <NewMovie onAdd={this.addMovie} />
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,58 +1,118 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { ErrorMessage } from '../ErrorMessage';
+import './NewMovie.scss';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: any;
+};
+
+// eslint-disable-next-line max-len
+const patternUrl = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [isError, setIsError] = useState(false);
+
+  const increase = () => {
+    setCount(current => current + 1);
+  };
+
+  let isAddMovie = true;
+
+  function showErrorMessage(): boolean {
+    setIsError(true);
+    setTimeout(setIsError, 5000, false);
+
+    return false;
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!patternUrl.test(imgUrl) && !patternUrl.test(imdbUrl)) {
+      isAddMovie = showErrorMessage();
+    }
+
+    if (isAddMovie) {
+      increase();
+      setTitle('');
+      setDescription('');
+      setImgUrl('');
+      setImdbUrl('');
+      setImdbId('');
+      onAdd(title, description, imgUrl, imdbUrl, imdbId);
+    }
+  }
 
   return (
-    <form className="NewMovie" key={count}>
-      <h2 className="title">Add a movie</h2>
+    <>
+      <form
+        className="NewMovie"
+        key={count}
+        onSubmit={(event) => handleSubmit(event)}
+      >
+        <h2 className="title">Add a movie</h2>
 
-      <TextField
-        name="title"
-        label="Title"
-        value=""
-        onChange={() => {}}
-        required
-      />
+        <TextField
+          name="title"
+          label="Title"
+          value={title}
+          onChange={(event) => setTitle(event)}
+          required
+        />
 
-      <TextField
-        name="description"
-        label="Description"
-        value=""
-      />
+        <TextField
+          name="description"
+          label="Description"
+          value={description}
+          onChange={(event) => setDescription(event)}
+        />
 
-      <TextField
-        name="imgUrl"
-        label="Image URL"
-        value=""
-      />
+        <TextField
+          name="imgUrl"
+          label="Image URL"
+          value={imgUrl}
+          onChange={(event) => setImgUrl(event)}
+          required
+        />
 
-      <TextField
-        name="imdbUrl"
-        label="Imdb URL"
-        value=""
-      />
+        <TextField
+          name="imdbUrl"
+          label="Imdb URL"
+          value={imdbUrl}
+          onChange={(event) => setImdbUrl(event)}
+          required
+        />
 
-      <TextField
-        name="imdbId"
-        label="Imdb ID"
-        value=""
-      />
+        <TextField
+          name="imdbId"
+          label="Imdb ID"
+          value={imdbId}
+          onChange={(event) => setImdbId(event)}
+          required
+        />
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="submit"
-            data-cy="submit-button"
-            className="button is-link"
-          >
-            Add
-          </button>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="submit"
+              data-cy="submit-button"
+              className="button is-link"
+              disabled={!(title && imgUrl && imdbUrl && imdbId)}
+            >
+              Add
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+
+      {isError && <ErrorMessage />}
+    </>
   );
 };
