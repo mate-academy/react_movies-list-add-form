1d7f6e65c8dc7a2e54c0363b765caffda60a9e80
Yevheniya Deryka <evgeniyaderika@gmail.com>
2022-10-24 23:51:01 +0300

    Component for adding new movie was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
+import { Movie } from './types/Movie';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => {
+export const App: React.FC = () => {
+  const [moviesList, setMoviesList] = useState(moviesFromServer);
+
+  const addMovie = (movie: Movie) => {
+    setMoviesList(movies => [...movies, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovie} />
       </div>
     </div>
   );
