4e30eebd21cbf8cb4ccc177fb14ee3a913ed617e
Dima <ddikey1@gmail.com>
2022-10-24 15:24:46 +0300

    Implemented adding new movie functionality with validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import { FunctionComponent, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
-export const App = () => {
+export const App: FunctionComponent = () => {
+  const [movieList, setMovieList] = useState(moviesFromServer);
+  const onAddMovie = (movie: Movie) => {
+    setMovieList([...movieList, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movieList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={onAddMovie} />
       </div>
     </div>
   );
