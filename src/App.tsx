e170ddb7ccc43c1085078ed88e8f9c304209a5be
Kamil Witek <kamilprogramuje@gmail.com>
2022-11-08 23:08:25 +0100

    all tests passed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [movies, setMovies] = useState(moviesFromServer);
+  const handleAddmovie = ({
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  }: Movie) => {
+    setMovies(state => (
+      [
+        ...state,
+        {
+          title,
+          description,
+          imgUrl,
+          imdbUrl,
+          imdbId,
+        },
+      ]
+    ));
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={handleAddmovie} />
       </div>
     </div>
   );
