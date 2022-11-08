ba3e1cb81714e64f1e4b85b8ce0a76f2a7179065
Serhii Romaniuk <deepspark@gmail.com>
2022-11-08 10:19:19 +0200

    small fix

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -24,9 +24,6 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
   };
 
   const isValidFilled = () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars,max-len
-    // const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
-
     return (
       title
       && imgUrl
