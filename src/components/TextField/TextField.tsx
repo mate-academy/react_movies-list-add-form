3cb361c6bcbba878035f629e6664d6f05fa21699
Sapna Choudhary <sapnachoudhary97491@gmail.com>
2022-11-18 17:59:19 +0100

    Removed missing comma

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -6,7 +6,7 @@ type Props = {
   value: string,
   label?: string,
   required?: boolean,
-  onChange?: (newValue: string) => void
+  onChange?: (newValue: string) => void,
 };
 
 function getRandomDigits() {
@@ -18,7 +18,7 @@ export const TextField: React.FC<Props> = ({
   value,
   label = name,
   required = false,
-  onChange = () => {}
+  onChange = () => {},
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
