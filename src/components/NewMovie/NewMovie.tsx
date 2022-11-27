d55c07a7ea6b546ced9a4689ac25969c5eaf5471
Boris Kerekesha <kerekesha90@ukr.net>
2022-11-27 18:06:41 +0200

    without comments

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -7,8 +7,6 @@ type OnAdd = {
 };
 
 export const NewMovie: React.FC<OnAdd> = ({ onAdd }) => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
   const [count, setCount] = useState(0);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
