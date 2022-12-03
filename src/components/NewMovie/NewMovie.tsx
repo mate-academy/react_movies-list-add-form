385f7c4da1fccd1fed4d158782133a266d79794f
postoronnimV <vadimkrvts03@gmail.com>
2022-12-03 18:58:02 +0200

    added custom url validation

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,9 +6,13 @@ interface Props {
   onAdd: (newMovie: Movie) => void
 }
 
+const isUrlValid = (url: string, regExp: RegExp) => {
+  return regExp.test(url);
+};
+
 export const NewMovie: FC<Props> = ({ onAdd }) => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
 
   const [count, setCount] = useState(0);
 
@@ -18,13 +22,14 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
 
-  const submitApprove = title && imdbId && imdbUrl && imgUrl;
+  const isImgUrlValid = isUrlValid(imgUrl, pattern);
+  const isImdbUrlValid = isUrlValid(imdbUrl, pattern);
+
+  const urlValidation = isImgUrlValid && isImdbUrlValid;
+
+  const submitApprove = title && imdbId && imdbUrl && imgUrl && urlValidation;
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-
-    if (!submitApprove) {
-      return;
-    }
 
     const newMovie: Movie = {
       title,
@@ -73,6 +78,7 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
         label="Image URL"
         value={imgUrl}
         onChange={setImgUrl}
+        isUrlValid={isImgUrlValid}
         required
       />
 
@@ -81,6 +87,7 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
         label="Imdb URL"
         value={imdbUrl}
         onChange={setImdbUrl}
+        isUrlValid={isImdbUrlValid}
         required
       />
 
