9dde4c88a95c7f7e7b4eaa7d030f11593d51d961
p-omogit-e <polinakotyrlo@gmail.com>
2022-12-08 14:19:43 +0200

    edited

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 import { Movie } from './types/Movie';
 
-const movies = [...moviesFromServer];
-
 export const App = () => {
   const [movie, setMovie] = useState(moviesFromServer);
 
   function addNewMovie(newMovie: Movie) {
-    movies.push(newMovie);
-    setMovie(movies);
+    setMovie([
+      ...movie,
+      newMovie,
+    ]);
   }
 
   return (
