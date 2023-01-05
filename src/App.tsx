9593e0250fa466276283637b24157b7e991ee480
Lefteris Loizides <loizides.lef@gmail.com>
2023-01-05 06:09:19 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import './App.scss';
+import { useState, FC } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
-import moviesFromServer from './api/movies.json';
 
-export const App = () => {
+import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
+import './App.scss';
+
+export const App: FC<{}> = () => {
+  const [visibleMovies, setMovies] = useState(moviesFromServer);
+
+  const onAdd = (movie: Movie) => {
+    setMovies(state => [...state, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
+
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={onAdd} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,107 @@
-import { useState } from 'react';
+import { useState, FC } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void
+};
+
+export const NewMovie: FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [hasInvalidUrl, setHasInvalidUrl] = useState(false);
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const newMovie = {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  };
+
+  const isDisabled = [
+    title,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  ].some(field => /^\s*$/.test(field))
+  || hasInvalidUrl;
+
+  const resetForm = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbId('');
+    setImdbUrl('');
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={event => {
+        event.preventDefault();
+        onAdd(newMovie);
+        setCount(state => state + 1);
+        resetForm();
+      }}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
         required
+        onChange={(newValue: string) => (
+          setTitle(newValue)
+        )}
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={(newValue: string) => (
+          setDescription(newValue)
+        )}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        required
+        checkUrl={(check: boolean) => setHasInvalidUrl(check)}
+        onChange={(newValue: string) => {
+          setImgUrl(newValue);
+        }}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        required
+        checkUrl={(check: boolean) => setHasInvalidUrl(check)}
+        onChange={(newValue: string) => {
+          setImdbUrl(newValue);
+        }}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        required
+        onChange={(newValue: string) => (
+          setImdbId(newValue)
+        )}
       />
 
       <div className="field is-grouped">
@@ -48,6 +110,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isDisabled}
           >
             Add
           </button>
