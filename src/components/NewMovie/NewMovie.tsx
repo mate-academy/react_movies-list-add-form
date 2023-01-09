79c683b6169dcffcb8a6c15cddeb1553ddac79a7
Artem Hrechuk <grechuck.artem.ag@gmail.com>
2023-01-09 22:16:29 +0200

    added validation for URLs

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -16,7 +16,15 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
   const [imdbUrl, setImdbUrl] = useState(defaultValue);
   const [imdbId, setImdbId] = useState(defaultValue);
 
-  const isFormFilled = title && imgUrl && imdbUrl && imdbId;
+  const isFormFilled
+     = title.trim() && imgUrl.trim() && imdbUrl.trim() && imdbId.trim();
+
+  const isValidParams = (value: string): boolean => {
+    // eslint-disable-next-line max-len
+    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+    return !!value.match(pattern);
+  };
 
   const increaseCountofMovies = () => {
     setCount(currentCount => currentCount + 1);
@@ -33,6 +41,18 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
   const handleSubmit = () => {
     increaseCountofMovies();
 
+    if (!isValidParams(imgUrl)) {
+      setImgUrl('Invalid URL');
+
+      return;
+    }
+
+    if (!isValidParams(imdbUrl)) {
+      setImdbUrl('Invalid URL');
+
+      return;
+    }
+
     if (isFormFilled) {
       const newMovie: Movie = {
         title,
@@ -43,9 +63,9 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
       };
 
       onAdd(newMovie);
-    }
 
-    clearForm();
+      clearForm();
+    }
   };
 
   return (
