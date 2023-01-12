df9c6d0940e34995e0c93aa6f39caf3146d6c727
Anton Martyniuk <antonmartyniuk@gmail.com>
2023-01-12 21:17:39 +0200

    Solution react-movies-list-add-form

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,58 +1,165 @@
-import { useState } from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+type State = {
+  count: number,
+  titleValue: string,
+  descriptionValue: string,
+  imgUrlValue: string,
+  imdbUrlValue: string,
+  imdbIdValue: string,
+};
+
+export class NewMovie extends React.Component<Props, State> {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  state = {
+    count: 0,
+    titleValue: '',
+    descriptionValue: '',
+    imgUrlValue: '',
+    imdbUrlValue: '',
+    imdbIdValue: '',
+  };
 
-  return (
-    <form className="NewMovie" key={count}>
-      <h2 className="title">Add a movie</h2>
+  chanheTitleValue = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ titleValue: event.target.value });
+  };
 
-      <TextField
-        name="title"
-        label="Title"
-        value=""
-        onChange={() => {}}
-        required
-      />
+  chanheDescriptionValue = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ descriptionValue: event.target.value });
+  };
 
-      <TextField
-        name="description"
-        label="Description"
-        value=""
-      />
+  chanheImgUrlValue = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ imgUrlValue: event.target.value });
+  };
 
-      <TextField
-        name="imgUrl"
-        label="Image URL"
-        value=""
-      />
+  chanheImdbUrlValue = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ imdbUrlValue: event.target.value });
+  };
 
-      <TextField
-        name="imdbUrl"
-        label="Imdb URL"
-        value=""
-      />
+  chanheImdbIdValue = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ imdbIdValue: event.target.value });
+  };
 
-      <TextField
-        name="imdbId"
-        label="Imdb ID"
-        value=""
-      />
+  newMovie = () => {
+    return {
+      title: this.state.titleValue,
+      description: this.state.descriptionValue,
+      imgUrl: this.state.imgUrlValue,
+      imdbUrl: this.state.imdbUrlValue,
+      imdbId: this.state.imdbIdValue,
+    };
+  };
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="submit"
-            data-cy="submit-button"
-            className="button is-link"
-          >
-            Add
-          </button>
+  onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.onAdd(this.newMovie());
+
+    this.setState(state => ({
+      count: state.count + 1,
+      titleValue: '',
+      descriptionValue: '',
+      imgUrlValue: '',
+      imdbUrlValue: '',
+      imdbIdValue: '',
+    }));
+  };
+
+  buttonAvailability = () => {
+    const {
+      titleValue,
+      imgUrlValue,
+      imdbUrlValue,
+      imdbIdValue,
+    } = this.state;
+
+    return (titleValue === ''
+    || imgUrlValue === ''
+    || imdbUrlValue === ''
+    || imdbIdValue === '');
+  };
+
+  render(): React.ReactNode {
+    const {
+      count,
+      titleValue,
+      descriptionValue,
+      imgUrlValue,
+      imdbUrlValue,
+      imdbIdValue,
+    } = this.state;
+
+    return (
+      <form
+        className="NewMovie"
+        key={count}
+        onSubmit={this.onSubmit}
+      >
+        <h2 className="title">Add a movie</h2>
+
+        <TextField
+          name="title"
+          label="Title"
+          value={titleValue}
+          required
+          onChange={this.chanheTitleValue}
+        />
+
+        <TextField
+          name="description"
+          label="Description"
+          value={descriptionValue}
+          onChange={this.chanheDescriptionValue}
+        />
+
+        <TextField
+          name="imgUrl"
+          label="Image URL"
+          value={imgUrlValue}
+          required
+          onChange={this.chanheImgUrlValue}
+        />
+
+        <TextField
+          name="imdbUrl"
+          label="Imdb URL"
+          value={imdbUrlValue}
+          required
+          onChange={this.chanheImdbUrlValue}
+        />
+
+        <TextField
+          name="imdbId"
+          label="Imdb ID"
+          value={imdbIdValue}
+          required
+          onChange={this.chanheImdbIdValue}
+        />
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="submit"
+              data-cy="submit-button"
+              className="button is-link"
+              disabled={(
+                titleValue === ''
+                || imgUrlValue === ''
+                || imdbUrlValue === ''
+                || imdbIdValue === ''
+              )}
+            >
+              Add
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
-  );
-};
+      </form>
+    );
+  }
+}
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,12 +1,12 @@
 import classNames from 'classnames';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 type Props = {
   name: string,
   value: string,
   label?: string,
   required?: boolean,
-  onChange?: (newValue: string) => void,
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void
 };
 
 function getRandomDigits() {
@@ -18,7 +18,7 @@ export const TextField: React.FC<Props> = ({
   value,
   label = name,
   required = false,
-  onChange = () => {},
+  onChange,
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
@@ -43,7 +43,7 @@ export const TextField: React.FC<Props> = ({
           type="text"
           placeholder={`Enter ${label}`}
           value={value}
-          onChange={event => onChange(event.target.value)}
+          onChange={onChange}
           onBlur={() => setToched(true)}
         />
       </div>
