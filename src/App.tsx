54296957a035d007869635359a794a18abbcca53
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-01-16 13:52:45 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [updatedMoviesList, setUpdatedMoviesList] = useState(moviesFromServer);
+
+  const addMovie = (newMovie: Movie) => {
+    setUpdatedMoviesList(movie => [...movie, newMovie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={updatedMoviesList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovie} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,96 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [newTitle, setNewTitle] = useState('');
+  const [newDesc, setNewDesc] = useState('');
+  const [newImgURL, setNewImgURL] = useState('');
+  const [newImdbURL, setNewImdbURL] = useState('');
+  const [newImdbId, setNewImdbId] = useState('');
+
+  const reset = () => {
+    setNewTitle('');
+    setNewDesc('');
+    setNewImgURL('');
+    setNewImdbURL('');
+    setNewImdbId('');
+    setCount(current => current + 1);
+  };
+
+  const handleAdding = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newMovie: Movie = {
+      title: newTitle,
+      description: newDesc,
+      imgUrl: newImgURL,
+      imdbUrl: newImdbURL,
+      imdbId: newImdbId,
+    };
+
+    onAdd(newMovie);
+
+    reset();
+  };
+
+  const isBtnEnabled = newTitle.trim()
+    && newImgURL.trim()
+    && newImdbURL.trim()
+    && newImdbId.trim();
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleAdding}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={newTitle}
+        onChange={(event) => setNewTitle(event)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={newDesc}
+        onChange={(event) => setNewDesc(event)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={newImgURL}
+        onChange={(event) => setNewImgURL(event)}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={newImdbURL}
+        onChange={(event) => setNewImdbURL(event)}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={newImdbId}
+        onChange={setNewImdbId}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +99,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isBtnEnabled}
           >
             Add
           </button>
