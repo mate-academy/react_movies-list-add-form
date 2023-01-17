dd0fe2fd3e2bd445a32dc42e91255f0e08a9f83d
Yana Gonchar <johanna.gonchar@icloud.com>
2023-01-17 08:40:49 +0100

    React - Movies List Add Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import './App.scss';
+import { useState } from 'react';
+
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
+import { Movie } from './types/Movie';
+
 import moviesFromServer from './api/movies.json';
 
 export const App = () => {
+  const [movies, setMovies] = useState(moviesFromServer);
+
+  const addMovieToList = (newMovie: Movie) => {
+    setMovies(previousMovies => [
+      ...previousMovies,
+      newMovie,
+    ]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovieToList} />
       </div>
     </div>
   );
