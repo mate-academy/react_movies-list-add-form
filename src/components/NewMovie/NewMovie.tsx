6c1e27758f817433dab3209c72b0bb9b04ea9a1e
Anastasiia-Khudych <anastasiiakhudych@gmail.com>
2023-02-07 20:51:40 +0200

    change form submit

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -16,7 +16,11 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
 
-  const canSubmit = !!(title && imgUrl && imdbUrl && imdbId);
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  const isValidUrl = pattern.test(imgUrl) && pattern.test(imdbUrl);
+  const canSubmit = !!(title && imgUrl && imdbUrl && imdbId) && isValidUrl;
 
   const createMovieCard = () => {
     const newMovie: Movie = {
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -28,6 +28,7 @@ export const TextField: React.FC<Props> = ({
   const hasError = touched && required && !value;
   // eslint-disable-next-line max-len
   const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
   const nonValidUrl = touched && value && !pattern.test(value)
     && (name === 'imgUrl' || name === 'imdbUrl');
 
