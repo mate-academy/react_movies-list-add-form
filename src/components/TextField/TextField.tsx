b301ab8520fd16db6ea0430f8b60b10400273450
miamarichka <ilchyshyn.marichka@gmail.com>
2023-02-10 22:09:18 +0300

    solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -18,14 +18,19 @@ export const TextField: React.FC<Props> = ({
   value,
   label = name,
   required = false,
-  onChange = () => {},
+  onChange = () => { },
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
   // To show errors only if the field was touched (onBlur)
   const [touched, setToched] = useState(false);
-  const hasError = touched && required && !value;
+  const [textareaValue, setTextareaValue] = useState(value);
+  const hasError = touched && required && !textareaValue;
+
+  const isNeedPattern = name.toLowerCase().includes('url');
+  // eslint-disable-next-line max-len, no-useless-escape
+  const pattern = '/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A - Za - z0 - 9. -] +| (?: www\.| [-;:&=+$, \w]+@)[A - Za - z0 - 9. -] +) ((?: \/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/';
 
   return (
     <div className="field">
@@ -36,14 +41,18 @@ export const TextField: React.FC<Props> = ({
       <div className="control">
         <input
           id={id}
+          pattern={isNeedPattern ? pattern : undefined}
           data-cy={`movie-${name}`}
           className={classNames('input', {
             'is-danger': hasError,
           })}
           type="text"
           placeholder={`Enter ${label}`}
-          value={value}
-          onChange={event => onChange(event.target.value)}
+          value={textareaValue}
+          onChange={event => {
+            setTextareaValue(event.target.value);
+            onChange((event.target.value));
+          }}
           onBlur={() => setToched(true)}
         />
       </div>
