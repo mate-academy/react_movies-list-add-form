40a1315dae35b40368e231850668c937c6cc0197
Dashaverz <dashaverzhanskayaa@gmail.com>
2023-02-12 23:41:54 +0100

    fixed

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -14,6 +14,9 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
 
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)$/;
+
   const clearForm = () => {
     setTitle('');
     setDescription('');
@@ -38,8 +41,16 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
     setCount(prevCount => prevCount + 1);
   };
 
-  const requiredFields = [title, imgUrl, imdbUrl, imdbId];
-  const isWholeFormField = requiredFields.every(field => !!field.trim());
+  const valid = (input: string) => {
+    return pattern.test(input);
+  };
+
+  const isWholeFormField = title === ''
+    || imgUrl === ''
+    || imdbUrl === ''
+    || imdbId === ''
+    || !valid(imdbUrl)
+    || !valid(imdbUrl);
 
   return (
     <form
@@ -69,6 +80,8 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         label="Image URL"
         value={imgUrl}
         onChange={setImgUrl}
+        required
+        callbackValid={valid}
       />
 
       <TextField
@@ -76,6 +89,8 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         label="Imdb URL"
         value={imdbUrl}
         onChange={setImdbUrl}
+        required
+        callbackValid={valid}
       />
 
       <TextField
@@ -83,6 +98,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         label="Imdb ID"
         value={imdbId}
         onChange={setImdbId}
+        required
       />
 
       <div className="field is-grouped">
@@ -91,7 +107,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={!isWholeFormField}
+            disabled={isWholeFormField}
           >
             Add
           </button>
