b85c27173932c65847bc3a476d98d1bcc6c9be28
Oleh Chermak <ochermak2002@gmail.com>
2023-02-15 15:24:31 +0200

    Fixed

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -18,13 +18,6 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
 
   const isFieldsFilled = title && imgUrl && imdbUrl && imdbId;
 
-  const isValidUrl = (url: string) => {
-    // eslint-disable-next-line max-len
-    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
-
-    return pattern.test(url);
-  };
-
   const clearForm = () => {
     setTitle('');
     setDescription('');
@@ -35,12 +28,6 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-
-    if (!isValidUrl(imdbUrl) || !isValidUrl(imgUrl)) {
-      clearForm();
-
-      return;
-    }
 
     const movie = {
       title,
