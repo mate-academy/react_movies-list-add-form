8ff4fcaff7572ceeaca26fe6daa54753d4b728f2
Deniskakaka <krollik1000000@gmail.com>
2023-02-20 14:09:43 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
@@ -8,16 +8,17 @@ import { Movie } from './types/Movie';
 export const App = () => {
   const [listMovie, setListMovie] = useState(moviesFromServer);
 
+  const addMovieInList = useCallback((movie: Movie) => {
+    setListMovie([...listMovie, movie]);
+  }, []);
+
   return (
     <div className="page">
       <div className="page-content">
         <MoviesList movies={listMovie} />
       </div>
       <div className="sidebar">
-        <NewMovie onAdd={(movie: Movie) => {
-          setListMovie([...listMovie, movie]);
-        }}
-        />
+        <NewMovie onAdd={addMovieInList} />
       </div>
     </div>
   );
