666686ae25d087c4b63cc0bf3c533474f04d22bb
Skuratovskyi Vlad <vladskur0369@gmail.com>
2023-03-06 18:33:40 +0200

    Solution v0.1

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,10 +1,72 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
+type AddType = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
+export const NewMovie = ({
+  onAdd,
+}: {
+  onAdd: React.Dispatch<React.SetStateAction<AddType[]>>;
+}) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, countEdit] = useState(0);
+  const [titleName, titleNameEdit] = useState('');
+  const [descriptionName, descriptionNameEdit] = useState('');
+  const [imgUrlName, imgUrlNameEdit] = useState('');
+  const [imdbUrlName, imdbUrlNameEdit] = useState('');
+  const [imdbIdName, imdbIdNameEdit] = useState('');
+
+  const [isDisabledButton, isDisabledButtonEdit] = useState(true);
+
+  useEffect(() => {
+    const isCanSubmit
+      = titleName !== ''
+      && imgUrlName !== ''
+      && imdbUrlName !== ''
+      && imdbIdName !== '';
+
+    if (isCanSubmit) {
+      isDisabledButtonEdit(false);
+    } else {
+      isDisabledButtonEdit(true);
+    }
+  }, [titleName, descriptionName, imgUrlName, imdbUrlName, imdbIdName]);
+
+  const clearForm = () => {
+    titleNameEdit('');
+    descriptionNameEdit('');
+    imgUrlNameEdit('');
+    imdbUrlNameEdit('');
+    imdbIdNameEdit('');
+  };
+
+  const handleSubmit = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+
+    const newFilm = {
+      title: titleName,
+      description: descriptionName,
+      imgUrl: imgUrlName,
+      imdbUrl: imdbUrlName,
+      imdbId: imdbUrlName,
+    };
+
+    countEdit((oldCount) => oldCount + 1);
+
+    onAdd((viewFilm) => [...viewFilm, newFilm]);
+    clearForm();
+
+    return false;
+  };
 
   return (
     <form className="NewMovie" key={count}>
@@ -13,33 +75,40 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={titleName}
+        onChange={titleNameEdit}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={descriptionName}
+        onChange={descriptionNameEdit}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrlName}
+        onChange={imgUrlNameEdit}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrlName}
+        onChange={imdbUrlNameEdit}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbIdName}
+        onChange={imdbIdNameEdit}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +117,8 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            onClick={handleSubmit}
+            disabled={isDisabledButton}
           >
             Add
           </button>
