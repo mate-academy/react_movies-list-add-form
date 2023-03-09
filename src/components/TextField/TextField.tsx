de5894fd60fbf49d54bec1d2974f748a0a1d0b92
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-03-09 19:55:13 +0100

    solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -20,12 +20,15 @@ export const TextField: React.FC<Props> = ({
   required = false,
   onChange = () => {},
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
-  // To show errors only if the field was touched (onBlur)
   const [touched, setToched] = useState(false);
+
+  // eslint-disable-next-line
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
   const hasError = touched && required && !value;
+  const hasValidateError = touched && value && !pattern.test(value);
 
   return (
     <div className="field">
@@ -50,6 +53,10 @@ export const TextField: React.FC<Props> = ({
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+
+      {hasValidateError && (label === 'Image URL' || label === 'Imdb URL') && (
+        <p className="help is-danger">{`${label} is invalid`}</p>
       )}
     </div>
   );
