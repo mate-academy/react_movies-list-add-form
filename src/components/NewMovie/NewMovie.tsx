55212474814a9c020e5a4deb1525eeb4dc0fdf32
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-03-09 14:40:50 +0200

    react add form created

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,136 @@
-import { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void
+};
+
+enum FieldType {
+  TITLE = 'title',
+  DESCRIPTION = 'description',
+  IMAGEURL = 'imgUrl',
+  IMDBURL = 'imdbUrl',
+  IMDBID = 'imdbId',
+}
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  // to reset touched status of all the `FieldType`s
+  const [count, increaseCount] = useState(0);
+  const [buttonDisabled, disableButton] = useState(true);
+
+  const [textFields, changeField] = useState({
+    [FieldType.TITLE]: '',
+    [FieldType.DESCRIPTION]: '',
+    [FieldType.IMAGEURL]: '',
+    [FieldType.IMDBURL]: '',
+    [FieldType.IMDBID]: '',
+  });
+
+  const [validUrls, setUrlValid] = useState({
+    [FieldType.IMAGEURL]: false,
+    [FieldType.IMDBURL]: false,
+  });
+
+  const isDataValid = (): boolean => {
+    const { title, imdbId } = textFields;
+    const { imdbUrl, imgUrl } = validUrls;
+
+    return Boolean(title && imdbId && imdbUrl && imgUrl);
+  };
+
+  const handleUrlValidation = (fieldName: string, status: boolean): void => {
+    setUrlValid({
+      ...validUrls,
+      [fieldName]: status,
+    });
+  };
+
+  const handleTextField = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+
+    changeField({
+      ...textFields,
+      [name]: value,
+    });
+
+    disableButton(!isDataValid());
+  };
+
+  const submitForm = () => {
+    onAdd({
+      title: textFields[FieldType.TITLE],
+      description: textFields[FieldType.DESCRIPTION],
+      imgUrl: textFields[FieldType.IMAGEURL],
+      imdbUrl: textFields[FieldType.IMDBURL],
+      imdbId: textFields[FieldType.IMDBID],
+    });
+
+    changeField({
+      [FieldType.TITLE]: '',
+      [FieldType.DESCRIPTION]: '',
+      [FieldType.IMAGEURL]: '',
+      [FieldType.IMDBURL]: '',
+      [FieldType.IMDBID]: '',
+    });
+
+    increaseCount(prev => prev + 1);
+    disableButton(true);
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={(event) => {
+        event.preventDefault();
+        submitForm();
+      }}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
-        name="title"
+        name={FieldType.TITLE}
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={textFields[FieldType.TITLE]}
+        onChange={handleTextField}
         required
       />
 
       <TextField
-        name="description"
+        name={FieldType.DESCRIPTION}
         label="Description"
-        value=""
+        value={textFields[FieldType.DESCRIPTION]}
+        onChange={handleTextField}
       />
 
       <TextField
-        name="imgUrl"
+        name={FieldType.IMAGEURL}
         label="Image URL"
-        value=""
+        value={textFields[FieldType.IMAGEURL]}
+        isValid={validUrls[FieldType.IMAGEURL]}
+        onChange={handleTextField}
+        onValidation={handleUrlValidation}
+        required
       />
 
       <TextField
-        name="imdbUrl"
+        name={FieldType.IMDBURL}
         label="Imdb URL"
-        value=""
+        value={textFields[FieldType.IMDBURL]}
+        isValid={validUrls[FieldType.IMDBURL]}
+        onChange={handleTextField}
+        onValidation={handleUrlValidation}
+        required
       />
 
       <TextField
-        name="imdbId"
+        name={FieldType.IMDBID}
         label="Imdb ID"
-        value=""
+        value={textFields[FieldType.IMDBID]}
+        onChange={handleTextField}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +139,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={buttonDisabled}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,12 +1,14 @@
 import classNames from 'classnames';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 type Props = {
   name: string,
   value: string,
+  isValid?: boolean,
   label?: string,
   required?: boolean,
-  onChange?: (newValue: string) => void,
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void,
+  onValidation?: (name: string, status: boolean) => void,
 };
 
 function getRandomDigits() {
@@ -16,16 +18,39 @@ function getRandomDigits() {
 export const TextField: React.FC<Props> = ({
   name,
   value,
+  isValid = false,
   label = name,
   required = false,
   onChange = () => {},
+  onValidation = () => {},
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
-  // To show errors only if the field was touched (onBlur)
+  const [isUrl] = useState(() => {
+    if (name.toLowerCase().includes('url')) {
+      return true;
+    }
+
+    return false;
+  });
+
   const [touched, setToched] = useState(false);
+
   const hasError = touched && required && !value;
+  const hasInvalidError = !isValid && touched && isUrl && value;
+
+  // eslint-disable-next-line max-len
+  const patternUrl = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  // eslint-disable-next-line max-len
+  const patternImgUrl = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)\.(jpg|jpeg|png|gif)$/;
+
+  const handleUrlInput = (text: string, fieldName: string): void => {
+    const status = fieldName === 'imdbUrl'
+      ? patternUrl.test(text)
+      : patternImgUrl.test(text);
+
+    onValidation(fieldName, status);
+  };
 
   return (
     <div className="field">
@@ -35,21 +60,42 @@ export const TextField: React.FC<Props> = ({
 
       <div className="control">
         <input
+          name={name}
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError || hasInvalidError,
           })}
           type="text"
           placeholder={`Enter ${label}`}
           value={value}
-          onChange={event => onChange(event.target.value)}
+          onChange={(event) => {
+            onChange(event);
+
+            if (isUrl) {
+              const {
+                name: fieldName,
+                value: fieldValue,
+              } = event.target;
+
+              handleUrlInput(fieldValue, fieldName);
+            }
+          }}
           onBlur={() => setToched(true)}
         />
       </div>
 
       {hasError && (
-        <p className="help is-danger">{`${label} is required`}</p>
+        <p className="help is-danger">
+          {`${label} is required`}
+        </p>
+      )}
+
+      {hasInvalidError && (
+        <p className="help is-danger">
+          Link must start with www. or https://
+          {name === 'imgUrl' && ' and end with .jpeg/.png/etc.'}
+        </p>
       )}
     </div>
   );
