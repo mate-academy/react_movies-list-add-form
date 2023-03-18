5f5b772d6b639e7840350f9ed4d1f5852d5dc640
Vadym Prydatok <vadymprydatok@gmail.com>
2023-03-18 21:37:44 +0200

    movies_list_with_form

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,50 +1,122 @@
-import { useState } from 'react';
+/* eslint-disable max-len */
+import { SyntheticEvent, useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const isDisabled = !title || !imgUrl || !imdbUrl || !imdbId;
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  const urlValidation = (url: string) => {
+    if (pattern.test(url)) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const handleChangeTitle = (e: string) => {
+    setTitle(e);
+  };
+
+  const handleChangeDescription = (e: string) => {
+    setDescription(e);
+  };
+
+  const handleChangeImgUrl = (e: string) => {
+    setImgUrl(e);
+  };
+
+  const handleChangeImdbUrl = (e: string) => {
+    setImdbUrl(e);
+  };
+
+  const handleChangeImdbId = (e: string) => {
+    setImdbId(e);
+  };
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    onAdd(newMovie);
+    setCount(prev => prev + 1);
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleSubmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={handleChangeTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={handleChangeDescription}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={handleChangeImgUrl}
+        urlValidation={urlValidation}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={handleChangeImdbUrl}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={handleChangeImdbId}
+        required
       />
 
       <div className="field is-grouped">
         <div className="control">
           <button
+            disabled={isDisabled}
             type="submit"
             data-cy="submit-button"
             className="button is-link"
