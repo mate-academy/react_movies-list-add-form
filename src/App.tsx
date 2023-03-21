0f5ae9eea2da5bfd503af321bfb1c8d8c22d2998
Dmytro Hubych <dh.hubych@gmail.com>
2023-03-21 16:59:43 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
+import { Movie } from './types/Movie';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
+const callbackCopyMovies = (movies: Movie[]) => {
+  return movies.map((movie: Movie) => ({ ...movie }));
+};
+
 export const App = () => {
+  const [movies, setMovies] = useState(callbackCopyMovies(moviesFromServer));
+
+  const handlerNewMovie = (movie: Movie) => {
+    const copyMovies: Movie[] = callbackCopyMovies(movies);
+
+    copyMovies.push(movie);
+
+    setMovies(copyMovies);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={handlerNewMovie} />
       </div>
     </div>
   );
