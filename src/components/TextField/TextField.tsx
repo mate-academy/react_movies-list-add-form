0e403cc13b15127617168a9470065a083f3886d0
Bogdan <bogdan_mykhailov@icloud.com>
2023-03-30 21:01:50 +0300

    :construction: edit correct validation

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
+import { getValidUrl } from '../../utils/helpers';
+import { pattern } from '../../utils/constants';
 
 type Props = {
   name: string,
@@ -23,7 +25,11 @@ export const TextField: React.FC<Props> = ({
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
   const [touched, setTouched] = useState(false);
+  const correctValue = getValidUrl(pattern, value);
   const hasError = touched && required && !value;
+  const hasValidateError = touched && value && !correctValue && (
+    name === 'imdbUrl' || name === 'imgUrl'
+  );
 
   return (
     <div className="field">
@@ -48,6 +54,10 @@ export const TextField: React.FC<Props> = ({
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+
+      {hasValidateError && (
+        <p className="help is-danger">{`Wrong ${label} address`}</p>
       )}
     </div>
   );
