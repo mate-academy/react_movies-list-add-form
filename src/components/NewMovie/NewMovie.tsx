bb628be22dc12a88862cde9cefc4673a0cbe64d0
Sergey Mironenko <serg1prime@gmail.com>
2023-03-30 22:04:04 +0300

    Movies list add form second commit

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -7,8 +7,6 @@ type Props = {
 };
 
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
   const [count, setCount] = useState(0);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
@@ -19,6 +17,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [validImdbUrl, setValidImdbUrl] = useState(true);
 
   const validator = (url: string) => {
+    // eslint-disable-next-line
     const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-  +=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
     if (!pattern.test(url)) {
