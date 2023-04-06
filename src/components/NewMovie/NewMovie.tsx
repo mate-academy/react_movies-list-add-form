3a1acda3f73b6214280359b2fe56dcdd731fa609
Kateryna Savelieva <ksavelyevaaa@gmail.com>
2023-04-06 13:53:03 +0300

    deployed page

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { checkUrlValidity } from '../../helpers/helperCheck';
 import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
