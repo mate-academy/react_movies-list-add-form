0dfe7cd3b9f3330ce10fcb0fc9bc981a692c0620
Sergey Vorobey <sergey88nik@gmail.com>
2023-04-11 23:42:13 +0300

    Movies list - Add Form was solved

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,114 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie = ({ onAdd }: Props) => {
+  const [count, setCount] = useState<number>(0);
+
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  const isUrlValid = (url: string): boolean => {
+    return pattern.test(url);
+  };
+
+  const [formData, setFormData] = useState<Movie>({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const checkForm = (formData.title
+    && formData.imgUrl
+    && formData.imdbUrl
+    && formData.imdbId
+    && isUrlValid(formData.imgUrl)
+    && isUrlValid(formData.imdbUrl)
+  );
+
+  const resetForm = () => {
+    setFormData({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  const newMovie = {
+    title: formData.title,
+    description: formData.description,
+    imgUrl: formData.imgUrl,
+    imdbUrl: formData.imdbUrl,
+    imdbId: formData.imdbId,
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setCount((current: number) => current + 1);
+    onAdd(newMovie);
+    resetForm();
+  };
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target;
+
+    setFormData((prevState: Movie) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={formData.title}
+        onChange={handleChange}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={formData.description}
+        onChange={handleChange}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={formData.imgUrl}
+        onChange={handleChange}
+        isUrlValid={isUrlValid}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={formData.imdbUrl}
+        onChange={handleChange}
+        isUrlValid={isUrlValid}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={formData.imdbId}
+        onChange={handleChange}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +117,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!checkForm}
           >
             Add
           </button>
