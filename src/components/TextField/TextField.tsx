e9ed4cf609956f61e57fc22a668ab2bf14eda73e
Sergey Kovyazin <mr.roland1989@gmail.com>
2023-04-12 08:15:32 +0300

    Solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -6,26 +6,33 @@ type Props = {
   value: string,
   label?: string,
   required?: boolean,
-  onChange?: (newValue: string) => void,
+  onChange: (newValue: string) => void,
 };
 
 function getRandomDigits() {
   return Math.random().toString().slice(2);
 }
 
+const validation = (url: string): boolean => {
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  return pattern.test(url);
+};
+
 export const TextField: React.FC<Props> = ({
   name,
   value,
   label = name,
   required = false,
-  onChange = () => {},
+  onChange,
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
-  // To show errors only if the field was touched (onBlur)
   const [touched, setToched] = useState(false);
-  const hasError = touched && required && !value;
+  const hasError = touched && required && !value.trim();
+
+  const isValidLabels = ['imgUrl', 'imdbUrl'].includes(name);
+  const isNotValid = isValidLabels && touched && !validation(value.trim());
 
   return (
     <div className="field">
@@ -50,6 +57,10 @@ export const TextField: React.FC<Props> = ({
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+
+      {isNotValid && (
+        <p className="help is-danger">{`Invalid ${label}`}</p>
       )}
     </div>
   );
