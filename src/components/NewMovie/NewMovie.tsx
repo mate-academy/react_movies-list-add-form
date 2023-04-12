ddd73fa3b77648214f924dff25cfc07bac65eca6
Dmytro Zhuk <dave.beetle2311@gmail.com>
2023-04-12 12:23:15 +0300

    Ready

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react';
+import React, { useState } from 'react';
 import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
@@ -7,8 +7,6 @@ type Props = {
 };
 
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
   const [count, setCount] = useState(0);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
@@ -16,26 +14,23 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
 
-  const trimmedTitle = title.trim();
-  const trimmedImgUrl = imgUrl.trim();
-  const trimmedImdbUrl = imdbUrl.trim();
-  const trimmedImdbId = imdbId.trim();
+  const isButtonDisabled =
+    title.trim() !== '' &&
+    imgUrl.trim() !== '' &&
+    imdbUrl.trim() !== '' &&
+    imdbId.trim() !== '';
 
-  const isButtonDisabled = !(trimmedTitle && trimmedImgUrl
-    && trimmedImdbUrl && trimmedImdbId);
-
-  const newMovie = {
-    title: trimmedTitle,
-    description,
-    imgUrl: trimmedImgUrl,
-    imdbUrl: trimmedImdbUrl,
-    imdbId: trimmedImdbId,
-  };
-
-  const handleSubmit = (event: FormEvent) => {
+  const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
-    onAdd(newMovie);
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
     setCount(curCount => curCount + 1);
     setTitle('');
     setDescription('');
@@ -97,7 +92,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={isButtonDisabled}
+            disabled={!isButtonDisabled}
           >
             Add
           </button>
