a777a20732997b708f434d4adf0d1e60dbdc56ec
Roman Kvas <fallenmadara@192.168.1.6>
2023-04-15 09:16:57 +0200

    Error checking

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,130 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (newMovie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const hasMovie = title && imdbUrl && imgUrl && imdbId;
+  const isSubmited = hasMovie === '';
+
+  const reset = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const validation = (url: string) => {
+    // eslint-disable-next-line max-len
+    const pattern = new RegExp(/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/);
+
+    return pattern.test(url);
+  };
+
+  const submitNewMovie = (
+    e: React.BaseSyntheticEvent<Event,
+    EventTarget & HTMLFormElement, EventTarget>,
+  ) => {
+    e.preventDefault();
+
+    // validation(imgUrl);
+    // validation(imdbUrl);
+
+    if (hasMovie && validation(imgUrl) && validation(imdbUrl)) {
+      const newFilm = {
+        title,
+        description,
+        imgUrl,
+        imdbUrl,
+        imdbId,
+      };
+
+      onAdd(newFilm);
+      setCount(prev => prev + 1);
+      reset();
+    }
+  };
+
+  const changeTitle = (str: string) => {
+    setTitle(str.trim());
+  };
+
+  const changeDES = (str: string) => {
+    setDescription(str.trim());
+  };
+
+  const changeImgUrl = (str: string) => {
+    setImgUrl(str.trim());
+  };
+
+  const changeImdbUrl = (str: string) => {
+    setImdbUrl(str.trim());
+  };
+
+  const changeImdbId = (str: string) => {
+    setImdbId(str.trim());
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={submitNewMovie}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={changeTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={changeDES}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={changeImgUrl}
+        required
+        isValidateUrl={() => validation(imgUrl)}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={changeImdbUrl}
+        required
+        isValidateUrl={() => validation(imdbUrl)}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={changeImdbId}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +133,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isSubmited}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -6,6 +6,7 @@ type Props = {
   value: string,
   label?: string,
   required?: boolean,
+  isValidateUrl?: (newValue: string) => boolean | undefined,
   onChange?: (newValue: string) => void,
 };
 
@@ -18,14 +19,37 @@ export const TextField: React.FC<Props> = ({
   value,
   label = name,
   required = false,
+  isValidateUrl = () => {},
   onChange = () => {},
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
-  // To show errors only if the field was touched (onBlur)
+  // To show errors only if the field was touched (onBlur) setErrorValidation
   const [touched, setToched] = useState(false);
+  const [errorValidate, setErrorValidate] = useState(false);
   const hasError = touched && required && !value;
+  const hasValidationError = errorValidate;
+
+  // const validateError = () => {
+  //   setErrorValidate(true);
+  // };
+
+  const validateUrl = () => {
+    if (!isValidateUrl(value)) {
+      setErrorValidate(false);
+    }
+
+    setErrorValidate(true);
+  };
+
+  // const validation = (url: string) => {
+  //   // eslint-disable-next-line max-len
+  //   const pattern = new RegExp(/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/);
+  //   console.log(pattern.test(url));
+
+  //   return pattern.test(url);
+  // };
 
   return (
     <div className="field">
@@ -43,10 +67,15 @@ export const TextField: React.FC<Props> = ({
           type="text"
           placeholder={`Enter ${label}`}
           value={value}
-          onChange={event => onChange(event.target.value)}
-          onBlur={() => setToched(true)}
+          onChange={event => [onChange(event.target.value), validateUrl]}
+          onBlur={() => [setToched(true)]}
+
         />
       </div>
+
+      {hasValidationError && (
+        <p className="help is-danger">Url is not correct</p>
+      )}
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
