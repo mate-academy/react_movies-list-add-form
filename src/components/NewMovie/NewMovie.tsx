a72e12ffb4738246faa64b0866e02a70525b7535
Serhii Nikolin <sergeynikolin@gmail.com>
2023-04-19 11:15:46 +0300

    Code edited

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable no-restricted-syntax */
 import React, { useState } from 'react';
 import { TextField } from '../TextField';
@@ -8,15 +9,12 @@ type Props = {
 };
 
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
   const [count, setCount] = useState(0);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [imgUrl, setImgUrl] = useState('');
   const [imdbUrl, setiImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
-  // const [isButtonDisabled, setButtonDisable] = useState(true);
   const newMovie: Movie = {
     title,
     description,
@@ -24,19 +22,14 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
     imdbUrl,
     imdbId,
   };
-  const newMovieProps = Object.entries(newMovie);
-  const isEmptyValue = !newMovieProps.every(prop => {
-    if (prop[0] === 'description') {
-      return true;
-    }
-
-    return !!prop[1];
-  });
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  const isIncorrectValue = !title
+  || !imdbId
+  || !pattern.test(imgUrl)
+  || !pattern.test(imdbUrl);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    // eslint-disable-next-line max-len
-    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
-
     event.preventDefault();
 
     if (pattern.test(imgUrl) && pattern.test(imdbUrl)) {
@@ -47,9 +40,6 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
       setImgUrl('');
       setiImdbUrl('');
       setImdbId('');
-    } else {
-      setImgUrl('');
-      setiImdbUrl('');
     }
   };
 
@@ -82,6 +72,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         value={imgUrl}
         onChange={setImgUrl}
         required
+        isValid={pattern.test(imgUrl)}
       />
 
       <TextField
@@ -90,6 +81,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         value={imdbUrl}
         onChange={setiImdbUrl}
         required
+        isValid={pattern.test(imdbUrl)}
       />
 
       <TextField
@@ -106,7 +98,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={isEmptyValue}
+            disabled={isIncorrectValue}
           >
             Add
           </button>
