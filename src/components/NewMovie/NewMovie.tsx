1a4e71c297e366e34e7ba87cfbf01f92068b26f9
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-04-24 15:16:36 +0300

    validation for rest inputs was added

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -7,11 +7,21 @@ export const NewMovie = ({ onAdd }: { onAdd: (newMovie: Movie) => void }) => {
   // to reset touched status of all the `Field`s
   const [count, setCount] = useState(0);
 
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [imgUrl, setImgUrl] = useState('');
-  const [imdbUrl, setImdbUrl] = useState('');
-  const [imdbId, setImdbId] = useState('');
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  } = newMovie;
 
   const isValid = (url: string): boolean => {
     const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/; // eslint-disable-line max-len
@@ -19,24 +29,22 @@ export const NewMovie = ({ onAdd }: { onAdd: (newMovie: Movie) => void }) => {
     return pattern.test(url);
   };
 
-  const isValidUrls = isValid(imgUrl) && isValid(imdbUrl);
-
-  const isFormValid = title && isValidUrls && imdbId;
-
-  const newMovie = {
-    title,
-    description,
-    imgUrl,
-    imdbUrl,
-    imdbId,
+  const isValidLength = (param: string) => {
+    return param.length >= 3;
   };
 
+  const isValidUrls = isValid(imgUrl) && isValid(imdbUrl);
+  const isValidImputs = isValidLength(imdbId) && isValidLength(title);
+  const isFormValid = isValidImputs && isValidUrls;
+
   const reset = () => {
-    setTitle('');
-    setDescription('');
-    setImgUrl('');
-    setImdbUrl('');
-    setImdbId('');
+    setNewMovie({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
   };
 
   const submitHandler = (event: FormEvent<HTMLFormElement>) => {
@@ -59,22 +67,23 @@ export const NewMovie = ({ onAdd }: { onAdd: (newMovie: Movie) => void }) => {
         name="title"
         label="Title"
         value={title}
-        onChange={setTitle}
+        onChange={(value) => setNewMovie({ ...newMovie, title: value })}
         required
+        validate={isValidLength}
       />
 
       <TextField
         name="description"
         label="Description"
         value={description}
-        onChange={setDescription}
+        onChange={(value) => setNewMovie({ ...newMovie, description: value })}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
         value={imgUrl}
-        onChange={setImgUrl}
+        onChange={(value) => setNewMovie({ ...newMovie, imgUrl: value })}
         required
         validate={isValid}
       />
@@ -83,7 +92,7 @@ export const NewMovie = ({ onAdd }: { onAdd: (newMovie: Movie) => void }) => {
         name="imdbUrl"
         label="Imdb URL"
         value={imdbUrl}
-        onChange={setImdbUrl}
+        onChange={(value) => setNewMovie({ ...newMovie, imdbUrl: value })}
         required
         validate={isValid}
       />
@@ -92,8 +101,9 @@ export const NewMovie = ({ onAdd }: { onAdd: (newMovie: Movie) => void }) => {
         name="imdbId"
         label="Imdb ID"
         value={imdbId}
-        onChange={setImdbId}
+        onChange={(value) => setNewMovie({ ...newMovie, imdbId: value })}
         required
+        validate={isValidLength}
       />
 
       <div className="field is-grouped">
