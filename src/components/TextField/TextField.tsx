b36d6493e5341e2782c3e6702d81fbcce07c2d97
yana-kotsulym <yanak7170@gmail.com>
2023-04-25 13:57:11 +0200

    Solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -20,12 +20,14 @@ export const TextField: React.FC<Props> = ({
   required = false,
   onChange = () => {},
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
-  // To show errors only if the field was touched (onBlur)
   const [touched, setToched] = useState(false);
   const hasError = touched && required && !value;
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  const adittionalValidation = !pattern.test(value) && value;
+  const urlField = name === 'imgUrl' || name === 'imdbUrl';
 
   return (
     <div className="field">
@@ -51,6 +53,9 @@ export const TextField: React.FC<Props> = ({
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
       )}
+
+      {(urlField && adittionalValidation)
+        && <p className="help is-danger">{`${label} doesn't match the url type`}</p>}
     </div>
   );
 };
