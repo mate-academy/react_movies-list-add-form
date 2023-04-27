6b6c70db6bcc39e0cb0fa179037d59ee159fa603
Rage1502 <karaulchenkoilja@gmail.com>
2023-04-27 21:50:17 +0300

    finish

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,96 @@
-import { useState } from 'react';
+import { FormEventHandler, useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
+// eslint-disable-next-line max-len
+const URL_PATTERN = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+const validateUrl = (url: string) => URL_PATTERN.test(url);
+
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie = ({ onAdd }: Props) => {
+  const [count, setCount] = useState(0);
+  const [movie, setMovie] = useState<Movie>({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const isValidUrls = validateUrl(movie.imgUrl) && validateUrl(movie.imdbUrl);
+
+  const isFormValid = movie.title && isValidUrls && movie.imdbId;
+
+  // const handleInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+  //   const { name, value } = event.target;
+  //
+  //   setMovie((prevMovie) => ({ ...prevMovie, [name]: value }));
+  // };
+
+  const handleInputChange = (key: string, value: string) => {
+    setMovie({
+      ...movie,
+      [key]: value,
+    });
+  };
+
+  const submitHandler: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    onAdd(movie);
+    setMovie({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+    setCount((prevCount) => prevCount + 1);
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={submitHandler}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movie.title}
+        onChange={handleInputChange}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={movie.description}
+        onChange={handleInputChange}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={movie.imgUrl}
+        onChange={handleInputChange}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={movie.imdbUrl}
+        onChange={handleInputChange}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={movie.imdbId}
+        onChange={handleInputChange}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +99,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFormValid}
           >
             Add
           </button>
