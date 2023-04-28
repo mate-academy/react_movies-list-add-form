12d0888c9c161c6a0374872e0d2e7bf7a609518b
Aleksandr <alexnovaklawyer@gmail.com>
2023-04-28 15:54:33 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,119 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+interface Props {
+  onAdd: (movie: Movie) => void;
+}
+
+export const NewMovie: FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [titleValue, setTitleValue] = useState('');
+  const [descritpionValue, setDescritpionValue] = useState('');
+  const [imgUrlValue, setImgUrlValue] = useState('');
+  const [imdbUrlValue, setImdbUrlValue] = useState('');
+  const [imdbIdValue, setImdbIdValueValue] = useState('');
+
+  // eslint-disable-next-line max-len
+  const isFieldFilled = titleValue && imgUrlValue && imdbIdValue && imdbUrlValue;
+
+  const handleSubmit = () => {
+    if (isFieldFilled) {
+      const newMovie: Movie = {
+        title: titleValue,
+        description: descritpionValue,
+        imgUrl: imgUrlValue,
+        imdbUrl: imdbUrlValue,
+        imdbId: imdbIdValue,
+      };
+
+      onAdd(newMovie);
+
+      setTitleValue('');
+      setDescritpionValue('');
+      setImgUrlValue('');
+      setImdbUrlValue('');
+      setImdbIdValueValue('');
+      setCount(count + 1);
+    }
+  };
+
+  const handleChange = (value: string, value1: string) => {
+    switch (value1) {
+      case 'title':
+        setTitleValue(value);
+        break;
+
+      case 'description':
+        setDescritpionValue(value);
+        break;
+
+      case 'imgUrl': {
+        setImgUrlValue(value);
+        break;
+      }
+
+      case 'imdbUrl': {
+        setImdbUrlValue(value);
+        break;
+      }
+
+      case 'imdbId':
+        setImdbIdValueValue(value);
+        break;
+
+      default:
+        break;
+    }
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleSubmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={titleValue}
+        onChange={handleChange}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={descritpionValue}
+        onChange={handleChange}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrlValue}
+        onChange={handleChange}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrlValue}
+        onChange={handleChange}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbIdValue}
+        onChange={handleChange}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +122,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFieldFilled}
           >
             Add
           </button>
