86b98a9163c9c1700235fcab1eb26ea4862fff97
Anastasia Radchenko <radchenckooo@gmail.com>
2023-04-28 11:28:42 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [visibleMovie, setVisibleMovie] = useState([...moviesFromServer]);
+
+  const addNewMovie = (
+    movie: Movie,
+  ) => {
+    const newMovie = {
+      title: movie.title,
+      description: movie.description,
+      imgUrl: movie.imgUrl,
+      imdbUrl: movie.imdbUrl,
+      imdbId: movie.imdbId,
+    };
+
+    setVisibleMovie(prevValue => [...prevValue, newMovie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovie} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addNewMovie} />
       </div>
     </div>
   );
