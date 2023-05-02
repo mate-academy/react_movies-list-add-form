2b4a9603a0a706df57f11bb7f899fdd85706b4a5
Yuliia Anisimova <anisimova.yuliia01@gmail.com>
2023-05-02 08:31:31 +0300

    Fixed URL validation

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -31,7 +31,13 @@ export const NewMovie: React.FC<Props> = ({
   // eslint-disable-next-line max-len
   const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
-  const checkValidUrl = (url: string) => pattern.test(url);
+  const checkValidUrl = (url?: string) => {
+    if (!url) {
+      return false;
+    }
+
+    return pattern.test(url);
+  };
 
   const updateNewMovie = (key: string, value: string) => {
     setNewMovie((currentMovie) => ({
@@ -42,6 +48,10 @@ export const NewMovie: React.FC<Props> = ({
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (!checkValidUrl(imgUrl) || !checkValidUrl(imdbUrl)) {
+      return;
+    }
 
     if (isFormValid) {
       onAdd(newMovie);
