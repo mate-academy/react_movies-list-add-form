a37a240f3a9d367781f6602b2b5052fd734ffec1
Viacheslav Saprykin <saprykinvp@gmail.com>
2023-05-05 19:43:48 +0300

    react_movies-list-add-form ver.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
+import { Movie } from './types/Movie';
+
 import moviesFromServer from './api/movies.json';
 
+import './App.scss';
+
 export const App = () => {
+  const [visibleMovie, setVisibleMovie] = useState([...moviesFromServer]);
+
+  const addMovie = (
+    movie: Movie,
+  ) => {
+    const newMovie = {
+      title: movie.title,
+      description: movie.description,
+      imgUrl: movie.imgUrl,
+      imdbUrl: movie.imdbUrl,
+      imdbId: movie.imdbId,
+    };
+
+    setVisibleMovie(prevValue => [...prevValue, newMovie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovie} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovie} />
       </div>
     </div>
   );
