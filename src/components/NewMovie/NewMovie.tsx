1eb75a99e2a482a6007afd837a2bdfbd07960bfc
Dima <dmitriy.movchan.02@gmail.com>
2023-05-08 21:15:58 +0300

    fixed

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -13,30 +13,49 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [img, setImg] = useState('');
-  const [imdGetUrl, setImdGetUrl] = useState('');
-  const [imdGetId, setImdGetId] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [imdGetUrlValidation, setImdGetUrlValidation] = useState(false);
+  const [imgValidation, setImgValidation] = useState(false);
 
-  const addNewFilm = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const addNewFilm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const newMovie: Movie = {
       title,
       description,
       imgUrl: img,
-      imdbUrl: imdGetUrl,
-      imdbId: imdGetId,
+      imdbUrl,
+      imdbId,
     };
 
     onAdd(newMovie);
     setTitle('');
     setDescription('');
     setImg('');
-    setImdGetUrl('');
-    setImdGetId('');
+    setImdbUrl('');
+    setImdbId('');
     setIncrementCount((prevcount) => prevcount + 1);
   };
 
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  const handleImdGetUrlChange = (value: string) => {
+    setImdbUrl(value);
+    setImdGetUrlValidation(pattern.test(value));
+  };
+
+  const handleImgGetUrlChange = (value: string) => {
+    setImg(value);
+    setImgValidation(pattern.test(value));
+  };
+
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={addNewFilm}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
@@ -58,23 +77,25 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
         name="imgUrl"
         label="Image URL"
         value={img}
-        onChange={setImg}
+        onChange={handleImgGetUrlChange}
+        pattern={pattern.source}
         required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={imdGetUrl}
-        onChange={setImdGetUrl}
+        value={imdbUrl}
+        onChange={handleImdGetUrlChange}
+        pattern={pattern.source}
         required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={imdGetId}
-        onChange={setImdGetId}
+        value={imdbId}
+        onChange={setImdbId}
         required
       />
 
@@ -84,8 +105,14 @@ export const NewMovie: FC<Props> = ({ onAdd }) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={!title || !img || !imdGetUrl || !imdGetId}
-            onClick={addNewFilm}
+            disabled={
+              !title
+              || !img
+              || !imdbUrl
+              || !setImdbId
+              || imdGetUrlValidation
+              || imgValidation
+            }
           >
             Add
           </button>
