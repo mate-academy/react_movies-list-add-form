98c7e36a05de26fd2ba5384736b6331ae46675d9
Leonid <leonidbondarchuk6@gmail.com>
2023-05-10 11:05:18 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,46 +1,53 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movies: Movie[]) => void
+  movies : Movie[]
+};
+
+const defaultMovie = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd, movies }) => {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(true);
+  const [movie, setMovie] = useState<Movie>({ ...defaultMovie });
+  const inputs = ['title', 'description', 'imgUrl', 'imdbUrl', 'imdbId'];
+
+  const addHendler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    onAdd([...movies, movie]);
+    setMovie({ ...defaultMovie });
+    setCount(movies.length);
+  };
+
+  const renderInput = (inputName: string) => {
+    return (
+      <TextField
+        key={inputName}
+        name={inputName}
+        label={inputName}
+        value={movie}
+        required={inputName !== 'description'}
+        setDisabled={setDisabled}
+        onChange={setMovie}
+        count={count}
+      />
+    );
+  };
 
   return (
     <form className="NewMovie" key={count}>
       <h2 className="title">Add a movie</h2>
 
-      <TextField
-        name="title"
-        label="Title"
-        value=""
-        onChange={() => {}}
-        required
-      />
-
-      <TextField
-        name="description"
-        label="Description"
-        value=""
-      />
-
-      <TextField
-        name="imgUrl"
-        label="Image URL"
-        value=""
-      />
-
-      <TextField
-        name="imdbUrl"
-        label="Imdb URL"
-        value=""
-      />
-
-      <TextField
-        name="imdbId"
-        label="Imdb ID"
-        value=""
-      />
+      {inputs.map(input => renderInput(input))}
 
       <div className="field is-grouped">
         <div className="control">
@@ -48,6 +55,8 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={disabled}
+            onClick={(e) => addHendler(e)}
           >
             Add
           </button>
--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,4 +1,5 @@
 export interface Movie {
+  [key : string]: string;
   title: string;
   description: string;
   imgUrl: string;
