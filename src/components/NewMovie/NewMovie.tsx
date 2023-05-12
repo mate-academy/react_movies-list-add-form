c04ca0d91f00cbbe6ba422b7f53d9bc477cbdf98
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-05-12 18:04:54 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,10 +1,71 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+interface PropsNewMovies {
+  onAdd: (newMovie: Movie) => void;
+
+}
+
+export const NewMovie = ({ onAdd }: PropsNewMovies) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [isUrl, setIsUrl] = useState(true);
+
+  const isRequired = title && description && imgUrl && imdbUrl
+   && imdbId && isUrl;
+  const resetValues = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const validURL = (url: string) => {
+    // eslint-disable-next-line max-len
+    const pattern = new RegExp(/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/);
+
+    return !!pattern.test(url);
+  };
+
+  const handleImgURL = (url: string) => {
+    if (!validURL(url)) {
+      setIsUrl(false);
+    }
+
+    setImgUrl(url);
+  };
+
+  const handleImdbUrl = (url: string) => {
+    if (!validURL(url)) {
+      setIsUrl(false);
+    }
+
+    setImdbUrl(url);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    onAdd(newMovie);
+
+    setCount(prevCount => prevCount + 1);
+
+    resetValues();
+  };
 
   return (
     <form className="NewMovie" key={count}>
@@ -13,33 +74,42 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={setTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={setDescription}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={handleImgURL}
+        required
       />
+      {!isUrl && (<p className="help is-danger">Check your URL address</p>)}
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={handleImdbUrl}
+        required
       />
+      {!isUrl && (<p className="help is-danger">Check your URL address</p>)}
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={setImdbId}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +118,8 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isRequired}
+            onClick={handleSubmit}
           >
             Add
           </button>
