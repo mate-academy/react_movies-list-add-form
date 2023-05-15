7e627897c23ea8f0da94783ac320e924f538dad0
Mikołaj <ostermi@wp.pl>
2023-05-15 11:56:14 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [currentMoviesList,
+    setCurrentMoviesList] = useState([...moviesFromServer]);
+
+  const addMovie = (movie: Movie) => {
+    setCurrentMoviesList(previousMoviesList => {
+      return [...previousMoviesList, movie];
+    });
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={currentMoviesList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovie} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,125 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+interface NewMovieProps {
+  onAdd: (movie: Movie) => void,
+}
+
+export const NewMovie = ({ onAdd }: NewMovieProps) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const defaultMovie = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  const [count, setCount] = useState(0);
+  const [movie, setMovie] = useState<Movie>({ ...defaultMovie });
+
+  const {
+    title,
+    description,
+    imgUrl,
+    imdbId,
+    imdbUrl,
+  } = movie;
+
+  const isValid = title.length && imgUrl.length
+      && imdbUrl.length && imdbId.length;
+
+  const submitMovie = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // const isValid = title.length && imgUrl.length
+    //   && imdbUrl.length && imdbId.length;
+
+    if (isValid) {
+      onAdd(movie);
+      setCount(prevCount => prevCount + 1);
+      setMovie({ ...defaultMovie });
+    }
+  };
+
+  const setTitle = (newTitle: string) => {
+    setMovie({
+      ...movie,
+      title: newTitle,
+    });
+  };
+
+  const setDescription = (newDescription: string) => {
+    setMovie({
+      ...movie,
+      description: newDescription,
+    });
+  };
+
+  const setImgUrl = (newImgUrl: string) => {
+    setMovie({
+      ...movie,
+      imgUrl: newImgUrl,
+    });
+  };
+
+  const setImdbUrl = (newImdbUrl: string) => {
+    setMovie({
+      ...movie,
+      imdbUrl: newImdbUrl,
+    });
+  };
+
+  const setImdbId = (newImdbId: string) => {
+    setMovie({
+      ...movie,
+      imdbId: newImdbId,
+    });
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={submitMovie}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={setTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={setDescription}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={setImgUrl}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={setImdbUrl}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={setImdbId}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +128,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isValid}
           >
             Add
           </button>
