0db9e3eb1d4a1a829f85191035dfe939ea9cd1bc
Ilia <koleshnik160799@gmail.com>
2023-05-17 11:05:41 +0300

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => {
+export const App: React.FC = () => {
+  const [allMovie, setNewMovie] = useState(moviesFromServer);
+  const addNewMovie = (
+    title:string,
+    description:string,
+    imgUrl:string,
+    imdbUrl:string,
+    imdbId:string,
+  ) => {
+    const newMovie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    setNewMovie([
+      ...allMovie, newMovie,
+    ]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={allMovie} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie addNewMovie={addNewMovie} />
       </div>
     </div>
   );
