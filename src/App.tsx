6fb193678b12c4942604408a3c2dbba4569f69fa
Nikitin Evgeny <ngenya81@gmail.com>
2023-05-22 18:03:28 +0300

    commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [moives, setMoies] = useState(moviesFromServer);
+  const addMoives = (movie: Movie) => {
+    setMoies((actual) => [...actual, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moives} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMoives} />
       </div>
     </div>
   );
