4ef9ba2f219a6dbc1877ad3d760d6d85d0c8f7e5
Zykhor Andrii <zuxoran@gmail.com>
2023-06-05 17:22:45 +0300

    task done

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,50 +1,109 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [movie, setMovie] = useState<Movie>(
+    {
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    },
+  );
+
+  const clearForm = () => {
+    setMovie(
+      {
+        title: '',
+        description: '',
+        imgUrl: '',
+        imdbUrl: '',
+        imdbId: '',
+      },
+    );
+  };
+
+  const {
+    title, description, imgUrl, imdbUrl, imdbId,
+  } = movie;
+
+  const isDisabled = (
+    title && imgUrl && imdbUrl && imdbId
+  );
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={event => {
+        event.preventDefault();
+        onAdd(movie);
+        setCount((prevCount => prevCount + 1));
+        clearForm();
+      }}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={(value) => {
+          setMovie((prevState) => ({ ...prevState, title: value }));
+        }}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={(value) => {
+          setMovie((prevState) => ({ ...prevState, description: value }));
+        }}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        required
+        onChange={(value) => {
+          setMovie((prevState) => ({ ...prevState, imgUrl: value.trim() }));
+        }}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        required
+        onChange={(value) => {
+          setMovie((prevState) => ({ ...prevState, imdbUrl: value.trim() }));
+        }}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        required
+        onChange={(value) => {
+          setMovie((prevState) => ({ ...prevState, imdbId: value.trim() }));
+        }}
       />
 
       <div className="field is-grouped">
         <div className="control">
           <button
+            disabled={!isDisabled}
             type="submit"
             data-cy="submit-button"
             className="button is-link"
