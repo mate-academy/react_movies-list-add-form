4f67dfb07aa1e9b48658d386d35cf9036abb6a80
Oleksiy Uchaev <corob.dir@gmail.com>
2023-06-07 18:47:55 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,53 +1,103 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (newMovie: Movie) => void,
+};
+
+export const NewMovie = ({ onAdd }: Props) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setmdbUrl] = useState('');
+
+  const [count, setCount] = useState(0);
+  const [errorCount, setErrorCount] = useState(0);
+
+  const newMovie: Movie = {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  };
+
+  const resetForm = () => {
+    setTitle('');
+    setImdbId('');
+    setImgUrl('');
+    setmdbUrl('');
+    setDescription('');
+  };
+
+  const handleSabmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    onAdd(newMovie);
+    setCount(current => (current + 1));
+    setErrorCount(0);
+    resetForm();
+  };
+
+  const isFormValid = title && imdbId && imdbUrl && imgUrl && errorCount === 0;
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleSabmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
-        label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={setTitle}
+        setErrorCount={setErrorCount}
         required
       />
 
       <TextField
         name="description"
-        label="Description"
-        value=""
+        value={description}
+        onChange={setDescription}
+        setErrorCount={setErrorCount}
+        required
       />
 
       <TextField
         name="imgUrl"
-        label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={setImgUrl}
+        setErrorCount={setErrorCount}
+        required
       />
 
       <TextField
         name="imdbUrl"
-        label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={setmdbUrl}
+        setErrorCount={setErrorCount}
+        required
       />
 
       <TextField
         name="imdbId"
-        label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={setImdbId}
+        setErrorCount={setErrorCount}
+        required
       />
 
-      <div className="field is-grouped">
+      <div className="field is-grouped is-justify-content-space-between">
         <div className="control">
           <button
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFormValid}
           >
             Add
           </button>
---
+++ src/utils/pattern.js
@@ -0,0 +1,5 @@
+export const pattern = {
+  // eslint-disable-next-line max-len
+  isLink: /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/,
+  isNotEmpty: /[\S\s]+[\S]+/,
+};
