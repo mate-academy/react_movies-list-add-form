7e2109ea175b0307c763034064e770deba4f7ff8
Veronika Pastushkova <d9169393507@gmai.com>
2023-06-14 21:44:23 +0300

    add task solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,48 +6,45 @@ type Props = {
   onAdd: (newMovie: Movie) => void,
 };
 
+const formBlank = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
 // eslint-disable-next-line max-len
 const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
-const validateUrl = (url: string) => {
-  if (pattern.test(url)) {
-    return url;
-  }
-
-  return 'URL is not valid';
-};
+const validateUrl = (url: string) => pattern.test(url);
 
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [imgUrl, setImgUrl] = useState('');
-  const [imdbUrl, setImdbUrl] = useState('');
-  const [imdbId, setImdbId] = useState('');
+  const [form, setForm] = useState(formBlank);
   const [count, setCount] = useState(0);
 
-  const isDisabled = !title || !imgUrl || !imdbUrl || !imdbId;
+  const {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  } = form;
+
+  const formChangeHandler = (key: string, value: string) => {
+    setForm(state => ({
+      ...state,
+      [key]: value,
+    }));
+  };
+
+  const isDisabled = !title || !imgUrl || !imdbUrl
+    || !imdbId || !validateUrl(imgUrl) || !validateUrl(imdbUrl);
 
   const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    validateUrl(imgUrl);
-    validateUrl(imdbUrl);
-
-    const newMovie = {
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
-    };
-
-    onAdd(newMovie);
-
-    setTitle('');
-    setDescription('');
-    setImgUrl('');
-    setImdbUrl('');
-    setImdbId('');
+    onAdd(form);
     setCount(count + 1);
   };
 
@@ -63,7 +60,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         name="title"
         label="Title"
         value={title}
-        onChange={setTitle}
+        onChange={(value) => formChangeHandler('title', value)}
         required
       />
 
@@ -71,14 +68,15 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         name="description"
         label="Description"
         value={description}
-        onChange={setDescription}
+        onChange={(value) => formChangeHandler('description', value)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
         value={imgUrl}
-        onChange={setImgUrl}
+        onChange={(value) => formChangeHandler('imgUrl', value)}
+        validation={validateUrl}
         required
       />
 
@@ -86,7 +84,8 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         name="imdbUrl"
         label="Imdb URL"
         value={imdbUrl}
-        onChange={setImdbUrl}
+        onChange={(value) => formChangeHandler('imdbUrl', value)}
+        validation={validateUrl}
         required
       />
 
@@ -94,7 +93,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         name="imdbId"
         label="Imdb ID"
         value={imdbId}
-        onChange={setImdbId}
+        onChange={(value) => formChangeHandler('imdbId', value)}
         required
       />
 
