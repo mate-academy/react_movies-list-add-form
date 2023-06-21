7487b00f8892094d855bc14f19f0f9157dbf9fa7
Oleksii Lysenko <nano77@ukr.net>
2023-06-21 16:37:56 +0300

    add functionality to react_movies-list-add-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import './App.scss';
+
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
+
 import moviesFromServer from './api/movies.json';
 
+import { Movie } from './types/Movie';
+
 export const App = () => {
+  const [addedMovies, setAddedMovies] = useState<[] | Movie[]>([]);
+
+  const addMovie = (movie: Movie) => {
+    setAddedMovies(state => [...state, movie]);
+  };
+
+  const visibleMovies = [...moviesFromServer, ...addedMovies];
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovie} />
       </div>
     </div>
   );
