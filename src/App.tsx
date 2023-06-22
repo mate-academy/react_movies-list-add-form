5ac64f69ad780e6a01253238efa28c13b091d78b
Andrii Mashutikov <mashutikov.a.o@gmail.com>
2023-06-22 19:02:12 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [visibleMovies, setVivsibleMovies] = useState(moviesFromServer);
+
+  const addMovieHandler = ({
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  }: Movie) => {
+    const movieToAdd = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    setVivsibleMovies(prev => [...prev, movieToAdd]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovieHandler} />
       </div>
     </div>
   );
