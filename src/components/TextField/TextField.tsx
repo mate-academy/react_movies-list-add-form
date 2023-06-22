f25cd72842333bcadc0fb2a9a9a8aff9a49718bb
Oleksii Lysenko <nano77@ukr.net>
2023-06-22 15:07:55 +0300

    get rid of comments

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -22,10 +22,8 @@ export const TextField: React.FC<Props> = ({
   onChange = () => { },
   validate = () => true,
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
-  // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const [isValid, setIsValid] = useState<null | boolean>(null);
 
