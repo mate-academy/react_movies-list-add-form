e27c66b79953edd880110a95f21f0f10e901aa82
Stepan Haida <stepanhaida@gmail.com>
2023-06-22 14:13:02 +0300

    presolution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,120 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+interface Props {
+  onAdd: React.Dispatch<React.SetStateAction<Movie[]>>
+}
+
+export const NewMovie:FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleChangeTitle = (text: string) => {
+    setTitle(text);
+  };
+
+  const handleChangeImdbUrl = (text: string) => {
+    setImdbUrl(text);
+  };
+
+  const handleChangeImgUrl = (text: string) => {
+    setImgUrl(text);
+  };
+
+  const handleChangeImdbId = (text: string) => {
+    setImdbId(text);
+  };
+
+  const handleChangeDescription = (text: string) => {
+    setDescription(text);
+  };
+
+  const isAllRequiredFilled = () => {
+    const fields = [title, imdbUrl, imgUrl, imdbId];
+
+    return fields.every(field => field);
+  };
+
+  const clearForm = () => {
+    setTitle('');
+    setDescription('');
+    setImdbId('');
+    setImdbUrl('');
+    setImgUrl('');
+  };
+
+  const handleSubmitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    clearForm();
+    setCount((currentCount) => currentCount + 1);
+
+    onAdd((movies) => {
+      const newMovie = {
+        title,
+        imdbUrl,
+        imgUrl,
+        imdbId,
+        description,
+      };
+
+      return [
+        ...movies,
+        newMovie,
+      ];
+    });
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={(e) => handleSubmitForm(e)}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={handleChangeTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={handleChangeDescription}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={handleChangeImgUrl}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={handleChangeImdbUrl}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={handleChangeImdbId}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +123,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isAllRequiredFilled()}
           >
             Add
           </button>
