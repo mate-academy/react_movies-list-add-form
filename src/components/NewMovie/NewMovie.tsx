d6b40fff8e423eb97a24e45c09e5eaa192bf6739
Vania Yarovyi <yarovyi@gmail.com>
2023-06-22 13:55:33 +0300

    rewrote functionality of inputs

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -5,6 +5,13 @@ export const NewMovie = () => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
   const [count] = useState(0);
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieDescription, setMovieDescription] = useState('');
+  const [movieImgUrl, setMovieImgUrl] = useState('');
+  const [movieImdbUrl, setMovieImdbUrl] = useState('');
+  const [movieImdbId, setMovieImdbId] = useState('');
+  const isEveryRequiredDataProvided = movieTitle
+    && movieImdbId && movieImdbUrl && movieImgUrl;
 
   return (
     <form className="NewMovie" key={count}>
@@ -13,33 +20,50 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movieTitle}
+        onChange={(newData) => (
+          setMovieTitle(newData)
+        )}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={movieDescription}
+        onChange={(newData) => (
+          setMovieDescription(newData)
+        )}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={movieImgUrl}
+        onChange={(newData) => (
+          setMovieImgUrl(newData)
+        )}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={movieImdbUrl}
+        onChange={(newData) => (
+          setMovieImdbUrl(newData)
+        )}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={movieImdbId}
+        onChange={(newData) => (
+          setMovieImdbId(newData)
+        )}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +72,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isEveryRequiredDataProvided}
           >
             Add
           </button>
