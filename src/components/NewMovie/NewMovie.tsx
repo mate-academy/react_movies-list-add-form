2ccd4cab2ee4dd68f421ba1e18a4c51302174e1b
Yehor Myronov <2726048@gmail.com>
2023-06-23 17:14:19 +0300

    corrections after review

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -3,39 +3,33 @@ import { TextField } from '../TextField';
 import { Movie } from '../../types/Movie';
 import { isValidHttpUrl } from '../../Helpers';
 
+const initialFormData = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
 type Props = {
   onAdd: (movie: Movie) => void;
 };
 
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [count, setCount] = useState(0);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [imgUrl, setImgUrl] = useState('');
-  const [imdbUrl, setImbdUrl] = useState('');
-  const [imdbId, setImbdId] = useState('');
+  const [formData, setFormData] = useState(initialFormData);
 
-  const isRequired = title
-    && isValidHttpUrl(imgUrl)
-    && isValidHttpUrl(imdbUrl)
-    && imdbId;
+  const isRequired = formData.title.trim()
+    && isValidHttpUrl(formData.imgUrl.trim())
+    && isValidHttpUrl(formData.imdbUrl.trim())
+    && formData.imdbId.trim();
 
   const clearForm = () => {
     setCount(current => current + 1);
-    setTitle('');
-    setDescription('');
-    setImgUrl('');
-    setImbdId('');
-    setImbdUrl('');
+    setFormData(initialFormData);
   };
 
-  const newMovie = {
-    title,
-    description,
-    imgUrl,
-    imdbUrl,
-    imdbId,
-  };
+  const newMovie = formData;
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -43,24 +37,11 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
     clearForm();
   };
 
-  const handleTitle = (value: string) => {
-    setTitle(value);
-  };
-
-  const handleDescription = (value: string) => {
-    setDescription(value);
-  };
-
-  const handleImgUrl = (value: string) => {
-    setImgUrl(value);
-  };
-
-  const handleImbdUrl = (value: string) => {
-    setImbdUrl(value);
-  };
-
-  const handleImbdId = (value: string) => {
-    setImbdId(value);
+  const handleFormData = (key: string, value: string) => {
+    setFormData((currentFormData) => ({
+      ...currentFormData,
+      [key]: value,
+    }));
   };
 
   return (
@@ -74,39 +55,39 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
       <TextField
         name="title"
         label="Title"
-        value={title}
-        onChange={(value) => handleTitle(value)}
+        value={formData.title}
+        onChange={(value) => handleFormData('title', value)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value={description}
-        onChange={(value) => handleDescription(value)}
+        value={formData.description}
+        onChange={(value) => handleFormData('description', value)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={imgUrl}
-        onChange={(value) => handleImgUrl(value)}
+        value={formData.imgUrl}
+        onChange={(value) => handleFormData('imgUrl', value)}
         required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={imdbUrl}
-        onChange={(value) => handleImbdUrl(value)}
+        value={formData.imdbUrl}
+        onChange={(value) => handleFormData('imdbUrl', value)}
         required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={imdbId}
-        onChange={(value) => handleImbdId(value)}
+        value={formData.imdbId}
+        onChange={(value) => handleFormData('imdbId', value)}
         required
       />
 
