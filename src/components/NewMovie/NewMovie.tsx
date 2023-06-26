27656d7f1e919ca19c9aa5bdb20f2702034b3323
Bohdan <6alfabravo9@gmail.com>
2023-06-26 20:20:52 +0300

    fix empty text fields problem and separate url validation logic

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { validateUrl } from '../../lib/helpers/urlValidator';
 import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
@@ -18,8 +19,6 @@ export const NewMovie: React.FC<NewMovieProps> = ({ addMovie }) => {
   const [count, setCount] = useState(0);
   const [formState, setFormState] = useState(initialFormState);
   const [isInvalidUrl, setIsInvalidUrl] = useState(false);
-  // eslint-disable-next-line max-len
-  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
   const {
     title,
@@ -29,12 +28,12 @@ export const NewMovie: React.FC<NewMovieProps> = ({ addMovie }) => {
     imdbId,
   } = formState;
 
-  const isFormFilled = title && imgUrl && imdbUrl && imdbId;
+  const isFormFilled = title.trim() && imgUrl && imdbUrl && imdbId.trim();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (!pattern.test(imdbUrl) || !pattern.test(imgUrl)) {
+    if (!validateUrl(imdbUrl) || !validateUrl(imgUrl)) {
       setIsInvalidUrl(true);
 
       return;
