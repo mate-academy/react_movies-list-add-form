7133918483ea940bb2aa7f60b47aa468e857e7fd
Victor Buldenko <gookidoo@gmail.com>
2023-06-27 13:57:13 +0300

    .

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { TextField } from '../TextField';
 import { Movie } from '../../types/Movie';
 
@@ -18,54 +18,52 @@ export const NewMovie: React.FC<Props> = ({ onAdd, listMovie }) => {
   const [imdbId, setId] = useState('');
   const [isDisable, setIsDisable] = useState(true);
 
-  useEffect(() => {
-    if (title && imgUrl && imdbUrl && imdbId) {
-      setIsDisable(false);
-    } else {
-      setIsDisable(true);
+  const onFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // eslint-disable-next-line max-len
+    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+    if (!pattern.test(imgUrl)) {
+      setImgUrl('');
+
+      return;
     }
+
+    if (!pattern.test(imdbUrl)) {
+      setImdbUrl('');
+
+      return;
+    }
+
+    onAdd(() => ([
+      ...listMovie,
+      {
+        title,
+        description,
+        imgUrl,
+        imdbUrl,
+        imdbId,
+      },
+    ]));
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setId('');
+    setCount(prev => prev + 1);
+    event.currentTarget.reset();
+  };
+
+  useEffect(() => {
+    setIsDisable(Boolean(title && imgUrl && imdbUrl && imdbId));
   }, [title, imgUrl, imdbUrl, imdbId]);
 
   return (
     <form
       className="NewMovie"
       key={count}
-      onSubmit={(e) => {
-        e.preventDefault();
-
-        // eslint-disable-next-line max-len
-        const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
-
-        if (!pattern.test(imgUrl)) {
-          setImgUrl('');
-
-          return;
-        }
-
-        if (!pattern.test(imdbUrl)) {
-          setImdbUrl('');
-
-          return;
-        }
-
-        onAdd(() => ([
-          ...listMovie,
-          {
-            title,
-            description,
-            imgUrl,
-            imdbUrl,
-            imdbId,
-          },
-        ]));
-        setTitle('');
-        setDescription('');
-        setImgUrl('');
-        setImdbUrl('');
-        setId('');
-        setCount(prev => prev + 1);
-        e.currentTarget.reset();
-      }}
+      onSubmit={onFormSubmit}
     >
       <h2 className="title">Add a movie</h2>
       {isDisable && (
