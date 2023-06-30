c4732d982f9cd474e130211025a81d6345b7df8e
Yura Enttsel <equikmla@gmail.com>
2023-06-30 20:29:18 +0300

    Solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,131 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [isValid, setIsValid] = useState({
+    imgUrl: true,
+    imdbUrl: true,
+  });
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+    description: '',
+  });
+
+  const isFieldsFilled = (movieToCheck: Movie): boolean => {
+    return Object.entries(movieToCheck).every(([key, value]) => {
+      return key !== 'description'
+        ? value.trim() !== ''
+        : typeof value === 'string';
+    });
+  };
+
+  const isUrlValid = (url: string): boolean => {
+    // eslint-disable-next-line max-len
+    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+    return pattern.test(url);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const validImdbUrl = isUrlValid(newMovie.imdbUrl);
+    const validImgUrl = isUrlValid(newMovie.imgUrl);
+
+    if (validImdbUrl && validImgUrl) {
+      onAdd(newMovie);
+      setCount(currentCount => currentCount + 1);
+      setNewMovie({
+        title: '',
+        imgUrl: '',
+        imdbUrl: '',
+        imdbId: '',
+        description: '',
+      });
+      setIsValid({
+        imgUrl: true,
+        imdbUrl: true,
+      });
+    } else {
+      setIsValid({
+        imgUrl: validImgUrl,
+        imdbUrl: validImdbUrl,
+      });
+    }
+  };
+
+  const handleChange = (value: string, name: string) => {
+    setNewMovie(currentMovie => ({
+      ...currentMovie,
+      [name]: value,
+    }));
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleSubmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={newMovie.title}
+        onChange={(value, name) => {
+          handleChange(value, name);
+        }}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={newMovie.description}
+        onChange={(value, name) => {
+          handleChange(value, name);
+        }}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={newMovie.imgUrl}
+        onChange={(value, name) => {
+          handleChange(value, name);
+        }}
+        isValid={isValid.imgUrl}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={newMovie.imdbUrl}
+        onChange={(value, name) => {
+          handleChange(value, name);
+        }}
+        isValid={isValid.imgUrl}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={newMovie.imdbId}
+        onChange={(value, name) => {
+          handleChange(value, name);
+        }}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +134,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFieldsFilled(newMovie)}
           >
             Add
           </button>
