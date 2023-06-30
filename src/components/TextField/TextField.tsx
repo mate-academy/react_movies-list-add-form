61de575b19b52c84dbf289fbbb0d10352b5f4b4b
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-06-30 20:51:41 +0300

    Added additional conditions to check correct input

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -16,6 +16,9 @@ function getRandomDigits() {
     .slice(2);
 }
 
+// eslint-disable-next-line max-len
+const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
 export const TextField: React.FC<Props> = ({
   name,
   value,
@@ -26,10 +29,28 @@ export const TextField: React.FC<Props> = ({
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
+  const [fieldErrorMessage, setFieldErrorMessage] = useState('');
 
-  // To show errors only if the field was touched (onBlur)
-  const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const validateTextField = (fieldData: string, isUrl = false): void => {
+    if (isUrl && !fieldData.match(pattern) && fieldData) {
+      setFieldErrorMessage('Please insert correct url');
+
+      return;
+    }
+
+    switch (true) {
+      case !fieldData:
+        setFieldErrorMessage(`${label} is required`);
+        break;
+      case (fieldData.length < 3) && !isUrl:
+        setFieldErrorMessage(`${label} requires min 2 characters`);
+        break;
+      default:
+        setFieldErrorMessage('');
+    }
+  };
+
+  const hasError = required && fieldErrorMessage;
 
   return (
     <div className="field">
@@ -48,12 +69,15 @@ export const TextField: React.FC<Props> = ({
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={(event) => validateTextField(
+            event.target.value,
+            label.toLowerCase().includes('url'),
+          )}
         />
       </div>
 
       {hasError && (
-        <p className="help is-danger">{`${label} is required`}</p>
+        <p className="help is-danger">{fieldErrorMessage}</p>
       )}
     </div>
   );
