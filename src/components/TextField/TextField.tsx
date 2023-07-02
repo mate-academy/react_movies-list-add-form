045e3b2f55a611391b9d3624032da13268eb41e1
Rodionsav <rodionsav@mate.academy>
2023-07-02 14:49:58 +0200

    Solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -22,7 +22,6 @@ export const TextField: React.FC<Props> = ({
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
