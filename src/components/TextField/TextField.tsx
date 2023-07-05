574516a82f314830c32065ca77d9d9027bdf019c
Olena Shevchenko <shevochkas@gmail.com>
2023-07-05 20:28:28 +0300

    fixed linter

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -31,7 +31,7 @@ export const TextField: React.FC<Props> = ({
   const handlerBlur = (event: React.BaseSyntheticEvent) => {
     // eslint-disable-next-line
     const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
-    const { typedValue } = event.target;
+    const typedValue = event.target.value;
 
     if (name === 'imgUrl' || name === 'imdbUrl') {
       if (typedValue.match(pattern)) {
