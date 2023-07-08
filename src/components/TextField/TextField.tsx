8489b853874d7fc66208e9587a77d4d1c4189da8
IlonaBorkovska <borkovska.io@gmail.com>
2023-07-08 21:53:40 +0300

    add task solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -24,12 +24,27 @@ export const TextField: React.FC<Props> = ({
   required = false,
   onChange = () => {},
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
-  // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
+  const [hasUrlError, setHasUrlError] = useState(false);
   const hasError = touched && required && !value;
+  const checkIsValidUrl = (url: string) => {
+    // eslint-disable-next-line
+    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+    return pattern.test(url);
+  };
+
+  const handleFieldBlur = () => {
+    if (name === 'imgUrl' || name === 'imdbUrl') {
+      const isValidUrl = checkIsValidUrl(value);
+
+      setHasUrlError(!isValidUrl);
+    }
+
+    setTouched(true);
+  };
 
   return (
     <div className="field">
@@ -48,12 +63,16 @@ export const TextField: React.FC<Props> = ({
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={handleFieldBlur}
         />
       </div>
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+
+      {hasUrlError && !hasError && (
+        <p className="help is-danger">The URL is invalid</p>
       )}
     </div>
   );
