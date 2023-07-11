ce1a9eccfd85fff49f3a8bc3c52c6505e6945e5b
Vitalii Drabynko <vpdrabynko@gmail.com>
2023-07-11 17:16:11 +0300

    fixed

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -16,7 +16,8 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [imdbId, setImdbId] = useState<string>('');
   const [imdbUrl, setImdbUrl] = useState<string>('');
 
-  const isButtonDisabled = !title || !imgUrl || !imdbId || !imdbUrl;
+  const isButtonDisabled = !title.trim() || !imgUrl.trim()
+    || !imdbId.trim() || !imdbUrl.trim();
 
   // eslint-disable-next-line max-len
   const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -23,7 +23,7 @@ export const TextField: React.FC<Props> = ({
   label = name,
   placeholder = `Enter ${label}`,
   required = false,
-  onChange = () => { },
+  onChange = () => {},
   pattern = undefined,
 }) => {
   // generage a unique id once on component load
