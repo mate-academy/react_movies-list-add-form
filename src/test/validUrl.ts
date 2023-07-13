372d022ae0736ceee8bf8a3c256f0c65386cd355
Danylo Pasko <danil.pasco@gmail.com>
2023-07-13 12:33:08 +0300

    fix2

--- src/test/validUrl.ts
+++ src/test/validUrl.ts
@@ -1,6 +1,16 @@
 export const isValidUrl = (value: string): boolean => {
   // eslint-disable-next-line max-len
-  const pattern = new RegExp(/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/);
+  const pattern = new RegExp(
+    '^('
+      + '((([A-Za-z]{3,9}:(?:\\/\\/)?)'
+      + '(?:[-;:&=+$,\\w]+@)?'
+      + '[A-Za-z0-9.-]+)|'
+      + '(?:www\\.|[-;:&=+$,\\w]+@)'
+      + '[A-Za-z0-9.-]+)'
+      + '((?:\\/[+~%\\/.\\w-_]*)?'
+      + '\\??(?:[-+=&;%@,.\\w_]*)#?(?:[,.!/\\\\\\w]*))?'
+      + ')$',
+  );
 
   return pattern.test(value);
 };
