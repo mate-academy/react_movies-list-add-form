44841b4db66551da55d917c4bd59c9999f535d5d
DefaultGitUser <usingforgit@gmail.com>
2023-07-13 12:39:55 +0300

    Sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [visibleMovies, setVisibleMovies] = useState(moviesFromServer);
+
+  const addNewMovie = (movie: Movie): void => {
+    setVisibleMovies((currentMovies) => [...currentMovies, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addNewMovie} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,10 +1,42 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
+import { urlPattern } from '../../services/validation/urlPattern';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [count] = useState(0);
+  const [title, setTitle] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [description, setDescription] = useState('');
+
+  const isDisabledButton = () => {
+    return !title.trim()
+      || !imgUrl.trim()
+      || !imdbUrl.trim()
+      || !imdbId.trim();
+  };
+
+  const reset = () => {
+    setTitle('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+    setDescription('');
+  };
+
+  const newMovie: Movie = {
+    title,
+    imgUrl,
+    description,
+    imdbUrl,
+    imdbId,
+  };
 
   return (
     <form className="NewMovie" key={count}>
@@ -13,33 +45,42 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={(value) => setTitle(value)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={(value) => setDescription(value)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={(value) => setImgUrl(value)}
+        valid={urlPattern.test(imgUrl)}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        valid={urlPattern.test(imdbUrl)}
+        value={imdbUrl}
+        onChange={(value) => setImdbUrl(value)}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={(value) => setImdbId(value)}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +89,12 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isDisabledButton()}
+            onClick={(e) => {
+              e.preventDefault();
+              onAdd(newMovie);
+              reset();
+            }}
           >
             Add
           </button>
