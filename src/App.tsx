ffa6e0a17a151946b77bf4fd598171d251809a6f
Anatolii Lykhohodenko <anatollykh10@gmail.com>
2023-07-17 20:09:54 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [preparedMovies, setPreparedMovies] = useState(moviesFromServer);
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={preparedMovies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={(movie: Movie) => {
+          setPreparedMovies([...preparedMovies, movie]);
+        }}
+        />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,56 +1,107 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const [titleValue, setTitleValue] = useState('');
+  const [descriptionValue, setDescriptionValue] = useState('');
+  const [imgUrlValue, setImgUrlValue] = useState('');
+  const [imbdUrlValue, setImbdUrlValue] = useState('');
+  const [imbdIdValue, setImbdIdValue] = useState('');
+
+  const increaseCount = () => {
+    setCount((prevCount) => {
+      const counter = prevCount + 1;
+
+      return counter;
+    });
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={event => {
+        event.preventDefault();
+        onAdd({
+          title: titleValue,
+          imgUrl: imgUrlValue,
+          imdbUrl: imbdUrlValue,
+          imdbId: imbdIdValue,
+          description: descriptionValue,
+        });
+
+        increaseCount();
+        setTitleValue('');
+        setImgUrlValue('');
+        setImbdUrlValue('');
+        setImbdIdValue('');
+        setDescriptionValue('');
+      }}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={titleValue}
+        onChange={(value) => setTitleValue(value)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={descriptionValue}
+        onChange={(value) => setDescriptionValue(value)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrlValue}
+        onChange={(value) => setImgUrlValue(value)}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imbdUrlValue}
+        onChange={(value) => setImbdUrlValue(value)}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imbdIdValue}
+        onChange={(value) => setImbdIdValue(value)}
+        required
       />
 
       <div className="field is-grouped">
         <div className="control">
+
           <button
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={
+              !titleValue && !imgUrlValue && !imbdIdValue && !imbdUrlValue
+            }
           >
             Add
           </button>
+
         </div>
       </div>
     </form>
