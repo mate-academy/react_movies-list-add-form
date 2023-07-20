b2d8c90c424174ca2c93f0b59230ced1e967c7a0
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-07-20 05:52:12 +0300

    Add advanced validation

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,6 +6,13 @@ interface Props {
   onAdd: (movie: Movie) => void;
 }
 
+const validateUrl = (url: string) => {
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  return pattern.test(url);
+};
+
 export const NewMovie = ({ onAdd }: Props) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
@@ -73,6 +80,7 @@ export const NewMovie = ({ onAdd }: Props) => {
         value={imgUrl}
         onChange={value => setImgUrl(value)}
         required
+        validate={validateUrl}
       />
 
       <TextField
@@ -81,6 +89,7 @@ export const NewMovie = ({ onAdd }: Props) => {
         value={imdbUrl}
         onChange={value => setImdbUrl(value)}
         required
+        validate={validateUrl}
       />
 
       <TextField
