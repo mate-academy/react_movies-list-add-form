bfcb883f5b94cf2ec59aec0d691dfe15142ba884
Artem Kravets <kravetsartyom@gmail.com>
2023-07-28 20:46:49 +0300

    Solution v2

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,108 +6,99 @@ type Props = {
   onAdd: (movie: Movie) => void;
 };
 
+const initialMovieState = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [count, setCount] = useState(0);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [imgUrl, setImgUrl] = useState('');
-  const [imdbUrl, setImdbUrl] = useState('');
-  const [imdbId, setImdbId] = useState('');
+  const [movie, setMovie] = useState(initialMovieState);
   const [validImg, setValidImg] = useState(true);
   const [validImdb, setValidImdb] = useState(true);
 
-  function handleTextField(setText: (value: string) => void) {
-    const handleText = (value: string) => {
-      setText(value);
-    };
+  const handleTextField = (field: string) => (value: string) => {
+    setMovie((prevMovie) => ({ ...prevMovie, [field]: value }));
+  };
 
-    return handleText;
-  }
-
-  const isDiabled = () => {
-    if (title && imgUrl && imdbUrl && imdbId && validImg === true
-      && validImdb === true) {
-      return false;
-    }
-
-    return true;
+  const isDisabled = () => {
+    return !(movie.title && movie.imgUrl && movie.imdbUrl
+      && movie.imdbId && validImg && validImdb);
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
+    const trimmedTitle = movie.title.trim();
+    const trimmedDescription = movie.description.trim();
+
     onAdd({
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
+      title: trimmedTitle,
+      description: trimmedDescription,
+      imgUrl: movie.imgUrl,
+      imdbUrl: movie.imdbUrl,
+      imdbId: movie.imdbId,
     });
 
-    setTitle('');
-    setDescription('');
-    setImgUrl('');
-    setImdbUrl('');
-    setImdbId('');
+    setMovie(initialMovieState);
     setValidImg(true);
     setValidImdb(true);
-    setCount((countPrev) => countPrev + 1);
+    setCount((prevCount) => prevCount + 1);
   };
 
   return (
-    <form
-      className="NewMovie"
-      key={count}
-      onSubmit={handleSubmit}
-    >
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value={title}
+        value={movie.title}
         setValidImg={setValidImg}
         setValidImdb={setValidImdb}
-        onChange={handleTextField(setTitle)}
+        onChange={handleTextField('title')}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value={description}
+        value={movie.description}
         setValidImg={setValidImg}
         setValidImdb={setValidImdb}
-        onChange={handleTextField(setDescription)}
+        onChange={handleTextField('description')}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={imgUrl}
+        value={movie.imgUrl}
         setValidImg={setValidImg}
         setValidImdb={setValidImdb}
-        onChange={handleTextField(setImgUrl)}
+        onChange={handleTextField('imgUrl')}
         required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={imdbUrl}
+        value={movie.imdbUrl}
         setValidImg={setValidImg}
         setValidImdb={setValidImdb}
-        onChange={handleTextField(setImdbUrl)}
+        onChange={handleTextField('imdbUrl')}
         required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={imdbId}
+        value={movie.imdbId}
         setValidImg={setValidImg}
         setValidImdb={setValidImdb}
-        onChange={handleTextField(setImdbId)}
+        onChange={handleTextField('imdbId')}
         required
       />
 
@@ -117,7 +108,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={isDiabled()}
+            disabled={isDisabled()}
           >
             Add
           </button>
