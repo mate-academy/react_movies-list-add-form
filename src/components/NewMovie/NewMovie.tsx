c6d52ddfe41e04aa20743a2dc55cb30f750b978b
Artem Kravets <kravetsartyom@gmail.com>
2023-07-29 14:59:54 +0300

    Solution v2

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -60,12 +60,17 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
 
   return (
     // eslint-disable-next-line max-len
-    <form className="NewMovie" key={count} onSubmit={event => handleSubmit(event)}>
+    <form className="NewMovie" key={count} onSubmit={(event) => handleSubmit(event)}>
       <h2 className="title">Add a movie</h2>
 
       <TextField name="title" label="Title" value={title} onChange={setTitle} required />
 
-      <TextField name="description" label="Description" value={description} onChange={setDescription} />
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={setDescription}
+      />
 
       <TextField name="imgUrl" label="Image URL" value={imgUrl} onChange={setImgUrl} required />
 
@@ -75,7 +80,12 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
 
       <div className="field is-grouped">
         <div className="control">
-          <button type="submit" data-cy="submit-button" className="button is-link" disabled={isDisabled}>
+          <button
+            type="submit"
+            data-cy="submit-button"
+            className="button is-link"
+            disabled={isDisabled}
+          >
             Add
           </button>
         </div>
