ecd8cbc90d50f9cbe9bade8198514a3009efb286
Volodymyr Alieksieiev <125214087+vovaAlieksieiev@users.noreply.github.com>
2023-08-07 13:05:52 +0200

    add task solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -13,9 +13,11 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [imgUrl, setImgUrl] = useState<string>('');
   const [imdbId, setImdbId] = useState<string>('');
   const [imdbUrl, setImdbUrl] = useState<string>('');
+
   const isButtonDisabled = !title.trim() || !imgUrl.trim()
     || !imdbId.trim() || !imdbUrl.trim();
 
+  // eslint-disable-next-line max-len
   const pattern = /^((([A-Za-z]{3,9}: (?: \/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?: www\.| [-;:&=+$, \w]+@)[A - Za - z0 - 9. -] +) ((?: \/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
   const handleSubmit = (event: React.FormEvent) => {
@@ -50,7 +52,6 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
       className="NewMovie"
       key={count}
       onSubmit={handleSubmit}
-
     >
       <h2 className="title">Add a movie</h2>
 
@@ -72,8 +73,8 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={imgUrl}
         pattern={pattern}
+        value={imgUrl}
         onChange={event => setImgUrl(event)}
         required
       />
@@ -81,8 +82,8 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={imdbUrl}
         pattern={pattern}
+        value={imdbUrl}
         onChange={event => setImdbUrl(event)}
         required
       />
