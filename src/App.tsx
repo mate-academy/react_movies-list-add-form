ea8ae6747c2902df55632a4d541a26ebd2066773
Ilona Romaniuk <ilona09041983@gmail.com>
2023-08-14 14:46:53 +0300

    Add new movie

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [moviesList, setMoviesList] = useState(moviesFromServer);
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={(movie: Movie) => {
+          setMoviesList((currentMoviesList) => ([...currentMoviesList, movie]));
+        }}
+        />
       </div>
     </div>
   );
