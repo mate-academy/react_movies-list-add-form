df952cd1e17e387eeddce14be783ef55ec702c0e
Dmytro Somik <dmytro.somik@mccann.com>
2023-08-21 16:00:44 +0100

    Solved

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -29,7 +29,7 @@ export const TextField: React.FC<Props> = ({
 
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const hasError = touched && required && !value.trim();
 
   return (
     <div className="field">
@@ -39,6 +39,7 @@ export const TextField: React.FC<Props> = ({
 
       <div className="control">
         <input
+          name={name}
           type="text"
           id={id}
           data-cy={`movie-${name}`}
@@ -49,6 +50,7 @@ export const TextField: React.FC<Props> = ({
           value={value}
           onChange={event => onChange(event.target.value)}
           onBlur={() => setTouched(true)}
+          required={required}
         />
       </div>
 
