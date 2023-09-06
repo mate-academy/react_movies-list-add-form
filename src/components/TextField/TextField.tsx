371484655bdf5e686133b74c4b62a5db6d3210a3
Anton Tsurkan <wesses1999@gmail.com>
2023-09-06 11:16:51 +0300

    add task solution: Add Form

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -8,6 +8,7 @@ type Props = {
   placeholder?: string,
   required?: boolean,
   onChange?: (newValue: string) => void,
+  validation?: boolean,
 };
 
 function getRandomDigits() {
@@ -16,6 +17,9 @@ function getRandomDigits() {
     .slice(2);
 }
 
+// eslint-disable-next-line max-len
+const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
 export const TextField: React.FC<Props> = ({
   name,
   value,
@@ -23,13 +27,21 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  validation = false,
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+
+  let hasError: boolean;
+
+  if (validation) {
+    hasError = touched && required && !value && !pattern.test(value);
+  } else {
+    hasError = touched && required && !value;
+  }
 
   return (
     <div className="field">
