7b760922ebd19e7f8d5619279d5699223dd0eb6a
Denys Poltorak <itwfrontend@gmail.com>
2023-09-07 16:02:47 +0300

    feat: make possible to add a new movie

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,130 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type TNewMovie = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: FC<TNewMovie> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [inputFields, setInputFields] = useState({
+    title: '',
+    description: '',
+    imageUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const onFormSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    onAdd({
+      title: inputFields.title,
+      description: inputFields.description,
+      imgUrl: inputFields.imageUrl,
+      imdbUrl: inputFields.imdbUrl,
+      imdbId: inputFields.imdbId,
+    });
+
+    setInputFields({
+      title: '',
+      description: '',
+      imageUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+
+    setCount(prev => prev + 1);
+  };
+
+  const onTitleHandler = (currentValue: string) => {
+    setInputFields((prevState) => ({
+      ...prevState,
+      title: currentValue,
+    }));
+  };
+
+  const onDescriptionHandler = (currentValue: string) => {
+    setInputFields((prevState) => ({
+      ...prevState,
+      description: currentValue,
+    }));
+  };
+
+  const onImageUrlHandler = (currentValue: string) => {
+    setInputFields((prevState) => ({
+      ...prevState,
+      imageUrl: currentValue,
+    }));
+  };
+
+  const onImdbUrlHandler = (currentValue: string) => {
+    setInputFields((prevState) => ({
+      ...prevState,
+      imdbUrl: currentValue,
+    }));
+  };
+
+  const onImdbIdHandler = (currentValue: string) => {
+    setInputFields((prevState) => ({
+      ...prevState,
+      imdbId: currentValue,
+    }));
+  };
+
+  const isAllFieldValid = Object
+    .entries(inputFields)
+    .filter(([key]) => key !== 'description')
+    .every(([, val]) => val);
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={onFormSubmitHandler}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={inputFields.title}
+        onChange={onTitleHandler}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={inputFields.description}
+        onChange={onDescriptionHandler}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={inputFields.imageUrl}
+        onChange={onImageUrlHandler}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={inputFields.imdbUrl}
+        onChange={onImdbUrlHandler}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={inputFields.imdbId}
+        onChange={onImdbIdHandler}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +133,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isAllFieldValid}
           >
             Add
           </button>
