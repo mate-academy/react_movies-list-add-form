7a694797126fc4530acaeccaf556cf21bb0186a2
Viktoriia Roik <viktoriaroik2103@gmail.com>
2023-09-07 15:26:50 +0300

    made form logic for adding NewMovie in MoviesList

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [moviesList, setMoviesList] = useState<Movie[]>(moviesFromServer);
+
+  const handleAddMovie = (movie: Movie) => {
+    setMoviesList(
+      (prevMoviesList) => [...prevMoviesList, movie],
+    );
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={handleAddMovie} />
       </div>
     </div>
   );
