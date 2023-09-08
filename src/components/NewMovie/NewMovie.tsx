14b2991dd753c2fb61bbe069412fd079edadebeb
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-09-08 10:49:01 +0300

    add task solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,10 +1,36 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { INewMovieProps } from './types';
+import { Movie } from '../../types/Movie';
+import { getAddMovieFormIsValid } from '../../validation/addMovie.validate';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+const initialFormValue: Movie = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
+export const NewMovie: React.FC<INewMovieProps> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [formValues, setFormValues] = useState(initialFormValue);
+  const isButtonDisabled = !getAddMovieFormIsValid(formValues);
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const key = event.target.name;
+
+    setFormValues(prevValues => ({
+      ...prevValues,
+      [key]: event.target.value.trimStart(),
+    }));
+  };
+
+  const handleAddMovie = () => {
+    onAdd(formValues);
+    setCount(prev => prev + 1);
+    setFormValues(initialFormValue);
+  };
 
   return (
     <form className="NewMovie" key={count}>
@@ -13,33 +39,40 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={formValues.title}
+        onChange={handleOnChange}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={formValues.description}
+        onChange={handleOnChange}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={formValues.imgUrl}
+        onChange={handleOnChange}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={formValues.imdbUrl}
+        onChange={handleOnChange}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={formValues.imdbId}
+        onChange={handleOnChange}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +81,8 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            onClick={handleAddMovie}
+            disabled={isButtonDisabled}
           >
             Add
           </button>
---
+++ src/validation/addMovie.validate.ts
@@ -0,0 +1,18 @@
+import { Movie } from '../types/Movie';
+import { IMDB_URL_REGEX } from '../constants/regex';
+
+export function getAddMovieFormIsValid(formValue: Movie) {
+  const {
+    title,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  } = formValue;
+
+  return !(
+    !title.trim()
+    || !imdbId.trim()
+    || !IMDB_URL_REGEX.test(imdbUrl)
+    || !IMDB_URL_REGEX.test(imgUrl)
+  );
+}
