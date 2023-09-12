e441a99e1d8a640979f01756fb1e1ecb3be969b7
Konrad <konrad.domalewski@gmail.com>
2023-09-12 16:33:17 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,109 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+function useForm(initialState: Record<string, string>) {
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = (name: string, value: string) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  return { formData, handleChange };
+}
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const { formData, handleChange } = useForm({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbId: '',
+    imdbUrl: '',
+  });
+
+  const isButtonDisabled
+  = !formData.title.trim()
+  || !formData.imgUrl.trim()
+  || !formData.imdbId.trim()
+  || !formData.imdbUrl.trim();
+
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!pattern.test(formData.imdbUrl) || !pattern.test(formData.imgUrl)) {
+      return;
+    }
+
+    const movie: Movie = {
+      title: formData.title,
+      description: formData.description,
+      imgUrl: formData.imgUrl,
+      imdbId: formData.imdbId,
+      imdbUrl: formData.imdbUrl,
+    };
+
+    onAdd(movie);
+
+    handleChange('title', '');
+    handleChange('description', '');
+    handleChange('imgUrl', '');
+    handleChange('imdbId', '');
+    handleChange('imdbUrl', '');
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={formData.title}
+        onChange={(value) => handleChange('title', value)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={formData.description}
+        onChange={(value) => handleChange('description', value)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        pattern={pattern}
+        value={formData.imgUrl}
+        onChange={(value) => handleChange('imgUrl', value)}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        pattern={pattern}
+        value={formData.imdbUrl}
+        onChange={(value) => handleChange('imdbUrl', value)}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={formData.imdbId}
+        onChange={(value) => handleChange('imdbId', value)}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +112,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isButtonDisabled}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -2,18 +2,17 @@ import classNames from 'classnames';
 import React, { useState } from 'react';
 
 type Props = {
-  name: string,
-  value: string,
-  label?: string,
-  placeholder?: string,
-  required?: boolean,
-  onChange?: (newValue: string) => void,
+  name: string;
+  value: string;
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+  onChange?: (newValue: string) => void;
+  pattern?: RegExp;
 };
 
 function getRandomDigits() {
-  return Math.random()
-    .toFixed(16)
-    .slice(2);
+  return Math.random().toFixed(16).slice(2);
 }
 
 export const TextField: React.FC<Props> = ({
@@ -23,37 +22,46 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  pattern,
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
-  // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
+
   const hasError = touched && required && !value;
+  const isValid = pattern ? pattern.test(value) : true;
+  const hasErrorUrl = !isValid && touched;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+
+    onChange(newValue); // Pass the new value as a string to the prop.
+  };
+
+  const handleBlur = () => {
+    setTouched(true);
+  };
 
   return (
     <div className="field">
       <label className="label" htmlFor={id}>
         {label}
       </label>
-
       <div className="control">
         <input
           type="text"
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError || hasErrorUrl,
           })}
           placeholder={placeholder}
           value={value}
-          onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
       </div>
-
-      {hasError && (
-        <p className="help is-danger">{`${label} is required`}</p>
+      {(hasError || hasErrorUrl) && (
+        <p className="help is-danger">{`${label} is ${hasError ? 'required' : 'invalid'}`}</p>
       )}
     </div>
   );
