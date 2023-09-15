320612b693f0b775dd3a23a362bbcca624bc4059
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-09-15 13:34:25 +0300

    commit

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -22,7 +22,14 @@ export const NewMovie = ({ onAdd }: NewMovieProps) => {
   const [count] = useState(0);
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   // eslint-disable-next-line max-len
-  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  const pattern = new RegExp(
+    /(?:(https?|ftp):)?\/\//.source
+    + /(?:([^:\n\r]+):([^@\n\r]+)@)?/.source
+    + /(?:www\.)?([^/\n\r]+)\//.source
+    + /(\/[^?\n\r]+)?/.source
+    + /(\?[^#\n\r]*)?/.source
+    + /(#[^\n\r]*)?/.source,
+  );
 
   const isValidUrl = (url: string) => pattern.test(url);
 
