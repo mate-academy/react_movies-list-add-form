87cc74ea36349594c9d648a459f57984e5d84f71
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-09-16 13:29:13 +0300

    commit

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -20,14 +20,8 @@ export const NewMovie = ({ onAdd }: NewMovieProps) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
   const [count] = useState(0);
-  const pattern = new RegExp(
-    /(?:(https?|ftp):)?\/\//.source
-    + /(?:([^:\n\r]+):([^@\n\r]+)@)?/.source
-    + /(?:www\.)?([^/\n\r]+)\//.source
-    + /(\/[^?\n\r]+)?/.source
-    + /(\?[^#\n\r]*)?/.source
-    + /(#[^\n\r]*)?/.source,
-  );
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
   const isValidUrl = (url: string) => pattern.test(url);
   const disablet = ((title.trim() === '' || !imgUrl || !imdbUrl || !imdbId)
     || !isValidUrl(imgUrl)
