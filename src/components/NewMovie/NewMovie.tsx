68b0f51559eaac9f4eb06fdbf9cd4cb8790934ad
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-09-16 13:34:50 +0300

    commit

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -21,7 +21,7 @@ export const NewMovie = ({ onAdd }: NewMovieProps) => {
   const [imdbId, setImdbId] = useState('');
   const [count] = useState(0);
   // eslint-disable-next-line max-len
-  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  const pattern = /^(https?:\/\/)?([a-zA-Z0-9.-]+)(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?$/;
   const isValidUrl = (url: string) => pattern.test(url);
   const disablet = ((title.trim() === '' || !imgUrl || !imdbUrl || !imdbId)
     || !isValidUrl(imgUrl)
