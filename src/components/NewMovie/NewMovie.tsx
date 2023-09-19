f7311f5fe921898d75bc2476f6aa55fc004565d1
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-09-19 14:35:08 +0300

    'done'

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,126 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [count, setCount] = useState(0);
+  const [button, setButton] = useState(false);
+  const handleTitleChange = (value: React.SetStateAction<string>) => {
+    setTitle(value);
+  };
+
+  const handleDescriptionChange = (value: React.SetStateAction<string>) => {
+    setDescription(value);
+  };
+
+  const handleImgUrlChange = (value: React.SetStateAction<string>) => {
+    setImgUrl(value);
+  };
+
+  const handleImdbUrlChange = (value: React.SetStateAction<string>) => {
+    setImdbUrl(value);
+  };
+
+  const handleImdbIdChange = (value: React.SetStateAction<string>) => {
+    setImdbId(value);
+  };
+
+  const reset = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!title || !imgUrl || !imdbUrl || !imdbId) {
+      return;
+    }
+
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    setCount(x => x + 1);
+    reset();
+  };
+
+  const checkButton = (item: string) => {
+    setButton(true);
+
+    if (!item.length) {
+      setButton(false);
+    }
+  };
+
+  useEffect(() => {
+    checkButton(title);
+  }, [title]);
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleSubmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={handleTitleChange}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={handleDescriptionChange}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={handleImgUrlChange}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={handleImdbUrlChange}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={handleImdbIdChange}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +129,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!button}
           >
             Add
           </button>
