f8305080221d47d70f4eccf5c95ddfa27a358c0b
Yuliia Dub <yuliia.kasaraba@gmail.com>
2023-09-30 10:32:02 +0300

    fix counter

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const NewMovie: React .FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [imdbId, setImdbId] = useState('');
@@ -41,6 +41,8 @@ export const NewMovie: React .FC<Props> = ({ onAdd }) => {
     setImdbId('');
     setImdbUrl('');
     setImgUrl('');
+
+    setCount(number => number + 1);
   };
 
   return (
