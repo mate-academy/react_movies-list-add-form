5105a8e94cab7242a370309fc50c585648c32556
Oleg Tarasov <tarasov.oleg.9779@gmail.com>
2023-10-11 19:52:48 +0300

    movies list add form v1.4

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,21 +6,27 @@ type Props = {
   onAdd: (movie: Movie) => void
 };
 
+const initialMovieState = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
 
   const [count, setCount] = useState(0);
 
-  const [movieDef, setMovieDef] = useState({
-    title: '',
-    description: '',
-    imgUrl: '',
-    imdbUrl: '',
-    imdbId: '',
-  });
+  const [movieDef, setMovieDef] = useState(initialMovieState);
 
-  const readyToSubmit = Object.values(movieDef).every((item) => item !== '');
+  const readyToSubmit = Object.entries(movieDef).every(
+    ([key, value]) => key === 'description'
+      || key === 'imdbId'
+      || (value.trim() !== '' && value !== undefined),
+  );
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -34,13 +40,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
       imgUrl: movieDef.imgUrl.trim(),
     });
 
-    setMovieDef({
-      title: '',
-      description: '',
-      imgUrl: '',
-      imdbUrl: '',
-      imdbId: '',
-    });
+    setMovieDef(initialMovieState);
   };
 
   const handleFieldChange = (name: string, value: string) => {
