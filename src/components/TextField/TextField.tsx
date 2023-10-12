83bc10980e75237367c69b74b208a9cc710bd86b
Dmytro Levchenko <levchenkod91@gmail.com>
2023-10-12 17:32:58 +0300

    solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -16,6 +16,9 @@ function getRandomDigits() {
     .slice(2);
 }
 
+// eslint-disable-next-line max-len
+const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
 export const TextField: React.FC<Props> = ({
   name,
   value,
@@ -29,7 +32,29 @@ export const TextField: React.FC<Props> = ({
 
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const [errorMsg, setErrorMsg] = useState(`${label} is required`);
+
+  const hasError = () => {
+    if ((name === 'imgUrl' || name === 'imdbUrl') && value) {
+      return touched && required && !value.match(pattern);
+    }
+
+    return touched && required && !value;
+  };
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+
+  const handleOnBlur = () => {
+    if ((name === 'imgUrl' || name === 'imdbUrl') && value) {
+      setErrorMsg('Pls enter correct URL');
+    } else {
+      setErrorMsg(`${label} is required`);
+    }
+
+    setTouched(true);
+  };
 
   return (
     <div className="field">
@@ -43,17 +68,17 @@ export const TextField: React.FC<Props> = ({
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError(),
           })}
           placeholder={placeholder}
           value={value}
-          onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onChange={handleOnChange}
+          onBlur={handleOnBlur}
         />
       </div>
 
-      {hasError && (
-        <p className="help is-danger">{`${label} is required`}</p>
+      {hasError() && (
+        <p className="help is-danger">{errorMsg}</p>
       )}
     </div>
   );
