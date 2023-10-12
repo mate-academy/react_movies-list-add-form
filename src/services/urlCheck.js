cd20f67c67439f38f9c6a535e0ba73bbf1b906ee
Eugene Ivchenko <ivchenkoevhen@gmail.com>
2023-10-12 12:03:30 +0300

    added function isUrlValid

---
+++ src/services/urlCheck.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line max-len
+const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+export const urlIsValid = (url) => {
+  if (pattern.test(url)) {
+    return true;
+  }
+
+  return false;
+};
