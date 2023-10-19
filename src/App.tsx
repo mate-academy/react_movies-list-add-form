da907c277d01148a617e8fa130c5f97f8492fb31
Vladyslav Atanov <gibepromt@gmail.com>
2023-10-19 21:41:33 +0300

    add spred for copy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import moviesFromServer from './api/movies.json';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies, setMovies] = useState<Movie[]>(moviesFromServer);
+  const [movies, setMovies] = useState<Movie[]>([...moviesFromServer]);
 
   return (
     <div className="page">
