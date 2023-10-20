19d651bd5b04a54cfd8bb0f770ba1394fdcfe4fe
Oksana Drozdyk <oksanadrozdik66@gmail.com>
2023-10-20 22:48:05 +0300

    react movies list form solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,10 +1,108 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+enum FieldType {
+  TITLE = 'title',
+  DESCRIPTION = 'descripton',
+  IMG_URL = 'imgUrl',
+  IMDB_URL = 'imdbUrl',
+  IMDB_ID = 'imdbId',
+}
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setHasTitleError] = useState<boolean>(true);
+
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [hasImgUrlError, setHasImgUrlError] = useState<boolean>(true);
+
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [hasImdbUrlError, setHasImdbUrlError] = useState<boolean>(true);
+
+  const [imdbId, setImdbId] = useState('');
+  const [hasImdbIdError, setHasImdbIdError] = useState<boolean>(true);
+
+  const hasFormerrors = (
+    hasTitleError || hasImgUrlError || hasImdbUrlError || hasImdbIdError
+  );
+
+  function clear() {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  }
+
+  function handleChange(text: string, field: FieldType) {
+    switch (field) {
+      case FieldType.TITLE:
+        setTitle(text);
+        setHasTitleError(false);
+        break;
+
+      case FieldType.DESCRIPTION:
+        setDescription(text);
+        break;
+
+      case FieldType.IMG_URL:
+        setImgUrl(text);
+        setHasImgUrlError(false);
+        break;
+
+      case FieldType.IMDB_URL:
+        setImdbUrl(text);
+        setHasImdbUrlError(false);
+        break;
+
+      case FieldType.IMDB_ID:
+        setImdbId(text);
+        setHasImdbIdError(false);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    if (!title) {
+      setHasTitleError(true);
+    }
+
+    if (!imgUrl) {
+      setHasImgUrlError(true);
+    }
+
+    if (!imdbUrl) {
+      setHasImdbUrlError(true);
+    }
+
+    if (!imdbId) {
+      setHasImdbIdError(true);
+    }
+
+    if (!hasFormerrors) {
+      onAdd({
+        title,
+        description,
+        imgUrl,
+        imdbUrl,
+        imdbId,
+      });
+      setCount(count + 1);
+      clear();
+    }
+  }
 
   return (
     <form className="NewMovie" key={count}>
@@ -13,33 +111,40 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={event => handleChange(event, FieldType.TITLE)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={event => handleChange(event, FieldType.DESCRIPTION)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={event => handleChange(event, FieldType.IMG_URL)}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={event => handleChange(event, FieldType.IMDB_URL)}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={event => handleChange(event, FieldType.IMDB_ID)}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +153,8 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            onClick={handleSubmit}
+            disabled={hasFormerrors}
           >
             Add
           </button>
