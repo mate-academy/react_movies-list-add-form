0651035e4ed566b52a9afe9bbaaf618ebc85140c
Vika Levik <levika002@gmail.com>
2023-10-20 15:44:00 +0300

    added form to movies list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [newMoviesList, setNewMoviesList] = useState(moviesFromServer);
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={newMoviesList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie
+          onAdd={(movie: Movie) => setNewMoviesList([...newMoviesList, movie])}
+        />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,10 +1,44 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+const defaultState = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
+export const NewMovie: React.FC<Props> = ({
+  onAdd,
+}) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [movieInfo, setMovieInfo] = useState<Movie>(defaultState);
+
+  const isAbleToSubmit = movieInfo.title && movieInfo.imgUrl
+  && movieInfo.imdbUrl && movieInfo.imdbId;
+
+  const handleAddNewMovie = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    movieInfo.title.trim();
+    movieInfo.description.trim();
+    movieInfo.imgUrl.trim();
+    movieInfo.imdbUrl.trim();
+    movieInfo.imdbId.trim();
+
+    onAdd(movieInfo);
+
+    setMovieInfo(defaultState);
+    setCount(count + 1);
+  };
 
   return (
     <form className="NewMovie" key={count}>
@@ -13,33 +47,52 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movieInfo.title}
+        onChange={(newTitle) => setMovieInfo({ ...movieInfo, title: newTitle })}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={movieInfo.description}
+        onChange={(newDescription) => setMovieInfo({
+          ...movieInfo,
+          description: newDescription,
+        })}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={movieInfo.imgUrl}
+        onChange={(newimgUrl) => setMovieInfo({
+          ...movieInfo,
+          imgUrl: newimgUrl,
+        })}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={movieInfo.imdbUrl}
+        onChange={(newimdbUrl) => setMovieInfo({
+          ...movieInfo,
+          imdbUrl: newimdbUrl,
+        })}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={movieInfo.imdbId}
+        onChange={(newimdbId) => setMovieInfo({
+          ...movieInfo,
+          imdbId: newimdbId,
+        })}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +101,8 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            onClick={handleAddNewMovie}
+            disabled={!isAbleToSubmit}
           >
             Add
           </button>
