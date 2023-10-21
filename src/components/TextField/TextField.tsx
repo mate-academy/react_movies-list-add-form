968a7cbb50ecb8682acc8ac69ad6fac64e50d9ef
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-10-22 00:30:28 +0300

    add task

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -24,12 +24,30 @@ export const TextField: React.FC<Props> = ({
   required = false,
   onChange = () => {},
 }) => {
-  // generage a unique id once on component load
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
+  const [errorMessage, setErrorMessage] = useState(`${label} is required`);
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const hasError = () => {
+    if ((name === 'imgUrl' || name === 'imdbUrl') && value) {
+      return touched && required && !value.match(pattern);
+    }
+
+    return touched && required && !value;
+  };
+
+  const handleOnBlur = () => {
+    if ((name === 'imgUrl' || name === 'imdbUrl')
+    && value && !value.match(pattern)) {
+      setErrorMessage('Wrong URL input');
+    } else {
+      setErrorMessage(`${label} is required`);
+    }
+
+    setTouched(true);
+  };
 
   return (
     <div className="field">
@@ -43,17 +61,17 @@ export const TextField: React.FC<Props> = ({
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError(),
           })}
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={handleOnBlur}
         />
       </div>
 
-      {hasError && (
-        <p className="help is-danger">{`${label} is required`}</p>
+      {hasError() && (
+        <p className="help is-danger">{errorMessage}</p>
       )}
     </div>
   );
