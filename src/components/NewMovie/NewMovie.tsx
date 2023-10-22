143871b0b73dfdbee97deb0c2b0f3260f7707039
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-10-22 18:44:55 +0300

    add

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,96 +1,99 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
 import { Movie } from '../../types/Movie';
 
-interface Props {
+type MovieProps = {
   onAdd: (movie: Movie) => void;
-}
+};
 
-export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+export const NewMovie = ({ onAdd }: MovieProps) => {
   const [count, setCount] = useState(0);
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
-  const [imdbUrl, setImdbUrl] = useState('');
-  const [imdbId, setImdbId] = useState('');
 
-  const checkInput = () => {
-    // eslint-disable-next-line max-len
-    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
-    const allInputReceived
-    = name.trim() && imageUrl.trim() && imdbUrl.trim() && imdbId.trim();
-    const regexMatched = imdbUrl.match(pattern) && imageUrl.match(pattern);
+  const [formValues, setFormValues] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
 
-    return allInputReceived && regexMatched;
+  const isButtonDisabled = () => {
+    const {
+      title,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    } = formValues;
+
+    return !(title.trim() && imgUrl.trim()
+      && imdbUrl.trim() && imdbId.trim());
   };
 
-  const clear = () => {
-    setName('');
-    setDescription('');
-    setImageUrl('');
-    setImdbUrl('');
-    setImdbId('');
+  const handleChange = (value: string, name: string) => {
+    setFormValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const reset = () => {
+    setFormValues({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  const handleSubmit = ((event: React.FormEvent) => {
     event.preventDefault();
 
-    if (checkInput()) {
-      onAdd({
-        title: name,
-        description,
-        imgUrl: imageUrl,
-        imdbUrl,
-        imdbId,
-      });
-      clear();
-      setCount(count + 1);
-    }
-  };
+    onAdd(formValues);
+
+    setCount(count + 1);
+    reset();
+  });
 
   return (
-    <form
-      className="NewMovie"
-      key={count}
-      onSubmit={handleSubmit}
-    >
+    <form className="NewMovie" onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value={name}
-        onChange={setName}
+        value={formValues.title}
+        onChange={(value) => handleChange(value, 'title')}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value={description}
-        onChange={setDescription}
+        value={formValues.description}
+        onChange={(value) => handleChange(value, 'description')}
       />
 
       <TextField
         name="imgUrl"
-        value={imageUrl}
+        value={formValues.imgUrl}
+        onChange={(value) => handleChange(value, 'imgUrl')}
         required
-        onChange={setImageUrl}
       />
 
       <TextField
         name="imdbUrl"
-        value={imdbUrl}
+        value={formValues.imdbUrl}
+        onChange={(value) => handleChange(value, 'imdbUrl')}
         required
-        onChange={setImdbUrl}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={imdbId}
+        value={formValues.imdbId}
+        onChange={(value) => handleChange(value, 'imdbId')}
         required
-        onChange={setImdbId}
       />
 
       <div className="field is-grouped">
@@ -99,7 +102,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={!checkInput()}
+            disabled={isButtonDisabled()}
           >
             Add
           </button>
