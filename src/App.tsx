5542c48a9aece8c27dce18919bef9685a71719bf
elenagrasovskaya <elenagrasovskaya@gmail.com>
2023-10-22 22:49:35 +0300

    React movies list all test passed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
+
+const loadedMovies = [...moviesFromServer];
 
 export const App = () => {
+  const [movies, setMovies] = useState(loadedMovies);
+
+  const addNewMovieHandler = (movie:Movie) => {
+    setMovies([...loadedMovies, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addNewMovieHandler} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,87 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [checkEmail, setCheckEmail] = useState(true);
+  const [submitData, setSubmitData] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (
+      !submitData?.title
+      || !submitData?.imgUrl
+      || !submitData?.imdbUrl
+      || !submitData.imdbId
+      || checkEmail
+    ) {
+      return;
+    }
+
+    onAdd(submitData);
+    setCount(count + 1);
+  };
+
+  const onFormDataChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSubmitData({ ...submitData, [event.target.name]: event.target.value });
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={onFormSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={submitData.title}
+        onChange={onFormDataChange}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={submitData.description}
+        onChange={onFormDataChange}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={submitData.imgUrl}
+        onChange={onFormDataChange}
+        required
+        checkIsValid={setCheckEmail}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={submitData.imdbUrl}
+        onChange={onFormDataChange}
+        required
+        checkIsValid={setCheckEmail}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={submitData.imdbId}
+        onChange={onFormDataChange}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +90,13 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={
+              !submitData?.title
+              || !submitData?.imgUrl
+              || !submitData?.imdbUrl
+              || !submitData.imdbId
+              || checkEmail
+            }
           >
             Add
           </button>
