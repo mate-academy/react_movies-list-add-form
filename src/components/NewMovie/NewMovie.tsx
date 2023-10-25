153a0befefd99d18f0a813efa26b67ae62efdda0
4xmplme <andreykav2002@gmail.com>
2023-10-26 00:04:43 +0300

    add task solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,58 +1,92 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type OnAdd = (movie: Movie) => void;
+
+type FormFields = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
+const getUrlPattern = (fieldName: string) => ({
+  // eslint-disable-next-line max-len
+  value: /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/,
+  message: `${fieldName} is not valid URL`,
+});
+
+export const NewMovie = ({ onAdd }: { onAdd: OnAdd }) => {
+  const methods = useForm<FormFields>({ mode: 'onBlur' });
+  const { handleSubmit, reset, formState } = methods;
+  const isDisabledSubmit = !formState.isValid || !formState.isDirty;
+
+  const onSubmit: SubmitHandler<FormFields> = (formFields) => {
+    onAdd(formFields);
+    reset();
+  };
+
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+    }
+  }, [formState, reset]);
 
   return (
-    <form className="NewMovie" key={count}>
-      <h2 className="title">Add a movie</h2>
+    <FormProvider {...methods}>
+      <form
+        className="NewMovie"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h2 className="title">Add a movie</h2>
 
-      <TextField
-        name="title"
-        label="Title"
-        value=""
-        onChange={() => {}}
-        required
-      />
+        <TextField
+          name="title"
+          label="Title"
+          required
+        />
 
-      <TextField
-        name="description"
-        label="Description"
-        value=""
-      />
+        <TextField
+          name="description"
+          label="Description"
+        />
 
-      <TextField
-        name="imgUrl"
-        label="Image URL"
-        value=""
-      />
+        <TextField
+          name="imgUrl"
+          label="Image URL"
+          required
+          getPattern={getUrlPattern}
+        />
 
-      <TextField
-        name="imdbUrl"
-        label="Imdb URL"
-        value=""
-      />
+        <TextField
+          name="imdbUrl"
+          label="Imdb URL"
+          required
+          getPattern={getUrlPattern}
+        />
 
-      <TextField
-        name="imdbId"
-        label="Imdb ID"
-        value=""
-      />
+        <TextField
+          name="imdbId"
+          label="Imdb ID"
+          required
+        />
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="submit"
-            data-cy="submit-button"
-            className="button is-link"
-          >
-            Add
-          </button>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="submit"
+              data-cy="submit-button"
+              disabled={isDisabledSubmit}
+              className="button is-link"
+            >
+              Add
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </FormProvider>
   );
 };
