0ea624d28e783bd59d3b0a74403c72433725a237
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-11-06 12:51:39 +0200

    revise onChange function

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -25,7 +25,7 @@ export const NewMovie:React.FC<Props> = ({ onAdd }) => {
   const isInputEmpty
     = !title.trim() || !imgUrl.trim() || !imdbUrl.trim() || !imdbId.trim();
 
- /* eslint-disable max-len */
+  /* eslint-disable max-len */
   const pattern
      = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
