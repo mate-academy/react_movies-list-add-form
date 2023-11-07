4d58839fb5a19d157a731885aaef1fefd273f374
Bohdan Antipenko <dr.antipenko2016@gmail.com>
2023-11-07 19:43:52 +0200

    Add validator

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -31,6 +31,22 @@ export const TextField: React.FC<Props> = ({
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
+  /* eslint-disable */
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  /* eslint-enable */
+  const validateField = (val = value) => {
+    switch (name) {
+      case 'imgUrl':
+        return val.match(pattern);
+        break;
+      case 'imdbUrl':
+        return val.match(pattern);
+        break;
+      default:
+        return true;
+        break;
+    }
+  };
 
   return (
     <div className="field">
@@ -54,6 +70,9 @@ export const TextField: React.FC<Props> = ({
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+      {!validateField() && touched && (
+        <p className="help is-danger">{`${label} is not correct`}</p>
       )}
     </div>
   );
