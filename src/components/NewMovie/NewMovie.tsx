7846be9e06ebab28a0a6ca7b2119c9f0ebf63094
Ivan Orinchak <124816409+IvanOrinchak@users.noreply.github.com>
2023-11-15 14:29:03 +0200

    movies-list

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,88 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+interface NewMovieProps {
+  onAdd: (details: Movie) => void;
+}
+
+export const NewMovie:React.FC<NewMovieProps> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [currTitle, setCurrTitle] = useState('');
+  const [currDescription, setCurrDescription] = useState('');
+  const [currImgUrl, setCurrImgUrl] = useState('');
+  const [currImdbUrl, setCurrImdbUrl] = useState('');
+  const [currImdbId, setCurrImdbId] = useState('');
+
+  const disabledButton = currTitle !== ''
+  && currImgUrl !== '' && currImdbUrl !== '' && currImdbId !== '';
+
+  const handleButton = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    onAdd({
+      title: currTitle,
+      description: currDescription,
+      imgUrl: currImgUrl,
+      imdbUrl: currImdbUrl,
+      imdbId: currImdbId,
+    });
+
+    setCurrTitle('');
+    setCurrDescription('');
+    setCurrImgUrl('');
+    setCurrImdbUrl('');
+    setCurrImdbId('');
+    setCount(count + 1);
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleButton}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={currTitle}
+        onChange={(even) => setCurrTitle(even)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={currDescription}
+        onChange={(even) => setCurrDescription(even)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={currImgUrl}
+        onChange={(even) => setCurrImgUrl(even)}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={currImdbUrl}
+        onChange={(even) => setCurrImdbUrl(even)}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={currImdbId}
+        onChange={(even) => setCurrImdbId(even)}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +91,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!disabledButton}
           >
             Add
           </button>
