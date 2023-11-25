dbd7f68301983c2bfdf428fca20018630ca6ed41
Катерина Сагун <kattya.sagun@gmail.com>
2023-11-25 20:11:08 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { TextField } from '../TextField';
 import { Movie } from '../../types/Movie';
+import { pattern } from '../utils/utils';
 
 interface Props {
   onAdd: (movie: Movie) => void;
@@ -15,9 +16,7 @@ export const NewMovie:React.FC<Props> = ({ onAdd }) => {
     imdbUrl: '',
     imdbId: '',
   });
-  const [submitButton, setSubmitButton] = useState(true);
-  /* eslint-disable-next-line */
-  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  const [isSubmitButtonDisabled, setIsSubmitButtonDisabled] = useState(true);
 
   useEffect(() => {
     if (formData.title
@@ -26,9 +25,9 @@ export const NewMovie:React.FC<Props> = ({ onAdd }) => {
       && formData.imdbId
       && pattern.test(formData.imgUrl)
       && pattern.test(formData.imdbUrl)) {
-      setSubmitButton(false);
+      setIsSubmitButtonDisabled(false);
     } else {
-      setSubmitButton(true);
+      setIsSubmitButtonDisabled(true);
     }
   }, [formData]);
 
@@ -112,7 +111,7 @@ export const NewMovie:React.FC<Props> = ({ onAdd }) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={submitButton}
+            disabled={isSubmitButtonDisabled}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
+import { pattern } from '../utils/utils';
 
 type Props = {
   name: string,
@@ -31,8 +32,7 @@ export const TextField: React.FC<Props> = ({
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
-  /* eslint-disable-next-line */
-  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
   const isInvalid = validation && value && !pattern.test(value);
 
   return (
