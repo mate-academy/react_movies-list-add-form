3ac8d6e2b2f8b84df9aefbd1a0a3c09b88fe079d
Daniil <danil.beshlebnov@gmail.com>
2023-11-28 17:57:49 +0200

    first commit

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,109 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const InitialFormInfo: Movie = {
+    title: '',
+    description: '',
+    imdbUrl: '',
+    imdbId: '',
+    imgUrl: '',
+  };
+
+  const [formInfo, setFormInfo] = useState<Movie>(InitialFormInfo);
+  const {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+    imdbId,
+  } = formInfo;
+
+  const validateUrl = (url: string): boolean => {
+    // eslint-disable-next-line
+    const regExUrl = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+    return regExUrl.test(url);
+  };
+
+  const validImgUrl = validateUrl(imgUrl);
+  const validImdbUrl = validateUrl(imdbUrl);
+
+  const handleInputChange = (value: string, field: string) => {
+    setFormInfo(prevInfo => ({ ...prevInfo, [field]: value }));
+  };
+
+  const clearForm = () => {
+    setFormInfo(InitialFormInfo);
+  };
+
+  const handleDataForm = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const data = { ...formInfo };
+
+    onAdd(data);
+    clearForm();
+    setCount(prevCount => prevCount + 1);
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleDataForm}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={handleInputChange}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={handleInputChange}
+
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={handleInputChange}
+        regEx={validImgUrl}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={handleInputChange}
+        regEx={validImdbUrl}
+        required
+
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={handleInputChange}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +112,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!title || !imdbId || !imdbUrl || !imgUrl}
           >
             Add
           </button>
