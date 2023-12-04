8e1eaf524a040b73f4c94f8f2fdcb85271b83c16
Nazar Boblyk <fs.boblyk.nazar@gmail.com>
2023-12-04 18:19:48 +0200

    fixed class is-danger

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -48,7 +48,7 @@ export const TextField: React.FC<Props> = ({
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError || hasValidationUrlError,
           })}
           placeholder={placeholder}
           value={value}
