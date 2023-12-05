36370cf0609e19003ed67be1beee5bd4a774c9fa
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-05 16:30:55 +0200

    movies list solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [moviesToRender, setMoviesToRender]
+    = useState<Movie[]>(moviesFromServer);
+
+  const handleAddMovie = (movie: Movie) => {
+    setMoviesToRender((currMovies) => [
+      ...currMovies,
+      movie,
+    ]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesToRender} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={handleAddMovie} />
       </div>
     </div>
   );
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -7,6 +7,7 @@ type Props = {
   label?: string,
   placeholder?: string,
   required?: boolean,
+  onValidate?: (str: string) => boolean,
   onChange?: (newValue: string) => void,
 };
 
@@ -23,13 +24,24 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  onValidate,
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
-  // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const [isValid, setIsValid] = useState(true);
+
+  const hasError = touched && required && (!value || !value.trim());
+
+  const handleBlur = () => {
+    setTouched(true);
+
+    if (onValidate) {
+      const valid = onValidate(value);
+
+      setIsValid(valid);
+    }
+  };
 
   return (
     <div className="field">
@@ -47,13 +59,18 @@ export const TextField: React.FC<Props> = ({
           })}
           placeholder={placeholder}
           value={value}
+          required={required}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={handleBlur}
         />
       </div>
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+
+      {(!isValid && value) && (
+        <p className="help is-danger">Type correct link</p>
       )}
     </div>
   );
