d9c3da94c44a7e6ba7821bc0a92636acb1d02304
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2023-12-06 20:15:29 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,117 @@
+// import './NewMovie.scss';
+// import classNames from 'classnames';
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (newMovie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const [title, setTitle] = useState('');
+
+  const [description, setDescription] = useState('');
+
+  const [imdbId, setImdbId] = useState('');
+
+  const [imgUrl, setImgUrl] = useState('');
+
+  const [imdbUrl, setImdbUrl] = useState('');
+
+  let isDisabled = true;
+
+  if (title && imgUrl && imdbUrl && imdbId) {
+    isDisabled = false;
+  }
+
+  const handleAdd = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!title || !imgUrl || !imdbUrl || !imdbId) {
+      return;
+    }
+
+    // eslint-disable-next-line max-len
+    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+
+    // I know that my url validation is wrong, please, give me advice how to improve it
+    if (imgUrl.match(pattern)) {
+      setImgUrl(imgUrl);
+    }
+
+    if (imdbUrl.match(pattern)) {
+      setImdbUrl(imdbUrl);
+    }
+
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    setCount(count + 1);
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      action="../../api/movies"
+      method="Post"
+      className="NewMovie"
+      key={count}
+      onSubmit={handleAdd}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={setTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={setDescription}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl.trim()}
+        onChange={setImgUrl}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl.trim()}
+        onChange={setImdbUrl}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId.trim()}
+        onChange={setImdbId}
+        required
       />
 
       <div className="field is-grouped">
@@ -47,7 +119,8 @@ export const NewMovie = () => {
           <button
             type="submit"
             data-cy="submit-button"
-            className="button is-link"
+            className="button is-link active"
+            disabled={isDisabled}
           >
             Add
           </button>
