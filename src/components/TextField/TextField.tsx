76ce1ec52b12b85087dd34d027dfd2ab7f4a6d17
Yuriy Sobakar <raizen.dj@gmail.com>
2023-12-07 17:15:52 +0200

    Solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -7,7 +7,8 @@ type Props = {
   label?: string,
   placeholder?: string,
   required?: boolean,
-  onChange?: (newValue: string) => void,
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  // onBlur?: (is: boolean) => void
 };
 
 function getRandomDigits() {
@@ -23,6 +24,7 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  // onBlur = () => {},
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
@@ -39,6 +41,7 @@ export const TextField: React.FC<Props> = ({
 
       <div className="control">
         <input
+          name={name}
           type="text"
           id={id}
           data-cy={`movie-${name}`}
@@ -46,8 +49,8 @@ export const TextField: React.FC<Props> = ({
             'is-danger': hasError,
           })}
           placeholder={placeholder}
-          value={value}
-          onChange={event => onChange(event.target.value)}
+          defaultValue={value}
+          onChange={event => onChange(event)}
           onBlur={() => setTouched(true)}
         />
       </div>
