85efb98cd190395122e7bfef12889752c6e95b92
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2023-12-11 14:52:48 +0200

    add solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -8,6 +8,7 @@ type Props = {
   placeholder?: string,
   required?: boolean,
   onChange?: (newValue: string) => void,
+  validation?: boolean,
 };
 
 function getRandomDigits() {
@@ -22,7 +23,8 @@ export const TextField: React.FC<Props> = ({
   label = name,
   placeholder = `Enter ${label}`,
   required = false,
-  onChange = () => {},
+  onChange = () => { },
+  validation = false,
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
@@ -30,6 +32,10 @@ export const TextField: React.FC<Props> = ({
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
+
+  /* eslint-disable-next-line */
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  const isInvalid = validation && value && !pattern.test(value);
 
   return (
     <div className="field">
@@ -54,6 +60,10 @@ export const TextField: React.FC<Props> = ({
 
       {hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
+      )}
+
+      {isInvalid && (
+        <p className="help is-danger">{`${label} link is not valid`}</p>
       )}
     </div>
   );
