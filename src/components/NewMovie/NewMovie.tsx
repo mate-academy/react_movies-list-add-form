e0252a737e489596b435ddfbc0ac987776fabc2a
Adrian <adrian.krause.ak@gmail.com>
2023-12-15 17:07:39 +0100

    Created state for whole form.

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,62 +1,45 @@
 import { FormEventHandler, useState } from 'react';
 import { TextField } from '../TextField';
 import { Movie } from '../../types/Movie';
+import { Form } from '../../types/Form';
 
 type Props = {
   onAdd: (movie: Movie) => void;
+};
+
+const EMPTY_FORM = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
 };
 
 export const NewMovie = ({ onAdd }: Props) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
   const [count, setCount] = useState(0);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [image, setImage] = useState('');
-  const [imdbUrl, setImdbUrl] = useState('');
-  const [imdbId, setImdbId] = useState('');
+  const [form, setForm] = useState<Form>(EMPTY_FORM);
 
-  const handleTitle = (newValue: string) => {
-    setTitle(newValue);
-  };
-
-  const handleDescription = (newValue: string) => {
-    setDescription(newValue);
-  };
-
-  const handleImage = (newValue: string) => {
-    setImage(newValue);
-  };
-
-  const handleImdbUrl = (newValue: string) => {
-    setImdbUrl(newValue);
-  };
-
-  const handleImdbId = (newValue: string) => {
-    setImdbId(newValue);
+  const handleChange = (key: keyof Form, value: string) => {
+    setForm(prev => ({
+      ...prev,
+      [key]: value,
+    }));
   };
 
   const isFormValid = () => {
-    return title.trim() && image.trim() && imdbUrl.trim() && imdbId.trim();
+    return form.title.trim()
+    && form.imdbId.trim() && form.imdbUrl.trim() && form.imgUrl.trim();
   };
 
   const handleSubmit: FormEventHandler = (event) => {
     event.preventDefault();
 
     if (isFormValid()) {
-      onAdd({
-        title,
-        description,
-        imgUrl: image,
-        imdbUrl,
-        imdbId,
-      });
+      onAdd(form);
       setCount(count + 1);
-      setTitle('');
-      setDescription('');
-      setImage('');
-      setImdbUrl('');
-      setImdbId('');
+      setForm(EMPTY_FORM);
     }
   };
 
@@ -67,39 +50,39 @@ export const NewMovie = ({ onAdd }: Props) => {
       <TextField
         name="title"
         label="Title"
-        value={title}
-        onChange={handleTitle}
+        value={form.title}
+        onChange={(newValue) => handleChange('title', newValue)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value={description}
-        onChange={handleDescription}
+        value={form.description}
+        onChange={(newValue) => handleChange('description', newValue)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={image}
-        onChange={handleImage}
+        value={form.imgUrl}
+        onChange={(newValue) => handleChange('imgUrl', newValue)}
         required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={imdbUrl}
-        onChange={handleImdbUrl}
+        value={form.imdbUrl}
+        onChange={(newValue) => handleChange('imdbUrl', newValue)}
         required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={imdbId}
-        onChange={handleImdbId}
+        value={form.imdbId}
+        onChange={(newValue) => handleChange('imdbId', newValue)}
         required
       />
 
---
+++ src/types/Form.ts
@@ -0,0 +1,7 @@
+export type Form = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
