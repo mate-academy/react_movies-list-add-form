f39dd6e126cdb8fb1be43146cfccee8f7d7c219b
Denys Vasylenkoe <kapodenim@gmail.com>
2023-12-19 20:21:26 +0200

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [moviesActual, setMovies] = useState<Movie[]>(moviesFromServer);
+
+  const AddMovie = ({
+    title, description, imgUrl, imdbUrl, imdbId,
+  }:Movie) => {
+    const newMovie = {
+      title: title.trim(),
+      description: description.trim(),
+      imgUrl: imgUrl.trim(),
+      imdbUrl: imdbUrl.trim(),
+      imdbId: imdbId.trim(),
+    };
+
+    setMovies(actualMovies => [...actualMovies, newMovie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesActual} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={AddMovie} />
       </div>
     </div>
   );
