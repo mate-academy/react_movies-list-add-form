9485441b8ee3d0eb35e540db627dbb2d6286bc59
Alina <afesh4ak@gmail.com>
2024-01-12 17:37:39 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,68 +1,58 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { TextField } from '../TextField';
-
-interface Movie {
-  title: string;
-  description: string;
-  imgUrl: string;
-  imdbUrl: string;
-  imdbId: string;
-}
+import { Movie } from '../../types/Movie';
 
 interface NewMovieProps {
   onAdd: (movie: Movie) => void;
 }
 
+// const isValidUrl = (url: string): boolean => {
+//   const pattern = hereisproblem;
+
+//   return pattern.test(url);
+// };
+
 export const NewMovie: React.FC<NewMovieProps> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
-  const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    imgUrl: '',
-    imdbUrl: '',
-    imdbId: '',
-  });
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
 
-  const [allFieldsFilled, setAllFieldsFilled] = useState(false);
-
-  useEffect(() => {
-    setAllFieldsFilled(
-      formData.title.trim() !== ''
-      && formData.imgUrl.trim() !== ''
-      && formData.imdbUrl.trim() !== ''
-      && formData.imdbId.trim() !== '',
-    );
-  }, [formData]);
-
-  const handleChange = (name: string, value: string) => {
-    setFormData({ ...formData, [name]: value });
-
-    setAllFieldsFilled(
-      formData.title.trim() !== ''
-      && formData.imgUrl.trim() !== ''
-      && formData.imdbUrl.trim() !== ''
-      && formData.imdbId.trim() !== '',
-    );
+  const data = {
+    title: title.trim(),
+    description: description.trim(),
+    imgUrl: imgUrl.trim(),
+    imdbUrl: imdbUrl.trim(),
+    imdbId: imdbId.trim(),
   };
+
+  const isFormValid = Boolean(
+    data.title
+    && data.imdbId
+    && data.imdbUrl
+    && data.imgUrl,
+  );
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (allFieldsFilled) {
-      onAdd(formData);
-
-      setFormData({
-        title: '',
-        description: '',
-        imgUrl: '',
-        imdbUrl: '',
-        imdbId: '',
-      });
-
-      setAllFieldsFilled(false);
+    if (!isFormValid) {
+      return;
     }
+
+    onAdd(data);
+
+    setTitle('');
+    setDescription('');
+    setImdbId('');
+    setImdbUrl('');
+    setImgUrl('');
+
+    setCount(prevCount => prevCount + 1);
   };
 
   return (
@@ -72,37 +62,39 @@ export const NewMovie: React.FC<NewMovieProps> = ({ onAdd }) => {
       <TextField
         name="title"
         label="Title"
-        value={formData.title}
-        onChange={(value) => handleChange('title', value)}
+        value={title}
+        onChange={setTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value={formData.description}
-        onChange={(value) => handleChange('description', value)}
+        value={description}
+        onChange={setDescription}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={formData.imgUrl}
-        onChange={(value) => handleChange('imgUrl', value)}
+        value={imgUrl}
+        onChange={setImgUrl}
+        // validate={(value) => isValidUrl(value)}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={formData.imdbUrl}
-        onChange={(value) => handleChange('imdbUrl', value)}
+        value={imdbUrl}
+        onChange={setImdbUrl}
+        // validate={(value) => isValidUrl(value)}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={formData.imdbId}
-        onChange={(value) => handleChange('imdbId', value)}
+        value={imdbId}
+        onChange={setImdbId}
       />
 
       <div className="field is-grouped">
@@ -111,7 +103,7 @@ export const NewMovie: React.FC<NewMovieProps> = ({ onAdd }) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={!allFieldsFilled}
+            disabled={!isFormValid}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -8,6 +8,7 @@ type Props = {
   placeholder?: string,
   required?: boolean,
   onChange?: (newValue: string) => void,
+  validate?: (value: string) => boolean;
 };
 
 function getRandomDigits() {
@@ -23,6 +24,7 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  validate = () => true,
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
@@ -30,6 +32,14 @@ export const TextField: React.FC<Props> = ({
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
+
+  const handleBlur = () => {
+    setTouched(true);
+
+    if (!validate(value)) {
+      setTouched(true);
+    }
+  };
 
   return (
     <div className="field">
@@ -48,7 +58,7 @@ export const TextField: React.FC<Props> = ({
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={handleBlur}
         />
       </div>
 
