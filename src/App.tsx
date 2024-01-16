4169525aa5b8be238a4c83b2265e02fd10b86590
Yaroslav Samoilenko <yasik2255@live.com>
2024-01-16 13:18:15 +0200

    add solution, with URL validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
+
 import './App.scss';
+
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [visibleMovies, setVisibleMovies] = useState(moviesFromServer);
+
+  /* function addMovie(movie: Movie) {
+    setVisibleMovies([...visibleMovies, movie]);
+  } */
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie
+          onAdd={(movie: Movie) => {
+            setVisibleMovies([...visibleMovies, movie]);
+          }}
+        />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,103 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
+import { ValidURL } from '../../types/ValidURL';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+interface Props {
+  onAdd: (newMovie: Movie) => void;
+}
+
+export const NewMovie = (props: Props) => {
+  const { onAdd } = props;
+
+  const [count, setCount] = useState(0);
+
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieDescription, setMovieDescription] = useState('');
+  const [movieImgURL, setMovieImgURL] = useState('');
+  const [movieImdbURL, setMovieImdbURL] = useState('');
+  const [movieImdbId, setMovieImdbId] = useState('');
+
+  function isValidURL(url: string): ValidURL {
+  // eslint-disable-next-line max-len
+    const regex = new RegExp(/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/);
+    const check = regex.test(url);
+    const result: ValidURL = {
+      isValid: check,
+      errorMessage: check ? 'OK' : 'Invalid URL',
+    };
+
+    return result;
+  }
+
+  function submitMovie() {
+    const newMovie: Movie = {
+      title: movieTitle,
+      description: movieDescription,
+      imgUrl: movieImgURL,
+      imdbUrl: movieImdbURL,
+      imdbId: movieImdbId,
+    };
+
+    onAdd(newMovie);
+  }
+
+  const canConfirm = movieTitle && movieImdbId
+                     && isValidURL(movieImgURL).isValid
+                     && isValidURL(movieImdbURL).isValid;
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={() => {
+        submitMovie();
+        setCount((currentCount) => currentCount + 1);
+      }}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movieTitle}
+        onChange={setMovieTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={movieDescription}
+        onChange={setMovieDescription}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={movieImgURL}
+        onChange={setMovieImgURL}
+        required
+        // eslint-disable-next-line react/jsx-no-bind
+        checkValid={isValidURL}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={movieImdbURL}
+        onChange={setMovieImdbURL}
+        required
+        // eslint-disable-next-line react/jsx-no-bind
+        checkValid={isValidURL}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={movieImdbId}
+        onChange={setMovieImdbId}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +106,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!canConfirm}
           >
             Add
           </button>
