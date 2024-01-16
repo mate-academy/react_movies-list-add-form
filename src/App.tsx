ae3d5f2f4b9da0b52b62c9eea937efa4908a1b22
Vlad Pashko <vladpavloggg@gmail.com>
2024-01-16 22:17:36 +0200

    completed add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
+import { Movie } from './types/Movie';
 import moviesFromServer from './api/movies.json';
 
 export const App = () => {
+  const [moviesList, setMoviesList] = React.useState<Movie[]>(moviesFromServer);
+
+  const addNewMovie = (newMovie: Movie) => {
+    setMoviesList(previousList => [...previousList, newMovie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addNewMovie} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,97 @@
-import { useState } from 'react';
+import React from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+const InitialFieldsOfMovie: Movie = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+type Field =
+'title'
+| 'description'
+| 'imgUrl'
+| 'imdbUrl'
+| 'imdbId';
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = React.useState(0);
+  const [fieldsOfMovie, setFieldsOfMovie] = React
+    .useState(InitialFieldsOfMovie);
+
+  const isDisabled = !(
+    fieldsOfMovie.title
+    && fieldsOfMovie.imgUrl
+    && fieldsOfMovie.imdbUrl
+    && fieldsOfMovie.imdbId);
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    onAdd({
+      title: fieldsOfMovie.title,
+      description: fieldsOfMovie.description,
+      imgUrl: fieldsOfMovie.imgUrl,
+      imdbUrl: fieldsOfMovie.imdbUrl,
+      imdbId: fieldsOfMovie.imdbId,
+    });
+
+    setCount((prevValue) => prevValue + 1);
+    setFieldsOfMovie(InitialFieldsOfMovie);
+  };
+
+  const changeFieldHandler = (name: Field, value: string) => {
+    setFieldsOfMovie(prevFields => ({ ...prevFields, [name]: value }));
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={submitHandler}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={fieldsOfMovie.title}
+        onChange={(value) => changeFieldHandler('title', value)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={fieldsOfMovie.description}
+        onChange={(value) => changeFieldHandler('description', value)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={fieldsOfMovie.imgUrl}
+        onChange={(value) => changeFieldHandler('imgUrl', value)}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={fieldsOfMovie.imdbUrl}
+        onChange={(value) => changeFieldHandler('imdbUrl', value)}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={fieldsOfMovie.imdbId}
+        onChange={(value) => changeFieldHandler('imdbId', value)}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +100,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isDisabled}
           >
             Add
           </button>
