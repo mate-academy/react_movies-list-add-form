051022f3baf2aab3fe679d65c63e017dd84a2124
Oleksandr Rezanov <aleksandrrezanov17@gmail.com>
2024-01-17 15:41:52 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [movies, setMovies] = useState(moviesFromServer);
+  const [newMovieId, setNewMovieId] = useState('s');
+  const addMovie = (movie: Movie) => {
+    setMovies(existingMovies => [...existingMovies, movie]);
+    setNewMovieId(Math.random().toString(16).slice(2));
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie
+          onAdd={addMovie}
+          key={newMovieId}
+        />
       </div>
     </div>
   );
