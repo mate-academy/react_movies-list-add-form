f73d430c63a44a7c62e05fa115ea0466399b4ba2
Vasil Liakhivskyi <vazilx@gmail.com>
2024-01-22 19:49:06 +0100

    uppdate

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,33 +6,65 @@ type Props = {
   onAdd: (movie: Movie) => void;
 };
 
+// eslint-disable-next-line
+const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
   const [count] = useState(0);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [imgUrl, setImgUrl] = useState('');
-  const [imdbUrl, setImdbUrl] = useState('');
-  const [imdbId, setImdbId] = useState('');
-  const [hasError, setHasError] = useState(true);
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setNewMovie((prevMovie) => ({ ...prevMovie, [name]: value }));
+  };
+
+  const [hasError, setHasError] = useState({
+    title: true,
+    description: false,
+    imgUrl: true,
+    imdbUrl: true,
+    imdbId: true,
+  });
+
+  const getHesError = (name: string, v: boolean) => {
+    setHasError((prevHasError) => (
+      { ...prevHasError, [name]: v }
+    ));
+  };
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (!title || !imgUrl || !imdbUrl || !imdbId) {
+    if (
+      !newMovie.title.trim()
+      || !newMovie.imgUrl.trim()
+      || !newMovie.imdbUrl.trim()
+      || !newMovie.imdbId.trim()
+    ) {
       return;
     }
 
-    const movie: Movie = {
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
-    };
+    if (!pattern.test(newMovie.imgUrl) || !pattern.test(newMovie.imdbUrl)) {
+      return;
+    }
 
-    onAdd(movie);
+    onAdd(newMovie);
+    setNewMovie({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
   };
 
   return (
@@ -46,45 +78,45 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
       <TextField
         name="title"
         label="Title"
-        value={title}
-        onChange={setTitle}
+        value={newMovie.title}
+        onChange={handleChange}
         required
-        setHasError={setHasError}
+        getHesError={getHesError}
       />
 
       <TextField
         name="description"
         label="Description"
-        value={description}
-        onChange={setDescription}
-        setHasError={setHasError}
+        value={newMovie.description}
+        onChange={handleChange}
+        getHesError={getHesError}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={imgUrl}
-        onChange={setImgUrl}
+        value={newMovie.imgUrl}
+        onChange={handleChange}
         required
-        setHasError={setHasError}
+        getHesError={getHesError}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={imdbUrl}
-        onChange={setImdbUrl}
+        value={newMovie.imdbUrl}
+        onChange={handleChange}
         required
-        setHasError={setHasError}
+        getHesError={getHesError}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={imdbId}
-        onChange={setImdbId}
+        value={newMovie.imdbId}
+        onChange={handleChange}
         required
-        setHasError={setHasError}
+        getHesError={getHesError}
       />
 
       <div className="field is-grouped">
@@ -93,7 +125,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={hasError}
+            disabled={Object.values(hasError).includes(true)}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -7,8 +7,8 @@ type Props = {
   label?: string,
   placeholder?: string,
   required?: boolean,
-  onChange?: (newValue: string) => void,
-  setHasError: (newValue: boolean) => void,
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  getHesError: (name: string, v: boolean) => void,
 };
 
 function getRandomDigits() {
@@ -24,7 +24,7 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => { },
-  setHasError,
+  getHesError,
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
@@ -32,26 +32,27 @@ export const TextField: React.FC<Props> = ({
   // To show errors only if the field was touched (onBlur)
   const [errorMaseg, setErrorMaseg] = useState('');
 
+  // eslint-disable-next-line
   const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
   const verify = () => {
     if (required && !value) {
       setErrorMaseg(`${label} is required`);
-      setHasError(true);
+      getHesError(name, true);
 
       return;
     }
 
     if (label.includes('URL') && !pattern.test(value)) {
       setErrorMaseg(`${label} is not valid URL`);
-      setHasError(true);
+      getHesError(name, true);
     }
   };
 
   const change = (event: React.ChangeEvent<HTMLInputElement>) => {
     setErrorMaseg('');
-    onChange(event.target.value);
-    setHasError(false);
+    onChange(event);
+    getHesError(name, false);
   };
 
   return (
@@ -72,6 +73,7 @@ export const TextField: React.FC<Props> = ({
           value={value}
           onChange={change}
           onBlur={verify}
+          name={name}
         />
       </div>
 
