7f9e17a0fcbb473f34eaedbe0922d216a06cccc0
avakiel <79792776+avakiel@users.noreply.github.com>
2024-01-22 17:45:58 +0200

    fix comments

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,16 +1,10 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
+import { urlValidator } from '../../utils';
 
 interface NewMovieProps {
-  onAdd: (
-    movie: {
-      title: string,
-      description: string,
-      imgUrl: string,
-      imdbUrl: string,
-      imdbId: string,
-    }
-  ) => void
+  onAdd: (movie: Movie) => void
 }
 
 export const NewMovie: React.FC<NewMovieProps> = ({ onAdd }) => {
@@ -23,22 +17,11 @@ export const NewMovie: React.FC<NewMovieProps> = ({ onAdd }) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
 
-  const urlValidator = (urlString: string): boolean => {
-    // eslint-disable-next-line max-len
-    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
-
-    if (!urlString.match(pattern)) {
-      return false;
-    }
-
-    return true;
-  };
-
   const isDisabled
-  = !title
-  || !urlValidator(imgUrl)
-  || !urlValidator(imdbUrl)
-  || !imdbId;
+    = !title
+    || !urlValidator(imgUrl)
+    || !urlValidator(imdbUrl)
+    || !imdbId;
 
   function reset() {
     setTitle('');
