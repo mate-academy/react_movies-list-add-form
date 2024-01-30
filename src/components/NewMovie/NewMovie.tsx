34a6214c6a597b5f7ae2bbf4395b2205d74e3e6c
Tatiana <tanya_potapenko@bigmir.net>
2024-01-30 19:37:30 +0100

    react_movie-list-form

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,48 +6,136 @@ export const NewMovie = () => {
   // to reset touched status of all the `Field`s
   const [count] = useState(0);
 
+  const [formState, setFormState] = useState({
+    title: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+    description: '',
+  });
+
+  const isValidUrl = (value: string) => {
+    // eslint-disable-next-line
+    const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+    return pattern.test(value);
+  };
+
+  const [hasTitleError, setHasTitleError] = useState(true);
+  const [hasImgUrlError, setHasImgUrlError] = useState(true);
+  const [hasImdbUrlError, setHasImdbUrlError] = useState(true);
+  const [hasImdbIdError, setHasImdbIdError] = useState(true);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+
+    setFormState(prev => ({ ...prev, [name]: value }));
+  };
+
+  const areRequiredFieldsFilled = () => {
+    return (
+      formState.title.trim() !== ''
+      && formState.imgUrl.trim() !== ''
+      && formState.imdbUrl.trim() !== ''
+      && formState.imdbId.trim() !== ''
+    );
+  };
+
+  // const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+  //   const { name, value } = e.target;
+
+  //   switch (name) {
+  //     case 'title':
+  //       setHasTitleError(value.trim() === '');
+  //       break;
+  //     case 'imgUrl':
+  //       setHasImgUrlError(value.trim() === '');
+  //       break;
+  //     case 'imdbUrl':
+  //       setHasImdbUrlError(value.trim() === '');
+  //       break;
+  //     case 'imdbId':
+  //       setHasImdbIdError(value.trim() === '');
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!(hasTitleError
+      || hasImgUrlError || hasImdbUrlError || hasImdbIdError)) {
+      setFormState({
+        title: '',
+        imgUrl: '',
+        imdbUrl: '',
+        imdbId: '',
+        description: '',
+      });
+    }
+
+    setHasTitleError(false);
+    setHasImgUrlError(false);
+    setHasImdbUrlError(false);
+    setHasImdbIdError(false);
+  };
+
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleSubmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={formState.title}
+        onChange={handleChange}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={formState.description}
+        onChange={handleChange}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={formState.imgUrl}
+        onChange={handleChange}
+        customValidation={isValidUrl}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={formState.imdbUrl}
+        onChange={handleChange}
+        customValidation={isValidUrl}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={formState.imdbId}
+        onChange={handleChange}
       />
 
       <div className="field is-grouped">
         <div className="control">
           <button
             type="submit"
+            disabled={!areRequiredFieldsFilled()}
             data-cy="submit-button"
             className="button is-link"
+            onClick={() => {}}
           >
             Add
           </button>
