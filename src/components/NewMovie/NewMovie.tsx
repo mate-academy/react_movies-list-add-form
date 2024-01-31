a44e3305180139aee5a7d44e8bdba5c85e3e82cc
Dmytrosp <dmytrosp@gmail.com>
2024-01-31 14:14:53 +0200

    fix

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,46 +1,53 @@
-import React, { useState } from 'react';
-import { TextField } from '../TextField';
-import { Movie } from '../../types/Movie';
+import React, { useState } from "react";
+import { TextField } from "../TextField";
+import { Movie } from "../../types/Movie";
 
 type Props = {
   onAdd: (movie: Movie) => void;
 };
 
-const EMPTY = '';
+const initialMovieState = {
+  title: "",
+  description: "",
+  imgUrl: "",
+  imdbUrl: "",
+  imdbId: "",
+};
 
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [count, setCount] = useState(0);
 
-  const [title, setTitle] = useState(EMPTY);
-  const [description, setDescription] = useState(EMPTY);
-  const [imgUrl, setImgUrl] = useState(EMPTY);
-  const [imdbUrl, setImdbUrl] = useState(EMPTY);
-  const [imdbId, setImdbId] = useState(EMPTY);
+  // const [title, setTitle] = useState(EMPTY);
+  // const [description, setDescription] = useState(EMPTY);
+  // const [imgUrl, setImgUrl] = useState(EMPTY);
+  // const [imdbUrl, setImdbUrl] = useState(EMPTY);
+  // const [imdbId, setImdbId] = useState(EMPTY);
+
+  // const activeButton
+  //   = !!title.trim() && !!imgUrl.trim() && !!imdbUrl.trim() && !!imdbId.trim();
+
+  const [movie, setMovie] = useState<Movie>(initialMovieState);
+
+  // const activeButton = !!Object.values(movie).every((value) => value.trim());
 
   const activeButton
-    = !!title.trim() && !!imgUrl.trim() && !!imdbUrl.trim() && !!imdbId.trim();
+    = !!movie.title.trim()
+    && !!movie.imgUrl.trim()
+    && !!movie.imdbUrl.trim()
+    && !!movie.imdbId.trim();
 
   const resetForm = () => {
-    setTitle(EMPTY);
-    setDescription(EMPTY);
-    setImgUrl(EMPTY);
-    setImdbUrl(EMPTY);
-    setImdbId(EMPTY);
+    setMovie(initialMovieState);
+  };
+
+  const handleInputChange = (key: string, value: string) => {
+    setMovie((prevInputs) => ({ ...prevInputs, [key]: value }));
   };
 
   const handleAddClick = () => {
-    const newMovie: Movie = {
-      title,
-      description,
-      imgUrl,
-      imdbUrl,
-      imdbId,
-    };
-
-    onAdd(newMovie);
+    onAdd(movie);
     resetForm();
-
-    setCount(currentCount => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
   };
 
   return (
@@ -50,39 +57,39 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
       <TextField
         name="title"
         label="Title"
-        value={title}
-        onChange={setTitle}
+        value={movie.title}
+        onChange={handleInputChange}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value={description}
-        onChange={setDescription}
+        value={movie.description}
+        onChange={handleInputChange}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={imgUrl}
-        onChange={setImgUrl}
+        value={movie.imgUrl}
+        onChange={handleInputChange}
         required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={imdbUrl}
-        onChange={setImdbUrl}
+        value={movie.imdbUrl}
+        onChange={handleInputChange}
         required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={imdbId}
-        onChange={setImdbId}
+        value={movie.imdbId}
+        onChange={handleInputChange}
         required
       />
 
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
+import { Validator } from '../../helpers/Validator';
 
 type Props = {
   name: string,
@@ -7,7 +8,7 @@ type Props = {
   label?: string,
   placeholder?: string,
   required?: boolean,
-  onChange?: (newValue: string) => void,
+  onChange?: (newKey: string, newValue: string) => void,
 };
 
 function getRandomDigits() {
@@ -15,9 +16,6 @@ function getRandomDigits() {
     .toFixed(16)
     .slice(2);
 }
-
-// eslint-disable-next-line max-len
-const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
 export const TextField: React.FC<Props> = ({
   name,
@@ -31,13 +29,14 @@ export const TextField: React.FC<Props> = ({
 
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
-  let hasValidationError = false;
 
-  if (value && (name === 'imgUrl' || name === 'imdbUrl')) {
-    if (!value.match(pattern)) {
-      hasValidationError = true;
-    }
-  }
+  const hasValidationError = Validator(value, name);
+
+  // if (value && (name === 'imgUrl' || name === 'imdbUrl')) {
+  //   if (!value.match(pattern)) {
+  //     hasValidationError = true;
+  //   }
+  // }
 
   return (
     <div className="field">
@@ -55,7 +54,7 @@ export const TextField: React.FC<Props> = ({
           })}
           placeholder={placeholder}
           value={value}
-          onChange={(event) => onChange(event.target.value)}
+          onChange={(event) => onChange(name, event.target.value)}
           onBlur={() => setTouched(true)}
         />
       </div>
