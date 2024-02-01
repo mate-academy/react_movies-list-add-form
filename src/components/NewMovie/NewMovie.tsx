070546d24b5d65e13eb2fdca0a342fbd27c83531
Ruslan Hrynchuk <r.momos@gmail.com>
2024-02-01 16:56:12 +0200

    Solution wo 1 test del react

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import classNames from 'classnames';
 import './NewMovie.scss';
 import { TextField } from '../TextField';
