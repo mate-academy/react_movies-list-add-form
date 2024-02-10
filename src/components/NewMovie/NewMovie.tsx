e0116dbbf2b28e69530b3eb9eb1e4a9ddaa5a0cf
Mykhailo Hubko <mixelios@gmail.com>
2024-02-10 22:28:50 +0100

    add movie to movies list

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,150 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [titleError, setTitleError] = useState(false);
+
+  const [description, setDescription] = useState('');
+  const [descriptionError, setDescriptionError] = useState(false);
+
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbUrlError, setiMdbUrlError] = useState(false);
+
+  const [imgUrl, setImgUrl] = useState('');
+  const [imgUrlError, setImgUrlError] = useState(false);
+
+  const [imdbId, setImdbId] = useState('');
+  const [imdbIdError, setImdbIdError] = useState(false);
+
+  const [buttonView, setButtonView] = useState(true);
+
+  const reset = () => {
+    setTitle('');
+    setDescription('');
+    setImdbUrl('');
+    setImgUrl('');
+    setImdbId('');
+    setButtonView(true);
+  };
+
+  const handleMovieAdd = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setTitleError(!title);
+    setDescriptionError(!description);
+    setiMdbUrlError(!imdbUrl);
+    setImgUrlError(!imgUrl);
+    setImdbIdError(!imdbId);
+
+    if (titleError
+      || descriptionError
+      || imdbUrlError
+      || imgUrlError
+      || imdbIdError) {
+      return;
+    }
+
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    setCount(count + 1);
+    reset();
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleMovieAdd}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={(newValue) => {
+          setTitle(newValue);
+          if (title && description && imdbUrl && imgUrl && imdbId) {
+            setButtonView(false);
+          } else {
+            setButtonView(true);
+          }
+        }}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={(newValue) => {
+          setDescription(newValue);
+          if (title && description && imdbUrl && imgUrl && imdbId) {
+            setButtonView(false);
+          } else {
+            setButtonView(true);
+          }
+        }}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={(newValue) => {
+          setImgUrl(newValue);
+          if (title && description && imdbUrl && imgUrl && imdbId) {
+            setButtonView(false);
+          } else {
+            setButtonView(true);
+          }
+        }}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={(newValue) => {
+          setImdbUrl(newValue);
+          if (title && description && imdbUrl && imgUrl && imdbId) {
+            setButtonView(false);
+          } else {
+            setButtonView(true);
+          }
+        }}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={(newValue) => {
+          setImdbId(newValue);
+          if (title && description && imdbUrl && imgUrl && imdbId) {
+            setButtonView(false);
+          } else {
+            setButtonView(true);
+          }
+        }}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +153,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={buttonView}
           >
             Add
           </button>
