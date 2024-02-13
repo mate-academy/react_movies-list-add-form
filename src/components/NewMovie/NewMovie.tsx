48d07823b446f0da375e2ac872c08e43c6ad0d94
Maksym Kalashnyk <maxplay123890@gmail.com>
2024-02-13 03:57:50 +0200

    add new solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,92 +1,86 @@
-// Import necessary libraries and components
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+const initialFields: Movie = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  // Increase the count after successful form submission
+  // to reset touched status of all the `Field`s
   const [count, setCount] = useState(0);
-  const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    imgUrl: '',
-    imdbUrl: '',
-    imdbId: '',
-  });
+  const [movieFields, setMovieFields] = useState<Movie>(initialFields);
 
-  const handleInputChange = (name: string, value: string) => {
-    setFormData({
-      ...formData,
-      [name]: value.trim(),
-    });
+  const isButtonDisabled = Object.entries(movieFields)
+    .map(([key, value]) => (!value.trim() && key !== 'description'))
+    .some(Boolean);
+
+  const handleField = (name: string, value: string) => {
+    setMovieFields(prev => ({ ...prev, [name]: value }));
   };
 
-  type FormKeys = keyof typeof formData;
-
-  const handleBlur = (name: FormKeys) => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: prevFormData[name].trim(),
-    }));
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    onAdd(movieFields);
 
-    if (formData.title
-        && formData.imgUrl && formData.imdbUrl && formData.imdbId) {
-      setFormData({
-        title: '',
-        description: '',
-        imgUrl: '',
-        imdbUrl: '',
-        imdbId: '',
-      });
-
-      setCount(count + 1);
-    }
+    setMovieFields(initialFields);
+    setCount(prev => prev + 1);
   };
 
   return (
-    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={handleSubmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value={formData.title}
-        onChange={(value) => handleInputChange('title', value)}
-        onBlur={() => handleBlur('title')}
+        value={movieFields.title}
+        onChange={(value) => handleField('title', value)}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value={formData.description}
-        onChange={(value) => handleInputChange('description', value)}
+        value={movieFields.description}
+        onChange={(value) => handleField('description', value)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={formData.imgUrl}
-        onChange={(value) => handleInputChange('imgUrl', value)}
-        onBlur={() => handleBlur('imgUrl')}
+        value={movieFields.imgUrl}
+        onChange={(value) => handleField('imgUrl', value)}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={formData.imdbUrl}
-        onChange={(value) => handleInputChange('imdbUrl', value)}
-        onBlur={() => handleBlur('imdbUrl')}
+        value={movieFields.imdbUrl}
+        onChange={(value) => handleField('imdbUrl', value)}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={formData.imdbId}
-        onChange={(value) => handleInputChange('imdbId', value)}
-        onBlur={() => handleBlur('imdbId')}
+        value={movieFields.imdbId}
+        onChange={(value) => handleField('imdbId', value)}
+        required
       />
 
       <div className="field is-grouped">
@@ -95,8 +89,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={!formData.title
-              || !formData.imgUrl || !formData.imdbUrl || !formData.imdbId}
+            disabled={isButtonDisabled}
           >
             Add
           </button>
