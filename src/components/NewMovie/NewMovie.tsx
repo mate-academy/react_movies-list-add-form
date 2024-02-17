51cc3076ab4b1768ff3447a4eb23a60292d1af47
Oleh Hanenko <hello.isaev@gmail.com>
2024-02-17 03:32:43 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,125 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  'onAdd': (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const [titleInput, setTitleInput] = useState('');
+  const [hasTitleError, setHasTitleError] = useState(true);
+
+  const [descriptionInput, setDescriptionInput] = useState('');
+
+  const [imgUrlInput, setImgUrlInput] = useState('');
+  const [hasImgUrlInputError, setHasImgUrlInputError] = useState(true);
+
+  const [imdbUrlInput, setImdbUrlInput] = useState('');
+  const [hasImdbUrlInputError, setHasImdbUrlInputError] = useState(true);
+
+  const [imdbIdInput, setImdbIdInput] = useState('');
+  const [hasImdbIdInputError, setImdbIdInputError] = useState(true);
+
+  const hasError = () => {
+    if (
+      !hasTitleError
+      && !hasImgUrlInputError
+      && !hasImdbUrlInputError
+      && !hasImdbIdInputError) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newMovie = {
+      title: titleInput.trim(),
+      description: descriptionInput.trim(),
+      imgUrl: imgUrlInput.trim(),
+      imdbUrl: imdbUrlInput.trim(),
+      imdbId: imdbIdInput.trim(),
+    };
+
+    onAdd(newMovie);
+    setCount(count + 1);
+
+    setTitleInput('');
+    setDescriptionInput('');
+    setImdbUrlInput('');
+    setImgUrlInput('');
+    setImdbIdInput('');
+
+    setHasTitleError(true);
+    setHasImgUrlInputError(true);
+    setHasImdbUrlInputError(true);
+    setImdbIdInputError(true);
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={(event) => onSubmit(event)}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={titleInput}
+        onChange={newValue => {
+          setTitleInput(newValue);
+          setHasTitleError(!newValue);
+        }}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={descriptionInput}
+        onChange={newValue => setDescriptionInput(newValue)}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrlInput}
+        onChange={newValue => {
+          setImgUrlInput(newValue);
+          setHasImgUrlInputError(!newValue);
+        }}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrlInput}
+        onChange={newValue => {
+          setImdbUrlInput(newValue);
+          setHasImdbUrlInputError(!newValue);
+        }}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbIdInput}
+        onChange={newValue => {
+          setImdbIdInput(newValue);
+          setImdbIdInputError(!newValue);
+        }}
+        required
       />
 
       <div className="field is-grouped">
@@ -48,6 +128,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={hasError()}
           >
             Add
           </button>
