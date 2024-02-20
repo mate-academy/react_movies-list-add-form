4478badf071647a6df4e5b0df1128c7defd2ac7e
Kyryb <kyrybcode@gmail.com>
2024-02-20 17:56:00 +0200

    form implemented

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,45 +1,106 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void,
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  // eslint-disable-next-line max-len
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const isTitleRequired = true;
+  const [description, setDescription] = useState('');
+  const isDescriptionRequired = false;
+  const [imgUrl, setImgUrl] = useState('');
+  const isImageUrlRequired = true;
+  const [imdbUrl, setImdbUrl] = useState('');
+  const isImdbUrlRequired = true;
+  const [imdbId, setImdbId] = useState('');
+  const isImdbIdRequired = true;
+  const hasErrors = (isTitleRequired && !title.trim())
+    || (isDescriptionRequired && !description.trim())
+    || (isImageUrlRequired && !imgUrl.trim())
+    || (isImdbUrlRequired && !imdbUrl.trim())
+    || (isImdbIdRequired && !imdbId.trim())
+    || !pattern.test(imgUrl)
+    || !pattern.test(imdbUrl);
+
+  const resetForm = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const onFormSubmit = (event: React.FormEvent) => {
+    const newMovie: Movie = {
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    };
+
+    event.preventDefault();
+    setCount(prev => prev + 1);
+    onAdd(newMovie);
+    resetForm();
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={onFormSubmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
-        required
+        value={title}
+        onChange={setTitle}
+        required={isTitleRequired}
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={setDescription}
+        required={isDescriptionRequired}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={setImgUrl}
+        required={isImageUrlRequired}
+        validationFunc={() => pattern.test(imgUrl)}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={setImdbUrl}
+        required={isImdbUrlRequired}
+        validationFunc={() => pattern.test(imdbUrl)}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={setImdbId}
+        required={isImdbIdRequired}
       />
 
       <div className="field is-grouped">
@@ -48,6 +109,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={hasErrors}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -8,6 +8,7 @@ type Props = {
   placeholder?: string,
   required?: boolean,
   onChange?: (newValue: string) => void,
+  validationFunc?: () => boolean,
 };
 
 function getRandomDigits() {
@@ -23,6 +24,7 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  validationFunc = () => true,
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
@@ -30,6 +32,7 @@ export const TextField: React.FC<Props> = ({
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
+  const hasValidationError = touched && !validationFunc();
 
   return (
     <div className="field">
@@ -43,18 +46,23 @@ export const TextField: React.FC<Props> = ({
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError || hasValidationError,
           })}
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
           onBlur={() => setTouched(true)}
+          // onFocus={() => setTouched(false)}
         />
       </div>
 
-      {hasError && (
+      {(hasError && (
         <p className="help is-danger">{`${label} is required`}</p>
-      )}
+      )) || (hasValidationError && (
+        <p className="help is-danger">{`${label} is not valid`}</p>
+      )) }
+
+      {console.log(touched, label)}
     </div>
   );
 };
