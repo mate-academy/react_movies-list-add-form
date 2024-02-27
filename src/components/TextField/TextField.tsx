476e20dec2508922d9f0a3f615c27efafe8837a5
Krzychu92 <krzysztof.cichy1@gmail.com>
2024-02-27 11:22:34 +0100

    test checker

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -7,7 +7,7 @@ type Props = {
   label?: string;
   placeholder?: string;
   required?: boolean;
-  onChange?: (event: string) => void;
+  onChange?: (newValue: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 function getRandomDigits() {
@@ -37,8 +37,8 @@ export const TextField: React.FC<Props> = ({
 
       <div className="control">
         <input
-          type="text"
           name={name}
+          type="text"
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
@@ -46,7 +46,7 @@ export const TextField: React.FC<Props> = ({
           })}
           placeholder={placeholder}
           value={value}
-          onChange={event => onChange(event.target.value)}
+          onChange={onChange}
           onBlur={() => setTouched(true)}
         />
       </div>
