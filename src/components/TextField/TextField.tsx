7e1dc25a29131d179e109ff2e9bd1b4fc9376937
White Rabbit <0983792928ag@gmail.com>
2024-03-05 17:58:23 +0100

    add task solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -16,6 +16,14 @@ function getRandomDigits() {
     .slice(2);
 }
 
+/* eslint max-len: ["error", { "ignoreRegExpLiterals": true }] */
+
+const validateUrl = (url: string) => {
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  return pattern.test(url);
+};
+
 export const TextField: React.FC<Props> = ({
   name,
   value,
@@ -30,6 +38,20 @@ export const TextField: React.FC<Props> = ({
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
+  const [urlError, setUrlError] = useState(false);
+
+  const onBlurHandler = (event: React.FocusEvent<HTMLInputElement>) => {
+    setTouched(true);
+    if (required && !value) {
+      return;
+    }
+
+    if (name === 'imgUrl' || name === 'imdbUrl') {
+      const isValidUrl = validateUrl(event.target.value);
+
+      setUrlError(!isValidUrl);
+    }
+  };
 
   return (
     <div className="field">
@@ -43,17 +65,21 @@ export const TextField: React.FC<Props> = ({
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError || urlError,
           })}
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={event => onBlurHandler(event)}
         />
       </div>
 
-      {hasError && (
-        <p className="help is-danger">{`${label} is required`}</p>
+      {(hasError || urlError) && (
+        <p className="help is-danger">
+          {
+            urlError ? 'Invalid URL format' : `${label} is required`
+          }
+        </p>
       )}
     </div>
   );
