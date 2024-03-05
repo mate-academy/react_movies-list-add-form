ee2805688c58ac8cd0f09f5cbd40b1093576a7e0
ArtyWhit <julwer1k@gmail.com>
2024-03-05 18:07:24 +0200

    finished the task

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,6 +6,10 @@ type Props = {
   onAdd: (movie: Movie) => void;
 };
 
+/* eslint-disable */
+const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+/* eslint-enable */
+
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [count, setCount] = useState(0);
   const [initialMovie] = useState({
@@ -17,11 +21,17 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   });
   const [movie, setMovie] = useState({ ...initialMovie });
 
+  /* eslint-disable */
+
   const isFormNotValid =
     movie.title.trim().length <= 0 ||
     movie.imgUrl.trim().length <= 0 ||
     movie.imdbUrl.trim().length <= 0 ||
-    movie.imdbId.trim().length <= 0;
+    movie.imdbId.trim().length <= 0 ||
+    !pattern.test(movie.imgUrl.trim()) ||
+    !pattern.test(movie.imdbUrl.trim());
+
+  /* eslint-enable */
 
   const clear = () => {
     setMovie(initialMovie);
@@ -79,6 +89,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
             imgUrl: value,
           }))
         }
+        pattern={pattern}
         required
       />
 
@@ -92,6 +103,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
             imdbUrl: value,
           }))
         }
+        pattern={pattern}
         required
       />
 
