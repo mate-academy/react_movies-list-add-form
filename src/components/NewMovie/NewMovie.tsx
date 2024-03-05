34b78b507fb05cc2918a3e220d92be8af5defbeb
dmitruz <dmitruz970@gmail.com>
2024-03-05 21:08:45 +0200

    fix code and add pattern

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,42 +1,55 @@
-import { useCallback, useMemo, useState } from 'react';
+/* eslint-disable prettier/prettier */
+import { useState } from 'react';
 import { TextField } from '../TextField';
 import { Movie } from '../../types/Movie';
 
 interface Props {
   onAdd: (movie: Movie) => void;
 }
+// eslint-disable-next-line
+const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
-const defaultFormValue = {
-  title: '',
-  description: '',
-  imgUrl: '',
-  imdbUrl: '',
-  imdbId: '',
-};
 
-export const NewMovie = ({ onAdd }: Props) => {
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [count, setCount] = useState(0);
-  const [isValid, setIsValid] = useState(false);
-  const [formValue, setFormValue] = useState<Movie>(defaultFormValue);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImageURL] = useState('');
+  const [imdbUrl, setImdbURL] = useState('');
+  const [imdbId, setImdbID] = useState('');
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setFormValue(defaultFormValue);
-    setCount(count + 1);
-    onAdd(formValue);
+  const cantSubmit = title.trim().length <= 0
+    || imgUrl.trim().length <= 0
+    || imdbId.trim().length <= 0
+    || imdbUrl.trim().length <= 0
+    || !pattern?.test(imgUrl.trim())
+    || !pattern?.test(imdbUrl.trim());
+
+  const reset = () => {
+    setTitle('');
+    setDescription('');
+    setImageURL('');
+    setImdbURL('');
+    setImdbID('');
   };
 
-  useMemo(() => {
-    const { title, imgUrl, imdbUrl, imdbId } = formValue;
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
 
-    setIsValid(
-      !!(title?.trim() && imgUrl?.trim() && imdbUrl?.trim() && imdbId?.trim()),
-    );
-  }, [formValue]);
+    if (cantSubmit) {
+      return;
+    }
 
-  const updateFormValue = useCallback((key: string, value: string) => {
-    setFormValue(previousValue => ({ ...previousValue, [key]: value }));
-  }, []);
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+    reset();
+    setCount(count + 1);
+  };
 
   return (
     <form className="NewMovie" key={count} onSubmit={handleSubmit}>
@@ -45,50 +58,42 @@ export const NewMovie = ({ onAdd }: Props) => {
       <TextField
         name="title"
         label="Title"
-        value={formValue.title}
-        onChange={newValue => {
-          updateFormValue('description', newValue);
-        }}
+        value={title}
+        onChange={setTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value={formValue.description}
-        onChange={newValue => {
-          updateFormValue('description', newValue);
-        }}
+        value={description}
+        onChange={setDescription}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={formValue.imgUrl}
+        value={imgUrl}
         required
-        onChange={newValue => {
-          updateFormValue('imgUrl', newValue);
-        }}
+        onChange={setImageURL}
+        pattern={pattern}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={formValue.imdbUrl}
+        value={imdbUrl}
         required
-        onChange={newValue => {
-          updateFormValue('imgUrl', newValue);
-        }}
+        onChange={setImdbURL}
+        pattern={pattern}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={formValue.imdbId}
+        value={imdbId}
+        onChange={setImdbID}
         required
-        onChange={newValue => {
-          updateFormValue('imdbId', newValue);
-        }}
       />
 
       <div className="field is-grouped">
@@ -97,7 +102,7 @@ export const NewMovie = ({ onAdd }: Props) => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
-            disabled={!isValid}
+            disabled={cantSubmit}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -8,6 +8,7 @@ type Props = {
   placeholder?: string;
   required?: boolean;
   onChange?: (newValue: string) => void;
+  pattern?: RegExp;
 };
 
 function getRandomDigits() {
@@ -20,14 +21,23 @@ export const TextField: React.FC<Props> = ({
   label = name,
   placeholder = `Enter ${label}`,
   required = false,
-  onChange = () => {},
+  // eslint-disable-next-line prettier/prettier
+  onChange = () => { },
+  pattern,
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
-  // To show errors only if the field was touched (onBlur)
+  const [patternError, setPatternError] = useState(false);
   const [touched, setTouched] = useState(false);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPatternError(() => {
+      return !pattern?.test(event.target.value.trim());
+    });
+
+    onChange(event.target.value);
+  };
+
   const hasError = touched && required && !value;
+  const hasValidError = !hasError && patternError && pattern && touched;
 
   return (
     <div className="field">
@@ -45,10 +55,14 @@ export const TextField: React.FC<Props> = ({
           })}
           placeholder={placeholder}
           value={value}
-          onChange={event => onChange(event.target.value)}
+          onChange={handleChange}
           onBlur={() => setTouched(true)}
         />
       </div>
+
+      {hasValidError && (
+        <p className="help is-danger">{`${label} is required`}</p>
+      )}
 
       {hasError && <p className="help is-danger">{`${label} is required`}</p>}
     </div>
