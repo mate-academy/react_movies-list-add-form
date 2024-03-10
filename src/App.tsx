0576b390136266651059cd563e811f5398c75fb9
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-03-10 21:01:19 +0100

    working solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [newMovie, setNewMovie] = useState(moviesFromServer);
+
+  const movieAdd = (movie: Movie) => {
+    setNewMovie(current => [...current, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={newMovie} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={movie => movieAdd(movie)} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,133 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie = ({ onAdd }: Props) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const handleTrim = () => {
+    if (
+      Object.values(newMovie)
+        .filter(key => key !== newMovie.description)
+        .some(item => item.trim() === '')
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  // eslint-disable-next-line max-len, prettier/prettier
+  const pattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  const handleTitleChange = (newVal: string) => {
+    setNewMovie(current => ({
+      ...current,
+      title: newVal,
+    }));
+  };
+
+  const handleDescriptionChange = (newVal: string) => {
+    setNewMovie(current => ({
+      ...current,
+      description: newVal,
+    }));
+  };
+
+  const handleImgUrlChange = (newVal: string) => {
+    if (pattern.test(newVal)) {
+      setNewMovie(current => ({
+        ...current,
+        imgUrl: newVal,
+      }));
+    }
+  };
+
+  const handleImdbUrlChange = (newVal: string) => {
+    if (pattern.test(newVal)) {
+      setNewMovie(current => ({
+        ...current,
+        imdbUrl: newVal,
+      }));
+    }
+  };
+
+  const handleImdbIdChange = (newVal: string) => {
+    setNewMovie(current => ({
+      ...current,
+      imdbId: newVal,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    handleTrim();
+    onAdd(newMovie);
+    setCount(count + 1);
+    setNewMovie({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" onSubmit={handleSubmit} key={count}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={newMovie.title}
+        onChange={newValue => handleTitleChange(newValue)}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={newMovie.description}
+        onChange={newValue => handleDescriptionChange(newValue)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={newMovie.imgUrl}
+        onChange={newValue => handleImgUrlChange(newValue)}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={newMovie.imdbUrl}
+        onChange={newValue => handleImdbUrlChange(newValue)}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={newMovie.imdbId}
+        onChange={newValue => handleImdbIdChange(newValue)}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +135,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={handleTrim()}
           >
             Add
           </button>
