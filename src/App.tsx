84731ae524032c0547680cce8137da8c62f9bd52
Olha Shvets <lv160888soi@gmail.com>
2024-03-11 14:19:18 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export const App: React.FC = () => {
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
         <NewMovie onAdd={addMovie} />
