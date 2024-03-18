422cdc9eee75d2649fc61f33a37e1eae47dad20b
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-03-18 13:54:28 +0100

    form added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import './App.scss';
-import { MoviesList } from './components/MoviesList';
-import { NewMovie } from './components/NewMovie';
+import { useState } from 'react';
+
 import moviesFromServer from './api/movies.json';
 
+import { MoviesList } from './components/MoviesList';
+import { NewMovie } from './components/NewMovie';
+
+import { Movie } from './types/Movie';
+
+import './App.scss';
+
 export const App = () => {
+  const [movies, setMovies] = useState(moviesFromServer);
+
+  const onAdd = (movie: Movie) => {
+    setMovies(oldMovies => [...oldMovies, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={onAdd} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,111 @@
 import { useState } from 'react';
+
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+type State = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [state, setState] = useState<State>({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const reset = () => {
+    setState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setState(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const hasEmptyFields = Object.keys(state)
+    .map((key: string): boolean => {
+      if (key === 'description') {
+        return false;
+      }
+
+      const value = state[key as keyof State];
+
+      return value === '' || (typeof value === 'string' && value.trim() === '');
+    })
+    .every(key => key === false);
+
+  const onSubmit = (e: React.FormEvent) => {
+    setCount(oldCount => oldCount + 1);
+    e.preventDefault();
+    reset();
+    onAdd(state);
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={onSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={state.title}
+        onChange={handleOnChange}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={state.description}
+        onChange={handleOnChange}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={state.imgUrl}
+        onChange={handleOnChange}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={state.imdbUrl}
+        onChange={handleOnChange}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={state.imdbId}
+        onChange={handleOnChange}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +113,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!hasEmptyFields}
           >
             Add
           </button>
