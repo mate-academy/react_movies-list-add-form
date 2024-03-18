46267950dcaca855b7074ee164852e0d017c1231
Yaroslav Horodynskyi <gorodinskiyyar@gmail.com>
2024-03-18 17:58:50 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import React, { useState } from 'react';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [posts, setPosts] = useState<Movie[]>([...moviesFromServer]);
+
+  const onAdds = (movie: Movie) => {
+    setPosts([...posts, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={posts} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={onAdds} />
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client';
+import React from 'react';
 import { App } from './App';
 import 'bulma/css/bulma.css';
 
