3ab29a51a74358112df44298d2fd9e4e986a3ddd
Viktoria Moroz <viktoriaandriivna25@gmail.com>
2024-03-19 10:13:09 +0200

    task with some errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
+import { useState } from 'react';
 
 export const App = () => {
+  const [movie, setMovie] = useState<Movie[]>(moviesFromServer);
+
+  const addMovie = (newMovie: Movie) => {
+    setMovie(currentMovie => [...currentMovie, newMovie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movie} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovie} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,34 +1,127 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd: onSubmit }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  // #region change handlers
+  const handleTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleDescription = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value);
+  };
+
+  const handleImgUrl = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImgUrl(event.target.value);
+  };
+
+  const handleImdbUrl = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImdbUrl(event.target.value);
+  };
+
+  const handleImdbId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImdbId(event.target.value);
+  };
+
+  const handleDisabledButton = () => {
+    if (!title || !imgUrl || !imdbUrl || !imdbId) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  // #endregion
+
+  const reset = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    onSubmit({
+      title,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    setCount(value => value + 1);
+    reset();
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      action="/api/movies"
+      method="POST"
+      className="NewMovie"
+      key={count}
+      onSubmit={handleSubmit}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={handleTitle}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={handleDescription}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        onChange={handleImgUrl}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrl}
+        onChange={handleImdbUrl}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbId}
+        onChange={handleImdbId}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
           <button
+            disabled={handleDisabledButton()}
             type="submit"
             data-cy="submit-button"
             className="button is-link"
