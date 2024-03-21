6ba0ae2f989ead09cb3a5846e828b760a2cd4425
Ihor Bahniuk <hattorikanto@gmail.com>
2024-03-21 17:43:49 +0200

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import './App.scss';
+
+import { Movie } from './types/Movie';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
+
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
   const [movies, setMovies] = useState(moviesFromServer);
   const [count, setCount] = useState(0);
+
+  const handleOnAdd = (movie: Movie) => {
+    setMovies(current => [...current, movie]);
+    setCount(current => current + 1);
+  };
 
   return (
     <div className="page">
@@ -14,13 +22,7 @@ export const App: React.FC = () => {
         <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie
-          key={count}
-          onAdd={movie => {
-            setMovies(current => [...current, movie]);
-            setCount(current => current + 1);
-          }}
-        />
+        <NewMovie key={count} onAdd={movie => handleOnAdd(movie)} />
       </div>
     </div>
   );
