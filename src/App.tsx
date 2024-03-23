53f1d55860d63c4526f59daaff50d43ae860c56d
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2024-03-23 23:39:55 +0100

    add task

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './types/Movie';
+import { useState } from 'react';
 
 export const App = () => {
+  const [newMovies, setNewMovies] = useState<Movie[]>([]);
+
+  const addNewMovie = (movie: Movie) => {
+    setNewMovies([...newMovies, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={[...moviesFromServer, ...newMovies]} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addNewMovie} />
       </div>
     </div>
   );
