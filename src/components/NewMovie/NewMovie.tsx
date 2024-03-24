4a27bfd4858a5066631678ed9ea01616ec202e6b
Ann Busyhina <070196ann@gmail.com>
2024-03-24 13:58:37 +0200

    Solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,37 +1,105 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const isFormValidation = Boolean(
+    title.trim() && imgUrl.trim() && imdbUrl.trim() && imdbId.trim(),
+  );
+
+  const reset = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setCount(count + 1);
+    reset();
+
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+  };
+
+  const fields = [
+    {
+      name: 'title',
+      label: 'Title',
+      required: true,
+      onChange: setTitle,
+      value: title,
+    },
+    {
+      name: 'description',
+      label: 'Description',
+      onChange: setDescription,
+      value: description,
+    },
+    {
+      name: 'imgUrl',
+      label: 'Image URL',
+      required: true,
+      onChange: setImgUrl,
+      value: imgUrl,
+    },
+    {
+      name: 'imdbUrl',
+      label: 'Imdb URL',
+      required: true,
+      onChange: setImdbUrl,
+      value: imdbUrl,
+    },
+    {
+      name: 'imdbId',
+      label: 'Imdb ID',
+      required: true,
+      onChange: setImdbId,
+      value: imdbId,
+    },
+  ];
+
+  const textFields = fields.map(field => (
+    <TextField
+      key={field.name}
+      name={field.name}
+      label={field.label}
+      value={field.value}
+      onChange={field.onChange}
+      required={field.required}
+    />
+  ));
 
   return (
     <form className="NewMovie" key={count}>
       <h2 className="title">Add a movie</h2>
-
-      <TextField
-        name="title"
-        label="Title"
-        value=""
-        onChange={() => {}}
-        required
-      />
-
-      <TextField name="description" label="Description" value="" />
-
-      <TextField name="imgUrl" label="Image URL" value="" />
-
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
-
-      <TextField name="imdbId" label="Imdb ID" value="" />
-
+      {textFields}
       <div className="field is-grouped">
         <div className="control">
           <button
+            onClick={handleSubmit}
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFormValidation}
           >
             Add
           </button>
