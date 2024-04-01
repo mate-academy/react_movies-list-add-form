269c02cdd2879fec2beecbaeb529641d51f2f993
Oleh Marushchak <westernpc22@gmail.com>
2024-04-01 23:28:06 +0300

    add solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,137 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import React from 'react';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+interface Props {
+  onSubmit: (newMovie: Movie) => void;
+}
+
+export const NewMovie: React.FC<Props> = ({ onSubmit }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [required, setRequired] = useState(true);
+
   const [count] = useState(0);
 
+  const emptyValid =
+    !title.trim() || !imgUrl.trim() || !imdbUrl.trim() || !imdbId.trim();
+
+  const pattern =
+    // eslint-disable-next-line max-len
+    /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  const isURL = (input: string) => {
+    return pattern.test(input);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRequired(true);
+    setTitle(event.target.value);
+  };
+
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setDescription(event.target.value);
+    setRequired(true);
+  };
+
+  const handleImgUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImgUrl(event.target.value);
+    setRequired(true);
+  };
+
+  const handleImdbUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImdbUrl(event.target.value);
+    setRequired(true);
+  };
+
+  const handleImdbIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImdbId(event.target.value);
+    setRequired(true);
+  };
+
+  const reset = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    if (emptyValid) {
+      return;
+    }
+
+    if (!isURL(imgUrl) || !isURL(imdbUrl)) {
+      return;
+    }
+
+    onSubmit({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    setRequired(false);
+
+    reset();
+  }
+
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" onSubmit={handleSubmit} key={count}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
-        required
+        value={title}
+        onChange={handleTitleChange}
+        required={required}
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={handleDescriptionChange}
+        required={required}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        onChange={handleImgUrlChange}
+        required={required}
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrl}
+        onChange={handleImdbUrlChange}
+        required={required}
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbId}
+        onChange={handleImdbIdChange}
+        required={required}
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +139,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={emptyValid}
           >
             Add
           </button>
