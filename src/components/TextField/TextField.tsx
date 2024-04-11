5c8d211be5544e67e7cbb5916634b50c1bd63b68
Oles Partyka <oles.partyka.29@gmail.com>
2024-04-12 01:58:46 +0200

    Solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,12 +1,12 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
-
 type Props = {
   name: string;
   value: string;
   label?: string;
   placeholder?: string;
   required?: boolean;
+  pattern?: RegExp | undefined;
   onChange?: (newValue: string) => void;
 };
 
@@ -20,21 +20,19 @@ export const TextField: React.FC<Props> = ({
   label = name,
   placeholder = `Enter ${label}`,
   required = false,
+  pattern,
   onChange = () => {},
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
-  // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
+  const hasUrlError = pattern && !pattern?.test(value) && touched;
 
   return (
     <div className="field">
       <label className="label" htmlFor={id}>
         {label}
       </label>
-
       <div className="control">
         <input
           type="text"
@@ -49,8 +47,9 @@ export const TextField: React.FC<Props> = ({
           onBlur={() => setTouched(true)}
         />
       </div>
-
       {hasError && <p className="help is-danger">{`${label} is required`}</p>}
+
+      {hasUrlError && <p className="help is-danger">URL is invalid</p>}
     </div>
   );
 };
