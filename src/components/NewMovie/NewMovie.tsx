f82b1e09a89b04dfaff322e52a64dd93993ad456
svyatoslav <svyatoslavik9@gmail.com>
2024-04-15 20:38:39 +0200

    add solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -56,6 +56,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
           setDescription(value);
         }}
         value={description}
+        required
       />
 
       <TextField
@@ -65,6 +66,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
           setImgUrl(value);
         }}
         value={imgUrl}
+        required
       />
 
       <TextField
@@ -74,6 +76,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
           setImdbUrl(value);
         }}
         value={imdbUrl}
+        required
       />
 
       <TextField
@@ -83,6 +86,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
           setImdbId(value);
         }}
         value={imdbId}
+        required
       />
 
       <div className="field is-grouped">
