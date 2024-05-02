a1675f6780b060ae38052fdcccf5f9d06ad5ead5
Vladislav <7evenLays@gmail.com>
2024-05-02 17:59:56 +0300

    forms solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,111 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (x: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+
+  const initialFilmInputs: Movie = {
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  };
+
+  const [count, setCount] = useState(0);
+  const [filmInputs, setFilmInputs] = useState(initialFilmInputs);
+  const [isValidUrl, setIsValidUrl] = useState(true);
+
+  const { title, description, imgUrl, imdbUrl, imdbId } = filmInputs;
+
+  const pattern =
+    // eslint-disable-next-line
+    /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+  const isValidForm = title && imgUrl && imdbUrl && imdbId;
+
+  const handleInputChangeText = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    // const key = event.target.name;
+    // const value = event.target.value;
+    const { name, value } = event.target;
+
+    if (name === 'imgUrl' || name === 'imdbUrl') {
+      const isValid = pattern.test(value);
+
+      setIsValidUrl(isValid);
+    }
+
+    setFilmInputs(prevState => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  const resetInputsOnSubmit = () => {
+    setFilmInputs(initialFilmInputs);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (isValidForm) {
+      onAdd(filmInputs);
+      resetInputsOnSubmit();
+      setCount(prevCount => prevCount + 1);
+    }
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={handleInputChangeText}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={handleInputChangeText}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        onChange={handleInputChangeText}
+        isValid={isValidUrl}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrl}
+        onChange={handleInputChangeText}
+        isValid={isValidUrl}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbId}
+        onChange={handleInputChangeText}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +113,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isValidForm}
           >
             Add
           </button>
