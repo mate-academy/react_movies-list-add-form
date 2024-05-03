0aac68ceba5c1db280ceb11b1132d73b770faac2
Denis Reznik <reznik.d.o12@gmail.com>
2024-05-03 05:03:10 +0300

    add solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,118 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+/* eslint-disable */
+// const pattern =
+//   /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [validateImgUrl, setValidateImgUrl] = useState(false);
+  const [validateImdbUrl, setValidateImdbUrl] = useState(false);
+
+  const reset = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbId('');
+    setImdbUrl('');
+  };
+
+  const validateImg = (value: boolean) => {
+    setValidateImgUrl(value);
+  };
+
+  const validateImdb = (value: boolean) => {
+    setValidateImdbUrl(value);
+  };
+
+  const isDisabled =
+    title === '' ||
+    imdbId === '' ||
+    imdbUrl === '' ||
+    imgUrl === '' ||
+    validateImgUrl ||
+    validateImdbUrl;
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onAdd({
+      title: title,
+      description: description || '',
+      imgUrl: imgUrl,
+      imdbUrl: imdbUrl,
+      imdbId: imdbId,
+    });
+    setCount(prev => prev + 1);
+    reset();
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={value => {
+          setTitle(value);
+        }}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={value => {
+          setDescription(value);
+        }}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        onChange={value => {
+          setImgUrl(value);
+        }}
+        handleValidate={value => validateImg(value)}
+        validation={validateImgUrl}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrl}
+        onChange={value => {
+          setImdbUrl(value);
+        }}
+        handleValidate={value => validateImdb(value)}
+        validation={validateImdbUrl}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbId}
+        onChange={value => {
+          setImdbId(value);
+        }}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +120,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isDisabled}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -6,9 +6,15 @@ type Props = {
   value: string;
   label?: string;
   placeholder?: string;
+  validation?: boolean;
   required?: boolean;
   onChange?: (newValue: string) => void;
+  handleValidate?: (newValue: boolean) => void;
 };
+
+/* eslint-disable */
+const pattern =
+  /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
 function getRandomDigits() {
   return Math.random().toFixed(16).slice(2);
@@ -20,14 +26,25 @@ export const TextField: React.FC<Props> = ({
   label = name,
   placeholder = `Enter ${label}`,
   required = false,
+  validation = false,
   onChange = () => {},
+  handleValidate = () => {},
 }) => {
-  // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
-  // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
+  const [valid, setValid] = useState(validation);
   const hasError = touched && required && !value;
+
+  const handleOnBlur = () => {
+    setTouched(true);
+
+    if (name === 'imgUrl' || name === 'imdbUrl') {
+      const newValid = !pattern.test(value);
+      handleValidate(newValid);
+      setValid(newValid);
+    }
+  };
 
   return (
     <div className="field">
@@ -41,16 +58,17 @@ export const TextField: React.FC<Props> = ({
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError || valid,
           })}
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={handleOnBlur}
         />
       </div>
 
       {hasError && <p className="help is-danger">{`${label} is required`}</p>}
+      {valid && <p className="help is-danger">{`${label} is not valid`}</p>}
     </div>
   );
 };
