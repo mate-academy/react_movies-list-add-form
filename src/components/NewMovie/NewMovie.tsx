7f001472c329a15cfd9a3082745b99528249eec6
Denis Reznik <reznik.d.o12@gmail.com>
2024-05-03 05:11:04 +0300

    delete comments

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,10 +6,6 @@ type Props = {
   onAdd: (movie: Movie) => void;
 };
 
-/* eslint-disable */
-// const pattern =
-//   /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
-
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [count, setCount] = useState(0);
   const [title, setTitle] = useState('');
