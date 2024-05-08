038419134a949d00e3c4a2328cc4b5ceb140b168
Oksana Palahecha <oksanapalahecha@gmail.com>
2024-05-08 21:08:59 +0300

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,127 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type State = {
+  title: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+  description: string;
+};
+
+export const NewMovie: React.FC<{ onAdd: (movie: Movie) => void }> = ({
+  onAdd,
+}) => {
+  const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState<State>({
+    title: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+    description: '',
+  });
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [errors, setErrors] = useState<Partial<State>>({});
+
+  const resetInputState = () => {
+    setInputValue({
+      title: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+      description: '',
+    });
+    setErrors({});
+  };
+
+  const handleInput = (value: string, key: keyof State) => {
+    setInputValue(prevState => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
+  const handleBlur = (key: keyof State) => {
+    if (!inputValue[key].trim()) {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        [key]: `${key} is required`,
+      }));
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const pattern =
+      // eslint-disable-next-line max-len
+      /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
+    if (
+      !inputValue.imgUrl.match(pattern) ||
+      !inputValue.imdbUrl.match(pattern)
+    ) {
+      return;
+    }
+
+    onAdd(inputValue as Movie);
+    resetInputState();
+    setCount(prevCount => prevCount + 1);
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" onSubmit={handleSubmit} key={count}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={inputValue.title}
+        onChange={value => handleInput(value, 'title')}
+        onBlur={() => handleBlur('title')}
         required
+        error={errors.title}
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={inputValue.description}
+        onChange={value => handleInput(value, 'description')}
+        onBlur={() => handleBlur('description')}
+        error={errors.description}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={inputValue.imgUrl}
+        onChange={value => handleInput(value, 'imgUrl')}
+        onBlur={() => handleBlur('imgUrl')}
+        required
+        error={errors.imgUrl}
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={inputValue.imdbUrl}
+        onChange={value => handleInput(value, 'imdbUrl')}
+        onBlur={() => handleBlur('imdbUrl')}
+        required
+        error={errors.imdbUrl}
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={inputValue.imdbId}
+        onChange={value => handleInput(value, 'imdbId')}
+        onBlur={() => handleBlur('imdbId')}
+        required
+        error={errors.imdbId}
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +129,12 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={
+              !inputValue.title ||
+              !inputValue.imgUrl ||
+              !inputValue.imdbUrl ||
+              !inputValue.imdbId
+            }
           >
             Add
           </button>
@@ -40,3 +143,5 @@ export const NewMovie = () => {
     </form>
   );
 };
+
+export default NewMovie;
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -8,6 +8,9 @@ type Props = {
   placeholder?: string;
   required?: boolean;
   onChange?: (newValue: string) => void;
+  onBlur?: () => void;
+  error?: string;
+  validate?: (value: string) => boolean;
 };
 
 function getRandomDigits() {
@@ -21,13 +24,25 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  onBlur = () => {},
+  error,
+  validate = () => true,
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const hasError = touched && required && !value.trim();
+  const additionalError = !validate(value.trim()) ? 'Invalid URL' : '';
+
+  const handleBlur = () => {
+    setTouched(true);
+    onBlur();
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
 
   return (
     <div className="field">
@@ -41,16 +56,20 @@ export const TextField: React.FC<Props> = ({
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError || error || additionalError,
           })}
           placeholder={placeholder}
           value={value}
-          onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
       </div>
 
-      {hasError && <p className="help is-danger">{`${label} is required`}</p>}
+      {(hasError || error || additionalError) && (
+        <p className="help is-danger">
+          {error || additionalError || `${label} is required`}
+        </p>
+      )}
     </div>
   );
 };
