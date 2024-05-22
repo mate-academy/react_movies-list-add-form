f2594ff2ba00f531fffc5a4400a2f3e6e10e2858
Iryna Lysko <goiralysko@gmail.com>
2024-05-22 18:15:33 +0300

    add movie form with validation

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,120 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [imgUrlisValid, setimgUrlisValid] = useState(false);
+  const [imdbUrlisValid, setimdbUrlisValid] = useState(false);
+
+  const requiredFields = [title, imgUrl, imdbUrl, imdbId];
+  const validFields = [imgUrlisValid, imdbUrlisValid];
+
+  function fieldsAreFilled(fields: string[]) {
+    return fields.every(field => field.trim() !== '');
+  }
+
+  function fieldsAreValid(fields: boolean[]) {
+    return fields.every(field => field === true);
+  }
+
+  function reset() {
+    setTitle('');
+    setDesc('');
+    setImgUrl('');
+    setImdbId('');
+    setImdbUrl('');
+  }
+
+  const handleSubmit = () => {
+    onAdd({
+      title,
+      imgUrl,
+      imdbId,
+      imdbUrl,
+      description: desc,
+    });
+
+    setCount(count + 1);
+
+    reset();
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={newTitle => setTitle(newTitle)}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={desc}
+        onChange={newDesc => setDesc(newDesc)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        onChange={newImgUrl => setImgUrl(newImgUrl)}
+        validator={(value: string, pattern: RegExp) => {
+          if (pattern.test(value)) {
+            setimgUrlisValid(true);
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+            return true;
+          }
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+          setimgUrlisValid(false);
+
+          return false;
+        }}
+        required
+      />
+
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrl}
+        onChange={newImdbUrl => setImdbUrl(newImdbUrl)}
+        validator={(value: string, pattern: RegExp) => {
+          if (pattern.test(value)) {
+            setimdbUrlisValid(true);
+
+            return true;
+          }
+
+          setimdbUrlisValid(false);
+
+          return false;
+        }}
+        required
+      />
+
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbId}
+        onChange={newImdbId => setImdbId(newImdbId)}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +122,10 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={
+              !fieldsAreFilled(requiredFields) ||
+              (fieldsAreFilled(requiredFields) && !fieldsAreValid(validFields))
+            }
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -8,6 +8,7 @@ type Props = {
   placeholder?: string;
   required?: boolean;
   onChange?: (newValue: string) => void;
+  validator?: (value: string, pattern: RegExp) => boolean;
 };
 
 function getRandomDigits() {
@@ -21,13 +22,23 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  validator = () => 'is required',
 }) => {
   // generage a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
+  const pattern =
+    // eslint-disable-next-line max-len
+    /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  const baseErrorMsg = 'is required';
+  const inputErrorMsg = 'contains a mistake';
+
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const [errorMessage, setErrorMessage] = useState(baseErrorMsg);
+  const [isValid, setIsValid] = useState(false);
+
+  const hasError = touched && required && (!value || (value && !isValid));
 
   return (
     <div className="field">
@@ -46,11 +57,29 @@ export const TextField: React.FC<Props> = ({
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={() => {
+            setTouched(true);
+
+            if (validator) {
+              const isOk = validator(value, pattern);
+
+              if (!isOk && value !== '') {
+                setErrorMessage(inputErrorMsg);
+                setIsValid(false);
+              }
+
+              if (isOk) {
+                setErrorMessage(baseErrorMsg);
+                setIsValid(true);
+              }
+            }
+          }}
         />
       </div>
 
-      {hasError && <p className="help is-danger">{`${label} is required`}</p>}
+      {hasError && (
+        <p className="help is-danger">{`${label} ${errorMessage}`}</p>
+      )}
     </div>
   );
 };
