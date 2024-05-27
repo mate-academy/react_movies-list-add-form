510fa83e55ba34ec7b18a5ea401259d190b59ff8
Maria Shmakova <masha.shmakova@gmail.com>
2024-05-27 17:37:21 +0300

    react_movies_list_add_form

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,105 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+
+  const initialInfo = {
+    title: '',
+    description: '',
+    imageURL: '',
+    imdbURL: '',
+    imdbID: '',
+  };
+
+  const pattern = new RegExp(
+    '^((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=+$,\\w]+@)?[A-Za-z0-9.-]+|' +
+      '(?:www\\.|[-;:&=+$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[+~%/.\\w-_]*)?' +
+      '\\??(?:[-+=&;%@,.\\w_]*)#?(?:[,.!/\\\\\\w]*))?)$',
+  );
+  const urlValidator = (value: string): string | undefined => {
+    return pattern.test(value) ? undefined : 'Invalid URL';
+  };
+
+  const [movieInfo, setMovieInfo] = useState(initialInfo);
+
+  const handleValueChange = (name: string, value: string) => {
+    setMovieInfo(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    onAdd({
+      title: movieInfo.title.trim(),
+      description: movieInfo.description.trim(),
+      imgUrl: movieInfo.imageURL.trim(),
+      imdbUrl: movieInfo.imdbURL.trim(),
+      imdbId: movieInfo.imdbID.trim(),
+    });
+
+    setCount(prevCount => prevCount + 1);
+    setMovieInfo(initialInfo);
+  };
+
+  const disabledButton: boolean =
+    !movieInfo.title.trim() ||
+    !movieInfo.imageURL.trim() ||
+    !movieInfo.imdbURL.trim() ||
+    !movieInfo.imdbID.trim();
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleFormSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movieInfo.title}
+        onChange={value => handleValueChange('title', value)}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={movieInfo.description}
+        onChange={value => handleValueChange('description', value)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={movieInfo.imageURL}
+        onChange={value => handleValueChange('imageURL', value)}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={movieInfo.imdbURL}
+        onChange={value => handleValueChange('imdbURL', value)}
+        required
+        validate={urlValidator}
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={movieInfo.imdbID}
+        onChange={value => handleValueChange('imdbID', value)}
+        required
+        validate={urlValidator}
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +107,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={disabledButton}
           >
             Add
           </button>
