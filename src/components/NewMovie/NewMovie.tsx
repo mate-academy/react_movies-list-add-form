db378662c1762b3225336b64e1001f5ff423f063
Hy-tapa-kot <fantom2569@gmail.com>
2024-06-06 17:19:51 +0300

    add task solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,109 @@
 import { useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+interface MovieForm {
+  title: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+  description: string;
+}
+
+export const NewMovie = ({
+  onAddMovie,
+}: {
+  onAddMovie: (movie: Movie) => void;
+}) => {
+  const [form, setForm] = useState<Movie>({
+    title: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+    description: '',
+  });
+
+  const [touched, setTouched] = useState<{ [key: string]: boolean }>({});
+
+  const handleBlur = (name: string) => {
+    setTouched(prev => ({ ...prev, [name]: true }));
+  };
+
+  const handleChange = (name: string, value: string) => {
+    setForm(prevForm => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
+  const isFormValid = () => {
+    const requiredFields = ['title', 'imgUrl', 'imdbUrl', 'imdbId'];
+    return requiredFields.every(field => !!form[field as keyof MovieForm]);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onAddMovie(form);
+    setForm({
+      title: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+      description: '',
+    });
+    setTouched({});
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={form.title}
+        onChange={value => handleChange('title', value)}
+        onBlur={() => handleBlur('title')}
         required
+        touched={!!touched.title}
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={form.description}
+        onChange={value => handleChange('description', value)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={form.imgUrl}
+        onChange={value => handleChange('imgUrl', value)}
+        onBlur={() => handleBlur('imgUrl')}
+        required
+        touched={!!touched.imgUrl}
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={form.imdbUrl}
+        onChange={value => handleChange('imdbUrl', value)}
+        onBlur={() => handleBlur('imdbUrl')}
+        required
+        touched={!!touched.imdbUrl}
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={form.imdbId}
+        onChange={value => handleChange('imdbId', value)}
+        onBlur={() => handleBlur('imdbId')}
+        required
+        touched={!!touched.imdbId}
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +111,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFormValid()}
           >
             Add
           </button>
