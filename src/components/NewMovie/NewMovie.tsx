d1ef36e88a04ce955f8f20c2c45179efed98397b
Hanna Svirina <26anna07@ukr.net>
2024-06-20 18:54:21 +0200

    solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -14,8 +14,16 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
 
+  const pattern =
+    /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+
   const makeValidation =
-    title.trim() && imgUrl.trim() && imdbUrl.trim() && imdbId.trim();
+    title.trim() &&
+    imgUrl.trim() &&
+    imdbUrl.trim() &&
+    imdbId.trim() &&
+    pattern.test(imgUrl) &&
+    pattern.test(imdbUrl);
 
   const reset = () => {
     setTitle('');
