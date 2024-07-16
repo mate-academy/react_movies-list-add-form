4f3f5d8e90b8e69bb6d9d7ec69cbfc3a8856d82d
Nazarii Siryi <aakkrryyttoo99@gmail.com>
2024-07-16 05:45:51 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import './App.scss';
-import { MoviesList } from './components/MoviesList';
-import { NewMovie } from './components/NewMovie';
-import moviesFromServer from './api/movies.json';
-import { useState } from 'react';
-import { Movie } from './types/Movie';
+// import './App.scss';
+// import { MoviesList } from './components/MoviesList';
+// import { NewMovie } from './components/NewMovie';
+// import moviesFromServer from './api/movies.json';
+// import { useState } from 'react';
+// import { Movie } from './types/Movie';
 
-export const App = () => {
-  const [movies, setMovies] = useState([...moviesFromServer]);
+// export const App = () => {
+//   const [movies, setMovies] = useState([...moviesFromServer]);
 
-  const newMovies = (movie: Movie) => {
-    setMovies(currentMovies => [...currentMovies, movie]);
-  };
+//   const newMovies = (movie: Movie) => {
+//     setMovies(currentMovies => [...currentMovies, movie]);
+//   };
 
-  return (
-    <div className="page">
-      <div className="page-content">
-        <MoviesList movies={movies} />
-      </div>
-      <div className="sidebar">
-        <NewMovie onAdd={newMovies} />
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="page">
+//       <div className="page-content">
+//         <MoviesList movies={movies} />
+//       </div>
+//       <div className="sidebar">
+//         <NewMovie onAdd={newMovies} />
+//       </div>
+//     </div>
+//   );
+// };
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,53 +1,50 @@
-import React, { useState } from 'react';
-import { TextField } from '../TextField';
-import { Movie } from '../../types/Movie';
+// import React, { useState } from 'react';
+// import { TextField } from '../TextField';
+// import { Movie } from '../../types/Movie';
 
+// interface NewMoviesProps {
+//   onAdd: (newMovie: Movie) => void;
+// }
 
-interface NewMoviesProps {
-  onAdd: (newMovie: Movie) => void;
-}
+// export const NewMovie = ({ onAdd }): NewMoviesProps => {
+//   // Increase the count after successful form submission
+//   // to reset touched status of all the `Field`s
 
-export const NewMovie = ({ onAdd }): NewMoviesProps => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
+//   // const [count, setCount] = useState(0);
+//   // const [title, setTitle] = useState('');
+//   // const [description, setDescription] = useState('');
 
+//   return (
+//     <form className="NewMovie" key={count}>
+//       <h2 className="title">Add a movie</h2>
 
-  const [count, setCount] = useState(0);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  
+//       <TextField
+//         name="title"
+//         label="Title"
+//         value=""
+//         onChange={() => {}}
+//         required
+//       />
 
-  return (
-    <form className="NewMovie" key={count}>
-      <h2 className="title">Add a movie</h2>
+//       <TextField name="description" label="Description" value="" />
 
-      <TextField
-        name="title"
-        label="Title"
-        value=""
-        onChange={() => {}}
-        required
-      />
+//       <TextField name="imgUrl" label="Image URL" value="" />
 
-      <TextField name="description" label="Description" value="" />
+//       <TextField name="imdbUrl" label="Imdb URL" value="" />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+//       <TextField name="imdbId" label="Imdb ID" value="" />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
-
-      <TextField name="imdbId" label="Imdb ID" value="" />
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="submit"
-            data-cy="submit-button"
-            className="button is-link"
-          >
-            Add
-          </button>
-        </div>
-      </div>
-    </form>
-  );
-};
+//       <div className="field is-grouped">
+//         <div className="control">
+//           <button
+//             type="submit"
+//             data-cy="submit-button"
+//             className="button is-link"
+//           >
+//             Add
+//           </button>
+//         </div>
+//       </div>
+//     </form>
+//   );
+// };
