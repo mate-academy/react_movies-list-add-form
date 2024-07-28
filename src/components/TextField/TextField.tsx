3934cbf0173f179f63db2a3d4bc5a2c20ccd10c7
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-07-28 17:44:58 +0200

    solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -44,7 +44,7 @@ export const TextField: React.FC<Props> = ({
             'is-danger': hasError,
           })}
           placeholder={placeholder}
-          value={value}
+          value={value.trim()}
           onChange={event => onChange(event.target.value)}
           onBlur={() => setTouched(true)}
         />
