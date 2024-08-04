cb26b5a95394449fbf27714d8f84dba8389e5627
Andrew <beliyandr@gmail.com>
2024-08-04 22:56:54 +0300

    1

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,6 +6,29 @@ export const NewMovie = () => {
   // to reset touched status of all the `Field`s
   const [count] = useState(0);
 
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const handleTextfieldChange = (newValue: string) => {
+    setTitle(newValue);
+  };
+
+  const handleDescriptionField = (newValue: string) => {
+    setDescription(newValue);
+  };
+  const handleImgUrlField = (newValue: string) => {
+    setImgUrl(newValue);
+  };
+  const handleImdbUrlField = (newValue: string) => {
+    setImdbUrl(newValue);
+  };
+  const handleImdbIdField = (newValue: string) => {
+    setImdbId(newValue);
+  };
+
   return (
     <form className="NewMovie" key={count}>
       <h2 className="title">Add a movie</h2>
@@ -13,18 +36,41 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={handleTextfieldChange}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={handleDescriptionField}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        onChange={handleImgUrlField}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrl}
+        onChange={handleImdbUrlField}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbId}
+        onChange={handleImdbIdField}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
