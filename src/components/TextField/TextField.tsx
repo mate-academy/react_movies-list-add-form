d12027e75c2be41edbff96723f097751200b0466
beliyandr <beliyandr@gmail.com>
2024-08-07 23:18:51 +0300

    add validation url / but i commented its because pattern have length more 80 letters

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -27,22 +27,21 @@ export const TextField: React.FC<Props> = ({
 
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
+  // const [hasErrorValidation, setHasErrorValidation] = useState(true);
   const hasError = touched && required && !value;
 
-  // const pattern =
-  //   /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  // function hasErrorValid(inputName: string, inputValue: string) {
+  //   if (inputName === 'imgUrl' || inputName === 'imdbUrl') {
+  //     const pattern =
+  //       /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
-  // const validationTextField = () => {
-  //   console.log(pattern.test(value));
+  //     const regex = new RegExp(pattern);
 
-  //   if (name === 'imgUrl') {
-  //     if (pattern.test(value)) {
-  //       hasError = false;
-  //     }
+  //     setHasErrorValidation(regex.test(inputValue));
+  //   } else {
+  //     return;
   //   }
-  // };
-
-  // validationTextField();
+  // }
 
   return (
     <div className="field">
@@ -62,11 +61,17 @@ export const TextField: React.FC<Props> = ({
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event)}
-          onBlur={() => setTouched(true)}
+          onBlur={() => {
+            setTouched(true);
+            // hasErrorValid(name, value);
+          }}
         />
       </div>
 
       {hasError && <p className="help is-danger">{`${label} is required`}</p>}
+      {/* {!hasErrorValidation && (
+        <p className="help is-info">{`${label} is not correctly, you don't show this information about film`}</p>
+      )} */}
     </div>
   );
 };
