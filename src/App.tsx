fb441bcd931bf0f6ebf259f8aa9f97eb389552a9
NataliaTopornitska <natalia.topornitska3@gmail.com>
2024-08-18 14:47:46 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,3 @@
-// import './App.scss';
-// import { MoviesList } from './components/MoviesList';
-// import { NewMovie } from './components/NewMovie';
-// import moviesFromServer from './api/movies.json';
-
-// export const App = () => {
-//   return (
-//     <div className="page">
-//       <div className="page-content">
-//         <MoviesList movies={moviesFromServer} />
-//       </div>
-//       <div className="sidebar">
-//         <NewMovie /* onAdd={(movie) => {}} */ />
-//       </div>
-//     </div>
-//   );
-// };
-
 import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
