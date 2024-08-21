826f30db6365f7eb0480d79ac34503b217378327
pryslipskyi.d <denys.pryslipskyi@nixs.com>
2024-08-21 15:04:42 +0200

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
+import { Movie } from './types/Movie';
 import moviesFromServer from './api/movies.json';
 
 export const App = () => {
+  const [movie, setMovie] = useState(moviesFromServer);
+
+  const onAdd = (newMovie: Movie) => {
+    setMovie(currentMovie => [...currentMovie, newMovie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movie} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={onAdd} />
       </div>
     </div>
   );
