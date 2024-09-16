0cda9bce2236d12b179e1d1dd541776d6d1655ea
Sasha Stopochkin <stopochkin23@gmail.com>
2024-09-16 22:06:50 +0300

    add task solution 6

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
 import { Movie } from '../../types/Movie';
-
 type NewMovieProps = {
   onAdd: (movie: Movie) => void;
 };
-
 export const NewMovie: React.FC<NewMovieProps> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
@@ -14,30 +12,13 @@ export const NewMovie: React.FC<NewMovieProps> = ({ onAdd }) => {
   const [imgUrl, setImgUrl] = useState('');
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
-
   const [description, setDescription] = useState('');
-
-  // const [errors, setErrors] = useState({
-  //   title: false,
-  //   imgUrl: false,
-  //   imdbUrl: false,
-  //   imdbId: false,
-  // });
-
   const isFormValid = () => {
     return title && imgUrl && imdbUrl && imdbId;
   };
 
-  // const handleBlur = (field: string, value: string) => {
-  //   setErrors(prev => ({
-  //     ...prev,
-  //     [field]: !value.trim(),
-  //   }));
-  // };
-
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-
     const newErrors = {
       title: !title,
       imgUrl: !imgUrl,
@@ -62,64 +43,50 @@ export const NewMovie: React.FC<NewMovieProps> = ({ onAdd }) => {
       };
 
       onAdd(newMovie);
-
       setTitle('');
       setImgUrl('');
       setImdbUrl('');
       setImdbId('');
     }
-
-    // setErrors(newErrors);
   };
 
   return (
     <form className="NewMovie" key={count}>
       <h2 className="title">Add a movie</h2>
-
       <TextField
         name="title"
         label="Title"
         value={title}
         onChange={setTitle}
-        // onBlur={() => handleBlur('title', title)}
         required
-        // hasError={errors.title}
       />
-
       <TextField
         name="description"
         label="Description"
         value={description}
         onChange={setDescription}
       />
-
       <TextField
         name="imgUrl"
         label="Image URL"
         value={imgUrl}
         onChange={setImgUrl}
-        // onBlur={() => handleBlur('imgUrl', imgUrl)}
-        // hasError={errors.imgUrl}
+        required
       />
-
       <TextField
         name="imdbUrl"
         label="Imdb URL"
         value={imdbUrl}
         onChange={setImdbUrl}
-        // onBlur={() => handleBlur('imdbUrl', imdbUrl)}
-        // hasError={errors.imdbUrl}
+        required
       />
-
       <TextField
         name="imdbId"
         label="Imdb ID"
         value={imdbId}
         onChange={setImdbId}
-        // onBlur={() => handleBlur('imdbId', imdbId)}
-        // hasError={errors.imdbId}
+        required
       />
-
       <div className="field is-grouped">
         <div className="control">
           <button
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
-
 type Props = {
   name: string;
   value: string;
@@ -8,10 +7,7 @@ type Props = {
   placeholder?: string;
   required?: boolean;
   onChange?: (newValue: string) => void;
-  // onBlur?: () => void;
-  // hasError?: boolean;
 };
-
 function getRandomDigits() {
   return Math.random().toFixed(16).slice(2);
 }
@@ -24,10 +20,7 @@ export const TextField: React.FC<Props> = ({
   required = false,
   onChange = () => {},
 }) => {
-  // generate a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
-  // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
   const hasError = touched && required && !value;
 
@@ -36,7 +29,6 @@ export const TextField: React.FC<Props> = ({
       <label className="label" htmlFor={id}>
         {label}
       </label>
-
       <div className="control">
         <input
           type="text"
@@ -51,7 +43,6 @@ export const TextField: React.FC<Props> = ({
           onBlur={() => setTouched(true)}
         />
       </div>
-
       {hasError && <p className="help is-danger">{`${label} is required`}</p>}
     </div>
   );
