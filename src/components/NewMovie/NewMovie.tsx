0be729235de3ab6fa91b7607bee22cfb6b7eb5e6
luter <newgglutt@gmail.com>
2024-09-28 23:19:47 +0300

    sss

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,97 +1,104 @@
 import React, { useState } from 'react';
-import { TextField } from '../TextField/TextField';
+import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie: React.FC = () => {
-  const [movie, setMovie] = useState({
-    title: '',
-    description: '',
-    imgUrl: '',
-    imdbUrl: '',
-    imdbId: '',
-  });
-  const [isFormValid, setIsFormValid] = useState(false);
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
 
-  const handleChange = (key: string, value: string) => {
-    setMovie(prev => ({ ...prev, [key]: value }));
+const emptyFormFields: Movie = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [formData, setFormData] = useState(emptyFormFields);
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormData(currentData => ({
+      ...currentData,
+      [name]: value,
+    }));
   };
 
-  const validateForm = () => {
-    const isValid =
-      movie.title.trim() && movie.imgUrl.trim() && movie.imdbUrl.trim();
+  const inactiveButton =
+    !formData.title.trim() ||
+    !formData.imgUrl.trim() ||
+    !formData.imdbUrl.trim() ||
+    !formData.imdbId.trim();
 
-    setIsFormValid(isValid);
-  };
-
-  const handleBlur = () => {
-    validateForm();
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (isFormValid) {
-      // Clear form
-      setMovie({
-        title: '',
-        description: '',
-        imgUrl: '',
-        imdbUrl: '',
-        imdbId: '',
-      });
-      setIsFormValid(false);
+
+    if (
+      formData.title &&
+      formData.imgUrl &&
+      formData.imdbUrl &&
+      formData.imdbId
+    ) {
+      onAdd(formData);
     }
+
+    setCount(currentCount => currentCount + 1);
+    setFormData(emptyFormFields);
   };
 
   return (
-    <form className="NewMovie" onSubmit={handleSubmit}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmitForm}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value={movie.title}
+        value={formData.title}
+        onChange={handleOnChange}
         required
-        onChange={value => handleChange('title', value)}
-        onBlur={handleBlur}
       />
 
       <TextField
         name="description"
         label="Description"
-        value={movie.description}
-        onChange={value => handleChange('description', value)}
+        value={formData.description}
+        onChange={handleOnChange}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value={movie.imgUrl}
+        value={formData.imgUrl}
+        onChange={handleOnChange}
         required
-        onChange={value => handleChange('imgUrl', value)}
-        onBlur={handleBlur}
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value={movie.imdbUrl}
+        value={formData.imdbUrl}
+        onChange={handleOnChange}
         required
-        onChange={value => handleChange('imdbUrl', value)}
-        onBlur={handleBlur}
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value={movie.imdbId}
-        onChange={value => handleChange('imdbId', value)}
+        value={formData.imdbId}
+        onChange={handleOnChange}
+        required
       />
 
       <div className="field is-grouped">
         <div className="control">
           <button
+            disabled={inactiveButton}
             type="submit"
+            data-cy="submit-button"
             className="button is-link"
-            disabled={!isFormValid}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,54 +1,51 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
-
 type Props = {
   name: string;
   value: string;
-  label: string;
+  label?: string;
   placeholder?: string;
   required?: boolean;
-  onChange?: (newValue: string) => void;
-  onBlur?: () => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
+function getRandomDigits() {
+  return Math.random().toFixed(16).slice(2);
+}
 export const TextField: React.FC<Props> = ({
   name,
   value,
-  label,
+  label = name,
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
-  onBlur = () => {},
 }) => {
-  const [hasError, setHasError] = useState(false); // Видалили 'touched', якщо він не використовується
+  const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
-  const handleBlur = () => {
-    if (required && !value.trim()) {
-      setHasError(true);
-    } else {
-      setHasError(false);
-    }
-
-    onBlur();
-  };
+  const [touched, setTouched] = useState(false);
+  const hasError = touched && required && !value;
 
   return (
     <div className="field">
-      <label className="label" htmlFor={name}>
+      <label className="label" htmlFor={id}>
         {label}
       </label>
       <div className="control">
         <input
           type="text"
-          id={name}
-          className={classNames('input', { 'is-danger': hasError })}
+          id={id}
+          data-cy={`movie-${name}`}
+          className={classNames('input', {
+            'is-danger': hasError,
+          })}
           placeholder={placeholder}
+          name={name}
           value={value}
-          onChange={event => onChange(event.target.value)}
-          onBlur={handleBlur}
+          onChange={onChange}
+          onBlur={() => setTouched(true)}
         />
-        {hasError && <p className="help is-danger">{`${label} is required`}</p>}
       </div>
+      {hasError && <p className="help is-danger">{`${label} is required`}</p>}
     </div>
   );
 };
