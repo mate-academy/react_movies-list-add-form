7ab5ad14ea3c42be05647ffa8cb2e920b5befc72
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-09-28 16:36:07 +0200

    commit message

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,100 @@
 import { useState } from 'react';
+import { Movie } from '../../types/Movie';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
+type InputTypes = 'title' | 'description' | 'imgUrl' | 'imdbUrl' | 'imdbId';
+
+type NewMovieProps = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<NewMovieProps> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const { title, description, imgUrl, imdbUrl, imdbId } = newMovie;
+
+  const hasValidInputs =
+    title.trim() &&
+    description.trim() &&
+    imgUrl.trim() &&
+    imdbUrl.trim() &&
+    imdbId.trim();
+
+  const reset = () => {
+    setNewMovie({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
+  function handleInputChange(name: InputTypes, value: string): void {
+    setNewMovie(prevMovie => ({
+      ...prevMovie,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    onAdd(newMovie);
+    reset();
+    setCount(count + 1);
+  }
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={newMovie.title}
+        onChange={value => handleInputChange('title', value)}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value=""
+        onChange={value => handleInputChange('title', value)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={newMovie.imgUrl}
+        onChange={value => handleInputChange('title', value)}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={newMovie.imdbUrl}
+        onChange={value => handleInputChange('title', value)}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={newMovie.imdbId}
+        onChange={value => handleInputChange('title', value)}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +102,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!hasValidInputs}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -10,6 +10,8 @@ type Props = {
   onChange?: (newValue: string) => void;
 };
 
+type InputTypes = 'title' | 'description' | 'imgUrl' | 'imdbUrl' | 'imdbId';
+
 function getRandomDigits() {
   return Math.random().toFixed(16).slice(2);
 }
@@ -20,14 +22,58 @@ export const TextField: React.FC<Props> = ({
   label = name,
   placeholder = `Enter ${label}`,
   required = false,
-  onChange = () => {},
+  // onChange = () => {},
 }) => {
   // generate a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imdbId, setImdbId] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [isValueChanged, setIsValueChanged] = useState(false);
+
+  function handleInputChange(
+    inputType: InputTypes,
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) {
+    setIsValueChanged(true);
+
+    switch (inputType) {
+      case 'title':
+        return setTitle(event.target.value);
+      case 'description':
+        return setDescription(event.target.value);
+      case 'imdbId':
+        return setImdbId(event.target.value);
+      case 'imdbUrl':
+        return setImdbUrl(event.target.value);
+      case 'imgUrl':
+        return setImgUrl(event.target.value);
+      default:
+        return;
+    }
+  }
+
+  function handleValue(nameProperty: InputTypes) {
+    switch (nameProperty) {
+      case 'title':
+        return title;
+      case 'description':
+        return description;
+      case 'imdbId':
+        return imdbId;
+      case 'imdbUrl':
+        return imdbUrl;
+      case 'imgUrl':
+        return imgUrl;
+    }
+  }
+
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const hasError = touched && required && !isValueChanged;
 
   return (
     <div className="field">
@@ -44,9 +90,10 @@ export const TextField: React.FC<Props> = ({
             'is-danger': hasError,
           })}
           placeholder={placeholder}
-          value={value}
-          onChange={event => onChange(event.target.value)}
+          value={handleValue(name as InputTypes)}
+          onChange={event => handleInputChange(name as InputTypes, event)}
           onBlur={() => setTouched(true)}
+          required={name !== 'description'}
         />
       </div>
 
