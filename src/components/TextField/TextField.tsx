794f0e44f00a12e9eabbeeb4d5d8a20afa2a1e3f
Roma Zuravliov <janmorozov1988@gmail.com>
2024-09-30 21:23:47 +0300

    add task solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -36,6 +36,7 @@ export const TextField: React.FC<Props> = ({
 
       <div className="control">
         <input
+          name={name}
           type="text"
           id={id}
           data-cy={`movie-${name}`}
