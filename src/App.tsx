0b54dac2a4ffac31f7bcfacb444e6346e888ca06
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2024-10-04 17:56:32 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [movieCollection, setMovieCollection] =
+    useState<Movie[]>(moviesFromServer);
+
+  function handleAddMovie(movie: Movie) {
+    setMovieCollection(movies => [...movies, movie]);
+  }
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movieCollection} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={movie => handleAddMovie(movie)} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,90 @@
-import { useState } from 'react';
+import { useState, FC, FormEvent } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+interface NewMovieProps {
+  onAdd: (Movie: Movie) => void;
+}
+
+export const NewMovie: FC<NewMovieProps> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+
+  const [newTitle, setNewTitle] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+  const [newImgUrl, setNewImgUrl] = useState('');
+  const [newImdbUrl, setNewImdbUrl] = useState('');
+  const [newImdbId, setNewImdbId] = useState('');
+
+  const isReadyToSubmit = newTitle && newImgUrl && newImdbUrl && newImdbId;
+
+  const newMovie: Movie = {
+    title: newTitle,
+    description: newDescription,
+    imgUrl: newImgUrl,
+    imdbUrl: newImdbUrl,
+    imdbId: newImdbId,
+  };
+
+  function handleFormReset() {
+    setNewDescription('');
+    setNewImdbId('');
+    setNewImdbUrl('');
+    setNewImgUrl('');
+    setNewTitle('');
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    setCount(current => current + 1);
+
+    onAdd(newMovie);
+
+    handleFormReset();
+  }
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={newTitle}
+        onChange={value => setNewTitle(value)}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={newDescription}
+        onChange={value => setNewDescription(value)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={newImgUrl}
+        onChange={value => setNewImgUrl(value)}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={newImdbUrl}
+        onChange={value => setNewImdbUrl(value)}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={newImdbId}
+        onChange={value => setNewImdbId(value)}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +92,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isReadyToSubmit}
           >
             Add
           </button>
