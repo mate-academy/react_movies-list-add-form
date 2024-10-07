950943f4478a7fc962cdd4f0fe102dd9682149f5
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-10-07 19:34:34 +0300

    react_movies-list-add-form

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [movieList, setMovieList] = useState<Movie[]>(moviesFromServer)
+
+  const movie = (newMovie:Movie) =>{
+    setMovieList(currentMovieList=>[...currentMovieList, newMovie])
+  }
+
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movieList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={movie} />
       </div>
     </div>
   );
