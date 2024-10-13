45334ea646c2390f1f02d37c7203c62d0d050341
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-10-13 18:44:20 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { useEffect, useState } from 'react';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [currentMovies, setCurrentMovies] = useState<Movie[]>(moviesFromServer);
+  const [newMovie, setNewMovie] = useState<Movie[] | []>([]);
+
+  useEffect(() => {
+    if (newMovie.length > 0) {
+      setCurrentMovies(prevMovies => [...prevMovies, ...newMovie]);
+      setNewMovie([]);
+    }
+  }, [newMovie]);
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={currentMovies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie setNewMovie={setNewMovie} />
       </div>
     </div>
   );
