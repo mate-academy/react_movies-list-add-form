323fea878d28e5cf5739d85f11487f31e48f20d8
Anna Agerone <olesnevych12@gmail.com>
2024-10-18 10:22:00 -0500

    Solution

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -3,10 +3,13 @@ import React, { useState } from 'react';
 
 type Props = {
   name: string;
-  value: string;
+  value?: string;
   label?: string;
   placeholder?: string;
   required?: boolean;
+  touched?: boolean;
+  setTouched?: (value: boolean) => void;
+  setRequired?: (value: boolean) => void;
   onChange?: (newValue: string) => void;
 };
 
@@ -19,15 +22,13 @@ export const TextField: React.FC<Props> = ({
   value,
   label = name,
   placeholder = `Enter ${label}`,
-  required = false,
   onChange = () => {},
+  required = true,
+  touched,
+  setTouched,
 }) => {
-  // generate a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
-
-  // To show errors only if the field was touched (onBlur)
-  const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const hasError = touched && required && value !== undefined && !value.trim();
 
   return (
     <div className="field">
@@ -41,16 +42,18 @@ export const TextField: React.FC<Props> = ({
           id={id}
           data-cy={`movie-${name}`}
           className={classNames('input', {
-            'is-danger': hasError,
+            'is-danger': hasError && name !== 'description',
           })}
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={() => setTouched?.(true)}
         />
       </div>
 
-      {hasError && <p className="help is-danger">{`${label} is required`}</p>}
+      {hasError && name !== 'description' && (
+        <p className="help is-danger">{`${label} is required`}</p>
+      )}
     </div>
   );
 };
