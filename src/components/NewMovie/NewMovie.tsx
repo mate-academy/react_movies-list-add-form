ae4b95b263d026486243e738e651b5972ca4f6a0
Lesia Bihun <lbr15@ukr.net>
2024-10-25 13:56:46 +0300

    add task solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -9,6 +9,9 @@ type Props = {
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
+
+  const pattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+
   const [count, setCount] = useState(0);
 
   const [title, setTitle] = useState('');
@@ -17,16 +20,31 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [imdbUrl, setImdbUrl] = useState('');
   const [imdbId, setImdbId] = useState('');
 
+  const [imgUrlError, setImgUrlError] = useState(false);
+  const [imdbUrlError, setImdbUrlError] = useState(false);
+
   const reset = () => {
     setTitle('');
     setDescription('');
     setImgUrl('');
     setImdbUrl('');
     setImdbId('');
+    setImgUrlError(false);
+    setImdbUrlError(false);
   };
 
   const handleAdd = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const isImgUrlValid = pattern.test(imgUrl);
+    const isImdbUrlValid = pattern.test(imdbUrl);
+
+    setImgUrlError(!isImgUrlValid);
+    setImdbUrlError(!isImdbUrlValid);
+
+    if (!isImgUrlValid || !isImdbUrlValid) {
+      return;
+    }
 
     setCount(prevCount => prevCount + 1);
 
@@ -41,7 +59,8 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
     reset();
   };
 
-  const isFormValid = title && imgUrl && imdbUrl && imdbId;
+  const isFormValid =
+    title && imgUrl && imdbUrl && imdbId && !imgUrlError && !imdbUrlError;
 
   return (
     <form className="NewMovie" key={count} onSubmit={handleAdd}>
@@ -52,6 +71,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         label="Title"
         value={title}
         onChange={event => setTitle(event)}
+        onBlur={() => {}}
         required
       />
 
@@ -67,22 +87,27 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
         label="Image URL"
         value={imgUrl}
         onChange={event => setImgUrl(event)}
+        onBlur={() => setImgUrlError(!pattern.test(imgUrl))}
         required
       />
+      {imgUrlError && <p className="help is-danger">Invalid Image URL</p>}
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
         value={imdbUrl}
         onChange={event => setImdbUrl(event)}
+        onBlur={() => setImdbUrlError(!pattern.test(imdbUrl))}
         required
       />
+      {imdbUrlError && <p className="help is-danger">Invalid IMDb URL</p>}
 
       <TextField
         name="imdbId"
         label="Imdb ID"
         value={imdbId}
         onChange={event => setImdbId(event)}
+        onBlur={() => {}}
         required
       />
 
