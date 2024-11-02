9d8168718b9939c58dc3095dcb13efb9052d3072
Nikita Serdyukov <nikita.serdyukov.20033002@gmail.com>
2024-11-02 19:16:31 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
 
 export const App = () => {
+  const [movies, setMovies] = useState(moviesFromServer);
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie
+          onAdd={movie => {
+            setMovies([...movies, movie]);
+          }}
+        />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,34 +1,95 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+  const [titleValue, setTitleValue] = useState('');
+  const [descriptionValue, setDescriptionValue] = useState('');
+  const [imgUrlValue, setImgUrlValue] = useState('');
+  const [imdbUrlValue, setImdbUrlValue] = useState('');
+  const [imdbIdValue, setImdbIdValue] = useState('');
+
+  const verified =
+    titleValue && imgUrlValue && imdbUrlValue && imdbIdValue ? true : false;
+
+  const reset = () => {
+    setTitleValue('');
+    setDescriptionValue('');
+    setImgUrlValue('');
+    setImdbUrlValue('');
+    setImdbIdValue('');
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (verified) {
+      onAdd({
+        title: titleValue,
+        description: descriptionValue,
+        imgUrl: imgUrlValue,
+        imdbUrl: imdbUrlValue,
+        imdbId: imdbIdValue,
+      });
+
+      reset();
+
+      setCount(c => c + 1);
+    }
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        required
+        value={titleValue}
+        onChange={newValue => setTitleValue(newValue)}
+      />
+
+      <TextField
+        name="description"
+        value={descriptionValue}
+        label="Description"
+        onChange={newValue => setDescriptionValue(newValue)}
+      />
+
+      <TextField
+        name="imgUrl"
+        value={imgUrlValue}
+        label="Image URL"
+        onChange={newValue => setImgUrlValue(newValue)}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrlValue}
+        onChange={newValue => setImdbUrlValue(newValue)}
+        required
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
-
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
-
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbIdValue}
+        onChange={newValue => setImdbIdValue(newValue)}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
           <button
+            disabled={!verified}
             type="submit"
             data-cy="submit-button"
             className="button is-link"
