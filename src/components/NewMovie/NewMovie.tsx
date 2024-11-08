9aca2ba7ddc7b6b174ae87a9004e6587218afd68
Vladyslav Humeniak <vladyslavhumenyak@gmail.com>
2024-11-08 14:06:03 +0200

    problems

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,119 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onSubmit: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onSubmit }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+
+  const [count, setCount] = useState(0);
+  const [titleChecker, setTitleChecker] = useState('');
+  const [descriptionChecker, setDescriptionChecker] = useState('');
+  const [imgUrlChecker, setImgUrlChecker] = useState('');
+  const [imdbUrlChecker, setImdbUrlChecker] = useState('');
+  const [imdbIdChecker, setImdbIdChecker] = useState('');
+
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    if (titleChecker && imgUrlChecker && imdbUrlChecker && imdbIdChecker) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [titleChecker, imgUrlChecker, imdbUrlChecker, imdbIdChecker]);
+
+  const handleTitleChecker = (title: string) => {
+    setTitleChecker(title);
+  };
+
+  const handleDescriptionChecker = (description: string) => {
+    setDescriptionChecker(description);
+  };
+
+  const handleImgUrlChecker = (imageUrl: string) => {
+    setImgUrlChecker(imageUrl);
+  };
+
+  const handleImdbUrlChecker = (imdbUrl: string) => {
+    setImdbUrlChecker(imdbUrl);
+  };
+
+  const handleImdbIdChange = (imdbId: string) => {
+    setImdbIdChecker(imdbId);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newMovie: Movie = {
+      title: titleChecker,
+      imgUrl: imgUrlChecker,
+      imdbUrl: imdbIdChecker,
+      imdbId: imdbIdChecker,
+    };
+
+    if (descriptionChecker.trim()) {
+      newMovie.description = descriptionChecker;
+    }
+
+    setCount(count + 1);
+
+    onSubmit(newMovie);
+
+    setTitleChecker('');
+    setDescriptionChecker('');
+    setImgUrlChecker('');
+    setImdbUrlChecker('');
+    setImdbIdChecker('');
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={titleChecker}
+        onChange={handleTitleChecker}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={descriptionChecker}
+        onChange={handleDescriptionChecker}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrlChecker}
+        onChange={handleImgUrlChecker}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrlChecker}
+        onChange={handleImdbUrlChecker}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbIdChecker}
+        onChange={handleImdbIdChange}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +121,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isDisabled}
           >
             Add
           </button>
