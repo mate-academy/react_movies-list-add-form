9cc92a11c42a1ab8372b5213203f7d247ab09092
NazariiDmytryshyn <nazarter98@gmail.com>
2024-11-15 14:00:20 +0200

    Solve

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,123 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+interface NewMovieProps {
+  onAdd: (newMovie: Movie) => void;
+}
+
+export const NewMovie: FC<NewMovieProps> = ({ onAdd }) => {
   // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  // to reset touched status of all the `Field`
+  const [count, setCount] = useState(0);
+  const [movie, setMovie] = useState({
+    title: '',
+    imgUrl: '',
+    imdbUrl: '',
+    description: '',
+    imdbId: '',
+  });
+  const [error, setError] = useState({
+    title: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const handleChanges = (fieldName: string, newValue: string) => {
+    setMovie(prevState => ({
+      ...prevState,
+      [fieldName]: newValue,
+    }));
+    setError(prevError => ({
+      ...prevError,
+      [fieldName]: '',
+    }));
+  };
+
+  const handleErrors = () => {
+    const newError = {
+      title: movie.title.trim() ? '' : 'Title is required',
+      imgUrl: movie.imgUrl.trim() ? '' : 'Image URL is required',
+      imdbUrl: movie.imdbUrl.trim() ? '' : 'Imdb URL is required',
+      imdbId: movie.imdbId.trim() ? '' : 'Imdb ID is required',
+    };
+
+    setError(newError);
+
+    return !Object.values(newError).some(value => value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (handleErrors()) {
+      onAdd(movie);
+
+      setMovie({
+        title: '',
+        imgUrl: '',
+        imdbUrl: '',
+        description: '',
+        imdbId: '',
+      });
+
+      setError({
+        title: '',
+        imgUrl: '',
+        imdbUrl: '',
+        imdbId: '',
+      });
+      setCount(prevCount => prevCount + 1);
+    }
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movie.title}
+        onChange={newValue => handleChanges('title', newValue)}
         required
+        errorMessage={error.title}
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={movie.description}
+        onChange={newValue => handleChanges('description', newValue)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={movie.imgUrl}
+        onChange={newValue => handleChanges('imgUrl', newValue)}
+        required
+        errorMessage={error.imgUrl}
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={movie.imdbUrl}
+        onChange={newValue => handleChanges('imdbUrl', newValue)}
+        required
+        errorMessage={error.imdbUrl}
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={movie.imdbId}
+        onChange={newValue => handleChanges('imdbId', newValue)}
+        required
+        errorMessage={error.imdbId}
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +125,14 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={
+              movie.title.length === 0 ||
+              movie.imdbId.length === 0 ||
+              movie.imdbUrl.length === 0 ||
+              movie.imgUrl.length === 0
+                ? true
+                : false
+            }
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -7,6 +7,7 @@ type Props = {
   label?: string;
   placeholder?: string;
   required?: boolean;
+  errorMessage?: string;
   onChange?: (newValue: string) => void;
 };
 
@@ -21,13 +22,14 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  errorMessage,
 }) => {
   // generate a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+  const hasError = (touched && required && !value.trim()) || !!errorMessage;
 
   return (
     <div className="field">
@@ -50,7 +52,11 @@ export const TextField: React.FC<Props> = ({
         />
       </div>
 
-      {hasError && <p className="help is-danger">{`${label} is required`}</p>}
+      {hasError && (
+        <p className="help is-danger">
+          {errorMessage || `${label} is required`}
+        </p>
+      )}
     </div>
   );
 };
