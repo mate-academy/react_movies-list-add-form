65e25ff9ede019a6d17f7801246ac7a3c435943c
Your_Name Your_Surname <your@email>
2024-12-10 14:24:30 +0200

    new solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -45,24 +45,10 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
     reset();
   };
 
-  const pattern = new RegExp(
-    `
-    ^((
-      ([A-Za-z]{3,9}:(?:\/\/)?)
-      (?:[-;:&=+$,\\w]+@)?
-      [A-Za-z0-9.-]+
-      |
-      (?:www\.|[-;:&=+$,\\w]+@)
-      [A-Za-z0-9.-]+
-    )(
-      (?:\\/[+~%/.\w-_]*)?
-      \\?(
-        [-+=&;%@,.\w_]*)
-      )#?
-      (?:[,.!/\\\\w]*))?)$
-  `,
-    'x',
-  );
+  /* eslint-disable */
+  const pattern =
+    /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  /* eslint-enable */
 
   const handleImgUrlChange = (event: string) => {
     if (pattern.test(event)) {
