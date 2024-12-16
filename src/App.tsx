63d1684c0ed21ad5438ca1170d1616f2d59f5483
Denys Raboshchuk <kontroler444@gmail.com>
2024-12-16 10:41:39 +0200

    Create logic of new film form

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,32 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
+
+type MovieObject = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
 
 export const App = () => {
+  const [moviesList, setMoviesList] = useState(moviesFromServer);
+
+  const addNewMovie = (movie: MovieObject) => {
+    const newMovieList = [...moviesList, movie];
+
+    setMoviesList(newMovieList);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={moviesList} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addNewMovie} />
       </div>
     </div>
   );
